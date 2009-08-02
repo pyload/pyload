41c82fc738df7dd4dfb46bcca1a6936a4c118fb5
KingZero <kingzero@pyload.org>
2009-08-02 02:55:27 +0200

    added captcha for megaupload

---
+++ captcha/MegauploadCom.py
@@ -0,0 +1,14 @@
+from captcha import OCR
+
+class MegauploadCom(OCR):
+    def __init__(self):
+        OCR.__init__(self)
+        
+    def get_captcha(self, image):
+        self.load_image(image)
+        self.threshold(2.8)
+        self.run_tesser()
+        return self.result_captcha
+
+if __name__ == '__main__':
+    ocr = MegauploadCom()
