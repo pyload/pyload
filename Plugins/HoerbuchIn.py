e07742dd06f1e34a39e02c5cdd917a26e4410ece
spoob <spoob@gmx.de>
2009-08-24 14:51:41 +0200

    Fixed Hoerbuch.in Plugin

--- Plugins/HoerbuchIn.py
+++ Plugins/HoerbuchIn.py
@@ -30,16 +30,14 @@ class HoerbuchIn(Plugin):
         url = self.parent.url
         html = self.req.load(url)
         temp_links = []
-        download_content = re.search("<BR><B>Download:</B>(.*?)<BR><B>", html).group(1)
-        tmp = re.findall('<A HREF="(http://www.hoerbuch.in/cj/out.php\?pct=.*?)" TARGET="_blank">Part \d+</A>', download_content)
-
-        for link in tmp:
-            for i in range(5):
+        download_container = ("Download", "Mirror #1", "Mirror #2", "Mirror #3")
+        for container in download_container:
+            download_content = re.search("<BR><B>" + container + ":</B>(.*?)<BR><B>", html).group(1)
+            tmp = re.findall('<A HREF="http://www.hoerbuch.in/cj/out.php\?pct=\d+&url=(http://rs\.hoerbuch\.in/.+?)" TARGET="_blank">Part \d+</A>', download_content)
+            if tmp == []: continue
+            for link in tmp:
                 link_html = self.req.load(link, cookies=True)
-                link_name = link.split("/")[-1]
-                if re.search("<TITLE>(.*)</TITLE>", link_html).group(1) == link_name:
-                    link_url = re.search('<FORM ACTION="(http://.*?)" METHOD="post"', link_html).group(1)
-                    temp_links.append(link_url)
-                    break
+                temp_links.append(re.search('<FORM ACTION="(http://.*?)" METHOD="post"', link_html).group(1))
+            break
 
         self.links = temp_links
