fbd15be9a429c0d62782e7acfc73758a24546b63
Jeix <devnull@localhost>
2010-06-16 20:49:47 +0200

    extended LinkList plugin. Now it's possible to add comments, packages and links as usual via linklist.

--- module/plugins/container/LinkList.py
+++ module/plugins/container/LinkList.py
@@ -9,8 +9,8 @@ class LinkList(Container):
     __version__ = "0.1"
     __pattern__ = r"(?!http://).*\.txt"
     __description__ = """Read Link Lists in txt format"""
-    __author_name__ = ("spoob")
-    __author_mail__ = ("spoob@pyload.org")
+    __author_name__ = ("spoob", "jeix")
+    __author_mail__ = ("spoob@pyload.org", "jeix@hasnomail.com")
 
     def __init__(self, parent):
         Container.__init__(self, parent)
@@ -19,13 +19,25 @@ class LinkList(Container):
         self.read_config()
 
     def proceed(self, linkList, location):
-        tmpLinks = []
         txt = open(linkList, 'r')
         links = txt.readlines()
+        packages = {"Parsed links":[],}
+        curPack = "Parsed links"
         for link in links:
             if link != "\n":
-                tmpLinks.append(link.replace("\n", ""))
+                if link.startswith(";"):
+                    continue
+                if link.startswith("[") and link.endswith("]\n"):
+                    # new package
+                    curPack = link[1:-2]
+                    packages[curPack] = []
+                    continue
+                packages[curPack].append(link.replace("\n", ""))
         txt.close()
+        
+        # empty Parsed links fix
+        if len(packages["Parsed links"]) < 1:
+            del packages["Parsed links"]
 
         if not self.parent.core.config['general']['debug_mode']:
             txt = open(linkList, 'w')
@@ -33,4 +45,4 @@ class LinkList(Container):
             txt.close()
             #@TODO: maybe delete read txt file?
             
-        self.links = tmpLinks
+        self.links = packages
