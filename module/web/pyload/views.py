547631e9bd9af99cbfc74cf1a8efa85fd0ab37a7
RaNaN <Mast3rRaNaN@hotmail.de>
2010-10-02 23:25:27 +0200

    fix 2.

--- module/web/pyload/views.py
+++ module/web/pyload/views.py
@@ -329,7 +329,7 @@ def config(request):
     
     accs = settings.PYLOAD.get_accounts()
     for plugin,accounts in accs.iteritems():
-        for user,data in accounts:
+        for user,data in accounts.iteritems():
             if data["trafficleft"] == -1:
                 data["trafficleft"] = _("unlimited")
             elif not data["trafficleft"]:
