04f6d2a4c55ab1d379ef9900361b97d72c672a39
RaNaN <Mast3rRaNaN@hotmail.de>
2011-02-10 18:19:40 +0100

    missing package init file

---
+++ module/database/__init__.py
@@ -0,0 +1,6 @@
+from DatabaseBackend import DatabaseBackend
+from DatabaseBackend import style
+
+from FileDatabase import FileHandler
+from UserDatabase import UserMethods
+from StorageDatabase import StorageMethods
