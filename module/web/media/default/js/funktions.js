d842815194d431c2ce3130b4c904f69817244048
RaNaN <Mast3rRaNaN@hotmail.de>
2011-03-09 23:50:50 +0100

    link parser from forum thx to ToFF, missing icons

--- module/web/media/default/js/funktions.js
+++ module/web/media/default/js/funktions.js
@@ -8,6 +8,31 @@ function HumanFileSize(size)
 	return (size == 0) ? "0 B" : (Math.round( size / a , 2) + " " + filesizename[i]);
 }
 
+function parseUri() {
+        var oldString = $("add_links").value;
+        var regxp= new RegExp('(ht|f)tp(s?):\/\/[a-zA-Z0-9\-\.\/\?=]+[<| |\"|\'|\r|\n|\t]{1}', 'g');
+        var resu=oldString.match(regxp);
+        if (resu == null) return;
+        var res="";
+        for(var i=0; i < resu.length; i++){
+               // remove the last char, if ...
+                if (resu[i].indexOf(" ") != -1){
+                        res = res + resu[i].replace(" ","\n");
+                } else if (resu[i].indexOf("\t") != -1){
+                        res = res + resu[i].replace("\t","\n");
+                } else if (resu[i].indexOf("\r") != -1){
+                        res = res + resu[i].replace("\r","\n");
+                } else if (resu[i].indexOf("\"") != -1){
+                        res = res + resu[i].replace("\"","\n");
+                } else if (resu[i].indexOf("<") != -1){
+                        res = res + resu[i].replace("<","\n");
+                } else {
+                        res = res + "\n" + resu[i].replace("'","\n");
+                }
+        }
+        $("add_links").value = res;
+}
+
 Array.prototype.remove = function(from, to) {
   var rest = this.slice((to || from) + 1 || this.length);
   this.length = from < 0 ? this.length + from : from;
