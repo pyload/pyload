2aac609c9826a68f0fabbf8be956600e4506608a
zoidberg10 <zoidberg@mujmail.cz>
2011-10-29 13:48:09 +0200

    update hellshare.cz

--- module/plugins/accounts/HellshareCz.py
+++ module/plugins/accounts/HellshareCz.py
@@ -13,7 +13,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, see <http://www.gnu.org/licenses/>.
-    
+
     @author: zoidberg
 """
 
@@ -22,33 +22,33 @@ import re
 
 class HellshareCz(Account):
     __name__ = "HellshareCz"
-    __version__ = "0.1"
+    __version__ = "0.11"
     __type__ = "account"
     __description__ = """hellshare.cz account plugin"""
     __author_name__ = ("zoidberg")
     __author_mail__ = ("zoidberg@mujmail.cz")
-    
-    CREDIT_LEFT_PATTERN = r'<a class="button-amount-1" href="/buy-credit/" title="Your current credit">([^<]+)</a>'
+
+    CREDIT_LEFT_PATTERN = r'<div class="credit-link">\s*<table>\s*<tr>\s*<th>(\d+)</th>'
 
     def loadAccountInfo(self, user, req):
         self.relogin(user)
         html = req.load("http://www.hellshare.com/")
-        
+
         found = re.search(self.CREDIT_LEFT_PATTERN, html)
         if found is None:
             credits = 0
         else:
             credits = int(found.group(1)) * 1024
-        
+
         return {"validuntil": -1, "trafficleft": credits}
-    
+
     def login(self, user, data, req):
-    
+
         html = req.load('http://www.hellshare.com/login?do=loginForm-submit', post={
                 "login": "Log in",
                 "password": data["password"],
                 "username": user
                 })
-                
+
         if "<p>You input a wrong user name or wrong password</p>" in html:
             self.wrongPassword()
