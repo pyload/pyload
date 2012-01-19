5f1d64d9b6ef0d5c1c8a3fabbf6139e1a2a6d2b4
RaNaN <Mast3rRaNaN@hotmail.de>
2012-01-20 00:15:43 +0100

    fix cnl for localhost domain

--- module/web/cnl_app.py
+++ module/web/cnl_app.py
@@ -18,7 +18,7 @@ except:
 def local_check(function):
     def _view(*args, **kwargs):
         if request.environ.get('REMOTE_ADDR', "0") in ('127.0.0.1', 'localhost') \
-        or request.environ.get('HTTP_HOST','0') == '127.0.0.1:9666':
+        or request.environ.get('HTTP_HOST','0') in ('127.0.0.1:9666', 'localhost:9666'):
             return function(*args, **kwargs)
         else:
             return HTTPError(403, "Forbidden")
