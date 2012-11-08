5cbfd1ebff6845f2824f0086969b733346858d5a
Nils Hesse <nphesse@gmail.com>
2012-11-08 13:06:06 +0100

    Added the reload.cc plugin

---
+++ module/plugins/accounts/ReloadCc.py
@@ -0,0 +1,47 @@
+from module.plugins.Account   import Account
+
+from module.common.json_layer import json_loads
+
+class ReloadCc(Account):
+    __name__ = "ReloadCc"
+    __version__ = "0.1"
+    __type__ = "account"
+    __description__ = """Reload.Cc account plugin"""
+    
+    __author_name__ = ("Reload Team")
+    __author_mail__ = ("hello@reload.cc")
+
+    def loadAccountInfo(self, user, req):
+        
+        # Get user data from premiumize.me
+        status = self.getAccountStatus(user, req)
+            
+        # Parse account info
+        account_info = {"validuntil": float(status['msg']['expires']),
+                        "pwdhash": status['msg']['hash'],
+                        "trafficleft": -1}
+
+        return account_info
+
+    def login(self, user, data, req):
+        
+        # Get user data from premiumize.me
+        status = self.getAccountStatus(user, req)
+        
+        # Check if user and password are valid
+        if status['status'] != "ok":
+            self.wrongPassword()
+
+    
+    def getAccountStatus(self, user, req):
+        pwd = "pwd=%s" % self.accounts[user]['password']
+
+        try:
+            pwd = "hash=%s" % self.accounts[user]['pwdhash']
+        except Exception:
+            pass
+
+        # Use reload.cc API v1 to retrieve account info and return the parsed json answer
+        answer = req.load("https://api.reload.cc/login?via=pyload&v=1&get_traffic=true&user=%s&%s" % (user, pwd))            
+        return json_loads(answer)
+        
---
+++ module/plugins/hoster/ReloadCc.py
@@ -0,0 +1,58 @@
+from module.plugins.Hoster    import Hoster
+
+from module.common.json_layer import json_loads
+
+class ReloadCc(Hoster):
+    __name__ = "ReloadCc"
+    __version__ = "0.1"
+    __type__ = "hoster"
+    __description__ = """Reload.Cc hoster plugin"""
+        
+    # Since we want to allow the user to specify the list of hoster to use we let MultiHoster.coreReady create the regex patterns for us using getHosters in our ReloadCc hook.
+    __pattern__ = None
+    
+    __author_name__ = ("Reload Team")
+    __author_mail__ = ("hello@reload.cc")
+
+    def process(self, pyfile):
+        # Check account
+        if not self.account or not self.account.canUse():
+            self.logError(_("Please enter a valid reload.cc account or deactivate this plugin"))
+            self.fail("No valid reload.cc account provided")
+        
+        # In some cases hostsers do not supply us with a filename at download, so we are going to set a fall back filename (e.g. for freakshare or xfileshare)
+        self.pyfile.name = self.pyfile.name.split('/').pop() # Remove everthing before last slash
+        
+        # Correction for automatic assigned filename: Removing html at end if needed
+        suffix_to_remove = ["html", "htm", "php", "php3", "asp", "shtm", "shtml", "cfml", "cfm"]
+        temp = self.pyfile.name.split('.')
+        if temp.pop() in suffix_to_remove:
+            self.pyfile.name = ".".join(temp)
+
+        # Get account data
+        (user, data) = self.account.selectAccount()
+        
+        pwd = "pwd=%s" % data['password']
+
+        try:
+            pwd = "hash=%s" % data['pwdhash']
+        except Exception:
+            pass
+
+        # Get rewritten link using the reload.cc api v1
+        answer = self.load("https://api.reload.cc/dl?via=pyload&v=1&user=%s&%s&uri=%s" % (user, pwd, self.pyfile.url))
+        data = json_loads(answer)                
+
+        # Check status and decide what to do
+        status = data['status']
+        if status == "ok":
+            self.download(data['link'], disposition=True)
+        # TODO: real error codes/messages
+        elif status == 400:
+            self.fail("Invalid link")
+        elif status == 404: 
+            self.offline()
+        elif status >= 500:
+            self.tempOffline()
+        else:
+            self.fail(data['msg'])
