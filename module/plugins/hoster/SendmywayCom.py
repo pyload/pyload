7e1d66ad5fdc3908e20d532d98ac819dd3437fe1
zoidberg10 <zoidberg@mujmail.cz>
2012-11-09 23:14:33 +0100

    update XFileSharingPro & add billionuploads,sendmyway,sharebees plugins

---
+++ module/plugins/hoster/SendmywayCom.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from module.plugins.hoster.XFileSharingPro import XFileSharingPro, create_getInfo
+
+
+class SendmywayCom(XFileSharingPro):
+    __name__ = "SendmywayCom"
+    __type__ = "hoster"
+    __pattern__ = r"http://(?:\w*\.)*?sendmyway.com/\w{12}"
+    __version__ = "0.01"
+    __description__ = """SendMyWay hoster plugin"""
+    __author_name__ = ("zoidberg")
+    __author_mail__ = ("zoidberg@mujmail.cz")
+
+    FILE_NAME_PATTERN = r'<p class="file-name" ><.*?>\s*(?P<N>.+)'
+    FILE_SIZE_PATTERN = r'<small>\((?P<S>\d+) bytes\)</small>'
+    HOSTER_NAME = "sendmyway.com"
+
+getInfo = create_getInfo(SendmywayCom)
