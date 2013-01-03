b7db9e6d8a549924584f50ca05409bd8d661b032
RaNaN <Mast3rRaNaN@hotmail.de>
2013-01-03 21:16:14 +0100

    import fix for py 2.5

--- module/api/__init__.py
+++ module/api/__init__.py
@@ -1,4 +1,8 @@
 __all__ = ["CoreApi", "ConfigApi", "DownloadApi", "DownloadPreparingApi", "FileApi",
            "CollectorApi", "UserInteractionApi", "AccountApi", "AddonApi"]
+
 # Import all components
-from .import *
+# from .import *
+# Above does not work in py 2.5
+for name in __all__:
+    __import__(__name__ + "." + name)
