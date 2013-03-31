9f7a6a0ac03ddf78bfedbb34b0e21becd441a4af
Stefano <l.stickell@yahoo.it>
2013-04-01 01:51:26 +0300

    DebridItaliaCom: two new hosters supported

--- module/plugins/hooks/DebridItaliaCom.py
+++ module/plugins/hooks/DebridItaliaCom.py
@@ -5,7 +5,7 @@ from module.plugins.internal.MultiHoster import MultiHoster
 
 class DebridItaliaCom(MultiHoster):
     __name__ = "DebridItaliaCom"
-    __version__ = "0.01"
+    __version__ = "0.02"
     __type__ = "hook"
     __config__ = [("activated", "bool", "Activated", "False"),
                   ("hosterListMode", "all;listed;unlisted", "Use for hosters (if supported)", "all"),
@@ -22,4 +22,5 @@ class DebridItaliaCom(MultiHoster):
                 "uploading.com", "megashares.com", "crocko.com", "filepost.com",
                 "bitshare.com", "share-links.biz", "putlocker.com", "uploaded.to",
                 "speedload.org", "rapidgator.net", "likeupload.net", "cyberlocker.ch",
-                "depositfiles.com", "extabit.com", "filefactory.com", "sharefiles.co"]
+                "depositfiles.com", "extabit.com", "filefactory.com", "sharefiles.co",
+                "ryushare.com", "tusfiles.net"]
