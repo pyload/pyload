6ea6cc0386ddb9de702b02b38ea10ff2f1e7b5cc
Stefano <l.stickell@yahoo.it>
2013-04-12 19:23:43 +0300

    MBLinkInfo: updated pattern
    http://forum.pyload.org/viewtopic.php?p=8957#p8957

--- module/plugins/crypter/MBLinkInfo.py
+++ module/plugins/crypter/MBLinkInfo.py
@@ -8,8 +8,8 @@ from module.plugins.Crypter import Crypter
 class MBLinkInfo(Crypter):
     __name__ = "MBLinkInfo"
     __type__ = "container"
-    __pattern__ = r"http://(?:www\.)?mblink\.info/\?id=(\d+)"
-    __version__ = "0.01"
+    __pattern__ = r"http://(?:www\.)?mblink\.info/?\?id=(\d+)"
+    __version__ = "0.02"
     __description__ = """MBLink.Info Container Plugin"""
     __author_name__ = ("Gummibaer", "stickell")
     __author_mail__ = ("Gummibaer@wiki-bierkiste.de", "l.stickell@yahoo.it")
