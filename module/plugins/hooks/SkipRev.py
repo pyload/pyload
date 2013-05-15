c2919a5896318c8874383aef40de9008f49d3d97
Vuolter <vuolter@gmail.com>
2013-05-15 16:18:38 +0200

    Added plugin SkipRev

---
+++ module/plugins/hooks/SkipRev.py
@@ -0,0 +1,41 @@
+  # -*- coding: utf-8 -*-
+
+"""
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License,
+    or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+    @author: Walter Purcaro
+"""
+
+from module.plugins.Hook import Hook
+
+
+class SkipRev(Hook):
+    __name__ = "SkipRev"
+    __version__ = "0.01"
+    __description__ = "Skip download when filename has rev extension"
+    __config__ = [
+        ("activated", "bool", "Activated", "False"),
+        ("number", "int", "Do not skip until rev part", "1")
+    ]
+    __author_name__ = ("Walter Purcaro")
+    __author_mail__ = ("vuolter@gmail.com")
+
+    def downloadPreparing(self, pyfile):
+        name = basename(pyfile.name)
+        number = self.getConfig("number")
+        if name.find(".rev") != -1:
+            for i in range(1, number):
+                if name.find(i + ".rev") != -1:
+                    return
+            pyfile.setStatus("skipped")
