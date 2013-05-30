7f618aa3a7886ddc942f7a83a297233b5c39c921
Seoester <seoester@googlemail.com>
2013-05-30 16:18:45 +0200

    Recreated StreamcloudEu plugin using XFileSharingPro

---
+++ module/plugins/hoster/StreamcloudEu.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from module.plugins.hoster.XFileSharingPro import XFileSharingPro, create_getInfo
+import re
+
+class StreamcloudEu(XFileSharingPro):
+    __name__ = "StreamcloudEu"
+    __type__ = "hoster"
+    __pattern__ = r"https?://(www\.)?streamcloud\.eu/"
+    __version__ = "0.01"
+    __description__ = """Streamcloud.eu hoster plugin"""
+    __author_name__ = ("seoester")
+    __author_mail__ = ("seoester@googlemail.com")
+
+    HOSTER_NAME = "streamcloud.eu"
+    DIRECT_LINK_PATTERN = r'file: "(https?://stor\d+\.streamcloud.eu:?\d*/.*/video\.mp4)",'
+
+    def getDownloadLink(self):
+        found = re.search(self.DIRECT_LINK_PATTERN, self.html, re.S)
+        if found:
+        	return found.group(1)
+
+        return XFileSharingPro.getDownloadLink(self)
