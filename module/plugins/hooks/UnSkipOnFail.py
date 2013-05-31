178d985f8ce2f7740842335fa519694c4e69241b
Walter Purcaro <vuolter@gmail.com>
2013-05-31 13:36:41 +0200

    Updated plugin UnSkipOnFail: little improves and code cleaning

--- module/plugins/hooks/UnSkipOnFail.py
+++ module/plugins/hooks/UnSkipOnFail.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+  # -*- coding: utf-8 -*-
 
 """
     This program is free software; you can redistribute it and/or modify
@@ -16,32 +16,35 @@
 
     @author: hgg
 """
+
+from module.plugins.Hook import Hook
+from module.utils import fs_encode
 from os.path import basename
 
-from module.utils import fs_encode
-from module.plugins.Hook import Hook
-from module.PyFile import PyFile
 
 class UnSkipOnFail(Hook):
     __name__ = 'UnSkipOnFail'
-    __version__ = '0.01'
-    __description__ = 'When a download fails, restart "skipped" duplicates.'
-    __config__ = [('activated', 'bool', 'Activated', True),]
-    __author_name__ = ('hagg',)
-    __author_mail__ = ('')
+    __version__ = '0.02'
+    __description__ = 'When a download fails, restart a "skipped" duplicate'
+    __config__ = [('activated', 'bool', 'Activated', False)]
+    __author_name__ = ('hagg', 'Walter Purcaro')
+    __author_mail__ = ('', 'vuolter@gmail.com')
 
     def downloadFailed(self, pyfile):
+        #: Check if pyfile is still "failed", maybe might have been restarted in meantime
+        if pyfile.status != 8:
+            return
         pyfile_name = basename(pyfile.name)
         pid = pyfile.package().id
-        msg = 'look for skipped duplicates for %s (pid:%s)...'
-        self.logInfo(msg % (pyfile_name, pid))
+        self.logInfo('look for skipped duplicates for %s (pid:%s)...' % (pyfile_name, pid))
         dups = self.findDuplicates(pyfile)
         for link in dups:
-            # check if link is "skipped"(=4)
+            #: check if link is "skipped"
             if link.status == 4:
                 lpid = link.packageID
                 self.logInfo('restart "%s" (pid:%s)...' % (pyfile_name, lpid))
-                self.setLinkStatus(link, "queued")
+                link.setStatus("queued")
+                return
 
     def findDuplicates(self, pyfile):
         """ Search all packages for duplicate links to "pyfile".
@@ -50,48 +53,21 @@ class UnSkipOnFail(Hook):
             of twolinks are compared (basename(link.name)).
             So this method returns a list of all links with equal
             package-folders and filenames as "pyfile", but except
-            the data for "pyfile" iotselöf.
-            It does MOT check the link's status.
+            the data for "pyfile" itself.
+            It does NOT check the link's status.
         """
         dups = []
         pyfile_name = fs_encode(basename(pyfile.name))
-        # get packages (w/o files, as most file data is useless here)
+        #: get packages (w/o files, as most file data is useless here)
         queue = self.core.api.getQueue()
         for package in queue:
-            # check if package-folder equals pyfile's package folder
+            #: check if package-folder equals pyfile's package folder
             if fs_encode(package.folder) == fs_encode(pyfile.package().folder):
-                # now get packaged data w/ files/links
+                #: now get packaged data w/ files/links
                 pdata = self.core.api.getPackageData(package.pid)
-                if pdata.links:
-                    for link in pdata.links:
-                        link_name = fs_encode(basename(link.name))
-                        # check if link name collides with pdata's name
-                        if link_name == pyfile_name:
-                            # at last check if it is not pyfile itself
-                            if link.fid != pyfile.id:
-                                dups.append(link)
+                for link in pdata.links:
+                    link_name = fs_encode(basename(link.name))
+                    #: check if link name collides with pdata's name AND at last check if it is not pyfile itself
+                    if link_name == pyfile_name and link.fid != pyfile.id:
+                        dups.append(link)
         return dups
-
-    def setLinkStatus(self, link, new_status):
-        """ Change status of "link" to "new_status".
-            "link" has to be a valid FileData object,
-            "new_status" has to be a valid status name
-              (i.e. "queued" for this Plugin)
-            It creates a temporary PyFile object using
-            "link" data, changes its status, and tells
-            the core.files-manager to save its data.
-        """
-        pyfile = PyFile(self.core.files,
-                        link.fid,
-                        link.url,
-                        link.name,
-                        link.size,
-                        link.status,
-                        link.error,
-                        link.plugin,
-                        link.packageID,
-                        link.order)
-        pyfile.setStatus(new_status)
-        self.core.files.save()
-        pyfile.release()
-
