3e3185458b8dd437a7d1876c61d17d6694ed5fdf
Walter Purcaro <vuolter@gmail.com>
2013-06-03 20:08:06 +0200

    Version 0.02: many bugfixes

--- module/plugins/hooks/ChunkControl.py
+++ module/plugins/hooks/ChunkControl.py
@@ -23,44 +23,62 @@ from re import search
 
 class ChunkControl(Hook):
     __name__ = "ChunkControl"
-    __version__ = "0.01"
-    __description__ = "Define chunks number according to the rules"
+    __version__ = "0.02"
+    __description__ = "Define chunks number"
     __config__ = [
         ("activated", "bool", "Activated", "False"),
-        ("premium", "int", "Chunks number for premium downloads", "1"),
-        ("free", "int", "Chunks number for free downloads", "1"),
-        ("hosters", "str", "List chunks number for hoster as hostername F# P# (comma separated)", "")
+        ("premium", "int", "Premium: Default chunks number", "1"),
+        ("free", "int", "Free: Default chunks number", "1"),
+        ("hosters", "str", "Set chunks number for hoster as \"hostername F# P#\" (comma separated)", "")
     ]
     __author_name__ = ("Walter Purcaro")
     __author_mail__ = ("vuolter@gmail.com")
 
-    def getChunks(self, hosters, premium):
+    event_map = {"downloadStarts": "downloadStarts"}
+
+    def getChunks(self, pluginname="baseplugin", premium=False, hosters=[]):
         for hoster in hosters:
             rules = hoster.lower().split()
-            if rules[0].strip() == pyfile.pluginname:
-                for rule in xrange(1, len(rules)):
-                    rule.strip()
-                    if (rule.startswith("p") and premium)
-                    or (rule.startswith("f") and not premium):
-                        chunks = search(r'\d+', rule)
+            if rules[0].strip() == pluginname.lower():
+                rules.pop(0)
+                for rule in rules:
+                    rule = rule.strip()
+                    if (rule.startswith("p") and premium) or (rule.startswith("f") and not premium):
+                        res = search('(\d+)', rule)
                     else:
                         continue
-                    return chunks if chunks else break
+                    if res:
+                        return int(res.group(0))
+                    else:
+                        break
         return self.getConf("premium") if premium else self.getConf("free")
 
+    def syncSetting(self, coresync=False):
+        now = self.config["download"]["chunks"]
+        last = self.info["last"]
+        if now != last:
+            self.logDebug("detected core chunks setting was changed: store newer")
+            self.info["orig"] = now
+        if coresync:
+            self.config["download"]["chunks"] = self.info["orig"]
+
     def downloadStarts(self, pyfile, url, filename):
-        premium = pyfile.premium
-        chunks = self.config["download"]["chunks"]
-        chunkslast = self.info["last"]
-        if chunks != chunkslast:
-            self.info["orig"] = chunks
-        hosters = self.getConf("hosters", "").replace('|', ',').replace(';', ',').split(',')
-        chunks = self.getChunks(hosters, premium)
+        premium = pyfile.plugin.premium
+        hosters = self.getConf("hosters")
+        if hosters:
+            hosters = hosters.replace('|', ',').replace(';', ',').split(',')
+            self.logDebug("overwrited default rules with %s" % hosters)
+        self.syncSetting()
+        chunks = self.getChunks(pyfile.pluginname, premium, hosters)
         self.config["download"]["chunks"] = self.info["last"] = chunks
+        self.logInfo("download file using %s chunks" % chunks)
 
     def unload(self):
-        self.config["download"]["chunks"] = self.info["orig"]
+        self.syncSetting(True)
 
     def coreReady(self):
-        chunks = self.core.config["download"]["chunks"]
+        chunks = self.config["download"]["chunks"]
         self.info = {"orig": chunks, "last": chunks}
+
+    def setup(self):
+        self.config = self.core.config
