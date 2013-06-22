19d10abe861dcb35d838f384a68b042f56490fcd
Stefano <l.stickell@yahoo.it>
2013-06-22 21:25:27 +0200

    UnrestrictLi: account plugin revisited

--- module/plugins/accounts/UnrestrictLi.py
+++ module/plugins/accounts/UnrestrictLi.py
@@ -21,18 +21,19 @@ from module.common.json_layer import json_loads
 
 class UnrestrictLi(Account):
     __name__ = "UnrestrictLi"
-    __version__ = "0.01"
+    __version__ = "0.02"
     __type__ = "account"
     __description__ = """Unrestrict.li account plugin"""
     __author_name__ = ("stickell")
     __author_mail__ = ("l.stickell@yahoo.it")
 
     def loadAccountInfo(self, user, req):
-        if 'unrestrict_user' not in req.cj.cookies:
-            self.login(user, self.getAccountData(user), req)
         json_data = req.load('http://unrestrict.li/api/jdownloader/user.php?format=json')
         self.logDebug("JSON data: " + json_data)
         json_data = json_loads(json_data)
+
+        if 'vip' in json_data['result'] and json_data['result']['vip'] == 0:
+            return {"premium": False}
 
         validuntil = json_data['result']['expires']
         trafficleft = int(json_data['result']['traffic'] / 1024)
@@ -40,8 +41,14 @@ class UnrestrictLi(Account):
         return {"premium": True, "validuntil": validuntil, "trafficleft": trafficleft}
 
     def login(self, user, data, req):
+        html = req.load("https://unrestrict.li/sign_in")
+
+        if 'solvemedia' in html:
+            self.logError("A Captcha is required. Go to http://unrestrict.li/sign_in and login, then retry")
+            return
+
         self.html = req.load("https://unrestrict.li/sign_in",
-                             post={"return": "",
-                                   "username": user, "password": data["password"], "signin": "Sign+in"})
-        if 'unrestrict_user' not in req.cj.cookies:
+                             post={"username": user, "password": data["password"], "signin": "Sign in"})
+
+        if 'sign_out' not in self.html:
             self.wrongPassword()
