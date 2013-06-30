b3b5847885dc38b40461878a7bbe65848a43c2fd
RaNaN <Mast3rRaNaN@hotmail.de>
2013-06-30 13:03:12 +0200

    added pylint config

--- tests/code_analysis.sh
+++ tests/code_analysis.sh
@@ -6,10 +6,11 @@ echo "Running pep8"
 
 pep8 pyload > pep8.txt
 
-echo "Running pyflakes"
-
+#echo "Running pyflakes"
 # pyflakes to pylint syntak
-find -name '*.py' |egrep -v '^.(/tests/|/pyload/lib)'|xargs pyflakes  > pyflakes.log || :
+#find -name '*.py' |egrep -v '^.(/tests/|/pyload/lib)'|xargs pyflakes  > pyflakes.log || :
 # Filter warnings and strip ./ from path
-cat pyflakes.log | awk -F\: '{printf "%s:%s: [E]%s\n", $1, $2, $3}' | grep -i -E -v "'_'|pypath|webinterface|pyreq|addonmanager" > pyflakes.txt
-sed -i 's/^.\///g' pyflakes.txt
+#cat pyflakes.log | awk -F\: '{printf "%s:%s: [E]%s\n", $1, $2, $3}' | grep -i -E -v "'_'|pypath|webinterface|pyreq|addonmanager" > pyflakes.txt
+#sed -i 's/^.\///g' pyflakes.txt
+
+pylint pyload > pylint.txt
