22cb0a470af188fa079cb77cafd18363bbe4c93a
Walter Purcaro <vuolter@gmail.com>
2013-07-11 04:14:25 +0200

    Version 0.08: now should working...

--- module/plugins/hooks/SkipRev.py
+++ module/plugins/hooks/SkipRev.py
@@ -1,6 +1,6 @@
   # -*- coding: utf-8 -*-
 
-"""
+'''
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 3 of the License,
@@ -15,7 +15,7 @@
     along with this program; if not, see <http://www.gnu.org/licenses/>.
 
     @author: Walter Purcaro
-"""
+'''
 
 from module.plugins.Hook import Hook
 from os.path import basename
@@ -23,23 +23,25 @@ from re import search
 
 
 class SkipRev(Hook):
-    __name__ = "SkipRev"
-    __version__ = "0.08"
-    __description__ = "Skip download when filename has rev extension"
+    __name__ = 'SkipRev'
+    __version__ = '0.09'
+    __description__ = 'Skip download when filename has rev extension'
     __config__ = [
-        ("activated", "bool", "Activated", "False"),
-        ("number", "int", "File's part until does not skip", "2")
+        ('activated', 'bool', 'Activated', 'False'),
+        ('number', 'int', 'File\'s part until does not skip', '2')
     ]
-    __author_name__ = ("Walter Purcaro")
-    __author_mail__ = ("vuolter@gmail.com")
+    __author_name__ = ('Walter Purcaro')
+    __author_mail__ = ('vuolter@gmail.com')
+
+    event_map = {'downloadPreparing': 'downloadPreparing'}
 
     def downloadPreparing(self, pyfile):
         name = basename(pyfile.name)
-        if not name.endswith(".rev"):
+        if not name.endswith('.rev'):
             return
-        number = self.getConfig("number")
+        number = self.getConfig('number')
         part = search(r'\.part(\d+)\.rev$', name)
         if not part or int(part.group(1)) <= number:
             return
-        self.logInfo("Skipping %s" % name)
-        pyfile.setStatus("skipped")
+        self.logInfo('Skipping file "%s"' % name)
+        pyfile.setStatus('skipped')
