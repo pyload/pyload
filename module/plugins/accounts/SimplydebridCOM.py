88f6fcac584da7e822d9ac4084553972c521f044
Ivo Buff <kagenoshin@gmx.ch>
2013-07-12 11:28:40 +0200

    Cleanup
    Some cleanup and changed tabs to 4 spaces

--- module/plugins/accounts/SimplydebridCOM.py
+++ module/plugins/accounts/SimplydebridCOM.py
@@ -5,35 +5,29 @@ import re
 from time import mktime, strptime
 
 class SimplydebridCOM(Account):
-	__name__ = "SimplydebridCOM"
-	__version__ = "0.1"
-	__type__ = "account"
-	__description__ = """Simply-Debrid.com account plugin"""
-	__author_name__ = ("Kagenoshin")
-	__author_mail__ = ("kagenoshin@gmx.ch")    
-	
-	def loadAccountInfo(self, user, req):
-		get_data = {
-		}
-		response = req.load("http://simply-debrid.com/api.php?login=2&u="+self.loginname+"&p="+self.password, get = get_data, decode = True, just_header = False)
-		if(response[len(response)-1] == ";"): #remove ; if the v entry ends with ;
-			response = response[0:len(response)-1]
-		data = [x.strip() for x in response.split(";")]
-		if str(data[0]) != "1":
-			account_info = {"trafficleft": 0, "validuntil": 0, "premium": False}
-		else:
-			account_info = {
-				"trafficleft": -1,
-				"validuntil": mktime(strptime(str(data[2]),"%d/%m/%Y")),
-				"premium": True         
-			}
-		return account_info
+    __name__ = "SimplydebridCOM"
+    __version__ = "0.1"
+    __type__ = "account"
+    __description__ = """Simply-Debrid.com account plugin"""
+    __author_name__ = ("Kagenoshin")
+    __author_mail__ = ("kagenoshin@gmx.ch")    
 
-	def login(self, user, data, req):
-		self.loginname = user
-		self.password = data["password"]
-		get_data = {
-		}
-		response = req.load("http://simply-debrid.com/api.php?login=1&u="+self.loginname+"&p="+self.password, get = get_data, decode = True, just_header = False) 
-		if response != "02: loggin success":
-			self.wrongPassword()
+    def loadAccountInfo(self, user, req):
+        response = req.load("http://simply-debrid.com/api.php?login=2&u="+self.loginname+"&p="+self.password, decode = True, just_header = False)
+        data = [x.strip() for x in response.split(";")]
+        if str(data[0]) != "1":
+            account_info = {"trafficleft": 0, "validuntil": 0, "premium": False}
+        else:
+            account_info = {
+                "trafficleft": -1,
+                "validuntil": mktime(strptime(str(data[2]),"%d/%m/%Y")),
+                "premium": True         
+            }
+        return account_info
+
+    def login(self, user, data, req):
+        self.loginname = user
+        self.password = data["password"]
+        response = req.load("http://simply-debrid.com/api.php?login=1&u="+self.loginname+"&p="+self.password, decode = True, just_header = False) 
+        if response != "02: loggin success":
+            self.wrongPassword()
--- module/plugins/hoster/SimplydebridCOM.py
+++ module/plugins/hoster/SimplydebridCOM.py
@@ -8,43 +8,40 @@ from time import sleep
 import re
 
 class SimplydebridCOM(Hoster):
-	__name__ = "SimplydebridCOM"
-	__version__ = "0.1"
-	__type__ = "hoster"
-	__pattern__ = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/sd.php/*"
-	__description__ = """simply-debrid.com hoster plugin"""
-	__author_name__ = ("Kagenoshin")
-	__author_mail__ = ("kagenoshin@gmx.ch")
-	
-	def setup(self):
-		self.resumeDownload = self.multiDL = True
-		self.chunkLimit = 1
-	
-	def process(self, pyfile):
-		#print pyfile.url
-		if not self.account:
-			self.logError(_("Please enter your simply-debrid.com account or deactivate this plugin"))
-			self.fail("No simply-debrid.com account provided")
-		
-		self.logDebug("simply-debrid.com: Old URL: %s" % pyfile.url)
-		
-		#fix the links for simply-debrid.com!
-		new_url = pyfile.url
-		new_url = new_url.replace("clz.to", "cloudzer.net/file")
-		new_url = new_url.replace("http://share-online", "http://www.share-online")
-		
-		if re.match(self.__pattern__, new_url):
-			new_url = new_url
-		else:
-			page = self.req.load('http://simply-debrid.com/api.php?dl='+new_url)#+'&u='+self.user+'&p='+self.account.getAccountData(self.user)['password'])
-			if(re.search(r'tiger\sLink',page,re.I) or re.search(r'Invalid\sLink',page,re.I) or (re.search(r'api',page,re.I) and re.search(r'error',page,re.I))):
-				self.fail('Unable to unrestrict link')
-			#print page
-			new_url = page
-		
-		#print new_url
-		self.setWait(5)
-		self.wait()
-		self.logDebug("Unrestricted URL: " + new_url)
+    __name__ = "SimplydebridCOM"
+    __version__ = "0.1"
+    __type__ = "hoster"
+    __pattern__ = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/sd.php/*"
+    __description__ = """simply-debrid.com hoster plugin"""
+    __author_name__ = ("Kagenoshin")
+    __author_mail__ = ("kagenoshin@gmx.ch")
 
-		self.download(new_url, disposition=True)
+    def setup(self):
+        self.resumeDownload = self.multiDL = True
+        self.chunkLimit = 1
+
+    def process(self, pyfile):
+        if not self.account:
+            self.logError(_("Please enter your simply-debrid.com account or deactivate this plugin"))
+            self.fail("No simply-debrid.com account provided")
+        
+        self.logDebug("simply-debrid.com: Old URL: %s" % pyfile.url)
+        
+        #fix the links for simply-debrid.com!
+        new_url = pyfile.url
+        new_url = new_url.replace("clz.to", "cloudzer.net/file")
+        new_url = new_url.replace("http://share-online", "http://www.share-online")
+        
+        if re.match(self.__pattern__, new_url):
+            new_url = new_url
+        else:
+            page = self.req.load('http://simply-debrid.com/api.php?dl='+new_url)#+'&u='+self.user+'&p='+self.account.getAccountData(self.user)['password'])
+            if('tiger Link' in page or 'Invalid Link' in page or ('API' in page and 'ERROR' in page)):
+                self.fail('Unable to unrestrict link')
+            new_url = page
+        
+        self.setWait(5)
+        self.wait()
+        self.logDebug("Unrestricted URL: " + new_url)
+        
+        self.download(new_url, disposition=True)
