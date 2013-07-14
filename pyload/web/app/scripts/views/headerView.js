d9f5207ce7424d9d9820e797f8fd19a99d328427
RaNaN <Mast3rRaNaN@hotmail.de>
2013-07-14 15:35:50 +0200

    also update file size when loading progress

--- pyload/web/app/scripts/views/headerView.js
+++ pyload/web/app/scripts/views/headerView.js
@@ -209,7 +209,8 @@ define(['jquery', 'underscore', 'backbone', 'app', 'models/ServerStatus', 'colle
                         if (file) {
                             file.set({
                                 progress: prog.getPercent(),
-                                eta: prog.get('eta')
+                                eta: prog.get('eta'),
+                                size: prog.get('total')
                             }, {silent: true});
                             file.setDownloadStatus(prog.get('download').status);
                             file.trigger('change:progress');
