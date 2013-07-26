0c1ba8c3a31c96c723702028bb683d26b97ee8bb
Stefano <l.stickell@yahoo.it>
2013-07-26 18:00:33 +0200

    Fastfix: renamed and code cleanup
    (cherry picked from commit 5eb39bfefee30c4ff26d7aff680543c1d3611fba)

---
+++ module/plugins/accounts/FastixRu.py
@@ -0,0 +1,32 @@
+from module.plugins.Account import Account
+from module.common.json_layer import json_loads
+
+
+class FastixRu(Account):
+    __name__ = "FastixRu"
+    __version__ = "0.02"
+    __type__ = "account"
+    __description__ = """Fastix account plugin"""
+    __author_name__ = ("Massimo, Rosamilia")
+    __author_mail__ = ("max@spiritix.eu")
+
+    def loadAccountInfo(self, user, req):
+        data = self.getAccountData(user)
+        page = req.load("http://fastix.ru/api_v2/?apikey=%s&sub=getaccountdetails" % (data["api"]))
+        page = json_loads(page)
+        points = page['points']
+        kb = float(points)
+        kb = kb * 1024 ** 2 / 1000
+        if points > 0:
+            account_info = {"validuntil": -1, "trafficleft": kb}
+        else:
+            account_info = {"validuntil": None, "trafficleft": None, "premium": False}
+        return account_info
+
+    def login(self, user, data, req):
+        page = req.load("http://fastix.ru/api_v2/?sub=get_apikey&email=%s&password=%s" % (user, data["password"]))
+        api = json_loads(page)
+        api = api['apikey']
+        data["api"] = api
+        if "error_code" in page:
+            self.wrongPassword()
