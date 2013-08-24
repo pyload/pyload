5c8cfe32756e04526e042ced723ffc7f796dfbd9
Stefano <l.stickell@yahoo.it>
2013-08-24 18:43:16 +0200

    Realdebrid: fixed #208

--- module/plugins/accounts/RealdebridCom.py
+++ module/plugins/accounts/RealdebridCom.py
@@ -5,13 +5,15 @@ from module.plugins.Account import Account
 
 class RealdebridCom(Account):
     __name__ = "RealdebridCom"
-    __version__ = "0.41"
+    __version__ = "0.42"
     __type__ = "account"
     __description__ = """Real-Debrid.com account plugin"""
     __author_name__ = ("Devirex, Hazzard")
     __author_mail__ = ("naibaf_11@yahoo.de")
 
     def loadAccountInfo(self, user, req):
+        if self.pin_code:
+            return {"premium": False}
         page = req.load("http://real-debrid.com/api/account.php")
         xml = dom.parseString(page)
         account_info = {"validuntil": int(xml.getElementsByTagName("expiration")[0].childNodes[0].nodeValue),
@@ -20,9 +22,10 @@ class RealdebridCom(Account):
         return account_info
 
     def login(self, user, data, req):
+        self.pin_code = False
         page = req.load("https://real-debrid.com/ajax/login.php", get={"user": user, "pass": data["password"]})
-        # page = req.load("https://real-debrid.com/login.html",
-        #                 post={"user": user, "pass": data["password"]}, cookies=True)
-
         if "Your login informations are incorrect" in page:
             self.wrongPassword()
+        elif "PIN Code required" in page:
+            self.logWarning('PIN code required. Please login to https://real-debrid.com using the PIN or disable the double authentication in your control panel on https://real-debrid.com.')
+            self.pin_code = True
