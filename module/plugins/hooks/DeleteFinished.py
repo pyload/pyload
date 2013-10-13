cab9957a3b078f3d0d8e74d53fcabac079adf2a2
Walter Purcaro <vuolter@gmail.com>
2013-10-13 16:23:52 +0200

    DeleteFinished: plugin rewritten and expanded

--- module/plugins/hooks/DeleteFinished.py
+++ module/plugins/hooks/DeleteFinished.py
@@ -23,62 +23,74 @@ from module.plugins.Hook import Hook
 
 class DeleteFinished(Hook):
     __name__ = 'DeleteFinished'
-    __version__ = '1.09'
-    __description__ = 'Automatically delete all finished packages from queue'
-    __config__ = [
-        ('activated', 'bool', 'Activated', 'False'),
-        ('interval', 'int', 'Delete every (hours)', '72'),
-        ('deloffline', 'bool', 'Delete packages with offline links', 'False')
-    ]
+    __version__ = '1.50'
+    __description__ = 'Automatically delete finished packages from queue'
+    __config__ = [('activated', 'bool', 'Activated', 'False'),
+                  ('mode', 'immediately;periodically', 'Delete package mode', 'periodically'),
+                  ('interval', 'int', 'Interval in hours', '72'),
+                  ('del_offline', 'bool', 'Delete packages with offline links', 'False'),
+                  ('del_password', 'bool', 'Delete packages with password', 'False'),
+                  ('del_statusmsg', 'bool', 'Delete packages with custom link status messages', 'False'),
+                  ('keep_latest', 'bool', 'Never delete lastest packages', 'False'),
+                  ('pack_number', 'int', 'Number of latest packages to keep', '5')]
     __author_name__ = ('Walter Purcaro')
     __author_mail__ = ('vuolter@gmail.com')
 
-    ## overwritten methods ##
-    def periodical(self):
-        if not self.info['sleep']:
-            deloffline = self.getConfig('deloffline')
-            mode = '0,1,4' if deloffline else '0,4'
-            msg = 'delete all finished packages in queue list (%s packages with offline links)'
-            self.logInfo(msg % ('including' if deloffline else 'excluding'))
-            self.deleteFinished(mode)
-            self.info['sleep'] = True
-            self.addEvent('packageFinished', self.wakeup)
+    def setup(self):
+        self.info = {}
+        self.packageFinished_orig = self.packageFinished
 
-    def pluginConfigChanged(self, plugin, name, value):
-        if name == 'interval' and value != self.interval:
-            self.interval = value * 3600
-            self.initPeriodical()
-
-    def unload(self):
-        self.removeEvent('packageFinished', self.wakeup)
+    def initPeriodical(self):
+        pass
 
     def coreReady(self):
-        self.info = {'sleep': True}
-        interval = self.getConfig('interval')
-        self.pluginConfigChanged('DeleteFinished', 'interval', interval)
-        self.addEvent('packageFinished', self.wakeup)
+        self.pluginConfigChanged(self.__name__, "mode", self.getConfig("mode"))
 
-    ## own methods ##
-    @style.queue
-    def deleteFinished(self, mode):
-        self.c.execute('DELETE FROM packages WHERE NOT EXISTS(SELECT 1 FROM links WHERE package=packages.id AND status NOT IN (%s))' % mode)
-        self.c.execute('DELETE FROM links WHERE NOT EXISTS(SELECT 1 FROM packages WHERE id=links.package)')
+    def schedule(self):
+        self.deleteFinished()
+        self.packageFinished = self.wakeup
+        self.info.schedule_cb = None
 
-    def wakeup(self, pypack):
-        self.removeEvent('packageFinished', self.wakeup)
-        self.info['sleep'] = False
+    def startTimer(self):
+        self.info.schedule_cb = self.core.scheduler.addJob(self.getConfig("interval"), self.deleteFinished, threaded=False)
 
-    ## event managing ##
-    def addEvent(self, event, func):
-        """Adds an event listener for event name"""
-        if event in self.m.events:
-            if func in self.m.events[event]:
-                self.logDebug('Function already registered %s' % func)
+    def stopTimer(self):
+        self.core.scheduler.removeJob(self.info.schedule_cb)
+        self.info.schedule_cb = None
+
+    def wakeup(self, arg1=None):
+        self.packageFinished = self.packageFinished_orig
+        self.startTimer()
+
+    def pluginConfigChanged(self, plugin, name, value):
+        if name == "mode":
+            if value == "immediately":
+                if self.info.schedule_cb:
+                    self.stopTimer()
+                self.packageFinished = self.deleteFinished
             else:
-                self.m.events[event].append(func)
-        else:
-            self.m.events[event] = [func]
+                self.wakeup()
+        elif name == "interval" and self.info.schedule_cb:
+            self.stopTimer()
+            self.startTimer()
 
-    def setup(self):
-        self.m = self.manager
-        self.removeEvent = self.m.removeEvent
+    @style.queue
+    def delete(self, password, packageorder, status, statusmsg):
+        self.c.execute('DELETE FROM packages WHERE queue ? ? AND NOT EXISTS(SELECT 1 FROM links WHERE package = packages.id AND status NOT IN (?)) ?',
+                       (password, packageorder, status, statusmsg))
+        self.c.execute('DELETE FROM links WHERE NOT EXISTS(SELECT 1 FROM packages WHERE id = links.package)')
+
+    def deleteFinished(self, arg1=None):
+        deloffline = self.getConfig('del_offline')
+        delpassword = self.getConfig('del_password')
+        delstatusmsg = self.getConfig('del_statusmsg')
+        keeplatest = max(self.getConfig('keep_latest'), 1)
+        packnumber = self.getConfig('pack_number')
+
+        password = "AND NOT password" if not delpassword else ""
+        packageorder = "AND packageorder < %s" % self.core.db._nextPackageOrder(1) - packnumber if keeplatest else ""
+        statuscode = "0,4" if not deloffline else "0,1,4"
+        statusmsg = "OR statusmsg" if not delstatusmsg else ""
+
+        self.logInfo("delete packages from queue")
+        self.delete(password=password, packageorder=packageorder, status=statuscode, statusmsg=statusmsg)
