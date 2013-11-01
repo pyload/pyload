d7bd49871848b3d3af393ea374f05e821b50a921
Walter Purcaro <vuolter@gmail.com>
2013-11-01 13:04:39 +0100

    Hoster: SockshareCom added

--- module/plugins/hoster/PutlockerCom.py
+++ module/plugins/hoster/PutlockerCom.py
@@ -27,7 +27,7 @@ class PutlockerCom(SimpleHoster):
     __name__ = "PutlockerCom"
     __type__ = "hoster"
     __pattern__ = r'http://(?:www\.)?putlocker\.com/(mobile/)?(file|embed)/(?P<ID>[A-Z0-9]+)'
-    __version__ = "0.30"
+    __version__ = "0.31"
     __description__ = """Putlocker.Com"""
     __author_name__ = ("jeix", "stickell", "Walter Purcaro")
     __author_mail__ = ("", "l.stickell@yahoo.it", "vuolter@gmail.com")
@@ -35,7 +35,9 @@ class PutlockerCom(SimpleHoster):
     FILE_OFFLINE_PATTERN = r"This file doesn't exist, or has been removed."
     FILE_INFO_PATTERN = r'site-content">\s*<h1>(?P<N>.+)<strong>\( (?P<S>[^)]+) \)</strong></h1>'
 
-    FILE_URL_REPLACEMENTS = [(__pattern__, r'http://www.putlocker.com/file/\g<ID>')]
+    def init(self):
+        self.file_info = {}
+        self.FILE_URL_REPLACEMENTS = [(self.__pattern__, r'http://www.' + self.__name__[:-3].lower() + '.com/file/\g<ID>')]
 
     def setup(self):
         self.multiDL = self.resumeDownload = True
@@ -63,6 +65,7 @@ class PutlockerCom(SimpleHoster):
                     r'(/get_file\.php\?download=[A-Z0-9]+&key=[a-z0-9]+)',
                     r'(/get_file\.php\?download=[A-Z0-9]+&key=[a-z0-9]+&original=1)',
                     r'<a href="/gopro\.php">Tired of ads and waiting\? Go Pro!</a>[\t\n\rn ]+</div>[\t\n\rn ]+<a href="(/.*?)"')
+        hostername = self.__name__[:-3].lower()
         for pattern in patterns:
             link = re.search(pattern, self.html)
             if link:
@@ -70,10 +73,10 @@ class PutlockerCom(SimpleHoster):
         else:
             link = re.search(r"playlist: '(/get_file\.php\?stream=[A-Za-z0-9=]+)'", self.html)
             if link:
-                self.html = self.load("http://www.putlocker.com" + link.group(1))
+                self.html = self.load("http://www." + hostername + ".com" + link.group(1))
                 link = re.search(r'media:content url="(http://.*?)"', self.html)
                 if not link:
-                    link = re.search("\"(http://media\\-b\\d+\\.putlocker\\.com/download/\\d+/.*?)\"", self.html)
+                    link = re.search("\"(http://media\\-b\\d+\\." + hostername + "\\.com/download/\\d+/.*?)\"", self.html)
             else:
                 self.parseError('Unable to detect a download link')
 
@@ -81,14 +84,14 @@ class PutlockerCom(SimpleHoster):
         if link.startswith("http://"):
             return link
         else:
-            return "http://www.putlocker.com" + link
+            return "http://www." + hostername + ".com" + link
 
     def processName(self, name_old):
         name = self.pyfile.name
         if name <= name_old:
             return
         name_new = re.sub(r'\.[^.]+$', "", name_old) + name[len(name_old):]
-        file = self.lastDownload
+        filename = self.lastDownload
         self.pyfile.name = name_new
-        rename(file, file.rsplit(name)[0] + name_new)
+        rename(filename, filename.rsplit(name)[0] + name_new)
         self.logInfo("%(name)s renamed to %(newname)s" % {"name": name, "newname": name_new})
