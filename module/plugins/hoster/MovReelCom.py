5283039bee5a380228929df72096d19f1547566c
Joris <joris.onghena.83@gmail.com>
2013-11-02 18:58:50 +0100

    Fixed MovReelCom. Now uses XFileSharingPro and adapted to new site.

--- module/plugins/hoster/MovReelCom.py
+++ module/plugins/hoster/MovReelCom.py
@@ -7,20 +7,17 @@ class MovReelCom(XFileSharingPro):
     __name__ = "MovReelCom"
     __type__ = "hoster"
     __pattern__ = r"http://movreel.com/.*"
-    __version__ = "1.10"
+    __version__ = "1.20"
     __description__ = """MovReel.com hoster plugin"""
     __author_name__ = ("JorisV83")
     __author_mail__ = ("jorisv83-pyload@yahoo.com")
 
     #FILE_NAME_PATTERN = r'<b>Filename:</b>(?P<N>.*?)<br>'
     #FILE_SIZE_PATTERN = r'<b>Size:</b>(?P<S>.*?)<br>'
-    FILE_INFO_PATTERN = r'You have requested <font color="red">http://movreel.com/.*/(?P<N>.+?)</font>.*\((?P<S>[\d.]+) (?P<U>..)\)</font>'
-    FILE_OFFLINE_PATTERN = r'<b>File Not Found</b>'
-    DIRECT_LINK_PATTERN = "var file_link = '(.*)';"
+    FILE_INFO_PATTERN = r'<h3>(?P<N>.+?) <small><sup>(?P<S>[\d.]+) (?P<U>..)</sup> </small></h3>'
+    FILE_OFFLINE_PATTERN = r'<b>File Not Found</b><br><br>'
+    DIRECT_LINK_PATTERN = r'<a href="(http://[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*/.*)">Download Link</a>'
     #OVR_DOWNLOAD_LINK_PATTERN = "var file_link = '(.*)';"
     HOSTER_NAME = "movreel.com"
-                     "method_free": "+Free+Download"}
-                     "method_premium": "", "down_direct": "1"}
     
-
 getInfo = create_getInfo(MovReelCom)
