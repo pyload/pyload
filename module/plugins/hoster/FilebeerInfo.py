152ad322994ab7fb6be84ba4275a606b7b5e03e6
Walter Purcaro <vuolter@gmail.com>
2013-12-11 17:33:52 +0100

    Hosters: Mark dead

--- module/plugins/hoster/FilebeerInfo.py
+++ module/plugins/hoster/FilebeerInfo.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-from module.plugins.internal.DeadHoster import DeadHoster, create_getInfo
+
+from module.plugins.internal.DeadHoster import DeadHoster, create_getInfo, create_getInfo
 
 
 class FilebeerInfo(DeadHoster):
     __name__ = "FilebeerInfo"
     __type__ = "hoster"
-    __pattern__ = r"http://(?:www\.)?filebeer\.info/(?!\d*~f)(?P<ID>\w+).*"
+    __pattern__ = r"https?://(?:www\.)?filebeer\.info/(?!\d*~f)(?P<ID>\w+)"
     __version__ = "0.03"
     __description__ = """Filebeer.info plugin"""
     __author_name__ = ("zoidberg")
