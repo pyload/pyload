3429f43adf22b6e4c5e2b7e02e901904e937466b
Stefano <l.stickell@yahoo.it>
2013-12-29 17:15:20 +0100

    Keep2share: fixed #445

--- module/plugins/hoster/Keep2shareCC.py
+++ module/plugins/hoster/Keep2shareCC.py
@@ -28,14 +28,14 @@ class Keep2shareCC(SimpleHoster):
     __name__ = "Keep2shareCC"
     __type__ = "hoster"
     __pattern__ = r"https?://(?:www\.)?(keep2share|k2s|keep2s)\.cc/file/(?P<ID>\w+)"
-    __version__ = "0.06"
+    __version__ = "0.07"
     __description__ = """Keep2share.cc hoster plugin"""
     __author_name__ = ("stickell")
     __author_mail__ = ("l.stickell@yahoo.it")
 
     FILE_NAME_PATTERN = r'File: <span>(?P<N>.+)</span>'
     FILE_SIZE_PATTERN = r'Size: (?P<S>[^<]+)</div>'
-    FILE_OFFLINE_PATTERN = r'File not found or deleted|Sorry, this file is blocked or deleted'
+    FILE_OFFLINE_PATTERN = r'File not found or deleted|Sorry, this file is blocked or deleted|Error 404'
 
     DIRECT_LINK_PATTERN = r'To download this file with slow speed, use <a href="([^"]+)">this link</a>'
     WAIT_PATTERN = r'Please wait ([\d:]+) to download this file'
