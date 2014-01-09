f4c20371ac320416f3f848c14f73902812e2a5a3
Stefano <l.stickell@yahoo.it>
2014-01-09 12:23:00 +0100

    Uptobox: fixed file info retrieve See #423
    (cherry picked from commit fb630e7ffcc3d36dedcae7e14bc77b4a8098f03e)

--- pyload/plugins/hoster/UptoboxCom.py
+++ pyload/plugins/hoster/UptoboxCom.py
@@ -6,12 +6,12 @@ class UptoboxCom(XFileSharingPro):
     __name__ = "UptoboxCom"
     __type__ = "hoster"
     __pattern__ = r"http://(?:\w*\.)*?uptobox.com/\w{12}"
-    __version__ = "0.06"
+    __version__ = "0.07"
     __description__ = """Uptobox.com hoster plugin"""
     __author_name__ = ("zoidberg")
     __author_mail__ = ("zoidberg@mujmail.cz")
 
-    FILE_INFO_PATTERN = r'<h2>\s*Download File\s*<span[^>]*>(?P<N>[^>]+)</span></h2>\s*[^\(]*\((?P<S>[^\)]+)\)</h2>'
+    FILE_INFO_PATTERN = r'<div class="para_title">(?P<N>.+) \((?P<S>[^)]+)\)</div>'
     FILE_OFFLINE_PATTERN = r'<center>File Not Found</center>'
     HOSTER_NAME = "uptobox.com"
 
