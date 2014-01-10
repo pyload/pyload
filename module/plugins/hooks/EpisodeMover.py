26577ae47f9d0de9ea7d78b70be5f94aed423fd9
littleskunk <jens.heimbuerge@googlemail.com>
2014-01-11 00:41:49 +0100

    Update EpisodeMover.py
    Bugfix in cleanFolder to clean subfolders as well

--- module/plugins/hooks/EpisodeMover.py
+++ module/plugins/hooks/EpisodeMover.py
@@ -720,6 +720,7 @@ class EpisodeMover(Hook):
                         os.remove(file_)
                         filelist.append(f)
             elif os.path.isdir(file_):
+                cleanFolder(file_)
                 if self.__isEmptyDir(file_):
                     if os.path.exists(file_):
                         os.rmdir(file_)
