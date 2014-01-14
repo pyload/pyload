3bb3683ef7d281faed73a537a5fd09ed852f800a
Walter Purcaro <vuolter@gmail.com>
2014-01-14 13:39:11 +0100

    Crypter: DailymotionBatch: Code cosmetics

--- module/plugins/crypter/DailymotionBatch.py
+++ module/plugins/crypter/DailymotionBatch.py
@@ -30,7 +30,7 @@ class DailymotionBatch(Crypter):
     __type__ = "crypter"
     __pattern__ = r"https?://(?:www\.)?dailymotion\.com/((playlists/)?(?P<TYPE>playlist|user)/)?(?P<ID>[\w^_]+)(?(TYPE)|#)"
     __version__ = "0.01"
-    __description__ = """Dailymotion Channel & Playlist Decrypter"""
+    __description__ = """Dailymotion channel & playlist decrypter"""
     __author_name__ = ("Walter Purcaro")
     __author_mail__ = ("vuolter@gmail.com")
 
@@ -49,7 +49,7 @@ class DailymotionBatch(Crypter):
 
         name = playlist["name"]
         owner = playlist["owner.screenname"]
-        return id, name, owner
+        return name, owner
 
     def _getPlaylists(self, user_id, page=1):
         ref = "user/%s/playlists" % user_id
@@ -66,8 +66,8 @@ class DailymotionBatch(Crypter):
             for item in self._getPlaylists(user_id, page + 1):
                 yield item
 
-    def getPlaylists(self, user):
-        return [self.getPlaylistInfo(id) for id in self._getPlaylists(user)]
+    def getPlaylists(self, user_id):
+        return [(id,) + self.getPlaylistInfo(id) for id in self._getPlaylists(user_id)]
 
     def _getVideos(self, id, page=1):
         ref = "playlist/%s/videos" % id
@@ -84,8 +84,8 @@ class DailymotionBatch(Crypter):
             for item in self._getVideos(id, page + 1):
                 yield item
 
-    def getVideos(self, id):
-        return list(self._getVideos(id))[::-1]
+    def getVideos(self, playlist_id):
+        return list(self._getVideos(playlist_id))[::-1]
 
     def decrypt(self, pyfile):
         m = re.match(self.__pattern__, pyfile.url)
@@ -94,18 +94,18 @@ class DailymotionBatch(Crypter):
 
         if m_type == "playlist":
             self.logDebug("Url recognized as Playlist")
-            p = self.getPlaylistInfo(m_id)
-            playlists = [p] if p else None
+            p_info = self.getPlaylistInfo(m_id)
+            playlists = [(m_id,) + p_info] if p_info else None
         else:
             self.logDebug("Url recognized as Channel")
             playlists = self.getPlaylists(m_id)
-            self.logDebug("%s playlist/s found on channel %s" % (len(playlists), m_id))
+            self.logDebug("%s playlist\s found on channel \"%s\"" % (len(playlists), m_id))
 
         if not playlists:
-            self.fail()
+            self.fail("No playlist available")
 
         for p_id, p_name, p_owner in playlists:
             p_videos = self.getVideos(p_id)
             p_folder = save_join(self.config['general']['download_folder'], p_owner, p_name)
-            self.logDebug("%s video/s found on playlist \"%s\"" % (len(p_videos), p_name))
+            self.logDebug("%s video\s found on playlist \"%s\"" % (len(p_videos), p_name))
             self.packages.append((p_name, p_videos, p_folder))  #: folder is NOT recognized by pyload 0.4.9!
