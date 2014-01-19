16fde11d68f632c533bb48ed3d2f9f271e615c22
igel-kun <mathiaswe@gmx.de>
2014-01-19 23:34:22 +0100

    Update FilestubeCom.py
    two-step link extraction, based on the copy-paste box and, if unavailable, the download links

--- module/plugins/crypter/FilestubeCom.py
+++ module/plugins/crypter/FilestubeCom.py
@@ -18,6 +18,26 @@
 from module.plugins.internal.SimpleCrypter import SimpleCrypter
 
 
+# -*- coding: utf-8 -*-
+
+############################################################################
+# This program is free software: you can redistribute it and/or modify     #
+# it under the terms of the GNU Affero General Public License as           #
+# published by the Free Software Foundation, either version 3 of the       #
+# License, or (at your option) any later version.                          #
+#                                                                          #
+# This program is distributed in the hope that it will be useful,          #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+# GNU Affero General Public License for more details.                      #
+#                                                                          #
+# You should have received a copy of the GNU Affero General Public License #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.    #
+############################################################################
+
+import re
+from module.plugins.internal.SimpleCrypter import SimpleCrypter
+
 class FilestubeCom(SimpleCrypter):
     __name__ = "FilestubeCom"
     __type__ = "crypter"
@@ -27,5 +47,18 @@ class FilestubeCom(SimpleCrypter):
     __author_name__ = ("stickell")
     __author_mail__ = ("l.stickell@yahoo.it")
 
-    LINK_PATTERN = r"link_download'\]\); renderGo\('(http://[^)]+)'\);"
+    COPY_PASTE_LINKS = r'<pre id="copy_paste_links".*?>(.*?)</pre>'
+    LINK_PATTERN2 = r'<a class="gobut" href="(http://.+?)" title="Download Now!" rel="nofollow"'
     TITLE_PATTERN = r"<title>(?P<title>.+) download"
+
+    def getLinks(self):
+      linklist = re.search(self.COPY_PASTE_LINKS, self.html, re.MULTILINE)
+      if linklist:
+        matches = re.findall('^(.*?)$', linklist)
+        return matches
+      else:
+        matches = re.findall(self.LINK_PATTERN, self.html)
+        if matches:
+          return matches
+        else:
+          raise Exception("no links to extract")
