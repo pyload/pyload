f1682fe07f9a5c5d8cd652f22228422ee41d1f09
Walter Purcaro <vuolter@gmail.com>
2014-01-25 18:05:12 +0100

    Crypter: MultiuploadCom: Fully rewritten

--- module/plugins/crypter/MultiuploadCom.py
+++ module/plugins/crypter/MultiuploadCom.py
@@ -1,64 +1,69 @@
 # -*- coding: utf-8 -*-
 
+###############################################################################
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  @author: Walter Purcaro
+###############################################################################
+
 import re
-from time import time
 
-from module.plugins.Crypter import Crypter
 from module.common.json_layer import json_loads
+from module.plugins.internal.SimpleCrypter import SimpleCrypter
 
 
-class MultiuploadCom(Crypter):
+class MultiuploadCom(SimpleCrypter):
     __name__ = "MultiuploadCom"
     __type__ = "crypter"
-    __pattern__ = r"http://(?:www\.)?multiupload.com/(\w+)"
-    __version__ = "0.01"
+    __pattern__ = r"http://(?:www\.)?multiupload\.(com|nl)/(?P<ID>\w+)"
+    __version__ = "0.02"
     __description__ = """MultiUpload.com crypter"""
-    __config__ = [("preferedHoster", "str", "Prefered hoster list (bar-separated) ", "multiupload"),
-                  ("ignoredHoster", "str", "Ignored hoster list (bar-separated) ", "")]
-    __author_name__ = ("zoidberg")
-    __author_mail__ = ("zoidberg@mujmail.cz")
+    __config__ = [("grab_ddl", "bool", "Grab direct download link", "True"),
+                  ("grab_tempoff", "bool", "Grab links not yet fully uploaded", "True"),
+                  ("grab_del", "bool", "Grab deleted links", "False"),
+                  ("ignored", "str", "List of hosters to ignore (comma separated)", "")]
+    __author_name__ = "Walter Purcaro"
+    __author_mail__ = "vuolter@gmail.com"
 
-    ML_LINK_PATTERN = r'<div id="downloadbutton_" style=""><a href="([^"]+)"'
+    TITLE_PATTERN = r'color:#000000;">(?P<title>.*) <font'
 
-    def decrypt(self, pyfile):
-        self.html = self.load(pyfile.url)
-        found = re.search(self.ML_LINK_PATTERN, self.html)
-        ml_url = found.group(1) if found else None
+    def getLinks(self):
+        links = []
 
-        json_list = json_loads(self.load("http://multiupload.com/progress/", get={
-            "d": re.search(self.__pattern__, pyfile.url).group(1),
-            "r": str(int(time() * 1000))
-        }))
-        new_links = []
+        if self.getConfig("grab_ddl"):
+            ddl_pattern = r'<div id="downloadbutton_" style=""><a href="(.+)" id="dlbutton">'
+            ddl_found = re.search(ddl_pattern, self.html)
+            if ddl_found:
+                ddl_url = ddl_found.group(1)
+                links.append(ddl_url)
 
-        prefered_set = map(lambda s: s.lower().split('.')[0], set(self.getConfig("preferedHoster").split('|')))
+        id = re.match(self.__pattern__, self.pyfile.url).group("ID")
+        resource_page = self.load("http://multiupload.com/progress/", get={"d": id})
+        resource = json_loads(resource_page)
 
-        if ml_url and 'multiupload' in prefered_set:
-            new_links.append(ml_url)
+        ignored_set = set(self.getConfig("ignored").replace(' ', '').split(','))
 
-        for link in json_list:
-            if link['service'].lower() in prefered_set and int(link['status']) and not int(link['deleted']):
-                url = self.getLocation(link['url'])
-                if url:
-                    new_links.append(url)
+        for link in resource:
+            ready = link["status"] == '1'
+            complete = True if self.getConfig("grab_tempoff") else link["progress"] == "100"
+            deleted = False if self.getConfig("grab_del") else link["deleted"] == '1'
+            ignored = link["service"].lower() in ignored_set
 
-        if not new_links:
-            ignored_set = map(lambda s: s.lower().split('.')[0], set(self.getConfig("ignoredHoster").split('|')))
+            if ready and complete and not deleted and not ignored:
+                header = self.load(link["url"], just_header=True)
+                if "location" in header:
+                    url = header["location"]
+                    links.append(url)
 
-            if 'multiupload' not in ignored_set:
-                new_links.append(ml_url)
-
-            for link in json_list:
-                if link['service'].lower() not in ignored_set and int(link['status']) and not int(link['deleted']):
-                    url = self.getLocation(link['url'])
-                    if url:
-                        new_links.append(url)
-
-        if new_links:
-            self.core.files.addLinks(new_links, self.pyfile.package().id)
-        else:
-            self.fail('Could not extract any links')
-
-    def getLocation(self, url):
-        header = self.load(url, just_header=True)
-        return header['location'] if "location" in header else None
+        return links
