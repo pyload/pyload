24973fe7c4c6269597907fdf77b51827521cb376
Walter Purcaro <vuolter@gmail.com>
2014-03-25 11:58:53 +0100

    Hoster: ShareFilesCo: Mark dead

--- module/plugins/hoster/ShareFilesCo.py
+++ module/plugins/hoster/ShareFilesCo.py
@@ -1,28 +1,16 @@
 # -*- coding: utf-8 -*-
-import re
 
-from module.plugins.hoster.XFileSharingPro import XFileSharingPro, create_getInfo
+from module.plugins.internal.DeadHoster import DeadHoster, create_getInfo
 
 
-class ShareFilesCo(XFileSharingPro):
+class ShareFilesCo(DeadHoster):
     __name__ = "ShareFilesCo"
     __type__ = "hoster"
-    __pattern__ = r"http://(www\.)?sharefiles\.co/\w{12}"
-    __version__ = "0.01"
+    __pattern__ = r'http://(?:www\.)?sharefiles\.co/\w{12}'
+    __version__ = "0.02"
     __description__ = """Sharefiles.co hoster plugin"""
-    __author_name__ = ("stickell")
-    __author_mail__ = ("l.stickell@yahoo.it")
-
-    HOSTER_NAME = "sharefiles.co"
-
-    def startDownload(self, link):
-        link = link.strip()
-        if link.startswith('http://adf.ly'):
-            link = re.sub('http://adf.ly/\d+/', '', link)
-        if self.captcha:
-            self.correctCaptcha()
-        self.logDebug('DIRECT LINK: %s' % link)
-        self.download(link)
+    __author_name__ = "stickell"
+    __author_mail__ = "l.stickell@yahoo.it"
 
 
 getInfo = create_getInfo(ShareFilesCo)
