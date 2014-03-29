145508f9795e8fcec79b95b8a3af6e3e24d669b7
Stefano <l.stickell@yahoo.it>
2014-03-29 22:42:15 +0100

    Youtube: fixed #498

--- module/plugins/hoster/YoutubeCom.py
+++ module/plugins/hoster/YoutubeCom.py
@@ -36,7 +36,7 @@ class YoutubeCom(Hoster):
     __name__ = "YoutubeCom"
     __type__ = "hoster"
     __pattern__ = r"https?://(?:[^/]*?)youtube\.com/watch.*?[?&]v=.*"
-    __version__ = "0.38"
+    __version__ = "0.39"
     __config__ = [("quality", "sd;hd;fullhd;240p;360p;480p;720p;1080p;3072p", "Quality Setting", "hd"),
                   ("fmt", "int", "FMT/ITAG Number (5-102, 0 for auto)", 0),
                   (".mp4", "bool", "Allow .mp4", True),
@@ -102,7 +102,7 @@ class YoutubeCom(Hoster):
         streams = re.search(r'"url_encoded_fmt_stream_map": "(.*?)",', html).group(1)
         streams = [x.split('\u0026') for x in streams.split(',')]
         streams = [dict((y.split('=', 1)) for y in x) for x in streams]
-        streams = [(int(x['itag']), "%s&signature=%s" % (unquote(x['url']), x['sig'])) for x in streams]
+        streams = [(int(x['itag']), unquote(x['url'])) for x in streams]
         #self.logDebug("Found links: %s" % streams)
         self.logDebug("AVAILABLE STREAMS: %s" % [x[0] for x in streams])
 
