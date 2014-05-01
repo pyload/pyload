fc3611f0d00cb956e0eab2339f161433c071b889
t4skforce <t4skforce@t4skforce-m11x.neumair.net>
2014-05-01 16:08:36 +0200

    Added Hostersupport for: http://lemuploads.com/

---
+++ module/plugins/hoster/LemUploadsCom.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License,
+    or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+    @author: t4skforce
+"""
+
+# Test links:
+# BigBuckBunny_320x180.mp4 - 61.7 Mb - http://lemuploads.com/uwol0aly9dld
+
+from module.plugins.hoster.XFileSharingPro import XFileSharingPro, create_getInfo
+
+
+class LemUploadsCom(XFileSharingPro):
+    __name__ = "LemUploadsCom"
+    __type__ = "hoster"
+    __pattern__ = r'http://(?:www\.)?lemuploads.com/\w{12}'
+    __version__ = "0.01"
+    __description__ = """LemUploads.com hoster plugin"""
+    __author_name__ = ("t4skforce")
+    __author_mail__ = ("t4skforce1337[AT]gmail[DOT]com")
+
+    HOSTER_NAME = "lemuploads.com"
+
+    FILE_NAME_PATTERN = r'<b>Password:</b></div>\s*<h2>(?P<N>[^<]+)</h2>'
+
+
+getInfo = create_getInfo(LemUploadsCom)
