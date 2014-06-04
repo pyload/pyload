fed5b2767ea2a49f00bae56ddec72521e811b681
stickell <l.stickell@yahoo.it>
2014-06-04 13:36:36 +0200

    [Zippyshare] fixed #647

--- module/plugins/hoster/ZippyshareCom.py
+++ module/plugins/hoster/ZippyshareCom.py
@@ -12,12 +12,12 @@ class ZippyshareCom(SimpleHoster):
     __name__ = "ZippyshareCom"
     __type__ = "hoster"
     __pattern__ = r'(?P<HOST>http://www\d{0,2}\.zippyshare.com)/v(?:/|iew.jsp.*key=)(?P<KEY>\d+)'
-    __version__ = "0.48"
+    __version__ = "0.49"
     __description__ = """Zippyshare.com hoster plugin"""
     __author_name__ = ("spoob", "zoidberg", "stickell", "skylab")
     __author_mail__ = ("spoob@pyload.org", "zoidberg@mujmail.cz", "l.stickell@yahoo.it", "development@sky-lab.de")
 
-    FILE_NAME_PATTERN = r'>Name:</font>\s*<font [^>]*>(?P<N>[^<]+)</font><br />'
+    FILE_NAME_PATTERN = r'<title>Zippyshare\.com - (?P<N>[^<]+)</title>'
     FILE_SIZE_PATTERN = r'>Size:</font>\s*<font [^>]*>(?P<S>[0-9.,]+) (?P<U>[kKMG]+)i?B</font><br />'
     FILE_INFO_PATTERN = r'document\.getElementById\(\'dlbutton\'\)\.href = "[^;]*/(?P<N>[^"]+)";'
     FILE_OFFLINE_PATTERN = r'>File does not exist on this server</div>'
