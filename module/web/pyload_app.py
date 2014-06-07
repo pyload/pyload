7590c003b016050d7fdb879da2964780557c7389
Stanley F <stanley.foerster@gmail.com>
2014-06-07 12:47:56 +0200

    - fixed time and dl limit fields - valid until now shows time

--- module/web/pyload_app.py
+++ module/web/pyload_app.py
@@ -269,13 +269,15 @@ def config():
             data.validuntil  = _("not available")
         else:
             t = time.localtime(data.validuntil)
-            data.validuntil  = time.strftime("%d.%m.%Y", t)
+            data.validuntil  = time.strftime("%d.%m.%Y - %H:%M:%S", t)
 
         if "time" in data.options:
             try:
                 data.options["time"] = data.options["time"][0]
             except:
                 data.options["time"] = "0:00-0:00"
+        else:
+            data.options["time"] = "0:00-0:00"
 
         if "limitDL" in data.options:
             data.options["limitdl"] = data.options["limitDL"][0]
