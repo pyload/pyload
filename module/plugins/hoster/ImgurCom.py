276a6193bbbf1d5fc7ef94c907b0c4a8dfac33ba
nath_schwarz <nathan.notwhite@gmail.com>
2014-06-24 03:14:09 +0200

    Added imgur.com support, works also on albums, however only the last downloaded picture will be shown in queue. Issue is known, solution not.

---
+++ module/plugins/hoster/ImgurCom.py
@@ -0,0 +1,27 @@
+import re
+from module.plugins.Hoster import Hoster
+import module.PyFile
+
+class ImgurCom(Hoster):
+  __name__ = "ImgurCom"
+  __type__ = "hoster"
+  __version__ = "0.1"
+  __description__ = """Imgur.com hoster plugin"""
+  __pattern__ = r"http[s]?://imgur\.com/*"
+  __config__ = []
+  __author_name_ = "nath_schwarz"
+  __author_mail_ = "nathan.notwhite@gmail.com"
+ 
+  def process(self, pyfile):
+    html = self.load(pyfile.url)
+   
+    parsed_urls = re.findall(r'i\.imgur\.com\/\w{7}\.\w{3,4}', html)
+   
+    if parsed_urls:
+      #for now only the last file will show up in queue - it still downloads all files
+      for single_url in parsed_urls:
+        single_url="http://"+dl
+        pyfile.name = re.search(r'\w{7}\.\w{3,4}', single_url).group(0)
+        print self.download(single_url)
+    else:
+      self.logInfo('No urls found')
