921be4a5f33080db926c664d560f473f5f1b8a42
nath_schwarz <nathan.notwhite@gmail.com>
2014-06-24 14:11:40 +0200

    Hoster-Plugin ImgurCom now downloads a single file

--- module/plugins/hoster/ImgurCom.py
+++ module/plugins/hoster/ImgurCom.py
@@ -1,27 +1,29 @@
 import re
 from module.plugins.Hoster import Hoster
-import module.PyFile
 
 class ImgurCom(Hoster):
   __name__ = "ImgurCom"
   __type__ = "hoster"
   __version__ = "0.1"
   __description__ = """Imgur.com hoster plugin"""
-  __pattern__ = r"http[s]?://imgur\.com/*"
+  __pattern__ = r"http[s]?://imgur\.com/\w{7}"
   __config__ = []
   __author_name_ = "nath_schwarz"
   __author_mail_ = "nathan.notwhite@gmail.com"
  
   def process(self, pyfile):
     html = self.load(pyfile.url)
+    
+    filetypes="(?:jpeg|jpg|png|gif|apng)"
+    
+    single_url = re.search(r'i\.imgur\.com/\w{7}\.'+filetypes, html)
    
-    parsed_urls = re.findall(r'i\.imgur\.com\/\w{7}\.\w{3,4}', html)
-   
-    if parsed_urls:
-      #for now only the last file will show up in queue - it still downloads all files
-      for single_url in parsed_urls:
-        single_url="http://"+single_url
-        pyfile.name = re.search(r'\w{7}\.\w{3,4}', single_url).group(0)
-        self.download(single_url)
+    if single_url:
+      #complete downloadable url
+      single_url="http://"+single_url.group(0)
+      print single_url
+      #parse name for file
+      pyfile.name = re.search(r'\w{7}\.'+filetypes, single_url).group(0)
+      self.download(single_url)
     else:
       self.logInfo('No urls found')
