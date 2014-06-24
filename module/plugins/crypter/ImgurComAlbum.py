2ba3716a6df655943b98fc0dcb43d851a68541de
nath_schwarz <nathan.notwhite@gmail.com>
2014-06-24 15:55:19 +0200

    Removed hoster-plugin, is now obsolote due to crypter-plugin

--- module/plugins/crypter/ImgurComAlbum.py
+++ module/plugins/crypter/ImgurComAlbum.py
@@ -6,12 +6,13 @@ class ImgurComAlbum(Crypter):
   __type__ = "crypter"
   __version__ = "0.1"
   __description__ = """Imgur.com decrypter plugin"""
-  __pattern__ = r"http[s]?://imgur\.com/(?:a|gallery)/\w{5,7}"
+  __pattern__ = r"http[s]?://imgur\.com/(?:a/|gallery/|)\w{5,7}"
   __config__ = []
   __author_name_ = "nath_schwarz"
   __author_mail_ = "nathan.notwhite@gmail.com"
- 
+  
   def decrypt(self, pyfile):
+    
     html = self.load(pyfile.url)
     
     filetypes="(?:jpeg|jpg|png|gif|apng)"
@@ -19,10 +20,13 @@ class ImgurComAlbum(Crypter):
     parsed_urls = re.findall(r'i\.imgur\.com/\w{7}\.'+filetypes, html)
    
     if parsed_urls:
+      #uniqize the list of parsed urls
       parsed_urls=list(set(parsed_urls))
       for i, url in enumerate(parsed_urls):
         parsed_urls[i] = "http://"+url
-      name = "imgurCom_" + re.search(r'imgur\.com/(?:a|gallery)/(\w{5,7})', pyfile.url).group(1)
+        self.logDebug('New url: '+parsed_urls[i])
+      name = "imgurCom_" + re.search(r'imgur\.com/(?:a/|gallery/|)(\w{5,7})', pyfile.url).group(1)
+      self.logDebug('Determined name for package: '+name)
       self.packages.append((name, parsed_urls, name))
     else:
       self.logInfo('No urls found')
