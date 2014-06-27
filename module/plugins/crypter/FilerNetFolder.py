ce58fe421ffadaeed044548505ae475c46881d3a
nath_schwarz <nathan.notwhite@gmail.com>
2014-06-27 21:18:35 +0200

    Using SimpleCryptor now

--- module/plugins/crypter/FilerNetFolder.py
+++ module/plugins/crypter/FilerNetFolder.py
@@ -1,35 +1,22 @@
 import re
-from module.plugins.Crypter import Crypter
+from module.plugins.internal.SimpleCrypter import SimpleCrypter
 
-class FilerNetFolder(Crypter):
+class FilerNetFolder(SimpleCrypter):
   __name__ = "FilerNetFolder"
   __type__ = "crypter"
-  __version__ = "0.1"
+  __version__ = "0.2"
   __description__ = """Filer.net decrypter plugin"""
   __pattern__ = r"http[s]?://filer\.net/folder/(?P<ID>\w{16})"
-  __config__ = []
   __author_name_ = "nath_schwarz"
   __author_mail_ = "nathan.notwhite@gmail.com"
-  
-  def decrypt(self, pyfile):
-    
-    html = self.load(pyfile.url)
-    
-    raw_urls = re.findall(r'(\w{16})"', html)
-    
-    if raw_urls:
-      parsed_urls = set()
-      
-      #Iterate over raw_urls and add add the set - uniqizes the urls automatically
-      for i, url in enumerate(raw_urls):
-        temp_url = "http://filer.net/get/"+re.search(r'\w{16}', url).group(0)
-        self.logDebug(temp_url)
-        parsed_urls.add(temp_url)
-     
-      #determine a name for the newly added package
-      name = re.search(r'h3>[\n.]*(?P<NAME>test).*<small', html).group("NAME")
-      self.logDebug('Determined name for package: '+name)
-      
-      self.packages.append((name, list(parsed_urls), name))
-    else:
-      self.logInfo('No urls found')
+
+  TITLE_PATTERN = r'<h3>(?P<title>.+) - <small'
+  LINK_PATTERN = r'/get/(\w{16})'
+
+  def getLinks(self):
+    urls = re.findall(LINK_PATTERN, self.html)
+    for url in urls:
+      urls[urls.index(url)] = "http://filer.net/get/"+re.search(r'\w{16}', url).group(0)
+    urls = list(set(urls))
+    return urls
+
