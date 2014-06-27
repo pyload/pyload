b3faeba5c9d039734e4841dd5925f25972ed1216
nath_schwarz <nathan.notwhite@gmail.com>
2014-06-27 23:59:50 +0200

    Removed redundant regex-brackets and switched to use a lambda-function

--- module/plugins/crypter/ImgurComAlbum.py
+++ module/plugins/crypter/ImgurComAlbum.py
@@ -4,18 +4,17 @@ from module.plugins.internal.SimpleCrypter import SimpleCrypter
 class ImgurComAlbum(SimpleCrypter):
   __name__ = "ImgurComAlbum"
   __type__ = "crypter"
-  __version__ = "0.2"
+  __version__ = "0.3"
   __description__ = """Imgur.com decrypter plugin"""
-  __pattern__ = r"http[s]?://imgur\.com/(a|gallery|)[/]?\w{5,7}"
+  __pattern__ = r"http[s]?://imgur\.com/(a|gallery|)/?\w{5,7}"
   __author_name_ = "nath_schwarz"
   __author_mail_ = "nathan.notwhite@gmail.com"
 
   TITLE_PATTERN = r'(?P<title>.+) - Imgur'
-  LINK_PATTERN = r'i\.imgur\.com/\w{7}[s]?\.(?:jpeg|jpg|png|gif|apng)'
+  LINK_PATTERN = r'i\.imgur\.com/\w{7}s?\.(?:jpeg|jpg|png|gif|apng)'
 
   def getLinks(self):
-    urls = re.findall(self.LINK_PATTERN, self.html)
-    for url in urls:
-      urls[urls.index(url)] = "http://"+ re.sub(r'(\w{7})s\.', r'\1.', url)
+    f = lambda url: "http://" + re.sub(r'(\w{7})s\.', r'\1.', url)
+    urls = map(f, re.findall(self.LINK_PATTERN, self.html))
     urls = list(set(urls))
     return urls
