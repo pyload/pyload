pyload/pyload

commit 01e76034bcad5e5aab7f6e141782071e5c328649

Fix account checkLogin + related code cosmetics

module/plugins/Account.py
@@ -36,16 +36,16 @@ class Account(Base):
     associated hoster plugin. Plugin should also provide `loadAccountInfo`
     """
     __name__ = "Account"
-    __version__ = "0.2"
+    __version__ = "0.3"
     __type__ = "account"
     __description__ = """Base account plugin"""
     __author_name__ = "mkaay"
     __author_mail__ = "mkaay@mkaay.de"
 
-    #: after that time [in minutes] pyload will relogin the account
-    login_timeout = 600
-    #: account data will be reloaded after this time
-    info_threshold = 600
+    #: after that time (in minutes) pyload will relogin the account
+    login_timeout = 10 * 60
+    #: after that time (in minutes) account data will be reloaded
+    info_threshold = 10 * 60
 
 
     def __init__(self, manager, accounts):
@@ -84,17 +84,20 @@ class Account(Base):
             self.logWarning(
                 _("Could not login with account %(user)s | %(msg)s") % {"user": user
                                                                         , "msg": _("Wrong Password")})
-            data["valid"] = False
-
+            success = data["valid"] = False
         except Exception, e:
             self.logWarning(
                 _("Could not login with account %(user)s | %(msg)s") % {"user": user
                                                                         , "msg": e})
-            data["valid"] = False
+            success = data["valid"] = False
             if self.core.debug:
                 print_exc()
+        else:
+            success = True
         finally:
-            if req: req.close()
+            if req:
+                req.close()
+            return success
 
     def relogin(self, user):
         req = self.getAccountRequest(user)
@@ -104,7 +107,7 @@ class Account(Base):
         if user in self.infos:
             del self.infos[user] #delete old information
 
-        self._login(user, self.accounts[user])
+        return self._login(user, self.accounts[user])
 
     def setAccounts(self, accounts):
         self.accounts = accounts
@@ -284,9 +287,10 @@ class Account(Base):
     def checkLogin(self, user):
         """ checks if user is still logged in """
         if user in self.timestamps:
-            if self.timestamps[user] + self.login_timeout * 60 < time():
+            if self.login_timeout > 0 and self.timestamps[user] + login_timeout * 60 > time():
                 self.logDebug("Reached login timeout for %s" % user)
-                self.relogin(user)
-                return False
-
-        return True
+                return self.relogin(user)
+            else:
+                return True
+        else:
+            return False

module/plugins/accounts/Ftp.py
@@ -11,4 +11,4 @@ class Ftp(Account):
     __author_name__ = "zoidberg"
     __author_mail__ = "zoidberg@mujmail.cz"
 
-    login_timeout = info_threshold = 1000000
+    login_timeout = info_threshold = -1  #: Unlimited

module/plugins/accounts/Http.py
@@ -11,4 +11,4 @@ class Http(Account):
     __author_name__ = "zoidberg"
     __author_mail__ = "zoidberg@mujmail.cz"
 
-    login_timeout = info_threshold = 1000000
+    login_timeout = info_threshold = -1  #: Unlimited

module/plugins/accounts/MovReelCom.py
@@ -11,8 +11,8 @@ class MovReelCom(XFSPAccount):
     __author_name__ = "t4skforce"
     __author_mail__ = "t4skforce1337[AT]gmail[DOT]com"
 
-    login_timeout = 60  #: after that time [in minutes] pyload will relogin the account
-    info_threshold = 30  #: account data will be reloaded after this time
+    login_timeout = 60
+    info_threshold = 30
 
     MAIN_PAGE = "http://movreel.com/"
 

module/plugins/accounts/StahnuTo.py
@@ -31,7 +31,6 @@ class StahnuTo(Account):
     __author_name__ = "zoidberg"
     __author_mail__ = "zoidberg@mujmail.cz"
 
-    #login_timeout = 60
 
     def loadAccountInfo(self, user, req):
         html = req.load("http://www.stahnu.to/")

module/plugins/accounts/TurbobitNet.py
@@ -31,7 +31,6 @@ class TurbobitNet(Account):
     __author_name__ = "zoidberg"
     __author_mail__ = "zoidberg@mujmail.cz"
 
-    #login_timeout = 60
 
     def loadAccountInfo(self, user, req):
         html = req.load("http://turbobit.net")

module/plugins/hoster/NetloadIn.py
@@ -176,9 +176,9 @@ class NetloadIn(Hoster):
                 return True
             if ">An access request has been made from IP address <" in page:
                 wait = self.get_wait_time(page)
-                if wait == 0:
+                if not wait:
                     self.logDebug("Netload: Wait was 0 setting 30")
-                    wait = 30
+                    wait = 30 * 60
                 self.logInfo(_("Netload: waiting between downloads %d s." % wait))
                 self.wantReconnect = True
                 self.setWait(wait)

commit 01890bf1370b801dd95c97559530cb57321a3559

[Account] fixed bug introduced in 01e76034bcad5e5aab7f6e141782071e5c328649

module/plugins/Account.py
@@ -287,7 +287,7 @@ class Account(Base):
     def checkLogin(self, user):
         """ checks if user is still logged in """
         if user in self.timestamps:
-            if self.login_timeout > 0 and self.timestamps[user] + self.login_timeout * 60 > time():
+            if self.login_timeout > 0 and self.timestamps[user] + self.login_timeout * 60 < time():
                 self.logDebug("Reached login timeout for %s" % user)
                 return self.relogin(user)
             else: