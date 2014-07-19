b3b82f960e553dbabbaf4f1d276b560e2cfffab0
Walter Purcaro <vuolter@gmail.com>
2014-07-19 14:48:40 +0200

    [StreamCz] Parse https

--- module/plugins/hoster/StreamCz.py
+++ module/plugins/hoster/StreamCz.py
@@ -16,6 +16,7 @@
 """
 
 import re
+
 from module.plugins.Hoster import Hoster
 from module.network.RequestFactory import getURL
 
@@ -36,16 +37,20 @@ def getInfo(urls):
 
 class StreamCz(Hoster):
     __name__ = "StreamCz"
+    __version__ = "0.2"
     __type__ = "hoster"
-    __pattern__ = r'http://(?:www\.)?stream.cz/[^/]+/\d+.*'
-    __version__ = "0.1"
+
+    __pattern__ = r'https?://(?:www\.)?stream\.cz/[^/]+/\d+.*'
+
     __description__ = """Stream.cz hoster plugin"""
     __author_name__ = "zoidberg"
     __author_mail__ = "zoidberg@mujmail.cz"
 
-    OFFLINE_PATTERN = r'<h1 class="commonTitle">Str.nku nebylo mo.n. nal.zt \(404\)</h1>'
     FILE_NAME_PATTERN = r'<link rel="video_src" href="http://www.stream.cz/\w+/(\d+)-([^"]+)" />'
+    OFFLINE_PATTERN = r'<h1 class="commonTitle">Str.nku nebylo mo.n. nal.zt \(404\)</h1>'
+
     CDN_PATTERN = r'<param name="flashvars" value="[^"]*&id=(?P<ID>\d+)(?:&cdnLQ=(?P<cdnLQ>\d*))?(?:&cdnHQ=(?P<cdnHQ>\d*))?(?:&cdnHD=(?P<cdnHD>\d*))?&'
+
 
     def setup(self):
         self.multiDL = True
