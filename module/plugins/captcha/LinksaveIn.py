pyload/pyload

commit 48c0c42fd6faffc56432d5f037cd575979f180cc

Removed all @author flags + key attributes cleanup for internal & hooks plugins

module/plugins/captcha/LinksaveIn.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from captcha import OCR
-import Image
-from os import sep
-from os.path import dirname
-from os.path import abspath
+from PIL import Image
 from glob import glob
+from os import sep
+from os.path import abspath, dirname
+
+from module.plugins.OCR import OCR
 
 
 class LinksaveIn(OCR):
     __name__ = "LinksaveIn"
+    __version__ = "0.1"
+    __type__ = "ocr"
+
+    __description__ = """Linksave.in ocr plugin"""
+    __author_name__ = "pyLoad Team"
+    __author_mail__ = "admin@pyload.org"
+
+
     def __init__(self):
         OCR.__init__(self)
         self.data_dir = dirname(abspath(__file__)) + sep + "LinksaveIn" + sep
@@ -140,10 +148,11 @@ class LinksaveIn(OCR):
 
         return final
 
+
 if __name__ == '__main__':
     import urllib
+
     ocr = LinksaveIn()
     testurl = "http://linksave.in/captcha/cap.php?hsh=2229185&code=ZzHdhl3UffV3lXTH5U4b7nShXj%2Bwma1vyoNBcbc6lcc%3D"
     urllib.urlretrieve(testurl, ocr.data_dir+"captcha.gif")
-
     print ocr.get_captcha(ocr.data_dir+'captcha.gif')

module/plugins/captcha/ShareonlineBiz.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 #
 #Copyright (C) 2009 kingzero, RaNaN
 #
@@ -17,10 +16,19 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 #
 ###
-from captcha import OCR
+
+from module.plugins.OCR import OCR
+
 
 class ShareonlineBiz(OCR):
     __name__ = "ShareonlineBiz"
+    __version__ = "0.1"
+    __type__ = "ocr"
+
+    __description__ = """Shareonline.biz ocr plugin"""
+    __author_name__ = "RaNaN"
+    __author_mail__ = "RaNaN@pyload.org"
+
 
     def __init__(self):
         OCR.__init__(self)
@@ -46,8 +54,10 @@ class ShareonlineBiz(OCR):
 
         #tesseract at 60%
 
+
 if __name__ == '__main__':
     import urllib
+
     ocr = ShareonlineBiz()
     urllib.urlretrieve("http://www.share-online.biz/captcha.php", "captcha.jpeg")
-    print  ocr.get_captcha('captcha.jpeg')
+    print ocr.get_captcha('captcha.jpeg')

module/plugins/accounts/ShareonlineBiz.py
@@ -13,8 +13,6 @@
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, see <http://www.gnu.org/licenses/>.
-
-    @author: mkaay
 """
 
 from module.plugins.Account import Account

commit a88d976e387c36eb44461c627dee6b3cae4e5cd3

Fix captcha import bug introduced in 48c0c42fd6faffc56432d5f037cd575979f180cc

module/plugins/captcha/LinksaveIn.py
@@ -5,7 +5,7 @@ from glob import glob
 from os import sep
 from os.path import abspath, dirname
 
-from module.plugins.OCR import OCR
+from captcha import OCR
 
 
 class LinksaveIn(OCR):

module/plugins/captcha/ShareonlineBiz.py
@@ -17,8 +17,7 @@
 #
 ###
 
-from module.plugins.OCR import OCR
-
+from captcha import OCR
 
 
 class ShareonlineBiz(OCR):