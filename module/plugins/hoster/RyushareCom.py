b328bcb438d9e939736137baea5c9e08f092f9c1
Lars Fricke <lars.fricke@gmail.com>
2014-08-12 19:41:52 +0200

    [RyushareCom] Fixed premium dl

--- module/plugins/hoster/RyushareCom.py
+++ module/plugins/hoster/RyushareCom.py
@@ -13,7 +13,7 @@ class RyushareCom(XFileSharingPro):
     __name__ = "RyushareCom"
     __type__ = "hoster"
     __pattern__ = r'http://(?:www\.)?ryushare\.com/\w+'
-    __version__ = "0.15"
+    __version__ = "0.16"
     __description__ = """Ryushare.com hoster plugin"""
     __author_name__ = ("zoidberg", "stickell", "quareevo")
     __author_mail__ = ("zoidberg@mujmail.cz", "l.stickell@yahoo.it", "quareevo@arcor.de")
@@ -23,7 +23,7 @@ class RyushareCom(XFileSharingPro):
     FILE_SIZE_PATTERN = r'You have requested <font color="red">[^<]+</font> \((?P<S>[\d\.]+) (?P<U>\w+)'
 
     WAIT_PATTERN = r'You have to wait ((?P<hour>\d+) hour[s]?, )?((?P<min>\d+) minute[s], )?(?P<sec>\d+) second[s]'
-    LINK_PATTERN = r'(http://([^/]*?ryushare.com|\d+\.\d+\.\d+\.\d+)(:\d+/d/|/files/\w+/\w+/)[^"\'<]+)'
+    DIRECT_LINK_PATTERN = r'<a href="([^"]+)">Click here to download</a>'
     SOLVEMEDIA_PATTERN = r'http:\/\/api\.solvemedia\.com\/papi\/challenge\.script\?k=(.*?)"'
 
     def getDownloadLink(self):
