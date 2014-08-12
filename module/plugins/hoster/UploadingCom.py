5ba14a2653ea58082f555405f7e7df548610c394
igel-kun <mathiaswe@gmx.de>
2014-08-12 14:09:21 +0200

    Update UploadingCom.py
    fixed OFFLINE_PATTERN

--- module/plugins/hoster/UploadingCom.py
+++ module/plugins/hoster/UploadingCom.py
@@ -33,7 +33,7 @@ class UploadingCom(SimpleHoster):
 
     FILE_NAME_PATTERN = r'id="file_title">(?P<N>.+)</'
     FILE_SIZE_PATTERN = r'size tip_container">(?P<S>[\d.]+) (?P<U>\w+)<'
-    OFFLINE_PATTERN = r'Page not found!'
+    OFFLINE_PATTERN = r'(?:Page|file) not found'
 
     def process(self, pyfile):
         # set lang to english
