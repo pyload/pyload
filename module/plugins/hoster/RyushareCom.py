bb2d9e1a0a89cf0d137e947d35429598c859adb1
Lars Fricke <lars.fricke@gmail.com>
2014-08-30 00:59:59 +0200

    [RyushareCom] Fixed premium dl

--- module/plugins/hoster/RyushareCom.py
+++ module/plugins/hoster/RyushareCom.py
@@ -12,7 +12,7 @@ from module.plugins.internal.CaptchaService import SolveMedia
 class RyushareCom(XFileSharingPro):
     __name__ = "RyushareCom"
     __type__ = "hoster"
-    __version__ = "0.15"
+    __version__ = "0.16"
 
     __pattern__ = r'http://(?:www\.)?ryushare\.com/\w+'
 
@@ -25,7 +25,7 @@ class RyushareCom(XFileSharingPro):
     FILE_SIZE_PATTERN = r'You have requested <font color="red">[^<]+</font> \((?P<S>[\d\.]+) (?P<U>\w+)'
 
     WAIT_PATTERN = r'You have to wait ((?P<hour>\d+) hour[s]?, )?((?P<min>\d+) minute[s], )?(?P<sec>\d+) second[s]'
-    LINK_PATTERN = r'(http://([^/]*?ryushare.com|\d+\.\d+\.\d+\.\d+)(:\d+/d/|/files/\w+/\w+/)[^"\'<]+)'
+    LINK_PATTERN = r'<a href="([^"]+)">Click here to download<'
     SOLVEMEDIA_PATTERN = r'http:\/\/api\.solvemedia\.com\/papi\/challenge\.script\?k=(.*?)"'
 
 
