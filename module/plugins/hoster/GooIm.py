bdd9b4cd2aec952cfd3284c8e471d1c274510e28
zapp-brannigan <fuerst.reinje@web.de>
2014-08-30 20:13:29 +0200

    Update GooIm.py
    The plugin was not working anymore.

--- module/plugins/hoster/GooIm.py
+++ module/plugins/hoster/GooIm.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+#
+# Test links:
+# https://goo.im/devs/liquidsmooth/3.x/codina/Nightly/LS-KK-v3.2-2014-08-01-codina.zip
 
 import re
 
@@ -8,35 +11,26 @@ from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo
 class GooIm(SimpleHoster):
     __name__ = "GooIm"
     __type__ = "hoster"
-    __version__ = "0.02"
+    __version__ = "0.03"
 
-    __pattern__ = r'http://(?:www\.)?goo\.im/.+'
+    __pattern__ = r'https?://(?:www\.)?goo\.im/.+'
 
     __description__ = """Goo.im hoster plugin"""
-    __author_name__ = "stickell"
-    __author_mail__ = "l.stickell@yahoo.it"
+    __author_name__ = "zapp-brannigan"
+    __author_mail__ = "fuerst.reinje@web.de"
 
-    FILE_NAME_PATTERN = r'<h3>Filename: (?P<N>.+)</h3>'
+    FILE_NAME_PATTERN = r'You will be redirected to .*(?P<N>[^/ ]+)  in'
     OFFLINE_PATTERN = r'The file you requested was not found'
 
 
     def setup(self):
-        self.chunkLimit = -1
         self.multiDL = self.resumeDownload = True
 
     def handleFree(self):
-        self.html = self.load(self.pyfile.url)
-        m = re.search(r'MD5sum: (?P<MD5>[0-9a-z]{32})</h3>', self.html)
-        if m:
-            self.check_data = {"md5": m.group('MD5')}
+        url = self.pyfile.url
+        self.html = self.load(url, cookies=True)
         self.wait(10)
-
-        header = self.load(self.pyfile.url, just_header=True)
-        if header['location']:
-            self.logDebug("Direct link: " + header['location'])
-            self.download(header['location'])
-        else:
-            self.parseError("Unable to detect direct download link")
+        self.download(url, cookies=True)
 
 
 getInfo = create_getInfo(GooIm)
