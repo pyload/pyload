22334b664ff2be7bbbe0627e4d65cf222bd29919
stickell <l.stickell@yahoo.it>
2014-09-11 12:16:03 +0200

    [MultiDebrid] renamed to Myfastline

--- module/plugins/accounts/MultiDebridCom.py
+++ module/plugins/accounts/MultiDebridCom.py
@@ -9,12 +9,10 @@ from module.common.json_layer import json_loads
 class MultiDebridCom(Account):
     __name__ = "MultiDebridCom"
     __type__ = "account"
-    __version__ = "0.01"
-
-    __description__ = """Multi-debrid.com account plugin"""
+    __version__ = "0.02"
+    __description__ = """Myfastfile.com account plugin"""
     __author_name__ = "stickell"
     __author_mail__ = "l.stickell@yahoo.it"
-
 
     def loadAccountInfo(self, user, req):
         if 'days_left' in self.json_data:
@@ -24,8 +22,8 @@ class MultiDebridCom(Account):
             self.logError('Unable to get account information')
 
     def login(self, user, data, req):
-        # Password to use is the API-Password written in http://multi-debrid.com/myaccount
-        html = req.load("http://multi-debrid.com/api.php",
+        # Password to use is the API-Password written in http://myfastfile.com/myaccount
+        html = req.load("http://myfastfile.com/api.php",
                              get={"user": user, "pass": data['password']})
         self.logDebug('JSON data: ' + html)
         self.json_data = json_loads(html)
