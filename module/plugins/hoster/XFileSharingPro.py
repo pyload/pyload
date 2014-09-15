11dfb643c48ce76e2744025a5d3474b9afce6b84
igel-kun <mathiaswe@gmx.de>
2014-09-15 22:09:41 +0200

    spacing...

--- module/plugins/hoster/XFileSharingPro.py
+++ module/plugins/hoster/XFileSharingPro.py
@@ -28,9 +28,9 @@ class XFileSharingPro(SimpleHoster):
     __description__ = """XFileSharingPro base hoster plugin"""
     __author_name__ = ("zoidberg", "stickell")
     __author_mail__ = ("zoidberg@mujmail.cz", "l.stickell@yahoo.it")
-
-	# support embedded files
-	FILE_URL_REPLACEMENTS = [(r'/embed-(\w{12}).*', r'/\1')]
+    
+    # support embedded files
+    FILE_URL_REPLACEMENTS = [(r'/embed-(\w{12}).*', r'/\1')]
     FILE_INFO_PATTERN = r'<tr><td align=right><b>Filename:</b></td><td nowrap>(?P<N>[^<]+)</td></tr>\s*.*?<small>\((?P<S>[^<]+)\)</small>'
     FILE_NAME_PATTERN = r'<input type="hidden" name="fname" value="(?P<N>[^"]+)"'
     FILE_SIZE_PATTERN = r'You have requested .*\((?P<S>[\d\.\,]+) ?(?P<U>\w+)?\)</font>'
