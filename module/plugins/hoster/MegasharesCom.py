34be7c9b90d5a4d5745914af7870d57d3a58f7ea
igel-kun <mathiaswe@gmx.de>
2014-09-15 22:55:12 +0200

    Update MegasharesCom.py

--- module/plugins/hoster/MegasharesCom.py
+++ module/plugins/hoster/MegasharesCom.py
@@ -1,20 +1,5 @@
 # -*- coding: utf-8 -*-
 
-"""
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License,
-    or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-    See the GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, see <http://www.gnu.org/licenses/>.
-"""
-
 import re
 from time import time
 from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo
@@ -23,8 +8,8 @@ from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo
 class MegasharesCom(SimpleHoster):
     __name__ = "MegasharesCom"
     __type__ = "hoster"
-    __pattern__ = r'http://(?:www\.)?megashares.com/.*'
     __version__ = "0.25"
+    __pattern__ = r'http://(?:www\.)?megashares.com/.*'
     __description__ = """Megashares.com hoster plugin"""
     __author_name__ = "zoidberg"
     __author_mail__ = "zoidberg@mujmail.cz"
@@ -40,6 +25,7 @@ class MegasharesCom(SimpleHoster):
     REACTIVATE_PASSPORT_PATTERN = r'<input[^>]*id="passport_num" value="(\w+)" />'
     REQUEST_URI_PATTERN = r'var request_uri = "([^"]+)";'
     NO_SLOTS_PATTERN = r'<dd class="red">All download slots for this link are currently filled'
+
 
     def setup(self):
         self.resumeDownload = True
