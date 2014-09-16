542bb076b6a51c779f73249b02ffb5fae6950841
synweap15 <shamdog+github@gmail.com>
2014-09-16 23:21:57 +0200

    fix to work with python 2.5

--- module/plugins/hoster/NoPremiumPl.py
+++ module/plugins/hoster/NoPremiumPl.py
@@ -63,14 +63,14 @@ class NoPremiumPl(SimpleHoster):
         self.get_username_password()
         try:
             data = self.runFileQuery(pyfile.url, 'fileinfo')
-        except Exception as e:
-            self.logDebug(str(e))
+        except Exception:
+            self.logDebug("runFileQuery error")
             self.tempOffline()
 
         try:
             parsed = loads(data)
-        except Exception as e:
-            self.logDebug(str(e))
+        except Exception:
+            self.logDebug("loads error")
             self.tempOffline()
 
         self.logDebug(parsed)
@@ -97,8 +97,8 @@ class NoPremiumPl(SimpleHoster):
 
         try:
             result_dl = self.runFileQuery(pyfile.url, 'filedownload')
-        except Exception as e:
-            self.logDebug(str(e))
+        except Exception:
+            self.logDebug("runFileQuery error #2")
             self.tempOffline()
 
         self.download(result_dl, disposition=True)
