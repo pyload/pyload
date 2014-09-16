5aeccc8f2b9607b5e1f992f54014f707b67e41cb
Guidobelix <guidobelix@hotmail.it>
2014-09-16 20:35:59 +0200

    Updated hoster.LomafileCom.py
    Changed FILE_NAME_PATTERN because of problems with certain file names.

--- module/plugins/hoster/LomafileCom.py
+++ module/plugins/hoster/LomafileCom.py
@@ -8,18 +8,19 @@ from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo
 class LomafileCom(SimpleHoster):
     __name__ = "LomafileCom"
     __type__ = "hoster"
-    __version__ = "0.2"
+    __version__ = "0.03"
 
     __pattern__ = r'https?://lomafile\.com/.+/[\w\.]+'
 
     __description__ = """ Lomafile.com hoster plugin """
-    __author_name__ = "nath_schwarz"
-    __author_mail__ = "nathan.notwhite@gmail.com"
+    __author_name__ = "nath_schwarz", "guidobelix"
+    __author_mail__ = "nathan.notwhite@gmail.com", "guidobelix@hotmail.it"
 
-    FILE_NAME_PATTERN = r'Filename:[^>]*>(?P<N>[\w\.]+)'
+    FILE_NAME_PATTERN = r'<a href="http://lomafile.com/w{12}/(?P<N>[\w\.]+?)">'
+#   Old definition of FILE_NAME_PATTERN replaced to deal with special characters
     FILE_SIZE_PATTERN = r'\((?P<S>\d+)\s(?P<U>\w+)\)'
-    OFFLINE_PATTERN = r'Software error'
-
+    OFFLINE_PATTERN = r'No such file with this filename'
+ 
 
     def handleFree(self):
         for _ in range(3):
@@ -54,8 +55,12 @@ class LomafileCom(SimpleHoster):
                 download_url = download_url.group(0)
                 self.logDebug("Download URL: %s" % download_url)
                 self.download(download_url)
+                break
         else:
             self.fail("Invalid captcha-code entered.")
+
+
+getInfo = create_getInfo(LomafileCom)
 
 
 getInfo = create_getInfo(LomafileCom)
