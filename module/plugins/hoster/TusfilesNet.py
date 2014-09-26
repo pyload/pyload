4a077f1ab66b201eb6e819ca3a8d18d54cc54cbf
Walter Purcaro <vuolter@gmail.com>
2014-09-26 12:56:29 +0200

    [TusfilesNet] Added support for Premium users
    https://github.com/pyload/pyload/pull/755

--- module/plugins/hoster/TusfilesNet.py
+++ module/plugins/hoster/TusfilesNet.py
@@ -6,13 +6,13 @@ from module.plugins.hoster.XFileSharingPro import XFileSharingPro, create_getInf
 class TusfilesNet(XFileSharingPro):
     __name__ = "TusfilesNet"
     __type__ = "hoster"
-    __version__ = "0.03"
+    __version__ = "0.04"
 
     __pattern__ = r'https?://(?:www\.)?tusfiles\.net/(?P<ID>\w+)'
 
-    __description__ = """Tusfiles.net hoster plugin"""
-    __author_name__ = "Walter Purcaro"
-    __author_mail__ = "vuolter@gmail.com"
+    __description__ = """ Tusfiles.net hoster plugin """
+    __author_name__ = ("Walter Purcaro", "guidobelix")
+    __author_mail__ = ("vuolter@gmail.com", "guidobelix@hotmail.it")
 
     HOSTER_NAME = "tusfiles.net"
 
@@ -26,6 +26,10 @@ class TusfilesNet(XFileSharingPro):
         self.multiDL = False
         self.chunkLimit = -1
         self.resumeDownload = True
+
+
+    def handlePremium(self):
+        return self.handleFree()
 
 
 getInfo = create_getInfo(TusfilesNet)
