b7341c99592c115a9b03715446a98df46a5676c4
prOq <anonim>
2014-09-28 20:32:27 +0200

    CatShareNet: ip check corrected; charset corrected; added resumeDownload()

--- module/plugins/hoster/CatShareNet.py
+++ module/plugins/hoster/CatShareNet.py
@@ -2,8 +2,6 @@
 
 import re
 
-from urllib import unquote
-
 from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo
 from module.plugins.internal.CaptchaService import ReCaptcha
 
@@ -11,47 +9,54 @@ from module.plugins.internal.CaptchaService import ReCaptcha
 class CatShareNet(SimpleHoster):
     __name__ = "CatShareNet"
     __type__ = "hoster"
-    __version__ = "0.02"
+    __version__ = "0.05"
 
-    __pattern__ = r'http://(?:www\.)?catshare\.net/\w+'
+    __pattern__ = r'http://(?:www\.)?catshare\.net/\w{16}'
 
     __description__ = """CatShare.net hoster plugin"""
-    __author_name__ = ("z00nx", "prOq")
-    __author_mail__ = ("z00nx0@gmail.com", None)
+    __author_name__ = ("z00nx", "prOq", "Walter Purcaro")
+    __author_mail__ = ("z00nx0@gmail.com", None, "vuolter@gmail.com")
 
 
-    FILE_INFO_PATTERN = r'<h3 class="pull-left"[^>]+>(?P<N>.+)</h3>\s+<h3 class="pull-right"[^>]+>(?P<S>.+)</h3>'
-    FILE_OFFLINE_PATTERN = r'Podany plik zosta'
+    FILE_INFO_PATTERN = r'<title>(?P<N>.+) \((?P<S>[\d.]+) (?P<U>\w+)\)<'
+    OFFLINE_PATTERN = r'Podany plik zosta. usuni.ty\s*</div>'
 
-    SECONDS_PATTERN = 'var\s+count\s+=\s+(\d+);'
+    IP_BLOCKED_PATTERN = r'>Nasz serwis wykry. .e Tw.j adres IP nie pochodzi z Polski.<'
+    SECONDS_PATTERN = 'var\scount\s=\s(\d+);'
     RECAPTCHA_KEY = "6Lfln9kSAAAAANZ9JtHSOgxUPB9qfDFeLUI_QMEy"
     LINK_PATTERN = r'<form action="(.+?)" method="GET">'
 
 
+    def setup(self):
+	self.resumeDownload = True
+	self.multiDL = self.premium
+
+    def getFileInfo(self):
+        m = re.search(self.IP_BLOCKED_PATTERN, self.html)
+        if m is not None:
+            self.fail("Only connections from Polish IP address are allowed")
+        return super(CatShareNet, self).getFileInfo()
+
+
     def handleFree(self):
         m = re.search(self.SECONDS_PATTERN, self.html)
-        if m is not None:
-            seconds = int(m.group(1))
-            self.logDebug("Seconds found", seconds)
-            self.wait(seconds + 1)
+        if m:
+            wait_time = int(m.group(1))
+            self.wait(wait_time, True)
 
-        # solve captcha and send solution
+        recaptcha = ReCaptcha(self)
         challenge, code = recaptcha.challenge(self.RECAPTCHA_KEY)
         self.html = self.load(self.pyfile.url,
-                              post={'recaptcha_challenge_field': challenge, 'recaptcha_response_field': code},
-                              decode=True,
-                              cookies=True,
-                              ref=True)
+                              post={'recaptcha_challenge_field': challenge,
+                                    'recaptcha_response_field': code})
 
-        # find download url
         m = re.search(self.LINK_PATTERN, self.html)
         if m is None:
             self.invalidCaptcha()
             self.retry(reason="Wrong captcha entered")
 
-        download_url = unquote(m.group(1))
-        self.logDebug("Download url: " + download_url)
-        self.download(download_url)
+        dl_link = m.group(1)
+        self.download(dl_link)
 
 
 getInfo = create_getInfo(CatShareNet)
