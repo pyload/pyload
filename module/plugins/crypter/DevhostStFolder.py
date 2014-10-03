33322d1b9fc5146c8b3cdc443d915d94f3ed0b52
zapp-brannigan <fuerst.reinje@web.de>
2014-10-03 17:31:04 +0200

    [Dev-Host] Folder support (partially)

---
+++ module/plugins/crypter/DevhostStFolder.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+# Test links:
+# http://d-h.st/users/shine/?fld_id=37263#files
+
+import re
+
+from urlparse import urljoin
+
+from module.plugins.internal.SimpleCrypter import SimpleCrypter
+
+
+class DevhostStFolder(SimpleCrypter):
+    __name__ = "DevhostStFolder"
+    __type__ = "crypter"
+    __version__ = "0.01"
+
+    __pattern__ = r"http://(?:www\.)?d-h\.st/users/\w+/\?fld_id=\d+"
+
+    __description__ = """d-h.st decrypter plugin"""
+    __author_name_ = "zapp-brannigan"
+    __author_mail_ = "fuerst.reinje@web.de"
+
+
+    LINK_PATTERN = r'width: 530px;"><a href="(.+?)"'
+
+
+    def getLinks(self):
+        return [urljoin("http://d-h.st", link) for link in re.findall(self.LINK_PATTERN, self.html)]
