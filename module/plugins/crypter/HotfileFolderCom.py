c6eb00c85ee7ee83621175003e6140cdddf8f35e
Walter Purcaro <vuolter@gmail.com>
2014-10-05 11:52:56 +0200

    [HotfileFolderCom] Fix broken __pattern__ (thx zapp-brannigan)

--- module/plugins/crypter/HotfileFolderCom.py
+++ module/plugins/crypter/HotfileFolderCom.py
@@ -6,9 +6,9 @@ from module.plugins.internal.DeadCrypter import DeadCrypter
 class HotfileFolderCom(DeadCrypter):
     __name__ = "HotfileFolderCom"
     __type__ = "crypter"
-    __version__ = "0.2"
+    __version__ = "0.3"
 
-    __pattern__ = r'https)://(?:www\.)?hotfile\.com/list/\w+/\w+'
+    __pattern__ = r'https?://(?:www\.)?hotfile\.com/list/\w+/\w+'
 
     __description__ = """Hotfile.com folder decrypter plugin"""
     __author_name__ = "RaNaN"
