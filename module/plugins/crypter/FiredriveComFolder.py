7a253238d4ba3486b90fba6547b955f6a3d31073
Walter Purcaro <vuolter@gmail.com>
2014-10-10 14:42:32 +0200

    [FiredriveComFolder] Mark dead

--- module/plugins/crypter/FiredriveComFolder.py
+++ module/plugins/crypter/FiredriveComFolder.py
@@ -1,29 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import re
-
-from module.plugins.internal.SimpleCrypter import SimpleCrypter
+from module.plugins.internal.DeadCrypter import DeadCrypter
 
 
-class FiredriveComFolder(SimpleCrypter):
+class FiredriveComFolder(DeadCrypter):
     __name__ = "FiredriveComFolder"
     __type__ = "crypter"
-    __version__ = "0.02"
+    __version__ = "0.03"
 
     __pattern__ = r'https?://(?:www\.)?(firedrive|putlocker)\.com/share/.+'
 
     __description__ = """Firedrive.com folder decrypter plugin"""
     __license__ = "GPLv3"
     __authors__ = [("Walter Purcaro", "vuolter@gmail.com")]
-
-
-    LINK_PATTERN = r'<div class="pf_item pf_(file|folder).+?public=\'(.+?)\''
-    TITLE_PATTERN = r'>Shared Folder "(.+)" | Firedrive<'
-    OFFLINE_PATTERN = r'class="sad_face_image"|>No such page here.<'
-    TEMP_OFFLINE_PATTERN = r'>(File Temporarily Unavailable|Server Error. Try again later)'
-
-
-    def getLinks(self):
-        return map(lambda x: "http://www.firedrive.com/%s/%s" %
-                   ("share" if x[0] == "folder" else "file", x[1]),
-                   re.findall(self.LINK_PATTERN, self.html))
