9a58e0d535cf8144c6da4cc86825779420137aca
Walter Purcaro <vuolter@gmail.com>
2014-10-10 14:16:56 +0200

    [EasybytezCom] Update LINK_PATTERN

--- module/plugins/hoster/EasybytezCom.py
+++ module/plugins/hoster/EasybytezCom.py
@@ -6,7 +6,7 @@ from module.plugins.internal.XFSPHoster import XFSPHoster, create_getInfo
 class EasybytezCom(XFSPHoster):
     __name__ = "EasybytezCom"
     __type__ = "hoster"
-    __version__ = "0.20"
+    __version__ = "0.21"
 
     __pattern__ = r'http://(?:www\.)?easybytez\.com/\w{12}'
 
@@ -21,7 +21,7 @@ class EasybytezCom(XFSPHoster):
     FILE_INFO_PATTERN = r'<span class="name">(?P<N>.+)</span><br>\s*<span class="size">(?P<S>[^<]+)</span>'
     OFFLINE_PATTERN = r'>File not available'
 
-    LINK_PATTERN = r'(http://(\w+\.(easyload|easybytez|zingload)\.(com|to)|\d+\.\d+\.\d+\.\d+)/files/\d+/\w+/[^"<]+)'
+    LINK_PATTERN = r'(http://(\w+\.(easybytez|easyload|ezbytez|zingload)\.(com|to)|\d+\.\d+\.\d+\.\d+)/files/\d+/\w+/.+?)["\'<]'
     OVR_LINK_PATTERN = r'<h2>Download Link</h2>\s*<textarea[^>]*>([^<]+)'
     ERROR_PATTERN = r'(?:class=["\']err["\'][^>]*>|<Center><b>)(.*?)</'
 
