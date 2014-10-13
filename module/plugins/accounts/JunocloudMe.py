f69b7a4a9ee93a0f0a267b5369bdf7a3fe0b819e
guidobelix <guidobelix@hotmail.it>
2014-10-13 23:24:16 +0200

    Improved pattern & fixed typos modified:   accounts/JunocloudMe.py modified:   crypter/JunocloudMeFolder.py modified:   hoster/JunocloudMe.py

--- module/plugins/accounts/JunocloudMe.py
+++ module/plugins/accounts/JunocloudMe.py
@@ -8,17 +8,15 @@ from module.plugins.internal.XFSPAccount import XFSPAccount
 from module.utils import parseFileSize
 
 
-class EasybytezCom(XFSPAccount):
+class JunocloudMe(XFSPAccount):
     __name__ = "JunocloudMe"
     __type__ = "account"
     __version__ = "0.01"
 
     __description__ = """Junocloud.me account plugin"""
     __license__ = "GPLv3"
-    __authors__ = ("guidobelix", "guidobelix@hotmail.it")
+    __authors__ = [("guidobelix", "guidobelix@hotmail.it")]
 
 
     HOSTER_URL = "http://www.junocloud.me/"
 
-
-        return {"validuntil": validuntil, "trafficleft": trafficleft, "premium": premium}
--- module/plugins/hoster/JunocloudMe.py
+++ module/plugins/hoster/JunocloudMe.py
@@ -3,12 +3,12 @@
 from module.plugins.internal.XFSPHoster import XFSPHoster, create_getInfo
 
 
-class EasybytezCom(XFSPHoster):
+class JunocloudMe(XFSPHoster):
     __name__ = "JunocloudMe"
     __type__ = "hoster"
     __version__ = "0.01"
 
-    __pattern__ = r'http://(?:www\.)?easybytez\.com/\w{12}'
+    __pattern__ = r'http://(?:\w+\.)?junocloud\.me/\w{12}'
 
     __description__ = """Junoclud.me hoster plugin"""
     __license__ = "GPLv3"
@@ -17,8 +17,20 @@ class EasybytezCom(XFSPHoster):
 
     HOSTER_NAME = "junocloud.me"
 
-    FILE_INFO_PATTERN = r'<span class="name">(?P<N>.+)</span><br>\s*<span class="size">(?P<S>[^<]+)</span>'
-    OFFLINE_PATTERN = r'>File not available'
+    FILE_URL_REPLACEMENTS = [(r'//(junocloud)', r'//dl3.\1')]
+    FILE_NAME_PATTERN = r'<p class="request_file">http://junocloud.me/w{12}/(?P<N>.+?)</p>'
+    FILE_SIZE_PATTERN = r'<p class="request_filesize">Size: (?P<S>[\d.]+) (?P<U>\w+)</p>'
+
+    OFFLINE_PATTERN = r'>No such file with this filename<'
+    TEMP_OFFLINE_PATTERN = r'The page may have been renamed, removed or be temporarily unavailable.<'
+
+
+    def handleFree(self):
+        # This line is required due to the getURL workaround. Can be removed in 0.4.10
+        self.html = self.load(self.pyfile.url, decode=not self.TEXT_ENCODING)
+        url = self.getDownloadLink()
+        self.logDebug("Download URL: %s" % url)
+        self.startDownload(url)
 
 
 getInfo = create_getInfo(JunocloudMe)
