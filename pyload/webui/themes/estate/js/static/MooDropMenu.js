0ca6e7c210a8cfb0be215a85a4a73c7dcc00bc67
Walter Purcaro <vuolter@gmail.com>
2014-10-17 02:23:53 +0200

    [themes] Restore static js

--- pyload/webui/themes/estate/js/static/MooDropMenu.js
+++ pyload/webui/themes/estate/js/static/MooDropMenu.js
@@ -19,68 +19,68 @@ provides: [MooDropMenu, Element.MooDropMenu]
 
 var MooDropMenu = new Class({
 
-  Implements: [Options, Events],
+	Implements: [Options, Events],
 
-  options: {
-    onOpen: function(el){
-      el.removeClass('close').addClass('open');
-    },
-    onClose: function(el){
-      el.removeClass('open').addClass('close');
-    },
-    onInitialize: function(el){
-      el.removeClass('open').addClass('close');
-    },
-    mouseoutDelay: 200,
-    mouseoverDelay: 0,
-    listSelector: 'ul',
-    itemSelector: 'li',
-    openEvent: 'mouseenter',
-    closeEvent: 'mouseleave'
-  },
+	options: {
+		onOpen: function(el){
+			el.removeClass('close').addClass('open');
+		},
+		onClose: function(el){
+			el.removeClass('open').addClass('close');
+		},
+		onInitialize: function(el){
+			el.removeClass('open').addClass('close');
+		},
+		mouseoutDelay: 200,
+		mouseoverDelay: 0,
+		listSelector: 'ul',
+		itemSelector: 'li',
+		openEvent: 'mouseenter',
+		closeEvent: 'mouseleave'
+	},
 
-  initialize: function(menu, options, level){
-    this.setOptions(options);
-    options = this.options;
+	initialize: function(menu, options, level){
+		this.setOptions(options);
+		options = this.options;
 
-    var menu = this.menu = document.id(menu);
+		var menu = this.menu = document.id(menu);
 
-    menu.getElements(options.itemSelector + ' > ' + options.listSelector).each(function(el){
+		menu.getElements(options.itemSelector + ' > ' + options.listSelector).each(function(el){
 
-      this.fireEvent('initialize', el);
+			this.fireEvent('initialize', el);
 
-      var parent = el.getParent(options.itemSelector),
-        timer;
+			var parent = el.getParent(options.itemSelector),
+				timer;
 
-      parent.addEvent(options.openEvent, function(){
-        parent.store('DropDownOpen', true);
+			parent.addEvent(options.openEvent, function(){
+				parent.store('DropDownOpen', true);
 
-        clearTimeout(timer);
-        if (options.mouseoverDelay) timer = this.fireEvent.delay(options.mouseoverDelay, this, ['open', el]);
-        else this.fireEvent('open', el);
+				clearTimeout(timer);
+				if (options.mouseoverDelay) timer = this.fireEvent.delay(options.mouseoverDelay, this, ['open', el]);
+				else this.fireEvent('open', el);
 
-      }.bind(this)).addEvent(options.closeEvent, function(){
-        parent.store('DropDownOpen', false);
+			}.bind(this)).addEvent(options.closeEvent, function(){
+				parent.store('DropDownOpen', false);
 
-        clearTimeout(timer);
-        timer = (function(){
-          if (!parent.retrieve('DropDownOpen')) this.fireEvent('close', el);
-        }).delay(options.mouseoutDelay, this);
+				clearTimeout(timer);
+				timer = (function(){
+					if (!parent.retrieve('DropDownOpen')) this.fireEvent('close', el);
+				}).delay(options.mouseoutDelay, this);
 
-      }.bind(this));
+			}.bind(this));
 
-    }, this);
-  },
+		}, this);
+	},
 
-  toElement: function(){
-    return this.menu
-  }
+	toElement: function(){
+		return this.menu
+	}
 
 });
 
 /* So you can do like this $('nav').MooDropMenu(); or even $('nav').MooDropMenu().setStyle('border',1); */
 Element.implement({
-  MooDropMenu: function(options){
-    return this.store('MooDropMenu', new MooDropMenu(this, options));
-  }
+	MooDropMenu: function(options){
+		return this.store('MooDropMenu', new MooDropMenu(this, options));
+	}
 });
