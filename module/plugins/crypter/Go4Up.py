73c0cfa488342c252a1122d5d9035b8334391252
reissdorf <reissdorf@domain.invalid>
2014-10-23 18:00:13 +0200

    Cosmetics

--- module/plugins/crypter/Go4Up.py
+++ module/plugins/crypter/Go4Up.py
@@ -2,17 +2,19 @@
 import re
 
 from module.plugins.internal.SimpleCrypter import SimpleCrypter
-from BeautifulSoup import BeautifulSoup
+from module.lib.BeautifulSoup import BeautifulSoup
 from urlparse import urlparse
 
+
 class Go4Up(SimpleCrypter):
+    """Go4up - Multiupload Service"""
 
     __name__ = "Go4Up"
     __type__ = "crypter"
     __version__ = "0.1"
     __pattern__ = r"http://(www\.)?go4up.com/dl/\w+.*"
     __config__ = [("preferedHoster", "str", "Prefered hoster list (pipe-separated) ", ""),
-              ("ignoredHoster", "str", "Ignored hoster list (pipe-separated) ", "")]
+                  ("ignoredHoster", "str", "Ignored hoster list (pipe-separated) ", "")]
     __description__ = """Go4Up plugin"""
     __license__ = "GPLv3"
     __authors__ = [("reissdorf", "reissdorf@domain.invalid")]
@@ -25,34 +27,36 @@ class Go4Up(SimpleCrypter):
     BASE_URL = 'http://go4up.com'
 
     def decrypt(self, pyfile):
+        """Extract hoster links"""
 
         self.html = self.load(pyfile.url, decode=True)
-        package_links = []
         self.prefered_hosters = self.getConfig("preferedHoster").split('|')
         self.ignored_hosters = self.getConfig("ignoredHoster").split('|')
 
+        package_links = []
         package_name, folder_name = self.getPackageNameAndFolder()
 
+        # Find hosters links (loaded with Ajax on the webpage)
         hosts_url = re.search(self.HOSTS_LOOKUP_PATTERN, self.html)
         self.html = self.load(self.BASE_URL + hosts_url.group(0), decode=True)
-
         hosts_links = re.findall(self.LINK_PATTERN, self.html)
 
+        # Iterate over the found links (if any) and add them to the package links list
         for link in hosts_links:
             found_package_links = self.find_package_links(link)
             if len(found_package_links) > 0:
                 package_links = package_links + found_package_links
 
-        self.logDebug("Package has %d links" % len(package_links))
-
         if len(package_links) > 0:
-            package_links.sort(key=lambda x: urlparse(link).hostname in self.prefered_hosters)
+            package_links.sort(key=lambda x: urlparse(x).hostname in self.prefered_hosters, reverse=True)
             self.packages = [(package_name, package_links, folder_name)]
         else:
             self.fail('Could not extract any links')
 
 
     def find_package_links(self, url):
+        """Find hoster links from Go4Up internal URL"""
+
         html = self.load(url, decode=True)
         soup = BeautifulSoup(html)
         container = soup.find('div', {'id': 'linklist'})
