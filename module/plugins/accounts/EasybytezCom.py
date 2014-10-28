8f2d17cb3c16c21da01315599864bdde20595bee
Walter Purcaro <vuolter@gmail.com>
2014-10-28 21:54:07 +0100

    [EasybytezCom] Remove unnecessary loadAccountInfo method

--- module/plugins/accounts/EasybytezCom.py
+++ module/plugins/accounts/EasybytezCom.py
@@ -19,38 +19,3 @@ class EasybytezCom(XFSPAccount):
 
 
     HOSTER_NAME = "easybytez.com"
-
-
-    def loadAccountInfo(self, user, req):
-        html = req.load(self.HOSTER_URL, get={'op': "my_account"}, decode=True)
-
-        validuntil = None
-        trafficleft = None
-        premium = False
-
-        m = re.search(self.VALID_UNTIL_PATTERN, html)
-        if m:
-            expiredate = m.group(1)
-            self.logDebug("Expire date: " + expiredate)
-
-            try:
-                validuntil = mktime(strptime(expiredate, "%d %B %Y"))
-            except Exception, e:
-                self.logError(e)
-
-            if validuntil > mktime(gmtime()):
-                premium = True
-                trafficleft = -1
-            else:
-                premium = False
-                validuntil = -1
-
-        m = re.search(self.TRAFFIC_LEFT_PATTERN, html)
-        if m:
-            trafficleft = m.group(1)
-            if "Unlimited" in trafficleft:
-                trafficleft = -1
-            else:
-                trafficleft = self.parseTraffic(trafficleft)
-
-        return {"validuntil": validuntil, "trafficleft": trafficleft, "premium": premium}
