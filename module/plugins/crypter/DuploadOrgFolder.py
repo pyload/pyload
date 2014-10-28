b6a85c2f635a2f1d020d06920c5e4ac9f82a153d
Walter Purcaro <vuolter@gmail.com>
2014-10-28 17:24:29 +0100

    [DuploadOrgFolder] Mark dead

--- module/plugins/crypter/DuploadOrgFolder.py
+++ module/plugins/crypter/DuploadOrgFolder.py
@@ -1,20 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from module.plugins.internal.SimpleCrypter import SimpleCrypter
+from module.plugins.internal.DeadCrypter import DeadCrypter
 
 
-class DuploadOrgFolder(SimpleCrypter):
+class DuploadOrgFolder(DeadCrypter):
     __name__    = "DuploadOrgFolder"
     __type__    = "crypter"
-    __version__ = "0.01"
+    __version__ = "0.02"
 
-    __pattern__ = r'http://(?:www\.)?dupload\.org/folder/\d+/'
-    __config__  = [("use_subfolder", "bool", "Save package to subfolder", True),
-                   ("subfolder_per_package", "bool", "Create a subfolder for each package", True)]
+    __pattern__ = r'http://(?:www\.)?dupload\.org/folder/\d+'
+    __config__  = []
 
     __description__ = """Dupload.org folder decrypter plugin"""
     __license__     = "GPLv3"
     __authors__     = [("stickell", "l.stickell@yahoo.it")]
-
-
-    LINK_PATTERN = r'<td style="[^"]+"><a href="(http://[^"]+)" target="_blank">[^<]+</a></td>'
