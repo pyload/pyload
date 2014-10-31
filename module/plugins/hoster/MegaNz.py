a591f5617de1280441012ddf846ccc1088963f47
Walter Purcaro <vuolter@gmail.com>
2014-11-01 00:05:44 +0100

    [MegaNz] Fix SSL protocol error (thx cocoto)

--- module/plugins/hoster/MegaNz.py
+++ module/plugins/hoster/MegaNz.py
@@ -3,11 +3,13 @@
 import random
 import re
 
-from Crypto.Cipher import AES
-from Crypto.Util import Counter
 from array import array
 from base64 import standard_b64decode
 from os import remove
+
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+from pycurl import SSL_CIPHER_LIST
 
 from module.common.json_layer import json_loads, json_dumps
 from module.plugins.Hoster import Hoster
@@ -16,7 +18,7 @@ from module.plugins.Hoster import Hoster
 class MegaNz(Hoster):
     __name__    = "MegaNz"
     __type__    = "hoster"
-    __version__ = "0.15"
+    __version__ = "0.16"
 
     __pattern__ = r'https?://(\w+\.)?mega\.co\.nz/#!([\w!-]+)'
 
@@ -128,6 +130,8 @@ class MegaNz(Hoster):
         attr = self.decryptAttr(dl['at'], key)
 
         pyfile.name = attr['n'] + self.FILE_SUFFIX
+
+        self.req.http.c.setopt(SSL_CIPHER_LIST, "RC4-MD5:DEFAULT")
 
         self.download(dl['g'])
         self.decryptFile(key)
