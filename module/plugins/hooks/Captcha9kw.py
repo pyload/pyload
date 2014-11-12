7699d4b88c6245bd31d94ff0b5954cdd7c08ca73
Walter Purcaro <vuolter@gmail.com>
2014-11-12 01:55:00 +0100

    [Captcha9kw] Fix str.strip

--- module/plugins/hooks/Captcha9kw.py
+++ module/plugins/hooks/Captcha9kw.py
@@ -17,7 +17,7 @@ from module.plugins.Hook import Hook
 class Captcha9kw(Hook):
     __name__    = "Captcha9kw"
     __type__    = "hook"
-    __version__ = "0.19"
+    __version__ = "0.20"
 
     __config__ = [("activated", "bool", "Activated", True),
                   ("force", "bool", "Force captcha resolving even if client is connected", True),
@@ -88,7 +88,7 @@ class Captcha9kw(Hook):
 
         for opt in self.getConfig("hoster_options").split('|'):
 
-            details = map(strip(), opt.split(':'))
+            details = map(str.strip, opt.split(':'))
 
             if not details or details[0].lower() != pluginname.lower():
                 continue
@@ -175,7 +175,7 @@ class Captcha9kw(Hook):
             self.fail(_("Too many captchas in queue"))
 
         for opt in self.getConfig("hoster_options").split('|'):
-            details = map(strip(), opt.split(':'))
+            details = map(str.strip, opt.split(':'))
 
             if not details or details[0].lower() != pluginname.lower():
                 continue
