adf154d358abfed43f49873cb08ba71379416471
Walter Purcaro <vuolter@gmail.com>
2014-11-21 18:19:00 +0100

    [EasybytezCom] Use XFSHoster INFO_PATTERN

--- module/plugins/hoster/EasybytezCom.py
+++ module/plugins/hoster/EasybytezCom.py
@@ -6,7 +6,7 @@ from module.plugins.internal.XFSHoster import XFSHoster, create_getInfo
 class EasybytezCom(XFSHoster):
     __name__    = "EasybytezCom"
     __type__    = "hoster"
-    __version__ = "0.22"
+    __version__ = "0.23"
 
     __pattern__ = r'http://(?:www\.)?easybytez\.com/\w{12}'
 
@@ -18,7 +18,6 @@ class EasybytezCom(XFSHoster):
 
     HOSTER_DOMAIN = "easybytez.com"
 
-    INFO_PATTERN = r'<span class="name">(?P<N>.+)</span><br>\s*<span class="size">(?P<S>[^<]+)</span>'
     OFFLINE_PATTERN = r'>File not available'
 
     LINK_PATTERN = r'(http://(\w+\.(easybytez|easyload|ezbytez|zingload)\.(com|to)|\d+\.\d+\.\d+\.\d+)/files/\d+/\w+/.+?)["\'<]'
