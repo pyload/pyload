817a3d66e6ddaf90e4649212dae4c240a0847c9c
fedeG <federicogonzalez761@gmail.com>
2014-12-07 17:54:34 -0300

    PEP-8, Python Zen, refactor and reduce code (part 4 in master script). Bash varible $VAR to ${VAR}
    Conflicts:
    	scripts/clonedigger.sh
    	scripts/code_analysis.sh

--- tests/code_analysis.sh
+++ tests/code_analysis.sh
@@ -4,12 +4,12 @@ PYLOAD="../pyload"  # Check pyload directory
 
 echo "Running sloccount ..."
 REPORT="sloccount.sc"
-sloccount --duplicates --wide --details $PYLOAD > $REPORT && echo "Done. Report saved to $REPORT"
+sloccount --duplicates --wide --details ${PYLOAD} > ${REPORT} && echo "Done. Report saved to $REPORT"
 
 echo "Running pep8 ..."
 REPORT="pep8.txt"
-pep8 $PYLOAD > $REPORT && echo "Done. Report saved to $REPORT"
+pep8 ${PYLOAD} > ${REPORT} && echo "Done. Report saved to $REPORT"
 
 echo "Running pylint ..."
 REPORT="pylint.txt"
-pylint --reports=no $PYLOAD > $REPORT && echo "Done. Report saved to $REPORT"
+pylint --reports=no ${PYLOAD} > ${REPORT} && echo "Done. Report saved to $REPORT"
