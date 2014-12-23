142aee553f077798295656ac597945abdecb4e40
Walter Purcaro <vuolter@gmail.com>
2014-12-23 13:57:57 +0100

    [CloudzillaTo] Fixup

--- module/plugins/hoster/CloudzillaTo.py
+++ module/plugins/hoster/CloudzillaTo.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import re
+
 from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo
-from module.common.json_layer import json_loads
 
 
 class CloudzillaTo(SimpleHoster):
     __name__    = "CloudzillaTo"
     __type__    = "hoster"
-    __version__ = "0.02"
+    __version__ = "0.03"
 
     __pattern__ = r'http://(?:www\.)?cloudzilla\.to/share/file/(?P<ID>[\w^_]+)'
 
@@ -16,24 +17,27 @@ class CloudzillaTo(SimpleHoster):
     __authors__     = [("Walter Purcaro", "vuolter@gmail.com")]
 
 
-    INFO_PATTERN    = r'>(?P<N>.+?)</span> <span class="size">\((?P<S>[\d.]+) (?P<U>[\w^_]+)'
+    INFO_PATTERN    = r'title="(?P<N>.+?)">\1</span> <span class="size">\((?P<S>[\d.]+) (?P<U>[\w^_]+)'
     OFFLINE_PATTERN = r'>File not found...<'
 
 
     def handleFree(self):
-        ticket = json_loads(self.load("http://www.cloudzilla.to/generateticket/",
-                                      post={'file_id': self.info['pattern']['ID'], 'key': ""}))['result']
+        self.html = self.load("http://www.cloudzilla.to/generateticket/",
+                              post={'file_id': self.info['pattern']['ID'], 'key': self.getPassword()})
 
-        if ticket['status'] is "error":
-            self.fail(ticket['status']['error'])
+        ticket = dict(re.findall(r'<(.+?)>([^<>]+?)</', self.html))
+
+        self.logDebug(ticket)
+
+        if 'error' in ticket:
+            self.fail(ticket['error'])
 
         if 'wait' in ticket:
-            wait_time = int(ticket['wait'])
-            self.wait(wait_time, wait_time > 5)
+            self.wait(int(ticket['wait']), int(ticket['wait']) > 5)
 
-        self.download("http://%(server)s/download/%(file_id)s/%(ticket_id)s" % {'server'   : ticket['server'],
-                                                                                'file_id'  : self.info['pattern']['ID'],
-                                                                                'ticket_id': ticket['ticket_id']})
+        self.link = "http://%(server)s/download/%(file_id)s/%(ticket_id)s" % {'server'   : ticket['server'],
+                                                                              'file_id'  : self.info['pattern']['ID'],
+                                                                              'ticket_id': ticket['ticket_id']})
 
 
     def handlePremium(self):
