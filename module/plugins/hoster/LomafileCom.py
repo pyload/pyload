c26db1f8fcd736d9de00095343779fd5519d565c
Walter Purcaro <vuolter@gmail.com>
2014-12-26 18:48:10 +0100

    [LomafileCom] Mark dead

--- module/plugins/hoster/LomafileCom.py
+++ module/plugins/hoster/LomafileCom.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from module.plugins.internal.XFSHoster import XFSHoster, create_getInfo
+from module.plugins.internal.DeadHoster import DeadHoster, create_getInfo
 
 
-class LomafileCom(XFSHoster):
+class LomafileCom(DeadHoster):
     __name__    = "LomafileCom"
     __type__    = "hoster"
-    __version__ = "0.51"
+    __version__ = "0.52"
 
     __pattern__ = r'http://lomafile\.com/\w{12}'
 
@@ -14,16 +14,6 @@ class LomafileCom(XFSHoster):
     __license__     = "GPLv3"
     __authors__     = [("nath_schwarz", "nathan.notwhite@gmail.com"),
                        ("guidobelix", "guidobelix@hotmail.it")]
-
-
-    HOSTER_DOMAIN = "lomafile.com"
-
-    NAME_PATTERN = r'<a href="http://lomafile\.com/w{12}/(?P<N>.+?)">'
-
-    OFFLINE_PATTERN      = r'>(No such file|Software error:<)'
-    TEMP_OFFLINE_PATTERN = r'The page may have been renamed, removed or be temporarily unavailable.<'
-
-    CAPTCHA_PATTERN = r'(http://lomafile\.com/captchas/[^"\']+)'
 
 
 getInfo = create_getInfo(LomafileCom)
