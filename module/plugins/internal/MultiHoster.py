f37514e315e076379ab73539ffa36e48671976cc
Walter Purcaro <vuolter@gmail.com>
2014-12-26 04:19:57 +0100

    [MultiHoster] Improve

--- module/plugins/internal/MultiHoster.py
+++ module/plugins/internal/MultiHoster.py
@@ -2,13 +2,13 @@
 
 import re
 
-from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo
+from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo, replace_patterns, set_cookies
 
 
 class MultiHoster(SimpleHoster):
     __name__    = "MultiHoster"
     __type__    = "hoster"
-    __version__ = "0.24"
+    __version__ = "0.25"
 
     __pattern__ = r'^unmatchable$'
 
@@ -17,12 +17,34 @@ class MultiHoster(SimpleHoster):
     __authors__     = [("Walter Purcaro", "vuolter@gmail.com")]
 
 
-    DIRECT_LINK   = True
+    LOGIN_ACCOUNT = True
 
 
     def setup(self):
         self.chunkLimit = 1
         self.multiDL    = self.premium
+
+
+    def prepare(self):
+        self.info      = {}
+        self.link      = ""     #@TODO: Move to hoster class in 0.4.10
+        self.directDL  = False  #@TODO: Move to hoster class in 0.4.10
+
+        if self.LOGIN_ACCOUNT and not self.account:
+            self.fail(_("Required account not found"))
+
+        self.req.setOption("timeout", 120)
+
+        if isinstance(self.COOKIES, list):
+            set_cookies(self.req.cj, self.COOKIES)
+
+        if self.DIRECT_LINK is None:
+            self.directDL = self.__pattern__ != r'^unmatchable$'
+        else:
+            self.directDL = self.DIRECT_LINK
+
+        self.pyfile.url = replace_patterns(self.pyfile.url,
+                                           self.FILE_URL_REPLACEMENTS if hasattr(self, "FILE_URL_REPLACEMENTS") else self.URL_REPLACEMENTS)  #@TODO: Remove FILE_URL_REPLACEMENTS check in 0.4.10
 
 
     def process(self, pyfile):
@@ -35,7 +57,7 @@ class MultiHoster(SimpleHoster):
         if not self.link and not self.lastDownload:
             self.preload()
 
-            if self.premium and (not self.CHECK_TRAFFIC or self.checkTrafficLeft()):
+            if self.premium and not self.CHECK_TRAFFIC or self.checkTrafficLeft():
                 self.logDebug("Handled as premium download")
                 self.handlePremium()
 
@@ -55,4 +77,4 @@ class MultiHoster(SimpleHoster):
         if self.premium:
             raise NotImplementedError
         else:
-            self.logError(_("Required account not found"))
+            self.fail(_("Required premium account not found"))
