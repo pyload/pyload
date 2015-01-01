2907ad099a4ac1faa1c5661feb02bbdbce0b5244
Walter Purcaro <vuolter@gmail.com>
2015-01-01 01:47:26 +0100

    [NowVideoSx] Improve __pattern__

--- module/plugins/hoster/NowVideoSx.py
+++ module/plugins/hoster/NowVideoSx.py
@@ -8,9 +8,9 @@ from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo
 class NowVideoSx(SimpleHoster):
     __name__    = "NowVideoSx"
     __type__    = "hoster"
-    __version__ = "0.08"
+    __version__ = "0.09"
 
-    __pattern__ = r'http://(?:www\.)?nowvideo\.(at|ch|co|eu|sx)/(video|mobile/#/videos)/(?P<ID>\w+)'
+    __pattern__ = r'http://(?:www\.)?nowvideo\.(at|ch|co|eu|li|sx)/(video|mobile/#/videos)/(?P<ID>\w+)'
 
     __description__ = """NowVideo.sx hoster plugin"""
     __license__     = "GPLv3"
