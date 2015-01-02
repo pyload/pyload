3639af423fab5d905e9222f0125aeea2dcf58118
Walter Purcaro <vuolter@gmail.com>
2015-01-02 01:17:16 +0100

    [RapiduNet] Account trafficleft

--- module/plugins/accounts/RapiduNet.py
+++ module/plugins/accounts/RapiduNet.py
@@ -11,7 +11,7 @@ from module.common.json_layer import json_loads
 class RapiduNet(Account):
     __name__    = "RapiduNet"
     __type__    = "account"
-    __version__ = "0.04"
+    __version__ = "0.05"
 
     __description__ = """Rapidu.net account plugin"""
     __license__     = "GPLv3"
@@ -22,6 +22,8 @@ class RapiduNet(Account):
     PREMIUM_PATTERN = r'>Account: <b>Premium'
 
     VALID_UNTIL_PATTERN = r'>Account: <b>\w+ \((\d+)'
+
+    TRAFFIC_LEFT_PATTERN = r'class="tipsyS"><b>(.+?)<'
 
 
     def loadAccountInfo(self, user, req):
@@ -37,6 +39,10 @@ class RapiduNet(Account):
         m = re.search(self.VALID_UNTIL_PATTERN, html)
         if m:
             validuntil = time() + (86400 * int(m.group(1)))
+
+        m = re.search(self.TRAFFIC_LEFT_PATTERN, html)
+        if m:
+            trafficleft = self.parseTraffic(m.group(1))
 
         return {'validuntil': validuntil, 'trafficleft': trafficleft, 'premium': premium}
 
