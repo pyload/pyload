d811bb9a436888e5c0f50c71fb6cdafa053ceb51
Walter Purcaro <vuolter@gmail.com>
2015-01-03 01:00:24 +0100

    [StreamcloudEu] Fix WAIT_PATTERN

--- module/plugins/hoster/StreamcloudEu.py
+++ module/plugins/hoster/StreamcloudEu.py
@@ -8,7 +8,7 @@ from module.plugins.internal.XFSHoster import XFSHoster, create_getInfo
 class StreamcloudEu(XFSHoster):
     __name__    = "StreamcloudEu"
     __type__    = "hoster"
-    __version__ = "0.09"
+    __version__ = "0.10"
 
     __pattern__ = r'http://(?:www\.)?streamcloud\.eu/\w{12}'
 
@@ -19,12 +19,14 @@ class StreamcloudEu(XFSHoster):
 
     HOSTER_DOMAIN = "streamcloud.eu"
 
+    WAIT_PATTERN = r'var count = (\d+)'
+
     LINK_PATTERN = r'file: "(http://(stor|cdn)\d+\.streamcloud\.eu:?\d*/.*/video\.(mp4|flv))",'
 
 
     def setup(self):
-        self.multiDL = True
-        self.chunkLimit = 1
+        self.multiDL        = True
+        self.chunkLimit     = 1
         self.resumeDownload = self.premium
 
 
