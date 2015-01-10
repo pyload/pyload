615e713cc874ffe4a1d15b2607c67b24ab2b4297
Walter Purcaro <vuolter@gmail.com>
2015-01-10 17:27:00 +0100

    [RehostTo] Fix typo (2)

--- module/plugins/hooks/RehostTo.py
+++ module/plugins/hooks/RehostTo.py
@@ -6,7 +6,7 @@ from module.plugins.internal.MultiHook import MultiHook
 class RehostTo(MultiHook):
     __name__    = "RehostTo"
     __type__    = "hook"
-    __version__ = "0.48"
+    __version__ = "0.49"
 
     __config__ = [("pluginmode"    , "all;listed;unlisted", "Use for plugins"                     , "all"),
                   ("pluginlist"    , "str"                , "Plugin list (comma separated)"       , ""   ),
@@ -25,5 +25,5 @@ class RehostTo(MultiHook):
         user, data = self.account.selectAccount()
         page = self.getURL("http://rehost.to/api.php",
                            get={'cmd'     : "get_supported_och_dl",
-                                'long_ses': self.getAccountData(user)['session']})
+                                'long_ses': self.account.getAccountData(user)['session']})
         return [x.strip() for x in page.replace("\"", "").split(",")]
