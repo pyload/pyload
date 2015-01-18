208e78f34848778fc9b4f3fe7fb5aed8e0663d5e
Walter Purcaro <vuolter@gmail.com>
2015-01-18 19:15:35 +0100

    [OneFichierCom] Fix handlePremium

--- module/plugins/hoster/OneFichierCom.py
+++ module/plugins/hoster/OneFichierCom.py
@@ -8,7 +8,7 @@ from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo
 class OneFichierCom(SimpleHoster):
     __name__    = "OneFichierCom"
     __type__    = "hoster"
-    __version__ = "0.76"
+    __version__ = "0.77"
 
     __pattern__ = r'https?://(?:www\.)?(?:(?P<ID1>\w+)\.)?(?P<HOST>1fichier\.com|alterupload\.com|cjoint\.net|d(es)?fichiers\.com|dl4free\.com|megadl\.fr|mesfichiers\.org|piecejointe\.net|pjointe\.com|tenvoi\.com)(?:/\?(?P<ID2>\w+))?'
 
@@ -54,7 +54,7 @@ class OneFichierCom(SimpleHoster):
 
 
     def handlePremium(self, pyfile):
-        return self.handleFree(pyfile)
+        self.download(pyfile.url, post={'dl': "Download", 'did': 0})
 
 
 getInfo = create_getInfo(OneFichierCom)
