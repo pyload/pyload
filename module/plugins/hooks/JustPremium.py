8f9535b4618007ac244007da90254e0cc9c2c502
Jens Hörnlein <jens.hoernlein@googlemail.com>
2015-01-31 16:27:36 +0100

    [JustPremium] Fixed https://github.com/pyload/pyload/issues/1107

--- module/plugins/hooks/JustPremium.py
+++ module/plugins/hooks/JustPremium.py
@@ -6,7 +6,7 @@ from module.plugins.Hook import Hook
 class JustPremium(Hook):
     __name__    = "JustPremium"
     __type__    = "hook"
-    __version__ = "0.18"
+    __version__ = "0.19"
 
     __config__ = [("freehosters", "bool", "Allow all freehosters and other unknown sites", False),
                   ("nicehoster" , "str" , "unblock this hosters (comma seperated)"       , "Zippyshare.com")]
@@ -29,8 +29,8 @@ class JustPremium(Hook):
             hosts    = hosts + "," + accounts[startid+3:accounts.find("'",startid+3)]
             accounts = accounts[0:startid] + accounts[endid:]
 
-        self.hosts    = map(str.strip, (hosts[1:] + "," + self.getConfig("nicehoster")).split(','))
-        self.badhosts = map(str.strip, accounts.replace("': [], '",",")[2:-6].split(','))
+        self.hosts    = map(lambda x: x.strip(), (hosts[1:] + "," + self.getConfig("nicehoster")).replace('.','').split(','))
+        self.badhosts = map(lambda x: x.strip(), accounts.replace("': [], '",",")[2:-6].split(','))
 
         self.logDebug("Hosts: %s" % ", ".join(self.hosts))
         self.logDebug("Bad hosts: %s" % ", ".join(self.badhosts))
