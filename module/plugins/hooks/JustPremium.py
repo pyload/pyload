fd138ca3e3292b39a0f6423d7dc4f871b5edaf9b
Jens Hörnlein <jens.hoernlein@googlemail.com>
2015-01-31 16:30:41 +0100

    [JustPremium] Added Support for Multihoster

--- module/plugins/hooks/JustPremium.py
+++ module/plugins/hooks/JustPremium.py
@@ -6,7 +6,7 @@ from module.plugins.Hook import Hook
 class JustPremium(Hook):
     __name__    = "JustPremium"
     __type__    = "hook"
-    __version__ = "0.19"
+    __version__ = "0.20"
 
     __config__ = [("freehosters", "bool", "Allow all freehosters and other unknown sites", False),
                   ("nicehoster" , "str" , "unblock this hosters (comma seperated)"       , "Zippyshare.com")]
@@ -38,15 +38,20 @@ class JustPremium(Hook):
 
     def linksAdded(self, links, pid):
         linkdict  = self.core.api.checkURLs(links)
-
-        #: Found at least one hoster with account
-        if not any(True for hoster in linkdict if hoster in self.hosts):
+       
+        # Check if Multihoster has overwritten Hoster-Plugins
+        multi_hosts = [hoster for hoster in self.core.pluginManager.hosterPlugins if 'new_name' in self.core.pluginManager.hosterPlugins[hoster]]
+        if multi_hosts:
+            self.logDebug("Hosts extended via Multihoster: %s" % ", ".join(multi_hosts + self.hosts))
+        
+        #: Found at least one hoster with account or multihoster
+        if not any(True for hoster in linkdict if hoster in set(multi_hosts + self.hosts)):
             return
 
         if self.getConfig("freehosters"):
-            bad_hosters = [hoster for hoster in linkdict.iterkeys() if hoster in self.badhosts]
+            bad_hosters = [hoster for hoster in linkdict.iterkeys() if hoster in set(self.badhosts) - set(multi_hosts)]
         else:
-            bad_hosters = [hoster for hoster in linkdict.iterkeys() if hoster not in self.hosts]
+            bad_hosters = [hoster for hoster in linkdict.iterkeys() if hoster not in set(multi_hosts + self.hosts)]
 
         for hoster in bad_hosters:
             self.logInfo(_("Remove links of hoster: %s") % hoster)
