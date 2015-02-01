e265a18ec18c4260531f977c98ebb13d2074380b
Walter Purcaro <vuolter@gmail.com>
2015-02-02 00:33:17 +0100

    [JustPremium] Cleanup

--- module/plugins/hooks/JustPremium.py
+++ module/plugins/hooks/JustPremium.py
@@ -8,7 +8,7 @@ from module.plugins.Hook import Hook
 class JustPremium(Hook):
     __name__    = "JustPremium"
     __type__    = "hook"
-    __version__ = "0.20"
+    __version__ = "0.21"
 
     __config__ = [("excluded", "str", "Exclude hosters (comma separated)", "")]
 
@@ -23,18 +23,23 @@ class JustPremium(Hook):
 
 
     def linksAdded(self, links, pid):
-        linkdict       = self.core.api.checkURLs(links)
-        premiumplugins = set(account.type for account in self.core.api.getAccounts(False) if account.valid and account.premium)
-        multihoster    = set(hoster for hoster in self.core.pluginManager.hosterPlugins if 'new_name' in self.core.pluginManager.hosterPlugins[hoster])
-        
+        hosterdict = self.core.pluginManager.hosterPlugins
+        linkdict   = self.core.api.checkURLs(links)
+
+        premiumplugins = set(account.type for account in self.core.api.getAccounts(False) \
+                             if account.valid and account.premium)
+        multihosters   = set(hoster for hoster in self.core.pluginManager.hosterPlugins \
+                             if 'new_name' in hosterdict[hoster] \
+                             and hosterdict[hoster]['new_name'] in premiumplugins)
+
         #: Found at least one hoster with account or multihoster
-        if not any(True for pluginname in linkdict if pluginname in premiumplugins | multihoster):
+        if not any(True for pluginname in linkdict if pluginname in premiumplugins | multihosters):
             return
 
         excluded = map(lambda domain: "".join(part.capitalize() for part in re.split(r'(\.|\d+)', domain) if part != '.'),
                        self.getConfig('excluded').replace(' ', '').replace(',', '|').replace(';', '|').split('|'))
 
-        for pluginname in set(linkdict.keys()) - (premiumplugins | set(excluded) | multihoster):
+        for pluginname in set(linkdict.keys()) - (premiumplugins | multihosters).union(excluded):
             self.logInfo(_("Remove links of plugin: %s") % pluginname)
             for link in linkdict[pluginname]:
                 self.logDebug("Remove link: %s" % link)
