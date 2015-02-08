30f247b9bb25cc6cc049a87c9525b7981e5673ea
Walter Purcaro <vuolter@gmail.com>
2015-02-08 02:07:14 +0100

    [ZippyshareCom] Typo

--- module/plugins/hoster/ZippyshareCom.py
+++ module/plugins/hoster/ZippyshareCom.py
@@ -9,7 +9,7 @@ from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo
 class ZippyshareCom(SimpleHoster):
     __name__    = "ZippyshareCom"
     __type__    = "hoster"
-    __version__ = "0.69"
+    __version__ = "0.70"
 
     __pattern__ = r'http://www\d{0,2}\.zippyshare\.com/v(/|iew\.jsp.*key=)(?P<KEY>[\w^_]+)'
 
@@ -51,7 +51,7 @@ class ZippyshareCom(SimpleHoster):
 
     def get_checksum(self):
         try:
-            z = re.search(r'var z = (\d+)', self.html).group(1)
+            z = int(re.search(r'var z = (\d+)', self.html).group(1))
             n = (z - 3) % 2
             b = (z - 3) % 3
 
