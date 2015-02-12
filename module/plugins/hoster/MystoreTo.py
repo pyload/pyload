1694a5c6b3c1bbe6e86b950d1c164b95bb27cc50
Walter Purcaro <vuolter@gmail.com>
2015-02-12 13:08:48 +0100

    [MystoreTo] Cleanup

--- module/plugins/hoster/MystoreTo.py
+++ module/plugins/hoster/MystoreTo.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-# Testlink: http://mystore.to/dl/mxcA50jKfP
+#
+# Test link:
+#   http://mystore.to/dl/mxcA50jKfP
 
 import re
 
@@ -9,32 +11,35 @@ from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo
 class MystoreTo(SimpleHoster):
     __name__    = "MystoreTo"
     __type__    = "hoster"
-    __version__ = "0.02"
+    __version__ = "0.03"
 
-    __pattern__ = r'https?://(?:www\.)?mystore.to/dl/.+'
+    __pattern__ = r'https?://(?:www\.)?mystore\.to/dl/.+'
 
     __description__ = """Mystore.to hoster plugin"""
     __license__     = "GPLv3"
     __authors__     = [("zapp-brannigan", "")]
 
 
-    NAME_PATTERN    = r'<h1>(?P<N>.*?)</h1>'
-    SIZE_PATTERN    = r'FILESIZE: (?P<S>[\d\.,]+) (?P<U>[\w^_]+)<'
-    OFFLINE_PATTERN = r'the file is no longer available'
+    NAME_PATTERN    = r'<h1>(?P<N>.+?)<'
+    SIZE_PATTERN    = r'FILESIZE: (?P<S>[\d\.,]+) (?P<U>[\w^_]+)'
+    OFFLINE_PATTERN = r'>file not found<'
 
 
     def setup(self):
-        self.chunkLimit = 1
-        self.resumeDownload = self.multiDL = True
+        self.chunkLimit     = 1
+        self.resumeDownload = True
+        self.multiDL        = True
 
 
     def handleFree(self, pyfile):
         try:
-            fid = re.search(r'wert="(.*?)"', self.html).group(1)
+            fid = re.search(r'wert="(.+?)"', self.html).group(1)
+
         except AttributeError:
             self.error(_("File-ID not found"))
-            
+
         self.link = self.load("http://mystore.to/api/download",
-                              post={"FID":fid})
+                              post={'FID': fid})
+
 
 getInfo = create_getInfo(MystoreTo)
