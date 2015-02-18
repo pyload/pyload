1f2ddda833a0d4d98c08ca69213262f4de6113e6
stickell <l.stickell@yahoo.it>
2015-02-18 11:17:15 +0100

    [180Upload] new offline pattern
    Should fix #1189

--- module/plugins/hoster/HundredEightyUploadCom.py
+++ module/plugins/hoster/HundredEightyUploadCom.py
@@ -6,14 +6,15 @@ from module.plugins.internal.XFSHoster import XFSHoster, create_getInfo
 class HundredEightyUploadCom(XFSHoster):
     __name__    = "HundredEightyUploadCom"
     __type__    = "hoster"
-    __version__ = "0.04"
+    __version__ = "0.05"
 
     __pattern__ = r'http://(?:www\.)?180upload\.com/\w{12}'
 
     __description__ = """180upload.com hoster plugin"""
     __license__     = "GPLv3"
     __authors__     = [("stickell", "l.stickell@yahoo.it")]
-
+    
+    OFFLINE_PATTERN = r'>File Not Found'
 
 
 getInfo = create_getInfo(HundredEightyUploadCom)
