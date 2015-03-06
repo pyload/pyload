311e9a1d9ca6450ec84e4a54ef47dbf488e83107
Walter Purcaro <vuolter@gmail.com>
2015-03-06 22:03:57 +0100

    [MediafireCom] Update

--- module/plugins/hoster/MediafireCom.py
+++ module/plugins/hoster/MediafireCom.py
@@ -3,126 +3,62 @@
 import re
 
 from module.plugins.internal.CaptchaService import SolveMedia
-from module.plugins.internal.SimpleHoster import SimpleHoster, parseFileInfo
-from module.network.RequestFactory import getURL
-
-
-def replace_eval(js_expr):
-    return js_expr.replace(r'eval("', '').replace(r"\'", r"'").replace(r'\"', r'"')
-
-
-def checkHTMLHeader(url):
-    try:
-        for _i in xrange(3):
-            header = getURL(url, just_header=True)
-
-            for line in header.splitlines():
-                line = line.lower()
-
-                if 'location' in line:
-                    url = line.split(':', 1)[1].strip()
-                    if 'error.php?errno=320' in url:
-                        return url, 1
-
-                    if not url.startswith('http://'):
-                        url = 'http://www.mediafire.com' + url
-
-                    break
-
-                elif 'content-disposition' in line:
-                    return url, 2
-            else:
-                break
-    except Exception:
-        return url, 3
-    else:
-        return url, 0
-
-
-def getInfo(urls):
-    for url in urls:
-        location, status = checkHTMLHeader(url)
-
-        if status:
-            file_info = (url, 0, status, url)
-        else:
-            file_info = parseFileInfo(MediafireCom, url, getURL(url, decode=True))
-
-        yield file_info
+from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo
 
 
 class MediafireCom(SimpleHoster):
     __name__    = "MediafireCom"
     __type__    = "hoster"
-    __version__ = "0.84"
+    __version__ = "0.85"
 
-    __pattern__ = r'http://(?:www\.)?mediafire\.com/(file/|(view/?|download\.php)?\?)(\w{11}|\w{15})($|/)'
+    __pattern__ = r'https?://(?:www\.)?mediafire\.com/(file/|view/\??|download(\.php\?|/))\w+'
 
     __description__ = """Mediafire.com hoster plugin"""
     __license__     = "GPLv3"
     __authors__     = [("zoidberg", "zoidberg@mujmail.cz"),
-                       ("stickell", "l.stickell@yahoo.it")]
+                       ("stickell", "l.stickell@yahoo.it"),
+                       ("Walter Purcaro", "vuolter@gmail.com")]
 
 
-    NAME_PATTERN    = r'<META NAME="description" CONTENT="(?P<N>[^"]+)"/>'
-    INFO_PATTERN    = r'oFileSharePopup\.ald\(\'(?P<ID>[^\']*)\',\'(?P<N>[^\']*)\',\'(?P<S>[^\']*)\',\'\',\'(?P<H>[^\']*)\'\)'
-    OFFLINE_PATTERN = r'class="error_msg_title"> Invalid or Deleted File. </div>'
+    NAME_PATTERN    = r'<META NAME="description" CONTENT="(?P<N>.+?)"/>'
+    SIZE_PATTERN    = r'<li>File size: <span>(?P<S>[\d.,]+) (?P<U>[\w^_]+)'
+    INFO_PATTERN    = r'oFileSharePopup\.ald\(\'.*?\',\'(?P<N>.+?)\',\'(?P<S>[\d.,]+)\s*(?P<U>[\w^_]+)\',\'\',\'(?P<H>.+?)\'\)'
+    OFFLINE_PATTERN = r'class="error_msg_title"'
+
+    LINK_FREE_PATTERN = r'kNO = "(.+?)"'
 
     PASSWORD_PATTERN = r'<form name="form_password"'
 
 
     def setup(self):
-        self.multiDL = False
-
-
-    def process(self, pyfile):
-        pyfile.url = re.sub(r'/view/?\?', '/?', pyfile.url)
-
-        self.link, result = checkHTMLHeader(pyfile.url)
-        self.logDebug("Location (%d): %s" % (result, self.link))
-
-        if result == 0:
-            self.html = self.load(self.link, decode=True)
-            self.checkCaptcha()
-            self.multiDL = True
-            self.check_data = self.getFileInfo()
-
-            if self.account:
-                self.handlePremium(pyfile)
-            else:
-                self.handleFree(pyfile)
-        elif result == 1:
-            self.offline()
-        else:
-            self.multiDL = True
-            self.download(self.link, disposition=True)
+        self.resumeDownload = True
+        self.multiDL        = True
 
 
     def handleFree(self, pyfile):
+        solvemedia  = SolveMedia(self)
+        captcha_key = solvemedia.detect_key()
+
+        if captcha_key:
+            response, challenge = solvemedia.challenge(captcha_key)
+            self.html = self.load(pyfile.url,
+                                  post={'adcopy_challenge': challenge,
+                                        'adcopy_response' : response},
+                                  decode=True)
+
         if self.PASSWORD_PATTERN in self.html:
             password = self.getPassword()
 
-            if password:
-                self.logInfo(_("Password protected link, trying ") + password)
-                self.html = self.load(self.link, post={"downloadp": password})
+            if not password:
+                self.fail(_("No password found"))
+            else:
+                self.logInfo(_("Password protected link, trying: ") + password)
+                self.html = self.load(self.link, post={'downloadp': password})
 
                 if self.PASSWORD_PATTERN in self.html:
                     self.fail(_("Incorrect password"))
-            else:
-                self.fail(_("No password found"))
 
-        m = re.search(r'kNO = r"(http://.*?)";', self.html)
-        if m is None:
-            self.error(_("No download URL"))
-
-        download_url = m.group(1)
-        self.download(download_url)
+        return super(MediafireCom, self).handleFree(pyfile)
 
 
-    def checkCaptcha(self):
-        solvemedia = SolveMedia(self)
-        response, challenge = solvemedia.challenge()
-        self.html = self.load(self.link,
-                              post={'adcopy_challenge': challenge,
-                                    'adcopy_response' : response},
-                              decode=True)
+getInfo = create_getInfo(MediafireCom)
