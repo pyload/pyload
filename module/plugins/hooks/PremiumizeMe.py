6db62380b80a144aca18335d50c724e9915d8038
Walter Purcaro <vuolter@users.noreply.github.com>
2015-03-18 22:20:17 +0100

    Hooks cleanup

--- module/plugins/hooks/PremiumizeMe.py
+++ module/plugins/hooks/PremiumizeMe.py
@@ -12,8 +12,6 @@ class PremiumizeMe(MultiHook):
     __config__ = [("pluginmode"    , "all;listed;unlisted", "Use for plugins"                     , "all"),
                   ("pluginlist"    , "str"                , "Plugin list (comma separated)"       , ""   ),
                   ("revertfailed"  , "bool"               , "Revert to standard download if fails", True ),
-                  ("retry"         , "int"                , "Number of retries before revert"     , 10   ),
-                  ("retryinterval" , "int"                , "Retry interval in minutes"           , 1    ),
                   ("reload"        , "bool"               , "Reload plugin list"                  , True ),
                   ("reloadinterval", "int"                , "Reload interval in hours"            , 12   )]
 
