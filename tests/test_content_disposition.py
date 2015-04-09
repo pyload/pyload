901090aae0f3e84473117c8df743da122b24db07
Meego <meego.pyload@laposte.net>
2015-04-09 11:56:41 +0200

    create tests for content disposition parsing

---
+++ tests/test_content_disposition.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+def get_filename_from_content_disposition(header):
+    """Parse request header to retrieve filename from content disposition"""
+    return False
+
+
+class ContentDispositionTests(unittest.TestCase):
+    """Test Content Disposition Headers to get filename"""
+
+if __name__ == '__main__':
+    unittest.main()
