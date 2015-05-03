e29c94aa82477289e5f0b2ac04b79e66f3242d45
GammaC0de <GammaC0de@users.noreply.github.com>
2015-05-04 00:27:17 +0300

    no need for LINK_PATTERN and NAME_PATTERN

--- module/plugins/internal/XFSCrypter.py
+++ module/plugins/internal/XFSCrypter.py
@@ -53,9 +53,6 @@ class XFSCrypter(SimpleCrypter):
 
     URL_REPLACEMENTS = [(r'&?per_page=\d+', ""), (r'[?/&]+$', ""), (r'(.+/[^?]+)$', r'\1?'), (r'$', r'&per_page=10000')]
 
-    LINK_PATTERN = r'<a href="(.+?)".*?>.+?(?:</a>)?\s*</(?:td|TD)>'
-    NAME_PATTERN = r'<[tT]itle>.*?\: (?P<N>.+) folder</[tT]itle>'
-
     OFFLINE_PATTERN      = r'>\s*\w+ (Not Found|file (was|has been) removed)'
     TEMP_OFFLINE_PATTERN = r'>\s*\w+ server (is in )?(maintenance|maintainance)'
 
