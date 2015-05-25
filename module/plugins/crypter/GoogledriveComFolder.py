b66a9380e5efba5b7ec44a81eaa8be2cda8ad8f6
Walter Purcaro <vuolter@users.noreply.github.com>
2015-05-25 13:49:57 +0200

    New crypter: GoogledriveComFolder

---
+++ module/plugins/crypter/GoogledriveComFolder.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*
+
+from module.plugins.internal.SimpleCrypter import SimpleCrypter, create_getInfo
+
+
+class GoogledriveComFolder(SimpleCrypter):
+    __name__    = "GoogledriveCom"
+    __type__    = "crypter"
+    __version__ = "0.01"
+
+    __pattern__ = r'https?://(?:www\.)?drive\.google\.com/folderview\?.*id=\w+'
+
+    __description__ = """Drive.google.com folder decrypter plugin"""
+    __license__     = "GPLv3"
+    __authors__     = [("Walter Purcaro", "vuolter@gmail.com")]
+
+
+    NAME_PATTERN    = r"folderName: '(?P<N>.+?)'"
+    LINK_PATTERN    = r'\[,"\w+"(?:,,".+?")?,"(.+?)"'
+    OFFLINE_PATTERN = r'<TITLE>'
+
+
+getInfo = create_getInfo(GoogledriveComFolder)
