6735b7b6a86ee824f5e4a2bae357526708d7e06a
GammaC0de <GammaC0de@users.noreply.github.com>
2015-06-04 23:41:56 +0300

    [OpenloadIo] fix NAME_PATTERN

--- module/plugins/hoster/OpenloadIo.py
+++ module/plugins/hoster/OpenloadIo.py
@@ -6,14 +6,14 @@ from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo
 class OpenloadIo(SimpleHoster):
     __name__    = "OpenloadIo"
     __type__    = "hoster"
-    __version__ = "0.01"
+    __version__ = "0.02"
 
     __pattern__ = r'https?://(?:www\.)?openload\.io/f/\w{11}'
 
     __description__ = """Openload.io hoster plugin"""
     __license__     = "GPLv3"
 
-    NAME_PATTERN = r'<span id="filename">(?P<N>.+)</'
+    NAME_PATTERN = r'<span id="filename">(?P<N>.+?)</'
     SIZE_PATTERN = r'<span class="count">(?P<S>[\d.,]+) (?P<U>[\w^_]+)<'
     OFFLINE_PATTERN = r">(We can't find the file you are looking for)"
 
