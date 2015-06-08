45b6242579ac65444fd278e5ecde96be29136bbe
Walter Purcaro <vuolter@users.noreply.github.com>
2015-06-08 05:49:47 +0200

    New plugins: FiledropperCom & FileuploadNet

---
+++ module/plugins/accounts/SharebeastCom.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from module.plugins.internal.XFSAccount import XFSAccount
+
+
+class SharebeastCom(XFSAccount):
+    __name__    = "SharebeastCom"
+    __type__    = "account"
+    __version__ = "0.01"
+
+    __description__ = """Sharebeast.com account plugin"""
+    __license__     = "GPLv3"
+    __authors__     = [("Walter Purcaro", "vuolter@gmail.com")]
+
+
+    HOSTER_DOMAIN = "sharebeast.com"
--- module/plugins/hoster/PromptfileCom.py
+++ module/plugins/hoster/PromptfileCom.py
@@ -18,10 +18,10 @@ class PromptfileCom(SimpleHoster):
     __authors__     = [("igel", "igelkun@myopera.com")]
 
 
-    INFO_PATTERN = r'<span style=".+?" title=".+?">(?P<N>.*?) \((?P<S>[\d.,]+) (?P<U>[\w^_]+)\)</span>'
+    INFO_PATTERN    = r'<span style=".+?" title=".+?">(?P<N>.*?) \((?P<S>[\d.,]+) (?P<U>[\w^_]+)\)</span>'
     OFFLINE_PATTERN = r'<span style=".+?" title="File Not Found">File Not Found</span>'
 
-    CHASH_PATTERN = r'<input type="hidden" name="chash" value="(.+?)" />'
+    CHASH_PATTERN     = r'<input type="hidden" name="chash" value="(.+?)" />'
     LINK_FREE_PATTERN = r'<a href=\"(.+)\" target=\"_blank\" class=\"view_dl_link\">Download File</a>'
 
 
@@ -30,17 +30,15 @@ class PromptfileCom(SimpleHoster):
         m = re.search(self.CHASH_PATTERN, self.html)
         if m is None:
             self.error(_("CHASH_PATTERN not found"))
+
         chash = m.group(1)
         self.logDebug("Read chash %s" % chash)
+
         # continue to stage2
         self.html = self.load(pyfile.url, decode=True, post={'chash': chash})
 
         # STAGE 2: get the direct link
-        m = re.search(self.LINK_FREE_PATTERN, self.html)
-        if m is None:
-            self.error(_("LINK_FREE_PATTERN not found"))
-
-        self.link = m.group(1)
+        return super(PromptfileCom, self).handleFree(pyfile)
 
 
 getInfo = create_getInfo(PromptfileCom)
