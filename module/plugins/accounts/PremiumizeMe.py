939ac2ea4b96e2d29fde6e094cfa1cfcbbd3a39b
Walter Purcaro <vuolter@users.noreply.github.com>
2015-06-15 07:12:58 +0200

    [PremiumizeMe] Account version up

--- module/plugins/accounts/PremiumizeMe.py
+++ module/plugins/accounts/PremiumizeMe.py
@@ -7,7 +7,7 @@ from module.plugins.internal.Account import Account
 class PremiumizeMe(Account):
     __name__    = "PremiumizeMe"
     __type__    = "account"
-    __version__ = "0.17"
+    __version__ = "0.18"
 
     __description__ = """Premiumize.me account plugin"""
     __license__     = "GPLv3"
