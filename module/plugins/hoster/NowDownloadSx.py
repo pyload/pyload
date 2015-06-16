badcc797c41e9ee8b226ff803e8f22553a70bd10
TWD <tmp_m_box-xxx@yahoo.it>
2015-06-16 12:05:29 +0200

    Update NowDownloadSx.py

--- module/plugins/hoster/NowDownloadSx.py
+++ module/plugins/hoster/NowDownloadSx.py
@@ -9,9 +9,9 @@ from module.utils import fixup
 class NowDownloadSx(SimpleHoster):
     __name__    = "NowDownloadSx"
     __type__    = "hoster"
-    __version__ = "0.09"
+    __version__ = "0.10"
 
-    __pattern__ = r'http://(?:www\.)?(nowdownload\.[a-zA-Z]{2,}/(dl/|download\.php.+?id=|mobile/(#/files/|.+?id=))|likeupload\.org/)\w+'
+    __pattern__ = r'http://(?:www\.)?(nowdownload\.([a-zA-Z]{2,})/(dl/|download\.php.+?id=|mobile/(#/files/|.+?id=))|likeupload\.org/)\w+'
     __config__  = [("use_premium", "bool", "Use premium account if available", True)]
 
     __description__ = """NowDownload.sx hoster plugin"""
@@ -26,7 +26,7 @@ class NowDownloadSx(SimpleHoster):
     TOKEN_PATTERN = r'"(/api/token\.php\?token=\w+)"'
     CONTINUE_PATTERN = r'"(/dl2/\w+/\w+)"'
     WAIT_PATTERN = r'\.countdown\(\{until: \+(\d+),'
-    LINK_FREE_PATTERN = r'(http://s\d+\.coolcdn\.info/nowdownload/.+?)["\']'
+    LINK_FREE_PATTERN = r'(http://s\d+(\.coolcdn\.info|\.mighycdndelivery\.com)/nowdownload/.+?)["\']'
 
     NAME_REPLACEMENTS = [("&#?\w+;", fixup), (r'<.*?>', '')]
 
@@ -49,7 +49,12 @@ class NowDownloadSx(SimpleHoster):
         else:
             wait = 60
 
-        baseurl = "http://www.nowdownload.at"
+        foundbu = re.match(self.__pattern__, self.pyfile.url)
+        if foundbu:
+            baseurl = "http://www.nowdownload." + foundbu.group(2)
+        else:
+            baseurl = "http://www.nowdownload.ch"
+            
         self.html = self.load(baseurl + str(tokenlink.group(1)))
         self.wait(wait)
 
