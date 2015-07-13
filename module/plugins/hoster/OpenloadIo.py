ea2955a1d09c0a86f8dcb9eed6a020602a4381c4
lpiter <revelation@o2.pl>
2015-07-13 23:03:06 +0200

    Openload.io regex adjustments.

--- module/plugins/hoster/OpenloadIo.py
+++ module/plugins/hoster/OpenloadIo.py
@@ -17,7 +17,7 @@ class OpenloadIo(SimpleHoster):
     SIZE_PATTERN = r'<span class="count">(?P<S>[\d.,]+) (?P<U>[\w^_]+)<'
     OFFLINE_PATTERN = r">(We can't find the file you are looking for)"
 
-    LINK_FREE_PATTERN = r'id="realGkSuckersdownload"><a href="(https?://[\w\.]+\.openload\.io/dl/.*?)"'
+    LINK_FREE_PATTERN = r'id="real\w*download"><a href="(https?://[\w\.]+\.openload\.io/dl/.*?)"'
 
     def setup(self):
         self.multiDL = True
