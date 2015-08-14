d15432dda3a06c08ad36901a72c6301f958b72e0
estaban <babedoudi@yahoo.fr>
2015-08-14 18:52:59 +0200

    Update OneFichierCom.py
    Increment version

--- module/plugins/hoster/OneFichierCom.py
+++ module/plugins/hoster/OneFichierCom.py
@@ -8,7 +8,7 @@ from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo
 class OneFichierCom(SimpleHoster):
     __name__    = "OneFichierCom"
     __type__    = "hoster"
-    __version__ = "0.86"
+    __version__ = "0.87"
     __status__  = "testing"
 
     __pattern__ = r'https?://(?:www\.)?(?:(?P<ID1>\w+)\.)?(?P<HOST>1fichier\.com|alterupload\.com|cjoint\.net|d(es)?fichiers\.com|dl4free\.com|megadl\.fr|mesfichiers\.org|piecejointe\.net|pjointe\.com|tenvoi\.com)(?:/\?(?P<ID2>\w+))?'
