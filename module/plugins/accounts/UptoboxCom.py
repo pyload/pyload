aba02a9b05d12ef1ce6f5ab1224e3781eca3caa5
benbox69 <dev@tollet.me>
2015-09-19 18:18:31 +0200

    Update UptoboxCom.py

--- module/plugins/accounts/UptoboxCom.py
+++ module/plugins/accounts/UptoboxCom.py
@@ -4,16 +4,16 @@ import json
 import re
 import time
 
-from module.plugins.internal.Account import Account
+from module.plugins.internal.XFSAccount import XFSAccount
 
 
-class UptoboxCom(Account):
+class UptoboxCom(XFSAccount):
 	__name__    = "UptoboxCom"
 	__type__    = "account"
-	__version__ = "0.10"
+	__version__ = "0.12"
 	__status__  = "testing"
 
-	__description__ = """Uptobox.com account plugin"""
+	__description__ = "Uptobox.com account plugin"
 	__license__     = "GPLv3"
 	__authors__     = [("benbox69", "dev@tollet.me")]
 
@@ -21,46 +21,6 @@ class UptoboxCom(Account):
 	HOSTER_DOMAIN = "uptobox.com"
 	HOSTER_URL    = "https://uptobox.com/"
 	LOGIN_URL     = "https://login.uptobox.com/logarithme"
-
-	VALID_UNTIL_PATTERN = r'Premium-Account expire: (\d{1,2} [\w^_]+ \d{4})'
-
-	def parse_info(self, user, password, data, req):
-
-		validuntil   = None
-        	trafficleft  = None
-       		premium      = None
-
-		data = self.get_data(user)
-		html = self.load(self.HOSTER_URL, get={'op': "my_account"})
-
-		p = re.compile(self.VALID_UNTIL_PATTERN)
-		
-		m = re.search(p, html)
-
-		if m:
-			expiredate = m.group(1).strip()
-			self.log_debug("Expire date: " + expiredate)
-
-            		try:
-                		validuntil = time.mktime(time.strptime(expiredate, "%d %B %Y"))
-
-            		except Exception, e:
-                		self.log_error(e)
-
-            		else:
-                		self.log_debug("Valid until: %s" % validuntil)
-
-                		if validuntil > time.mktime(time.gmtime()):
-                    			premium     = True
-                    			trafficleft = -1
-                		else:
-                    			premium    = False
-                    			validuntil = None  #: Registered account type (not premium)
-        	else:
-            		self.log_debug("VALID_UNTIL_PATTERN not found")
-
-		return {'validuntil': validuntil, 'trafficleft': trafficleft, 'premium': premium}
-
 
 	def login(self, user, password, data, req):
 
