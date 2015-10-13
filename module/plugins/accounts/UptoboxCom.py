f811e7fb5eb392f67d55d57bc2f3e4e2fc1ae3f7
GammaC0de <GammaC0de@users.noreply.github.com>
2015-10-13 09:59:26 +0300

    [UptoboxCom] fix "Version mismatch"

--- module/plugins/accounts/UptoboxCom.py
+++ module/plugins/accounts/UptoboxCom.py
@@ -10,7 +10,7 @@ from module.plugins.internal.XFSAccount import XFSAccount
 class UptoboxCom(XFSAccount):
     __name__    = "UptoboxCom"
     __type__    = "account"
-    __version__ = "0.15"
+    __version__ = "0.17"
     __status__  = "testing"
 
     __description__ = """Uptobox.com account plugin"""
