158af284441a2c0261f32398e6b30edfc05165a8
Walter Purcaro <vuolter@users.noreply.github.com>
2015-10-22 02:46:41 +0200

    New account: OpenloadCo

---
+++ module/plugins/accounts/OpenloadCo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from module.plugins.internal.XFSAccount import XFSAccount
+
+
+class OpenloadCo(XFSAccount):
+    __name__    = "OpenloadCo"
+    __type__    = "account"
+    __version__ = "0.01"
+    __status__  = "testing"
+
+    __description__ = """Openload.co account plugin"""
+    __license__     = "GPLv3"
+    __authors__     = [("Walter Purcaro", "vuolter@gmail.com")]
+
+
+    PLUGIN_DOMAIN = "openload.co"
