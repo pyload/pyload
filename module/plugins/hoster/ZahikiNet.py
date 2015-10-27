f7a415e914ee89bfc224b2dd6ddbaea45ff2fd46
Walter Purcaro <vuolter@users.noreply.github.com>
2015-10-27 19:57:03 +0100

    ZahikiNetis is DeadHoster

--- module/plugins/hoster/ZahikiNet.py
+++ module/plugins/hoster/ZahikiNet.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from module.plugins.internal.SimpleHoster import SimpleHoster, create_getInfo
+from module.plugins.internal.DeadHoster import DeadHoster, create_getInfo
 
 
-class ZahikiNet(SimpleHoster):
+class ZahikiNet(DeadHoster):
     __name__    = "ZahikiNet"
     __type__    = "hoster"
-    __version__ = "0.03"
+    __version__ = "0.04"
     __status__  = "testing"
 
     __pattern__ = r'https?://(?:www\.)?zahiki\.net/\w+/.+'
@@ -15,18 +15,6 @@ class ZahikiNet(SimpleHoster):
     __description__ = """Zahiki.net hoster plugin"""
     __license__     = "GPLv3"
     __authors__     = [("Walter Purcaro", "vuolter@gmail.com")]
-
-
-    NAME_PATTERN    = r'/(?P<N>.+?) </title>'
-    OFFLINE_PATTERN = r'>(Not Found|Il file selezionato non esiste)'
-
-    LINK_FREE_PATTERN = r'file: "(.+?)"'
-
-
-    def setup(self):
-        self.resume_download = True
-        self.multiDL = True
-        self.limitDL = 6
 
 
 getInfo = create_getInfo(ZahikiNet)
