8739af5043790ce34a847b805ddb5adba4caa5e0
GammaC0de <GammaC0de@users.noreply.github.com>
2015-12-29 03:17:27 +0200

    [HighWayMe] Version up

--- module/plugins/hoster/HighWayMe.py
+++ module/plugins/hoster/HighWayMe.py
@@ -9,7 +9,7 @@ from module.plugins.internal.misc import seconds_to_midnight
 class HighWayMe(MultiHoster):
     __name__    = "HighWayMe"
     __type__    = "hoster"
-    __version__ = "0.18"
+    __version__ = "0.19"
     __status__  = "testing"
 
     __pattern__ = r'https?://.+high-way\.my'
