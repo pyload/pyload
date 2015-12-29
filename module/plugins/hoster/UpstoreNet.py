ae6e826913ec334ce548245e5a0e0bab896532f5
GammaC0de <GammaC0de@users.noreply.github.com>
2015-12-29 03:51:09 +0200

    [UpstoreNet] Version up

--- module/plugins/hoster/UpstoreNet.py
+++ module/plugins/hoster/UpstoreNet.py
@@ -9,7 +9,7 @@ from module.plugins.internal.SimpleHoster import SimpleHoster
 class UpstoreNet(SimpleHoster):
     __name__    = "UpstoreNet"
     __type__    = "hoster"
-    __version__ = "0.10"
+    __version__ = "0.11"
     __status__  = "testing"
 
     __pattern__ = r'https?://(?:www\.)?upstore\.net/'
