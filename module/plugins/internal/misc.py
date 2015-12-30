19625b8e8f586f5c1a52ec08a3707f0b66e63ff1
GammaC0de <GammaC0de@users.noreply.github.com>
2015-12-30 14:45:33 +0200

    [Misc] Fix missing import

--- module/plugins/internal/misc.py
+++ module/plugins/internal/misc.py
@@ -12,6 +12,7 @@ import itertools
 import os
 import re
 import shutil
+import socket
 import string
 import sys
 import time
@@ -32,7 +33,7 @@ except ImportError:
 class misc(object):
     __name__    = "misc"
     __type__    = "plugin"
-    __version__ = "0.13"
+    __version__ = "0.14"
     __status__  = "stable"
 
     __pattern__ = r'^unmatchable$'
