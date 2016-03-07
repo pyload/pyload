461e7507bf4f424e758ab5b83082f687b51f6c27
Nitzo <nitzo2001@yahoo.com>
2016-03-07 19:48:34 +0200

    [UlzTo] fix #2377

--- module/plugins/captcha/UlozTo.py
+++ module/plugins/captcha/UlozTo.py
@@ -1,26 +1,35 @@
-try:
-        import adecaptcha.clslib as clslib
-        adecaptcha_available = True
-except ImportError:
-        adecaptcha_available = False
+# -*- coding: utf-8 -*-
 
 import os.path
 
+from module.plugins.internal.OCR import OCR
+
+try:
+    import adecaptcha.clslib as clslib
+except ImportError:
+    pass
 
 
-class UlozTo(object):
-    __name__ = "UlozTo"
-    def __init__(self, plugin=None):
-        pass
-        self._plugin=plugin
+class UlozTo(OCR):
+    __name__ =    "UlozTo"
+    __type__    = "captcha"
+    __version__ = "0.02"
+    __status__  = "testing"
+
+    __description__ = """UlozTo audio captcha service plugin"""
+    __license__     = "GPLv3"
+    __authors__     = [("sodd",      None                        ),
+                       ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
 
     def recognize(self, audio):
         """ Audio decoding - more info could be found at https://launchpad.net/adecaptcha """
         #print "!!!CAPTCHA :", audio
-        if adecaptcha_available:
-                cfg_file=os.path.join(os.path.split(clslib.__file__)[0], 'ulozto.cfg')
-                text= clslib.classify_audio_file(audio, cfg_file)
-                return text
-        else:
-                pass
+        try:
+            cfg_file = os.path.join(os.path.split(clslib.__file__)[0], 'ulozto.cfg')
+            text = clslib.classify_audio_file(audio, cfg_file)
+            return text
+
+        except NameError:
+            self.log_error(_("Unable to decode audio captcha"),
+                           _("Please install adecaptcha library from https://github.com/izderadicka/adecaptcha"))
