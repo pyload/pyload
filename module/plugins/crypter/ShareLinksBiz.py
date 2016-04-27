cc2d66e9f773ab6d907801987dba8c1349a17896
GammaC0de <GammaC0de@users.noreply.github.com>
2016-04-27 18:18:49 +0300

    [ShareLinksBiz] Version up

--- module/plugins/crypter/ShareLinksBiz.py
+++ module/plugins/crypter/ShareLinksBiz.py
@@ -11,7 +11,7 @@ from module.plugins.internal.Crypter import Crypter
 class ShareLinksBiz(Crypter):
     __name__    = "ShareLinksBiz"
     __type__    = "crypter"
-    __version__ = "1.24"
+    __version__ = "1.25"
     __status__  = "testing"
 
     __pattern__ = r'http://(?:www\.)?(share-links|s2l)\.biz/(?P<ID>_?\w+)'
