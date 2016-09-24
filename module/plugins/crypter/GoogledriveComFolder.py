2ee3a021e219fcee82ecd93566343503d95fbf36
GammaC0de <nitzo2001@yahoo.com>
2016-09-24 22:56:11 +0300

    [GoogledriveComFolder] Subfolder support

--- module/plugins/crypter/GoogledriveComFolder.py
+++ module/plugins/crypter/GoogledriveComFolder.py
@@ -8,14 +8,16 @@ from module.plugins.internal.misc import json
 class GoogledriveComFolder(Crypter):
     __name__    = "GoogledriveComFolder"
     __type__    = "crypter"
-    __version__ = "0.09"
+    __version__ = "0.10"
     __status__  = "testing"
 
     __pattern__ = r'https?://(?:www\.)?drive\.google\.com/(?:folderview\?.*id=|drive/(?:.+?/)?folders/)(?P<ID>[-\w]+)'
     __config__  = [("activated"         , "bool"          , "Activated"                                        , True     ),
                    ("use_premium"       , "bool"          , "Use premium account if available"                 , True     ),
                    ("folder_per_package", "Default;Yes;No", "Create folder for each package"                   , "Default"),
-                   ("max_wait"          , "int"           , "Reconnect if waiting time is greater than minutes", 10       )]
+                   ("max_wait"          , "int"           , "Reconnect if waiting time is greater than minutes", 10       ),
+                   ("dl_subfolders"     , "bool"          , "Download subfolders"                              , False    ),
+                   ("package_subfolder" , "bool"          , "Subfolder as a seperate package"                  , False    )]
 
     __description__ = """Drive.google.com folder decrypter plugin"""
     __license__     = "GPLv3"
@@ -42,6 +44,58 @@ class GoogledriveComFolder(Crypter):
             return None
 
 
+    def enum_folder(self, folder_id):
+        links = []
+        json_data = self.api_response("files", q="'%s' in parents" % folder_id,
+                                               pageSize=100,
+                                               fields="files/id,files/mimeType,nextPageToken")
+
+        if json_data is None:
+            self.fail("API error")
+
+        if 'error' in json_data:
+            self.fail(json_data['error']['message'])
+
+        for _f in json_data.get('files', []):
+            if _f['mimeType'] != "application/vnd.google-apps.folder":
+                links.append("https://drive.google.com/file/d/" + _f['id'])
+
+            elif self.config.get('dl_subfolders'):
+                if self.config.get('package_subfolder'):
+                    links.append("https://drive.google.com/drive/folders/" + _f['id'])
+
+                else:
+                    links.extend(self.enum_folder(_f['id']))
+
+        next_page = json_data.get('nextPageToken', None)
+        while next_page:
+            json_data = self.api_response("files", q="'%s' in parents" % folder_id,
+                                          pageToken=next_page,
+                                          pageSize=100,
+                                          fields="files/id,files/mimeType,nextPageToken")
+
+            if json_data is None:
+                self.fail("API error")
+
+            if 'error' in json_data:
+                self.fail(json_data['error']['message'])
+
+            for _f in json_data.get('files', []):
+                if _f['mimeType'] != "application/vnd.google-apps.folder":
+                    links.append("https://drive.google.com/file/d/" + _f['id'])
+
+                elif self.config.get('dl_subfolders'):
+                    if self.config.get('package_subfolder'):
+                        links.append("https://drive.google.com/drive/folders/" + _f['id'])
+
+                    else:
+                        links.extend(self.enum_folder(_f['id']))
+
+            next_page = json_data.get('nextPageToken', None)
+
+        return links
+
+
     def decrypt(self, pyfile):
         links = []
 
@@ -58,36 +112,7 @@ class GoogledriveComFolder(Crypter):
 
         pack_name = json_data.get('name', pyfile.package().name)
 
-        json_data = self.api_response("files", q="'%s' in parents" % self.info['pattern']['ID'],
-                                               pageSize=100,
-                                               fields="files/id,nextPageToken")
-
-        if json_data is None:
-            self.fail("API error")
-
-        if 'error' in json_data:
-            self.fail(json_data['error']['message'])
-
-        for _f in json_data.get('files', []):
-            links.append("https://drive.google.com/file/d/" + _f['id'])
-
-        next_page = json_data.get('nextPageToken', None)
-        while next_page:
-            json_data = self.api_response("files", q="'%s' in parents" % self.info['pattern']['ID'],
-                                          pageToken=next_page,
-                                          pageSize=100,
-                                          fields="files/id,nextPageToken")
-
-            if json_data is None:
-                self.fail("API error")
-
-            if 'error' in json_data:
-                self.fail(json_data['error']['message'])
-
-            for _f in json_data.get('files', []):
-                links.append("https://drive.google.com/file/d/" + _f['id'])
-
-            next_page = json_data.get('nextPageToken', None)
+        links = self.enum_folder(self.info['pattern']['ID'])
 
         if links:
             self.packages = [(pack_name, links, pack_name)]
