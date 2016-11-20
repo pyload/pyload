44e60a1947a86ce7e8f7a0caaeb1c851880a0762
GammaC0de <nitzo2001@yahoo.com>
2016-11-21 01:54:28 +0200

    [CloudFlareDdos] Add SimpleCrypter support

--- module/plugins/hooks/CloudFlareDdos.py
+++ module/plugins/hooks/CloudFlareDdos.py
@@ -3,10 +3,8 @@
 import inspect
 import re
 import urlparse
-import time
 
 from module.plugins.internal.Addon import Addon
-from module.plugins.internal.SimpleHoster import SimpleHoster
 from module.plugins.internal.misc import parse_html_header
 from module.network.HTTPRequest import BadHeader
 from module.plugins.captcha.ReCaptcha import ReCaptcha
@@ -18,11 +16,14 @@ def plugin_id(plugin):
              'id'        : "[%s]" % plugin.pyfile.id if plugin.pyfile else ""})
 
 
+def is_simple_plugin(obj):
+    return any(k.__name__ in ("SimpleHoster", "SimpleCrypter") for k in inspect.getmro(type(obj)))
+
 
 class CloudFlare(object):
     @staticmethod
-    def handle_function(addon_plugin, hoster_plugin, func_name, orig_func, args):
-        addon_plugin.log_debug(_("Calling %s() of %s") % (func_name, plugin_id(hoster_plugin)))
+    def handle_function(addon_plugin, owner_plugin, func_name, orig_func, args):
+        addon_plugin.log_debug(_("Calling %s() of %s") % (func_name, plugin_id(owner_plugin)))
 
         try:
             data = orig_func(*args[0], **args[1])
@@ -32,15 +33,15 @@ class CloudFlare(object):
         except BadHeader, e:
             addon_plugin.log_debug(_("%s(): got BadHeader exception %s") % (func_name, e.code))
 
-            header = parse_html_header(hoster_plugin.req.http.header if hasattr(hoster_plugin.req, "http")
-                                       else hoster_plugin.req.header)  # @NOTE: req can be a HTTPRequest or a Browser object
+            header = parse_html_header(owner_plugin.req.http.header if hasattr(owner_plugin.req, "http")
+                                       else owner_plugin.req.header)  # @NOTE: req can be a HTTPRequest or a Browser object
 
             if header.get('server') == "cloudflare-nginx":
                 if e.code == 403:
-                    data = CloudFlare._solve_cf_security_check(addon_plugin, hoster_plugin, e.content)
+                    data = CloudFlare._solve_cf_security_check(addon_plugin, owner_plugin, e.content)
 
                 elif e.code == 503:
-                    data = CloudFlare._solve_cf_ddos_challenge(addon_plugin, hoster_plugin, e.content)
+                    data = CloudFlare._solve_cf_ddos_challenge(addon_plugin, owner_plugin, e.content)
 
                 else:
                     addon_plugin.log_warning(_("Unknown CloudFlare response code %s") % e.code)
@@ -57,73 +58,86 @@ class CloudFlare(object):
 
 
     @staticmethod
-    def _solve_cf_ddos_challenge(addon_plugin, hoster_plugin, data):
-        addon_plugin.info(_("Detected CloudFlare's DDoS protection page"))
-        time.sleep(5)  # Cloudflare requires a delay before solving the challenge
-
-        last_url = hoster_plugin.req.lastEffectiveURL
-        urlp = urlparse.urlparse(hoster_plugin.pyfile.url)
-        domain = urlp.netloc
-        post_url = "%s://%s/cdn-cgi/l/chk_jschl" % (urlp.scheme, domain)
-
-        post_data = {}
-
+    def _solve_cf_ddos_challenge(addon_plugin, owner_plugin, data):
         try:
-            post_data['jschl_vc'] = re.search(r'name="jschl_vc" value="(\w+)"', data).group(1)
-            post_data['pass'] = re.search(r'name="pass" value="(.+?)"', data).group(1)
+            addon_plugin.log_info(_("Detected CloudFlare's DDoS protection page"))
+            owner_plugin.set_wait(5)  # Cloudflare requires a delay before solving the challenge
 
-            # Extract the arithmetic operation
-            js = re.search(r'setTimeout\(function\(\){\s+(var s,t,o,p,b,r,e,a,k,i,n,g,f.+?\r?\n[\s\S]+?a\.value =.+?)\r?\n',
-                           data).group(1)
-            js = re.sub(r'a\.value = (parseInt\(.+?\)).+', r'\1', js)
-            js = re.sub(r'\s{3,}[a-z](?: = |\.).+', "", js)
-            js = re.sub(r"[\n\\']", "", js)
-            js = js.replace('return', '')
+            last_url = owner_plugin.req.lastEffectiveURL
+            urlp = urlparse.urlparse(last_url)
+            domain = urlp.netloc
+            submit_url = "%s://%s/cdn-cgi/l/chk_jschl" % (urlp.scheme, domain)
+
+            get_params = {}
+
+            try:
+                get_params['jschl_vc'] = re.search(r'name="jschl_vc" value="(\w+)"', data).group(1)
+                get_params['pass'] = re.search(r'name="pass" value="(.+?)"', data).group(1)
+
+                # Extract the arithmetic operation
+                js = re.search(r'setTimeout\(function\(\){\s+(var s,t,o,p,b,r,e,a,k,i,n,g,f.+?\r?\n[\s\S]+?a\.value =.+?)\r?\n',
+                               data).group(1)
+                js = re.sub(r'a\.value = (parseInt\(.+?\)).+', r'\1', js)
+                js = re.sub(r'\s{3,}[a-z](?: = |\.).+', "", js)
+                js = re.sub(r"[\n\\']", "", js)
+
+            except Exception:
+                # Something is wrong with the page.
+                # This may indicate CloudFlare has changed their anti-bot technique.
+                owner_plugin.log_error(_("Unable to parse CloudFlare's DDoS protection page"))
+                return None  # Tell the exception handler to re-throw the exception
+
+            # Safely evaluate the Javascript expression
+            get_params['jschl_answer'] = str(int(owner_plugin.js.eval(js)) + len(domain))
+
+            owner_plugin.wait()  # Do the actual wait
+
+            return owner_plugin.load(submit_url,
+                                      get=get_params,
+                                      ref=last_url)
 
 
-        except Exception:
-            # Something is wrong with the page.
-            # This may indicate CloudFlare has changed their anti-bot technique.
-            hoster_plugin.log_error(_("Unable to parse CloudFlare's DDoS protection page"))
-            return None  # Tell the exception handler to re-throw the exception
-
-        # Safely evaluate the Javascript expression
-        post_data['jschl_answer'] = str(int(hoster_plugin.js.eval(js)) + len(domain))
-        return hoster_plugin.load(post_data,
-                                  post=post_data,
-                                  ref=last_url)
+        except Exception, e:
+            addon_plugin.log_error(e)
+            return None
 
 
     @staticmethod
-    def _solve_cf_security_check(addon_plugin, hoster_plugin,data):
-        last_url = hoster_plugin.req.lastEffectiveURL
+    def _solve_cf_security_check(addon_plugin, owner_plugin, data):
+        try:
+            last_url = owner_plugin.req.lastEffectiveURL
 
-        captcha = ReCaptcha(hoster_plugin.pyfile)
+            captcha = ReCaptcha(owner_plugin.pyfile)
 
-        captcha_key = captcha.detect_key(data)
-        if captcha_key:
-            addon_plugin.log_info(_("Detected CloudFlare's security check page"))
-            response, challenge = captcha.challenge(captcha_key, data)
-            return hoster_plugin.load(hoster_plugin.fixurl("/cdn-cgi/l/chk_captcha"),
-                                           get={'g-recaptcha-response': response},
-                                           ref=last_url)
+            captcha_key = captcha.detect_key(data)
+            if captcha_key:
+                addon_plugin.log_info(_("Detected CloudFlare's security check page"))
 
-        else:
-            addon_plugin.log_warning(_("Got unexpected CloudFlare html page"))
-            return None  # Tell the exception handler to re-throw the exception
+                response, challenge = captcha.challenge(captcha_key, data)
+                return owner_plugin.load(owner_plugin.fixurl("/cdn-cgi/l/chk_captcha"),
+                                               get={'g-recaptcha-response': response},
+                                               ref=last_url)
+
+            else:
+                addon_plugin.log_warning(_("Got unexpected CloudFlare html page"))
+                return None  # Tell the exception handler to re-throw the exception
+
+        except Exception, e:
+            addon_plugin.log_error(e)
+            return None
 
 
 class PreloadStub(object):
-    def __init__(self, addon_plugin, hoster_plugin):
+    def __init__(self, addon_plugin, owner_plugin):
         self.addon_plugin = addon_plugin
-        self.hoster_plugin = hoster_plugin
-        self.old_preload = hoster_plugin._preload
+        self.owner_plugin = owner_plugin
+        self.old_preload = owner_plugin._preload
 
 
     def my_preload(self, *args, **kwargs):
-        data = CloudFlare.handle_function(self.addon_plugin, self.hoster_plugin, "_preload", self.old_preload, (args, kwargs))
+        data = CloudFlare.handle_function(self.addon_plugin, self.owner_plugin, "_preload", self.old_preload, (args, kwargs))
         if data is not None:
-            self.hoster_plugin.data = data
+            self.owner_plugin.data = data
 
 
     def __repr__(self):
@@ -133,7 +147,7 @@ class PreloadStub(object):
 class CloudFlareDdos(Addon):
     __name__    = "CloudFlareDdos"
     __type__    = "hook"
-    __version__ = "0.03"
+    __version__ = "0.04"
     __status__  = "testing"
 
     __config__ = [("activated", "bool", "Activated" , False)]
@@ -151,7 +165,7 @@ class CloudFlareDdos(Addon):
     def deactivate(self):
         while len(self.stubs):
             stub = self.stubs.itervalues().next()
-            self._unoverride_preload(stub.hoster_plugin)
+            self._unoverride_preload(stub.owner_plugin)
 
         self._unoverride_get_url()
 
@@ -161,7 +175,7 @@ class CloudFlareDdos(Addon):
             self.log_debug(_("Unoverriding _preload() for %s") % plugin_id(plugin))
 
             stub = self.stubs.pop(id(plugin))
-            stub.hoster_plugin._preload = stub.old_preload
+            stub.owner_plugin._preload = stub.old_preload
 
         else:
             self.log_warning(_("No _preload() override found for %s, cannot un-override>") % plugin_id(plugin))
@@ -192,27 +206,31 @@ class CloudFlareDdos(Addon):
         self.pyload.requestFactory.getURL = self.old_get_url
 
 
-    def _find_account_plugin(self):
+    def _find_owner_plugin(self):
         """
-        Walk the callstack until we find SimpleHoster class
+        Walk the callstack until we find SimpleHoster or SimpleCrypter class
         Dirty but works.
         """
         f = frame = inspect.currentframe()
-        while True:
-            if f is None:
-                return  None
+        try:
+            while True:
+                if f is None:
+                    return  None
 
-            elif 'self' in f.f_locals and isinstance(f.f_locals['self'], SimpleHoster):
-                return f.f_locals['self']
+                elif 'self' in f.f_locals and is_simple_plugin(f.f_locals['self']):
+                    return f.f_locals['self']
 
-            else:
-                f = f.f_back
+                else:
+                    f = f.f_back
 
-        del frame
+        finally:
+            del frame
 
 
     def download_preparing(self, pyfile):
-        if not isinstance(pyfile.plugin, SimpleHoster):  #: Only SimpleHoster based plugins are supported
+        self.log_debug("download_preparing=%s" % type(pyfile.plugin).__name__)
+        #: Only SimpleHoster and SimpleCrypter based plugins are supported
+        if not is_simple_plugin(pyfile.plugin):
             self.log_debug(_("Skipping plugin %s") % plugin_id(pyfile.plugin))
             return
 
@@ -230,8 +248,9 @@ class CloudFlareDdos(Addon):
 
 
     def my_get_url(self, *args, **kwargs):
-        hotser_plugin = self._find_account_plugin()
+        hotser_plugin = self._find_owner_plugin()
         if hotser_plugin is None:
+            self.log_warning("Owner plugin not found, cannot process")
             return self.old_get_url(*args, **kwargs)
 
         else:
