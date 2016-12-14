ccbe3c263a7599b5452e0f24a7330ee7319ba3f1
Walter Purcaro <vuolter@users.noreply.github.com>
2016-12-14 21:51:11 +0100

    Shorten version

--- pyload/__init__.py
+++ pyload/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import unicode_literals
 from builtins import str
-__dev__ = True
-__version_info__ = (0, 5, 0, 0)
-__version__ = '.'.join(str(v) for v in __version_info__) + ("-dev" if __dev__ else "")
+
+__version_info__ = (0, 5, 0)
+__version__ = '.'.join(str(v) for v in __version_info__)
