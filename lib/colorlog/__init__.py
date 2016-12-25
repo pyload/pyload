1f8a8b513284795ab1d6ecdb715732b7deb9e94b
Walter Purcaro <vuolter@users.noreply.github.com>
2016-12-25 20:09:57 +0100

    Remove lib

--- lib/colorlog/__init__.py
+++
@@ -1,8 +0,0 @@
-"""A logging formatter for colored output"""
-
-from __future__ import absolute_import
-
-__all__ = ['ColoredFormatter', 'default_log_colors', 'escape_codes']
-
-from colorlog.colorlog import (
-    ColoredFormatter, default_log_colors, escape_codes)
