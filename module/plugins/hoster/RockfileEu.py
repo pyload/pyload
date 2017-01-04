ab8231cc4e81fc394013f9fc34a986870de4dedf
GammaC0de <nitzo2001@yahoo.com>
2017-01-04 02:39:37 +0200

    [New Hoster] RockfileEu

---
+++ module/plugins/hoster/RockfileEu.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from module.plugins.internal.SimpleHoster import SimpleHoster
+
+
+class RockfileEu(SimpleHoster):
+    __name__    = "RockfileEu"
+    __type__    = "hoster"
+    __version__ = "0.01"
+    __status__  = "testing"
+
+    __pattern__ = r'https?://(?:www\.)?rockfile\.eu/\w{12}.html'
+    __config__  = [("activated"   , "bool", "Activated"                                        , True),
+                   ("use_premium" , "bool", "Use premium account if available"                 , True),
+                   ("fallback"    , "bool", "Fallback to free download if premium fails"       , True),
+                   ("chk_filesize", "bool", "Check file size"                                  , True),
+                   ("max_wait"    , "int" , "Reconnect if waiting time is greater than minutes", 10  )]
+
+    __description__ = """Rockfile.eu hoster plugin"""
+    __license__     = "GPLv3"
+    __authors__     = [("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
+
+    NAME_PATTERN = r'name="fname" value="(?P<N>.+?)"'
+    SIZE_PATTERN = r'var iniFileSize = (\d+)'
+
+    WAIT_PATTERN      = r'<span id="countdown_str".+?>(\d+)</span>|You have to wait <b>([\w ,]+)<'
+    LINK_FREE_PATTERN = r'href="(http://.+?\.rfservers\.eu.+?)"'
+
+    COOKIES = [("rockfile.eu", "lang", "english")]
+
+
+    def setup(self):
+        self.resume_download = True
+
+
+    def handle_free(self, pyfile):
+        url, inputs = self.parse_html_form("action=''")
+
+        if not inputs:
+            self.error("Free download form not found")
+
+        self.data = self.load(pyfile.url, post=inputs)
+
+        self.check_errors()
+
+        captcha_code = "".join([chr(int(_x[2:4])) if _x[0:2] == '&#' else _x for _p, _x in
+                                sorted(re.findall(r'<span style=[\'"]color:#5d5d5d; text-shadow: 1px 1px #f2f2f2;.+?padding-left:(\d+)px;.+?[\'"]>(.+?)</span>', self.data),
+                                       key=lambda _i:int(_i[0]))])
+
+        if not captcha_code:
+            self.log_debug(self.data)
+            self.error("Captcha not found")
+
+        url, inputs = self.parse_html_form('name="F1"')
+
+        if not inputs:
+            self.error("Form F1 not found")
+
+        inputs['code'] = captcha_code
+
+        self.data = self.load(pyfile.url, post=inputs)
+
+        m = re.search(self.LINK_FREE_PATTERN, self.data)
+        if m:
+            self.link = m.group(1)
+        else:
+            self.log_debug(self.data)
+
