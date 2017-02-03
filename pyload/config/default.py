4a866c5ce852ccd795b6f49164e262dfdb88b03f
Walter Purcaro <vuolter@users.noreply.github.com>
2017-02-03 05:41:06 +0100

    Update config default settings

--- pyload/config/default.py
+++ pyload/config/default.py
@@ -12,95 +12,99 @@ def make_config(config):
     # Check if gettext is installed
     _ = lambda x: x
 
-    config.add_config_section("log", _("Log"), _("Description"), _("Long description"),
-                            [
-                                ("log_size", "int", _("Size in KiB"), 100),
-                                ("log_folder", "folder", _("Folder"), "Logs"),
-                                ("file_log", "bool", _("File Log"), True),
-                                ("log_count", "int", _("Count"), 5),
-                                ("log_rotate", "bool", _("Log Rotate"), True),
-                                ("console_color", "bool", _("Colorize Console"), True),
-                                ("color_theme", "light;full", _("Color Theme"), "light"),
-                            ])
+    config.add_config_section("general", _("General"), _("Description"), _("Long description"),
+        [
+            ("language"        , "en|de|fr|it|es|nl|sv|ru|pl|cs|sr|pt", _("Language")                         , "en"),
+            ("storage_folder"  , "folder"                             , _("Storage folder")                   , None),
+            ("min_storage_size", "int"                                , _("Min storage space (in MiB)")       , 512 ),
+            ("folder_pack"     , "bool"                               , _("Create folder for each package")   , True),
+            ("local_access"    , "bool"                               , _("No authentication on local access"), True),
+            ("niceness"        , "int"                                , _("pyLoad process priority")          , 0   )
+        ])
+
+    config.add_config_section("log", _("Logging"), _("Description"), _("Long description"),
+        [
+            ("activated"     , "bool"  , _("Activated")             , True ),
+            ("logfile"       , "bool"  , _("Save log to file")      , True ),
+            ("logfile_size"  , "int"   , _("Max file size (in KiB)"), 100  ),
+            ("logfile_folder", "folder", _("File folder")           , None ),
+            ("max_logfiles"  , "int"   , _("Max log files")         , 5    ),
+            ("rotate"        , "bool"  , _("Log rotate")            , True ),
+            ("debug"         , "bool"  , _("Debug mode")            , False),
+            ("verbose"       , "bool"  , _("Verbose mode")          , False),
+            ("color_console" , "bool"  , _("Color console")         , True )
+        ])
 
     config.add_config_section("permission", _("Permissions"), _("Description"), _("Long description"),
-                            [
-                                ("group", "str", _("Groupname"), "users"),
-                                ("change_dl", "bool", _("Change Group and User of Downloads"), False),
-                                ("change_file", "bool", _("Change file mode of downloads"), False),
-                                ("user", "str", _("Username"), "user"),
-                                ("file", "str", _("Filemode for Downloads"), "0644"),
-                                ("change_group", "bool", _("Change group of running process"), False),
-                                ("folder", "str", _("Folder Permission mode"), "0755"),
-                                ("change_user", "bool", _("Change user of running process"), False),
-                            ])
+        [
+            ("user"            , "str" , _("Username")                            , "user" ),
+            ("group"           , "str" , _("Groupname")                           , "users"),
+            ("foldermode"      , "str" , _("Folder mode")                         , "0o755"),
+            ("filemode"        , "str" , _("File mode")                           , "0o644"),
+            ("change_user"     , "bool", _("Change user of pyLoad process")       , False  ),
+            ("change_group"    , "bool", _("Change group of pyLoad process")      , False  ),
+            ("change_fileowner", "bool", _("Change user and group of saved files"), False  ),
+            ("change_filemode" , "bool", _("Change file mode of saved files")     , False  )
+        ])
 
-    config.add_config_section("general", _("General"), _("Description"), _("Long description"),
-                            [
-                                ("language", "en;de;fr;it;es;nl;sv;ru;pl;cs;sr;pt_BR", _("Language"), "en"),
-                                ("download_folder", "folder", _("Download Folder"), "Downloads"),
-                                ("folder_per_package", "bool", _("Create folder for each package"), True),
-                                ("debug_mode", "bool", _("Debug Mode"), False),
-                                ("min_free_space", "int", _("Min Free Space (MiB)"), 512),
-                                ("renice", "int", _("CPU Priority"), 0),
-                            ])
+    config.add_config_section("connection", _("Connections"), _("Description"), _("Long description"),
+        [
+            ("max_transfers", "int" , _("Max parallel transfers")             , 5    ),
+            ("max_speed"    , "int" , _("Max transfer speed (in KiB/s)")      , -1   ),
+            ("max_chunks"   , "int" , _("Max connections for single transfer"), -1   ),
+            ("wait"         , "int" , _("Active transfers while waiting")     , 2    ),  #@TODO: Recheck
+            ("skip"         , "bool", _("Skip existing files")                , False),
+            ("interface"    , "str" , _("Interface address to bind")          , None ),
+            ("ipv6"         , "bool", _("Allow IPv6")                         , False)
+        ])
 
     config.add_config_section("ssl", _("SSL"), _("Description"), _("Long description"),
-                            [
-                                ("cert", "file", _("SSL Certificate"), "ssl.crt"),
-                                ("activated", "bool", _("Activated"), False),
-                                ("key", "file", _("SSL Key"), "ssl.key"),
-                            ])
+        [
+            ("activated", "bool", _("Activated"), False    ),
+            ("cert"     , "file", _("Cert file"), "ssl.crt"),
+            ("key"      , "file", _("Key file") , "ssl.key")
+        ])
 
-    config.add_config_section("webui", _("WebUI"), _("Description"), _("Long description"),
-                            [
-                                ("template", "str", _("Template"), "default"),
-                                ("prefix", "str", _("Path Prefix"), ""),
-                                ("external", "bool", _("Served external"), False),
-                                ("server", "auto;threaded;fallback;fastcgi", _("Server"), "auto"),
-                                ("force_server", "str", _("Favor specific server"), ""),
-                                ("host", "ip", _("IP"), "localhost"),
-                                ("https", "bool", _("Use HTTPS"), False),
-                                ("port", "int", _("Port"), 8010),
-                                ("wshost", "ip", _("IP"), "localhost"),
-                                ("wsport", "int", _("Port"), 7447),
-                            ])
+    config.add_config_section("reconnect", _("Reconnection"), _("Description"), _("Long description"),
+        [
+            ("activated", "bool", _("Activated")                    , False ),
+            ("script"   , "str" , _("Script file")                  , None  ),
+            ("wait"     , "str" , _("Don't reconnect while waiting"), False )
+        ])
 
     config.add_config_section("proxy", _("Proxy"), _("Description"), _("Long description"),
-                            [
-                                ("username", "str", _("Username"), ""),
-                                ("proxy", "bool", _("Use Proxy"), False),
-                                ("address", "str", _("Address"), "localhost"),
-                                ("password", "password", _("Password"), ""),
-                                ("type", "http;socks4;socks5", _("Protocol"), "http"),
-                                ("port", "int", _("Port"), 7070),
-                            ])
+        [
+            ("activated", "bool"              , _("Activated") , False      ),
+            ("type"     , "http|socks4|socks5", _("Protocol")  , "http"     ),
+            ("host"     , "str"               , _("IP address"), "localhost"),
+            ("port"     , "int"               , _("Port")      , 7070       ),
+            ("username" , "str"               , _("Username")  , None       ),
+            ("password" , "str"               , _("Password")  , None       )
+        ])
 
-    config.add_config_section("reconnect", _("Reconnect"), _("Description"), _("Long description"),
-                            [
-                                ("activated", "bool", _("Use Reconnect"), False),
-                                ("method", "str", _("Method"), "./reconnect.sh"),
-                                ("wait", "str", _("Wait for reconnect"), False),
-                                ("startTime", "time", _("Start"), "0:00"),
-                                ("endTime", "time", _("End"), "0:00"),
-                            ])
+    config.add_config_section("webui", _("Web User Interface"), _("Description"), _("Long description"),
+        [
+            ("activated"   , "bool"                          , _("Activated")       , True       ),
+            ("server"      , "auto|threaded|fallback|fastcgi", _("Webserver")       , "auto"     ),            
+            ("host"        , "str"                           , _("IP address")      , "localhost"),
+            ("port"        , "int"                           , _("Port")            , 8010       ),            
+            ("force_server", "str"                           , _("Forced webserver"), None       ),
+            ("external"    , "bool"                          , _("Served external") , False      ),
+            ("prefix"      , "str"                           , _("Path prefix")     , None       ),
+            ("debug"       , "bool"                          , _("Debug mode")      , False      )  
+        ])
 
-    config.add_config_section("download", _("Download"), _("Description"), _("Long description"),
-                            [
-                                ("max_downloads", "int", _("Max parallel downloads"), 3),
-                                ("wait_downloads", "int", _("Start downloads while waiting"), 2),
-                                ("limit_speed", "bool", _("Limit download speed"), False),
-                                ("interface", "str", _("Download interface to bind (ip or Name)"), ""),
-                                ("skip_existing", "bool", _("Skip already existing files"), False),
-                                ("max_speed", "int", _("Max download speed in KiB/s"), -1),
-                                ("ipv6", "bool", _("Allow IPv6"), False),
-                                ("ssl", "bool", _("Force SSL downloads"), False),
-                                ("chunks", "int", _("Max connections for one download"), 3),
-                                ("restart_failed", "bool", _("Restart failed downloads on startup"), False),
-                            ])
+    config.add_config_section("remote", _("REST API Interface"), _("Description"), _("Long description"),
+        [
+            ("activated", "bool", _("Activated") , False    ),
+            ("host"     , "str" , _("IP address"), "0.0.0.0"),
+            ("port"     , "int" , _("Port")      , 7227     )
+        ])
 
-    config.add_config_section("downloadTime", _("Download Time"), _("Description"), _("Long description"),
-                            [
-                                ("start", "time", _("Start"), "0:00"),
-                                ("end", "time", _("End"), "0:00"),
-                            ])
+    config.add_config_section("update", _("Updates"), _("Description"), _("Long description"),
+        [
+            ("activated" , "bool", _("Activated")                    , True ),
+            ("nodebug"   , "bool", _("Don't update in debug mode")   , False),
+            ("periodical", "bool", _("Check for updates on schedule"), True ),
+            ("interval"  , "int" , _("Check interval (in days)")     , 1    )
+        ])
