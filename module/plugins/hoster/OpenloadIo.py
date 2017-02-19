af37fc4429b8088ff2bd7b1ec8b797f2692205b6
dischinator <dischinator@users.noreply.github.com>
2017-02-19 13:45:31 +0100

    [OpenloadIo] Increase version number

--- module/plugins/hoster/OpenloadIo.py
+++ module/plugins/hoster/OpenloadIo.py
@@ -10,7 +10,7 @@ from module.plugins.internal.misc import json
 class OpenloadIo(SimpleHoster):
     __name__    = "OpenloadIo"
     __type__    = "hoster"
-    __version__ = "0.16"
+    __version__ = "0.17"
     __status__  = "testing"
 
     __pattern__ = r'https?://(?:www\.)?openload\.(co|io)/(f|embed)/(?P<ID>[\w\-]+)'
