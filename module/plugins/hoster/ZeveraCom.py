3c23aefb43e9887ef93fe9260d61e63a16b2534e
GammaC0de <gammac0de@users.noreply.github.com>
2017-03-11 03:33:42 +0200

    [ZeveraCom] Unpdate

--- module/plugins/hoster/ZeveraCom.py
+++ module/plugins/hoster/ZeveraCom.py
@@ -7,20 +7,16 @@ from ..internal.MultiHoster import MultiHoster
 class ZeveraCom(MultiHoster):
     __name__ = "ZeveraCom"
     __type__ = "hoster"
-    __version__ = "0.37"
+    __version__ = "0.38"
     __status__ = "testing"
 
     __pattern__ = r'https?://(?:www\.)zevera\.com/(getFiles\.ashx|Members/download\.ashx)\?.*ourl=.+'
     __config__ = [("activated", "bool", "Activated", True),
                   ("use_premium", "bool", "Use premium account if available", True),
-                  ("fallback",
-                   "bool",
-                   "Fallback to free download if premium fails",
-                   False),
+                  ("fallback", "bool", "Fallback to free download if premium fails", False),
                   ("chk_filesize", "bool", "Check file size", True),
-                  ("max_wait", "int",
-                   "Reconnect if waiting time is greater than minutes", 10),
-                  ("revertfailed", "bool", "Revert to standard download if fails", True)]
+                  ("max_wait", "int","Reconnect if waiting time is greater than minutes", 10),
+                  ("revert_failed", "bool", "Revert to standard download if fails", True)]
 
     __description__ = """Zevera.com multi-hoster plugin"""
     __license__ = "GPLv3"
@@ -30,5 +26,4 @@ class ZeveraCom(MultiHoster):
     FILE_ERRORS = [("Error", r'action="ErrorDownload.aspx')]
 
     def handle_premium(self, pyfile):
-        self.link = "https://%s/getFiles.ashx?ourl=%s" % (
-            self.account.PLUGIN_DOMAIN, pyfile.url)
+        self.link = "https://zevera.com/getFiles.ashx?ourl=%s" % pyfile.url
