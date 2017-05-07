657b1b765fc214f91fe7098e95cf373ff16b22d2
GammaC0de <gammac0de@users.noreply.github.com>
2017-05-07 23:39:44 +0300

    [PornhubCom] Fix #2838

--- module/plugins/hoster/PornhubCom.py
+++ module/plugins/hoster/PornhubCom.py
@@ -1,45 +1,110 @@
 # -*- coding: utf-8 -*-
 
-import os
 import re
-import urlparse
 
-from ..internal.Hoster import Hoster
+from module.network.CookieJar import CookieJar
+from module.network.HTTPRequest import HTTPRequest
+
+from ..internal.misc import json
+from ..internal.SimpleHoster import SimpleHoster
+from ..internal.Plugin import Abort
 
 
-class PornhubCom(Hoster):
+class BIGHTTPRequest(HTTPRequest):
+    """
+    Overcome HTTPRequest's load() size limit to allow
+    loading very big web pages by overrding HTTPRequest's write() function
+    """
+
+    # @TODO: Add 'limit' parameter to HTTPRequest in v0.4.10
+    def __init__(self, cookies=None, options=None, limit=1000000):
+        self.limit = limit
+        HTTPRequest.__init__(self, cookies=cookies, options=options)
+
+    def write(self, buf):
+        """ writes response """
+        if self.limit and self.rep.tell() > self.limit or self.abort:
+            rep = self.getResponse()
+            if self.abort:
+                raise Abort()
+            f = open("response.dump", "wb")
+            f.write(rep)
+            f.close()
+            raise Exception("Loaded Url exceeded limit")
+
+        self.rep.write(buf)
+
+
+class PornhubCom(SimpleHoster):
     __name__ = "PornhubCom"
     __type__ = "hoster"
-    __version__ = "0.57"
+    __version__ = "0.58"
     __status__ = "testing"
 
-    __pattern__ = r'http://(?:www\.)?pornhub\.com/view_video\.php\?viewkey=\w+'
-    __config__ = [("activated", "bool", "Activated", True)]
+    __pattern__ = r'https?://(?:www\.)?pornhub\.com/view_video\.php\?viewkey=\w+'
+    __config__ = [("activated", "bool", "Activated", True),
+                  ("use_premium", "bool", "Use premium account if available", True),
+                  ("fallback", "bool", "Fallback to free download if premium fails", True),
+                  ("chk_filesize", "bool", "Check file size", True),
+                  ("max_wait", "int", "Reconnect if waiting time is greater than minutes", 10)]
 
     __description__ = """Pornhub.com hoster plugin"""
     __license__ = "GPLv3"
-    __authors__ = [("jeix", "jeix@hasnomail.de")]
+    __authors__ = [("jeix", "jeix@hasnomail.de"),
+                   ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
-    NAME_PATTERN = r'"video_title":"(.+?)"'
+    NAME_PATTERN = r'"video_title":"(?P<N>.+?)"'
+    TEMP_OFFLINE_PATTERN = r''
+    OFFLINE_PATTERN = r''
 
-    def process(self, pyfile):
-        html = self.load(pyfile.url)
 
-        m = re.findall(r'var player_quality_(\d+)\w+? = \'(.+?)\'', html)
+    @classmethod
+    def get_info(cls, url="", html=""):
+        info = super(PornhubCom, cls).get_info(url, html)
+        # Unfortunately, NAME_PATTERN does not include file extension so we blindly add '.mp4' as an extension.
+        # (hopefully all links are '.mp4' files)
+        if 'name' in info:
+            info['name'] += ".mp4"
+
+        return info
+
+    def setup(self):
+        self.resume_download = True
+        self.multiDL = True
+
+        try:
+            self.req.http.close()
+        except Exception:
+            pass
+
+        self.req.http = BIGHTTPRequest(
+            cookies=CookieJar(None),
+            options=self.pyload.requestFactory.getOptions(),
+            limit=2000000)
+
+    def handle_free(self, pyfile):
+        m = re.search(r'<div class="video-wrapper">.+?<script type="text/javascript">(.+?)</script>', self.data, re.S)
         if m is None:
-            self.error(_("video quality pattern not found"))
+            self.error(_("Player Javascript data not found"))
 
-        urls = dict(m)
+        script = m.group(1)
 
-        quality = str(max(int(q) for q in urls.keys()))
-
-        m = re.search(self.NAME_PATTERN, html)
+        m = re.search(r'qualityItems_\d+', script)
         if m is None:
-            self.error("name pattern not found")
+            self.error(_("`qualityItems` variable no found"))
 
-        link = urls[quality]
+        result_var = re.search(r'qualityItems_\d+', script).group(0)
 
-        ext = os.path.splitext(urlparse.urlparse(link).path)[1]
-        pyfile.name = m.group(1) + ext
+        script = "".join(re.findall(r'^\s*var .+', script, re.M))
+        script = re.sub(r"[\n\t]|/\*.+?\*/", "", script)
+        script += "JSON.stringify(%s);" % result_var
 
-        self.download(link)
+        res = self.js.eval(script)
+        json_data = json.loads(res)
+
+        urls = dict([(int(re.search("^(\d+)", _x['text']).group(0)), _x['url'])
+                     for _x in json_data if _x['url']])
+
+        quality = max(_q for _q in urls.keys())
+
+        self.link = urls[quality]
