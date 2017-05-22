7a048e742eb4cbc67c6c55ef03569d81a5ac3089
GammaC0de <gammac0de@users.noreply.github.com>
2017-05-22 18:59:24 +0300

    [DebridlinkFr] Update

--- module/plugins/hoster/DebridlinkFr.py
+++ module/plugins/hoster/DebridlinkFr.py
@@ -16,19 +16,15 @@ def args(**kwargs):
 class DebridlinkFr(MultiHoster):
     __name__ = "DebridlinkFr"
     __type__ = "hoster"
-    __version__ = "0.03"
+    __version__ = "0.04"
     __status__ = "testing"
 
     __pattern__ = r'^unmatchable$'
     __config__ = [("activated", "bool", "Activated", True),
                   ("use_premium", "bool", "Use premium account if available", True),
-                  ("fallback",
-                   "bool",
-                   "Fallback to free download if premium fails",
-                   False),
+                  ("fallback", "bool", "Fallback to free download if premium fails", False),
                   ("chk_filesize", "bool", "Check file size", True),
-                  ("max_wait", "int",
-                   "Reconnect if waiting time is greater than minutes", 10),
+                  ("max_wait", "int", "Reconnect if waiting time is greater than minutes", 10),
                   ("revert_failed", "bool", "Revert to standard download if fails", True)]
 
     __description__ = """Debrid-slink.fr multi-hoster plugin"""
@@ -61,6 +57,24 @@ class DebridlinkFr(MultiHoster):
         if res['result'] == "OK":
             self.link = res['value']['downloadLink']
             pyfile.name = res['value'].get('filename', None) or pyfile.name
-            self.resume_download = res['value'].get(
-                'resume') or self.resume_download
+            self.resume_download = res['value'].get('resume') or self.resume_download
             self.chunk_limit = res['value'].get('chunk') or self.chunk_limit
+
+        else:
+            err_code = res['ERR']
+            if err_code == "fileNotFound":
+                self.offline()
+
+            else:
+                err_message = {'notLink': "Check the 'link' parameter (Empty or bad)",
+                               'notDebrid': "Maybe the filehoster is down or the link is not online",
+                               'badFileUrl': "The link format is not valid",
+                               'hostNotValid': "The filehoster is not supported",
+                               'notFreeHost': "This filehoster is not available for the free member",
+                               'disabledHost': "The filehoster are disabled",
+                               'noGetFilename': "Unable to retrieve the file name",
+                               'maxLink': "Limitation of number links per day reached",
+                               'maxLinkHost': "Limitation of number links per day for this host reached"}.get(err_code)
+
+                self.log_error(err_message or "Unknown error")
+
