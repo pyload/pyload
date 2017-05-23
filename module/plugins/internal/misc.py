5723c282f7f9c1904c723ce1aa3928af55dabc9e
GammaC0de <gammac0de@users.noreply.github.com>
2017-05-24 01:56:21 +0300

    [misc] Update format_time

--- module/plugins/internal/misc.py
+++ module/plugins/internal/misc.py
@@ -41,7 +41,7 @@ except ImportError:
 class misc(object):
     __name__ = "misc"
     __type__ = "plugin"
-    __version__ = "0.44"
+    __version__ = "0.45"
     __status__ = "stable"
 
     __pattern__ = r'^unmatchable$'
@@ -243,11 +243,11 @@ def threaded(fn):
 def format_time(value):
     dt = datetime.datetime(1, 1, 1) + \
         datetime.timedelta(seconds=abs(int(value)))
-    days = ("%d days and " % (dt.day - 1)) if dt.day > 1 else ""
-    return days + ", ".join("%d %ss" % (getattr(dt, attr), attr)
+    days = ("%d days" % (dt.day - 1)) if dt.day > 1 else ""
+    res = ", ".join("%d %ss" % (getattr(dt, attr), attr)
                             for attr in ("hour", "minute", "second")
                             if getattr(dt, attr))
-
+    return days + (" and " if res else "") + res
 
 def format_size(value):
     for unit in ('B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB'):
