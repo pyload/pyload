c12f7a87def3d37f2e718661c7014560f78c4576
Walter Purcaro <vuolter@users.noreply.github.com>
2017-06-03 12:56:03 +0200

    Fix https://github.com/pyload/utils/issues/3 and import loops

--- src/pyload/utils/parse.py
+++ src/pyload/utils/parse.py
@@ -10,9 +10,8 @@ import urllib.parse
 
 from future import standard_library
 
-from . import convert, purge
+from . import convert, purge, web
 from .layer.legacy import hashlib_ as hashlib
-from .misc import translate
 from .time import seconds_to_midnight
 
 standard_library.install_aliases()
@@ -29,9 +28,8 @@ __booleanmap = {
     '1': True, 'yes': True, 'true': True, 'on': True,
     '0': False, 'no': False, 'false': False, 'off': False}
 
-@purge.args
 def boolean(text):
-    return __booleanmap.get(text.lower())
+    return __booleanmap.get(text.strip().lower())
 
 
 def entries(text, allow_whitespaces=False):
@@ -42,7 +40,6 @@ def entries(text, allow_whitespaces=False):
     return [entry for entry in re.split(pattr, text) if entry]
 
 
-@purge.args
 def hash(text):
     text = text.replace('-', '').lower()
     algop = '|'.join(hashlib.algorithms + ('adler32', 'crc(32)?'))
@@ -60,19 +57,16 @@ def hash(text):
     return checksum, algorithm
 
 
-@purge.args
 def name(text, strict=True):
-    from .web import parse
     try:
-        name = parse.name(text)
+        name = web.parse.name(text)
     except Exception:
         name = os.path.basename(text).strip()
     return name if strict else purge.name(name)
 
 
-@purge.args
-def mime(text):
-    return mimetypes.guess_type(text, strict=False)[0]
+def mime(text, strict=False):
+    return mimetypes.guess_type(text.strip(), strict)[0]
 
 
 __onewords = (
@@ -84,10 +78,9 @@ __tenwords = (
     "ninety")
 __re_number = re.compile(r'[\s-]+')
 
-@purge.args
 def number(text):
     try:
-        text = translate(text).lower()
+        text = web.translate(text).lower()
     except Exception:
         text = text.lower()
     o_tuple = [(w, i) for i, w in enumerate(__onewords)]
@@ -102,13 +95,12 @@ def number(text):
 
 __re_packs = re.compile(r'[^a-z0-9]+(?:(cd|part).*?\d+)?', flags=re.I)
 
-@purge.args
 def packs(nameurls):
     DEFAULT_URLNAME = "Unknown"
 
     packs = {}
     for urlname, url in nameurls:
-        urlname = name(urlname)
+        urlname = name(urlname, strict=False)
         urlname = os.path.splitext(urlname)[0].strip()
         urlname = __re_packs.sub('_', urlname).strip('_')
 
@@ -122,7 +114,6 @@ def packs(nameurls):
 
 __re_size = re.compile(r'(?P<S>[\d.,]+)\s*(?P<U>[a-zA-Z]*)')
 
-@purge.args
 def bytesize(text, unit=None):  # returns integer bytes
     DEFAULT_INPUTUNIT = 'byte'
 
@@ -144,10 +135,9 @@ __timemap = {
     'day': 60 ** 2 * 12, 'hr': 60 ** 2, 'hour': 60 ** 2, 'min': 60, 'sec': 1}
 __re_time = re.compile(r'(\d+|[a-zA-Z-]+)\s*(day|hr|hour|min|sec)|(\d+)')
 
-@purge.args
 def seconds(text):
     try:
-        text = translate(text).lower()
+        text = web.translate(text).lower()
     except Exception:
         text = text.lower()
     pattr = r'({0})\s+day|today|daily'.format('|'.join(__timewords))
