4f8ced0158ff4aeceed6cd10c6fa9e964dc32573
Walter Purcaro <vuolter@users.noreply.github.com>
2017-06-03 03:30:59 +0200

    Rename pyLoad.py to pyload.py

--- pyload.py
+++ pyload.py
@@ -10,13 +10,6 @@
 #          \  /
 #           \/
 
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-
-from future import standard_library
-standard_library.install_aliases()
 import os
 import sys
 
