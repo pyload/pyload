f60c01618d5aede70c707e4738d38d628ed8621e
GammaC0de <gammac0de@users.noreply.github.com>
2017-06-26 00:29:59 +0300

    [FilejokerNet] Fix #2875

--- module/plugins/hoster/FilejokerNet.py
+++ module/plugins/hoster/FilejokerNet.py
@@ -7,14 +7,13 @@ from ..internal.XFSHoster import XFSHoster
 class FilejokerNet(XFSHoster):
     __name__ = "FilejokerNet"
     __type__ = "hoster"
-    __version__ = "0.03"
+    __version__ = "0.04"
     __status__ = "testing"
 
     __pattern__ = r'https?://(?:www\.)?filejoker\.net/\w{12}'
     __config__ = [("activated", "bool", "Activated", True),
                   ("use_premium", "bool", "Use premium account if available", True),
-                  ("fallback", "bool",
-                   "Fallback to free download if premium fails", True),
+                  ("fallback", "bool", "Fallback to free download if premium fails", True),
                   ("chk_filesize", "bool", "Check file size", True),
                   ("max_wait", "int", "Reconnect if waiting time is greater than minutes", 10)]
 
@@ -25,8 +24,9 @@ class FilejokerNet(XFSHoster):
     PLUGIN_DOMAIN = "filejoker.net"
 
     WAIT_PATTERN = r'Please [Ww]ait (?:<span id="count" class="alert-success">)?([\w ]+?)(?:</span> seconds</p>| until the next download)'
-    RECAPTCHA_PATTERN = r'<div id="recaptcha_image" class="pic"></div>'
     ERROR_PATTERN = r'Wrong Captcha'
+
+    PREMIUM_ONLY_PATTERN = 'Free Members can download files no bigger'
 
     INFO_PATTERN = r'<div class="name-size">(?P<N>.+?) <small>\((?P<S>[\d.,]+) (?P<U>[\w^_]+)\)</small></div>'
     SIZE_REPLACEMENTS = [('Kb', 'KB'), ('Mb', 'MB'), ('Gb', 'GB')]
