ec0e32c46788bccb644a26749bf5eabb9c6bc875
GammaC0de <gammac0de@users.noreply.github.com>
2017-07-12 03:10:29 +0300

    [OpenloadIo] Code cosmetics

--- module/plugins/hoster/OpenloadIo.py
+++ module/plugins/hoster/OpenloadIo.py
@@ -11,7 +11,7 @@ from ..internal.SimpleHoster import SimpleHoster
 class OpenloadIo(SimpleHoster):
     __name__ = "OpenloadIo"
     __type__ = "hoster"
-    __version__ = "0.19"
+    __version__ = "0.20"
     __status__ = "testing"
 
     __pattern__ = r'https?://(?:www\.)?openload\.(co|io)/(f|embed)/(?P<ID>[\w\-]+)'
@@ -25,24 +25,21 @@ class OpenloadIo(SimpleHoster):
     __license__ = "GPLv3"
     __authors__ = [(None, None)]
 
+    OFFLINE_PATTERN = r'>We are sorry'
+
     # The API reference, that this implementation uses is available at
     # https://openload.co/api
     API_URL = 'https://api.openload.co/1'
 
-    _DOWNLOAD_TICKET_URI_PATTERN = '/file/dlticket?file=%s'
-    _DOWNLOAD_FILE_URI_PATTERN = '/file/dl?file=%s&ticket=%s&captcha_response=%s'
-    _FILE_INFO_URI_PATTERN = '/file/info?file=%s'
-
-    OFFLINE_PATTERN = r'>We are sorry'
-
     @classmethod
-    def _load_json(cls, uri):
-        return json.loads(get_url(cls.API_URL + uri))
+    def api_response(cls, file_id, method, **kwargs):
+        kwargs['file'] = file_id
+        return json.loads(get_url(cls.API_URL + "/file/" + method, get=kwargs))
 
     @classmethod
     def api_info(cls, url):
         file_id = re.match(cls.__pattern__, url).group('ID')
-        info_json = cls._load_json(cls._FILE_INFO_URI_PATTERN % file_id)
+        info_json = cls.api_response(file_id, "info")
         file_info = info_json['result'][file_id]
 
         return {'name': file_info['name'],
@@ -53,54 +50,44 @@ class OpenloadIo(SimpleHoster):
         self.chunk_limit = 1
 
     def handle_free(self, pyfile):
-        # If the link is being handled here, then it matches the file_id_pattern,
-        # therefore, we can call [0] safely.
         file_id = self.info['pattern']['ID']
 
-        while True:
-            ticket_json = self._load_json(self._DOWNLOAD_TICKET_URI_PATTERN % file_id)
+        ticket_json = self.api_response(file_id, "dlticket")
 
-            if ticket_json['status'] != 200:
-                self.log_error(ticket_json['msg'])
+        if ticket_json['status'] != 200:
+            self.log_error(ticket_json['msg'])
 
-            if ticket_json['status'] == 404:
-                self.offline(ticket_json['msg'])
+        if ticket_json['status'] == 404:
+            self.offline(ticket_json['msg'])
 
-            elif ticket_json['status'] == 509:
-                self.temp_offline(ticket_json['msg'])
+        elif ticket_json['status'] == 509:
+            self.temp_offline(ticket_json['msg'])
 
-            elif ticket_json['status'] != 200:
-                self.fail(ticket_json['msg'])
+        elif ticket_json['status'] != 200:
+            self.fail(ticket_json['msg'])
 
-            self.wait(ticket_json['result']['wait_time'])
+        self.wait(ticket_json['result']['wait_time'])
 
-            # check if a captcha is required for this download
-            captchaResponse = ''
-            if 'captcha_url' in ticket_json['result'] and ticket_json[
-                    'result']['captcha_url'] != False:
-                self.log_debug(
-                    'This download requires a captcha solution: %s' %
-                    (ticket_json['result']['captcha_url']))
-                captchaResponse = self.captcha.decrypt(
-                    ticket_json['result']['captcha_url'])
+        # check if a captcha is required for this download
+        captcha_response = ''
+        if 'captcha_url' in ticket_json['result'] and ticket_json[
+                'result']['captcha_url'] != False:
+            self.log_debug('This download requires a captcha solution: %s' %
+                           ticket_json['result']['captcha_url'])
+            captcha_response = self.captcha.decrypt(ticket_json['result']['captcha_url'])
 
-            ticket = ticket_json['result']['ticket']
+        ticket = ticket_json['result']['ticket']
 
-            download_json = self._load_json(
-                self._DOWNLOAD_FILE_URI_PATTERN %
-                (file_id, ticket, captchaResponse))
+        download_json = self.api_response(file_id, "dl", ticket=ticket, captcha_response=captcha_response)
 
-            # check download link request result status
-            if download_json['status'] == 403:
-                # wrong captcha, get new captcha and try again
-                self.log_debug('Captcha solution is incorrect')
-                continue
+        # check download link request result status
+        if download_json['status'] == 403:
+            # wrong captcha, get new captcha and try again
+            self.retry_captcha()
 
-            if download_json['status'] == 200:
-                # start downloading
-                break
+        elif download_json['status'] == 200:
+            self.link = download_json['result']['url']
 
+        else:
             # no status 403 or 200 means getting the download url failed, abort
             self.fail(download_json['msg'])
-
-        self.link = download_json['result']['url']
