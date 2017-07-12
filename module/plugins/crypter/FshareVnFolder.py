dd227e5b4796f6d8cbd1e2f4ccc247c1e54f3d53
GammaC0de <gammac0de@users.noreply.github.com>
2017-07-13 01:28:32 +0300

    [FshareVn][FshareVnFolder] Fix #1273

--- module/plugins/crypter/FshareVnFolder.py
+++ module/plugins/crypter/FshareVnFolder.py
@@ -6,18 +6,21 @@ from ..internal.SimpleCrypter import SimpleCrypter
 class FshareVnFolder(SimpleCrypter):
     __name__ = "FshareVnFolder"
     __type__ = "crypter"
-    __version__ = "0.07"
+    __version__ = "0.08"
     __status__ = "testing"
 
-    __pattern__ = r'http://(?:www\.)?fshare\.vn/folder/.+'
+    __pattern__ = r'https?://(?:www\.)?fshare\.vn/folder/.+'
     __config__ = [("activated", "bool", "Activated", True),
                   ("use_premium", "bool", "Use premium account if available", True),
-                  ("folder_per_package", "Default;Yes;No",
-                   "Create folder for each package", "Default"),
+                  ("folder_per_package", "Default;Yes;No", "Create folder for each package", "Default"),
                   ("max_wait", "int", "Reconnect if waiting time is greater than minutes", 10)]
 
     __description__ = """Fshare.vn folder decrypter plugin"""
     __license__ = "GPLv3"
-    __authors__ = [("zoidberg", "zoidberg@mujmail.cz")]
+    __authors__ = [("zoidberg", "zoidberg@mujmail.cz"),
+                   ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
-    LINK_PATTERN = r'<li class="w_80pc"><a href="(.+?)" target="_blank">'
+    OFFLINE_PATTERN = r'404</title>'
+    LINK_PATTERN = r'<a class="filename" .+? href="(.+?)"'
+
+    URL_REPLACEMENTS = [("http://", "https://")]
--- module/plugins/hoster/FshareVn.py
+++ module/plugins/hoster/FshareVn.py
@@ -15,7 +15,7 @@ def double_decode(m):
 class FshareVn(SimpleHoster):
     __name__ = "FshareVn"
     __type__ = "hoster"
-    __version__ = "0.29"
+    __version__ = "0.30"
     __status__ = "testing"
 
     __pattern__ = r'https?://(?:www\.)?fshare\.vn/file/.+'
@@ -30,11 +30,13 @@ class FshareVn(SimpleHoster):
     __authors__ = [("zoidberg", "zoidberg@mujmail.cz"),
                    ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
-    NAME_PATTERN = r'<i class="fa fa-file-zip-o"></i>\s+?(.+?)\s+?</div>'
-    SIZE_PATTERN = r'<i class="fa fa-hdd-o"></i>\s+?(?P<S>[\d.,]+) (?P<U>[\w^_]+)\s+?</div>'
+    NAME_PATTERN = r'<i class="fa fa-file-.+?"></i>\s*(?P<N>.+?)\s*</div>'
+    SIZE_PATTERN = r'<i class="fa fa-hdd-o"></i>\s*(?P<S>[\d.,]+) (?P<U>[\w^_]+)\s*</div>'
     OFFLINE_PATTERN = ur'Tập tin quý khách yêu cầu không tồn tại.'
 
     NAME_REPLACEMENTS = [("(.*)", double_decode)]
+
+    URL_REPLACEMENTS = [("http://", "https://")]
 
     def handle_free(self, pyfile):
         action, inputs = self.parse_html_form('id="download-form"')
@@ -55,6 +57,9 @@ class FshareVn(SimpleHoster):
         err_msg = json_data.get('msg')
         if err_msg:
             self.fail(err_msg)
+
+        elif 'url' not in json_data:
+            self.fail(_("Unexpected response"))
 
         self.wait(json_data.get('wait_time', 35))
 
