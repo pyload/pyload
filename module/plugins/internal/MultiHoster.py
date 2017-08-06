4fcbf51da2dc397289e7755d85032ef3ecf7c9fa
GammaC0de <gammac0de@users.noreply.github.com>
2017-08-07 01:12:42 +0300

    [MultiHoster] Fix revert to original hoster

--- module/plugins/internal/MultiHoster.py
+++ module/plugins/internal/MultiHoster.py
@@ -9,35 +9,30 @@ from .SimpleHoster import SimpleHoster
 class MultiHoster(SimpleHoster):
     __name__ = "MultiHoster"
     __type__ = "hoster"
-    __version__ = "0.64"
+    __version__ = "0.65"
     __status__ = "stable"
 
     __pattern__ = r'^unmatchable$'
     __config__ = [("activated", "bool", "Activated", True),
                   ("use_premium", "bool", "Use premium account if available", True),
-                  ("fallback",
-                   "bool",
-                   "Fallback to free download if premium fails",
-                   False),
+                  ("fallback", "bool", "Fallback to free download if premium fails", False),
                   ("chk_filesize", "bool", "Check file size", True),
-                  ("max_wait", "int",
-                   "Reconnect if waiting time is greater than minutes", 10),
+                  ("max_wait", "int", "Reconnect if waiting time is greater than minutes", 10),
                   ("revert_failed", "bool", "Revert to standard download if fails", True)]
 
     __description__ = """Multi hoster plugin"""
     __license__ = "GPLv3"
-    __authors__ = [("Walter Purcaro", "vuolter@gmail.com")]
+    __authors__ = [("Walter Purcaro", "vuolter@gmail.com"),
+                   ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
     OFFLINE_PATTERN = r'^unmatchable$'
     TEMP_OFFLINE_PATTERN = r'^unmatchable$'
 
     DIRECT_LINK = None
     LEECH_HOSTER = False
-    LOGIN_ACCOUNT = True
 
     def init(self):
-        self.PLUGIN_NAME = self.pyload.pluginManager.hosterPlugins.get(self.classname)[
-            'name']
+        self.PLUGIN_NAME = self.pyload.pluginManager.hosterPlugins.get(self.classname)['name']
 
     def _log(self, level, plugintype, pluginname, messages):
         messages = (self.PLUGIN_NAME,) + messages
@@ -59,8 +54,8 @@ class MultiHoster(SimpleHoster):
         SimpleHoster._prepare(self)
 
         if self.DIRECT_LINK is None:
-            self.direct_dl = self.__pattern__ != r'^unmatchable$' and re.match(
-                self.__pattern__, self.pyfile.url)
+            self.direct_dl = self.__pattern__ != r'^unmatchable$' and re.match(self.__pattern__, self.pyfile.url)
+
         else:
             self.direct_dl = self.DIRECT_LINK
 
@@ -69,16 +64,12 @@ class MultiHoster(SimpleHoster):
             SimpleHoster._process(self, thread)
 
         except Fail, e:
-            if self.config.get('revert_failed', True) and \
-               self.pyload.pluginManager.hosterPlugins.get(self.classname).get('new_module'):
-                hdict = self.pyload.pluginManager.hosterPlugins.get(
-                    self.classname)
+            hdict = self.pyload.pluginManager.hosterPlugins.get(self.pyfile.pluginname, {})
+            if self.config.get('revert_failed', True) and hdict.get('new_module'):
+                tmp_module = hdict.pop('new_module', None)
+                tmp_name = hdict.pop('new_name', None)
 
-                tmp_module = hdict['new_module']
-                tmp_name = hdict['new_name']
-                hdict.pop('new_module', None)
-                hdict.pop('new_name', None)
-
+                self.pyfile.plugin = None
                 self.pyfile.initPlugin()
 
                 hdict['new_module'] = tmp_module
@@ -87,7 +78,7 @@ class MultiHoster(SimpleHoster):
                 self.restart(_("Revert to original hoster plugin"))
 
             else:
-                raise Fail(e)
+                raise
 
     def handle_premium(self, pyfile):
         return self.handle_free(pyfile)
@@ -95,5 +86,6 @@ class MultiHoster(SimpleHoster):
     def handle_free(self, pyfile):
         if self.premium:
             raise NotImplementedError
+
         else:
-            self.fail(_("Required premium account not found"))
+            self.fail(_("MultiHoster download failed"))
