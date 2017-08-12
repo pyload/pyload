32cad0f34dc955ff81e093f7c9da4f4c2b1564bc
GammaC0de <gammac0de@users.noreply.github.com>
2017-08-12 21:16:44 +0300

    [FilerNet] Fix #2928

--- module/plugins/accounts/FilerNet.py
+++ module/plugins/accounts/FilerNet.py
@@ -9,16 +9,17 @@ from ..internal.Account import Account
 class FilerNet(Account):
     __name__ = "FilerNet"
     __type__ = "account"
-    __version__ = "0.12"
+    __version__ = "0.13"
     __status__ = "testing"
 
     __description__ = """Filer.net account plugin"""
     __license__ = "GPLv3"
-    __authors__ = [("stickell", "l.stickell@yahoo.it")]
+    __authors__ = [("stickell", "l.stickell@yahoo.it"),
+                   ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
     TOKEN_PATTERN = r'name="_csrf_token" value="(.+?)"'
-    WALID_UNTIL_PATTERN = r'Der Premium-Zugang ist gültig bis (.+)\.\s*</td>'
-    TRAFFIC_PATTERN = r'Traffic</th>\s*<td>(.+?)</td>'
+    VALID_UNTIL_PATTERN = ur'Der Premium-Zugang ist gültig bis (.+)\.\s*</td>'
+    TRAFFIC_LEFT_PATTERN = r'Traffic</th>\s*<td>([\d.,]+) (?:([\w^_]+))</td>'
     FREE_PATTERN = r'Account Status</th>\s*<td>\s*Free'
 
     def grab_info(self, user, password, data):
@@ -26,23 +27,25 @@ class FilerNet(Account):
 
         #: Free user
         if re.search(self.FREE_PATTERN, html):
-            return {'premium': False, 'validuntil': None, 'trafficleft': None}
+            return {'premium': False,
+                    'validuntil': None,
+                    'trafficleft': None}
 
-        until = re.search(self.WALID_UNTIL_PATTERN, html)
-        traffic = re.search(self.TRAFFIC_PATTERN, html)
+        until = re.search(self.VALID_UNTIL_PATTERN, html)
+        traffic = re.search(self.TRAFFIC_LEFT_PATTERN, html)
 
         if until and traffic:
-            validuntil = time.mktime(
-                time.strptime(
-                    until.group(1),
-                    "%d.%m.%Y %H:%M:%S"))
-            trafficleft = self.parse_traffic(traffic.group(1))
-            return {'premium': True, 'validuntil': validuntil,
+            validuntil = time.mktime(time.strptime(until.group(1), "%d.%m.%Y, %H:%M:%S"))
+            trafficleft = self.parse_traffic(traffic.group(1), traffic.group(2))
+            return {'premium': True,
+                    'validuntil': validuntil,
                     'trafficleft': trafficleft}
 
         else:
             self.log_error(_("Unable to retrieve account information"))
-            return {'premium': False, 'validuntil': None, 'trafficleft': None}
+            return {'premium': False,
+                    'validuntil': None,
+                    'trafficleft': None}
 
     def signin(self, user, password, data):
         html = self.load("https://filer.net/login")
