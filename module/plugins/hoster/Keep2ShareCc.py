e09189e6d2e2794dedac35c9b2f4832f34acea02
GammaC0de <gammac0de@users.noreply.github.com>
2017-09-11 01:43:48 +0300

    [Keep2ShareCc] Update

--- module/plugins/hoster/Keep2ShareCc.py
+++ module/plugins/hoster/Keep2ShareCc.py
@@ -12,7 +12,7 @@ from ..internal.misc import json
 class Keep2ShareCc(SimpleHoster):
     __name__ = "Keep2ShareCc"
     __type__ = "hoster"
-    __version__ = "0.42"
+    __version__ = "0.43"
     __status__ = "testing"
 
     __pattern__ = r'https?://(?:www\.)?(keep2share|k2s|keep2s)\.cc/file/(?P<ID>\w+)'
@@ -30,7 +30,7 @@ class Keep2ShareCc(SimpleHoster):
 
     DISPOSITION = False  # @TODO: Recheck in v0.4.10
 
-    URL_REPLACEMENTS = [(__pattern__ + ".*", "https://keep2s.cc/file/\g<ID>")]
+    URL_REPLACEMENTS = [(__pattern__ + ".*", "https://k2s.cc/file/\g<ID>")]
 
     API_URL = "https://keep2share.cc/api/v2/"
     #: See https://github.com/keep2share/api
