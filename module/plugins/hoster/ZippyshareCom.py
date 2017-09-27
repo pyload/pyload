b71152d4680f61d85d92b70e66da2a1227e33e55
GammaC0de <gammac0de@users.noreply.github.com>
2017-09-27 20:55:09 +0300

    [ZippyshareCom] Update

--- module/plugins/hoster/ZippyshareCom.py
+++ module/plugins/hoster/ZippyshareCom.py
@@ -12,14 +12,13 @@ from ..internal.SimpleHoster import SimpleHoster
 class ZippyshareCom(SimpleHoster):
     __name__ = "ZippyshareCom"
     __type__ = "hoster"
-    __version__ = "0.92"
+    __version__ = "0.93"
     __status__ = "testing"
 
-    __pattern__ = r'http://www\d{0,3}\.zippyshare\.com/v(/|iew\.jsp.*key=)(?P<KEY>[\w^_]+)'
+    __pattern__ = r'http://(?P<HOST>www\d{0,3}\.zippyshare\.com)/(?:[vd]/|view\.jsp.*key=)(?P<KEY>[\w^_]+)'
     __config__ = [("activated", "bool", "Activated", True),
                   ("use_premium", "bool", "Use premium account if available", True),
-                  ("fallback", "bool",
-                   "Fallback to free download if premium fails", True),
+                  ("fallback", "bool", "Fallback to free download if premium fails", True),
                   ("chk_filesize", "bool", "Check file size", True),
                   ("max_wait", "int", "Reconnect if waiting time is greater than minutes", 10)]
 
@@ -31,12 +30,14 @@ class ZippyshareCom(SimpleHoster):
 
     COOKIES = [("zippyshare.com", "ziplocale", "en")]
 
-    NAME_PATTERN = r'(<title>Zippyshare.com - |"/)(?P<N>[^/]+)(</title>|";)'
+    URL_REPLACEMENTS = [(__pattern__ + ".*", r'http://\g<HOST>/v/\g<KEY>/file.html')]
+
+    NAME_PATTERN = r'>Name:.+?">(?P<N>.+?)<'
     SIZE_PATTERN = r'>Size:.+?">(?P<S>[\d.,]+) (?P<U>[\w^_]+)'
     OFFLINE_PATTERN = r'does not exist (anymore )?on this server<'
-    TEMP_OFFLINE_PATTERN = None
+    TEMP_OFFLINE_PATTERN = r'^unmatchable$'
 
-    LINK_PREMIUM_PATTERN = r"document.location = '(.+?)'"
+    LINK_PATTERN = r"document.location = '(.+?)'"
 
     def setup(self):
         self.chunk_limit = -1
@@ -49,7 +50,7 @@ class ZippyshareCom(SimpleHoster):
 
         if captcha_key:
             try:
-                self.link = re.search(self.LINK_PREMIUM_PATTERN, self.data)
+                self.link = re.search(self.LINK_PATTERN, self.data)
                 self.captcha.challenge()
 
             except Exception, e:
