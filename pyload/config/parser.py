ce6fd30a530dfc711399290a150ac1043e7b894d
pyblub <frau-bla1@gmx.net>
2017-10-08 00:28:48 +0100

    change default octal values to strings
    replace oct with to_str as values are already strings
    int function for base 8 conversion expects a string object

--- pyload/config/parser.py
+++ pyload/config/parser.py
@@ -44,7 +44,7 @@ convert_map = {
     InputType.Password: to_str,
     InputType.Bool: bool,
     InputType.Float: float,
-    InputType.Octal: oct,
+    InputType.Octal: to_str,
     InputType.Size: parse.bytesize,
     InputType.Address: _parse_address,
     InputType.Bytes: to_bytes,
