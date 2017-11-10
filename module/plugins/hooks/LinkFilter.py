56b81844e715eabd3ee10959013f7bda364334d8
segelkma <m.segelken@posteo.de>
2017-11-10 01:37:05 +0100

    - Changed default behaviour: Only filters hoster links on default  … - Added whitelist filter function - Added option to force execution on all links

--- module/plugins/hooks/LinkFilter.py
+++ module/plugins/hooks/LinkFilter.py
@@ -1,17 +1,21 @@
 
 from ..internal.Addon import Addon
 
+import re
+from itertools import chain
 
 class LinkFilter(Addon):
     __name__ = "LinkFilter"
     __type__ = "hook"
-    __version__ = "0.14"
+    __version__ = "0.15"
     __status__ = "testing"
 
     __config__ = [("activated", "bool", "Activated", False),
-                  ("filter", "str", "Filter links containing (seperate by comma)", "ul.to,share-online.biz")]
+                  ("filter", "str", "Filter links containing (seperate by comma)", "ul.to,uploaded.net,share-online.biz"),
+                  ("whitelist", "bool", "Execute filter as whitelist", False),
+                  ("forceExecute", "bool", "Force execution on all links (also crypters, containers...)", False)]
 
-    __description__ = "Filters all added links"
+    __description__ = "Filters all added hoster links"
     __license__ = "GPLv3"
     __authors__ = [("segelkma", None)]
 
@@ -22,18 +26,46 @@ class LinkFilter(Addon):
         self.manager.removeEvent('linksAdded', self.filter_links)
 
     def filter_links(self, links, pid):
-        filter_entries = self.config.get('filter').split(',')
+        filters = self.config.get('filter').replace(' ', '').split(',')
+        if self.config.get('whitelist'):
+            self.whitelist(links, filters)
+        else:
+            self.blacklist(links, filters)
 
-        for filter in filter_entries:
-            if filter == "":
-                break
+    def whitelist(self, links, filters):
+        linkcount = len(links)
+        links[:] = [link for link in links if
+            not self.isHosterLink(link) or
+            [True for filter in filters if link.find(filter) != -1]]
+        linkcount -= len(links)
 
+        if linkcount > 0:
+            linkstring = '' if self.config.get('forceExecute') else 'hoster '
+            linkstring += 'links' if linkcount == 1 else 'links'
+            self.log_info(
+                'Whitelist filter removed %s %s not containing (%s)' % 
+                (linkcount, linkstring, ', '.join(filters)))
+
+    def blacklist(self, links, filters):
+        for filter in filters:
             linkcount = len(links)
-            links[:] = [link for link in links if link.find(filter) == -1]
+            links[:] = [link for link in links if
+                not self.isHosterLink(link) or
+                link.find(filter) == -1]
             linkcount -= len(links)
 
             if linkcount > 0:
-                linkstring = 'links' if linkcount > 1 else 'link'
+                linkstring = '' if self.config.get('forceExecute') else 'hoster '
+                linkstring += 'link' if linkcount == 1 else 'links'
                 self.log_info(
-                    "Removed %s %s containing %s" %
+                    'Blacklist filter removed %s %s containing %s' %
                     (linkcount, linkstring, filter))
+
+    def isHosterLink(self, link):
+        #declare all links as hoster links so the filter will work on all links
+        if self.config.get('forceExecute'):
+            return True
+        for name, value in chain(self.pyload.pluginManager.hosterPlugins.iteritems()):
+            if value['re'].match(link):
+                return True
+        return False
