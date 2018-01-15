14733a8e4dcd2bbe878b8f2494ae2cd998ee7f0a
yifanchen <yifanchen@synology.com>
2018-01-15 17:47:54 +0800

    [DailymotionCom] Able to identify URLs with domain "dai.ly".

--- module/plugins/hoster/DailymotionCom.py
+++ module/plugins/hoster/DailymotionCom.py
@@ -45,10 +45,10 @@ def get_info(urls):
 class DailymotionCom(Hoster):
     __name__ = "DailymotionCom"
     __type__ = "hoster"
-    __version__ = "0.28"
+    __version__ = "0.29"
     __status__ = "testing"
 
-    __pattern__ = r'https?://(?:www\.)?dailymotion\.com/.*video/(?P<ID>[\w^_]+)'
+    __pattern__ = r'https?://(?:www\.)?(dailymotion\.com/.*video|dai.ly)/(?P<ID>[\w^_]+)'
     __config__ = [("activated", "bool", "Activated", True),
                   ("quality", "Lowest;LD 144p;LD 240p;SD 384p;HQ 480p;HD 720p;HD 1080p;Highest", "Quality", "Highest")]
 
