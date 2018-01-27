47a5c93453b5ae8405b2103b6bda6fbad17725bb
OzzieIsaacs <ozzie.fernandez.isaacs@gmail.com>
2018-01-27 18:54:06 +0100

    Added missing string for translation updated german translation

--- module/web/media/js/pyplex/settings.js
+++ module/web/media/js/pyplex/settings.js
@@ -66,7 +66,7 @@ SettingsUI = (function() {
             }
         }
 
-        searchInput.attr('placeholder', 'Name of plugin');
+        searchInput.attr('placeholder', {{_('Name of plugin')}};
         searchInput.removeAttr('disabled');
         searchInput.on('input', function () {
             var query = searchInput.val();
