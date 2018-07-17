4cdb6013b434d15e94926b0e40e5de2423b780de
GammaC0de <gammac0de@users.noreply.github.com>
2018-07-17 22:46:40 +0300

    [RealdebridCom] Revert #3174 due to captcha requests (2) (fix #3209)

--- module/plugins/hoster/RealdebridCom.py
+++ module/plugins/hoster/RealdebridCom.py
@@ -11,25 +11,20 @@ def args(**kwargs):
 class RealdebridCom(MultiHoster):
     __name__ = "RealdebridCom"
     __type__ = "hoster"
-    __version__ = "0.77"
+    __version__ = "0.78"
     __status__ = "testing"
 
     __pattern__ = r'https?://((?:www\.|s\d+\.)?real-debrid\.com/dl?/|[\w^_]\.rdb\.so/d/)[\w^_]+'
     __config__ = [("use_premium", "bool", "Use premium account if available", True),
-                  ("fallback",
-                   "bool",
-                   "Fallback to free download if premium fails",
-                   False),
-                  ("revert_failed", "bool",
-                   "Revert to standard download if fails", True),
+                  ("fallback", "bool", "Fallback to free download if premium fails", False),
+                  ("revert_failed", "bool", "Revert to standard download if fails", True),
                   ("chk_filesize", "bool", "Check file size", True),
                   ("max_wait", "int", "Reconnect if waiting time is greater than minutes", 10)]
 
     __description__ = """Real-Debrid.com multi-hoster plugin"""
     __license__ = "GPLv3"
     __authors__ = [("Devirex Hazzard", "naibaf_11@yahoo.de"),
-                   ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com"),
-                   ("Synology PAT", "pat@synology.com")]
+                   ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
     API_URL = "https://api.real-debrid.com/rest/1.0"
 
@@ -43,11 +38,11 @@ class RealdebridCom(MultiHoster):
 
     def handle_premium(self, pyfile):
         user = self.account.accounts.keys()[0]
-        api_token = self.account.accounts[user]['auth_token']
+        api_token = self.account.accounts[user]["password"]
 
         data = self.api_response("/unrestrict/link",
                                  args(auth_token=api_token),
-                                 args(link=pyfile.url))
+                                 args(link=pyfile.url, password=self.get_password()))
 
         self.log_debug("Returned Data: %s" % data)
 
