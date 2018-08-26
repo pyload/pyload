c05610d1999ca9af18260fd3286c234a76d880f3
GammaC0de <gammac0de@users.noreply.github.com>
2018-08-26 23:32:55 +0300

    [TamperMonkey script] Better check

--- module/web/media/js/captcha-interactive.user.js
+++ module/web/media/js/captcha-interactive.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pyLoad Script for Interactive Captcha
 // @namespace    https://pyload.net/
-// @version      0.16
+// @version      0.17
 // @author       Michi-F, GammaC0de
 // @description  pyLoad Script for Interactive Captcha
 // @homepage     https://github.com/pyload/pyload
@@ -41,7 +41,7 @@
     window.addEventListener('message', function(e) {
         var request = JSON.parse(e.data);
 
-        if(typeof(request) === "object" && request.actionCode === "pyloadActivateInteractive") {
+        if(request.constructor === {}.constructor && request.actionCode === "pyloadActivateInteractive") {
             if (request.params.script) {
                 var sig = new KJUR.crypto.Signature({"alg": "SHA384withRSA", "prov": 'cryptojs/jsrsa'});
                 sig.init("-----BEGIN PUBLIC KEY-----\n" +
