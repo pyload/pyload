ecc82ff5ac0daab063f60aa6fcd81867e2180906
GammaC0de <gammac0de@users.noreply.github.com>
2018-11-18 21:50:24 +0200

    [Hoster] Fix https://github.com/pyload/pyload/issues/3273#issuecomment-439703436

--- module/plugins/internal/Hoster.py
+++ module/plugins/internal/Hoster.py
@@ -33,7 +33,7 @@ if not hasattr(__builtin__.property, "setter"):
 class Hoster(Base):
     __name__ = "Hoster"
     __type__ = "hoster"
-    __version__ = "0.73"
+    __version__ = "0.74"
     __status__ = "stable"
 
     __pattern__ = r'^unmatchable$'
@@ -506,11 +506,12 @@ class Hoster(Base):
 
         else:
             # Same file exists but it does not belongs to our pack, add a trailing counter
-            m = re.match(r'(.+?)(?: \((\d+)\))?(\..+)?$', self.pyfile.name)
+            name, ext = os.path.splitext(self.pyfile.name)
+            m = re.match(r'(.+?)(?:\((\d+)\))?$', name)
             dl_n = int(m.group(2) or "0")
 
             while True:
-                name = "%s (%s)%s" % (m.group(1), dl_n + 1, m.group(3) or "")
+                name = "%s (%s)%s" % (m.group(1), dl_n + 1, ext)
                 dl_file = fsjoin(dl_folder, pack_folder, name)
                 if not exists(dl_file):
                     break
