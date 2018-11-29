d806432dd93ce196f8d46ebe3db56bf330be1444
GammaC0de <gammac0de@users.noreply.github.com>
2018-11-29 05:08:13 +0200

    [MultiUpOrg] Fix #3278

--- module/plugins/crypter/MultiUpOrg.py
+++ module/plugins/crypter/MultiUpOrg.py
@@ -11,14 +11,13 @@ from ..captcha.ReCaptcha import ReCaptcha
 class MultiUpOrg(SimpleCrypter):
     __name__ = "MultiUpOrg"
     __type__ = "crypter"
-    __version__ = "0.12"
+    __version__ = "0.13"
     __status__ = "testing"
 
     __pattern__ = r'https?://(?:www\.)?multiup\.(?:org|eu)/(?:en/|fr/)?(?:(?P<TYPE>project|download|mirror)/)?\w+(?:/\w+)?'
     __config__ = [("activated", "bool", "Activated", True),
                   ("use_premium", "bool", "Use premium account if available", True),
-                  ("folder_per_package", "Default;Yes;No",
-                   "Create folder for each package", "Default"),
+                  ("folder_per_package", "Default;Yes;No", "Create folder for each package", "Default"),
                   ("max_wait", "int", "Reconnect if waiting time is greater than minutes", 10),
                   ("hosts_priority", "str", "Prefered hosts priority (bar-separated)", ""),
                   ("ignored_hosts", "str", "Ignored hosts list (bar-separated)", ""),
@@ -31,11 +30,12 @@ class MultiUpOrg(SimpleCrypter):
     NAME_PATTERN = r'<title>.*(?:Project|Projet|ownload|élécharger) (?P<N>.+?) (\(|- )'
     OFFLINE_PATTERN = r'File not found'
     TEMP_OFFLINE_PATTERN = r'^unmatchable$'
+    DIRECT_LINK = False
 
-    URL_REPLACEMENTS = [(r'https?://(?:www\.)?multiup\.(?:org|eu)/', "http://www.multiup.eu/"),
+    URL_REPLACEMENTS = [(r'https?://(?:www\.)?multiup\.(?:org|eu)/', "http://www.multiup.org/"),
                         (r'/fr/', "/en/")]
 
-    COOKIES = [("multiup.eu", "_locale", "en")]
+    COOKIES = [("multiup.org", "_locale", "en")]
 
     def get_links(self):
         m_type = self.info['pattern']['TYPE']
@@ -44,39 +44,17 @@ class MultiUpOrg(SimpleCrypter):
         grab_all = self.config.get('grab_all')
 
         if m_type == "project":
-            return re.findall(r'\n(http://www\.multiup\.eu/(?:en|fr)/download/.*)', self.data)
+            return re.findall(r'\n(http://www\.multiup\.org/(?:en|fr)/download/.*)', self.data)
 
         elif m_type in ("download", None):
-            recaptcha = ReCaptcha(self.pyfile)
-            captcha_key = recaptcha.detect_key()
-            if captcha_key is not None:
-                self.captcha = recaptcha
-                url, inputs =  self.parse_html_form()
-                mirror_page = urlparse.urljoin("http://www.multiup.eu/", url)
-                try:
-                    response, challenge = recaptcha.challenge(captcha_key)
-                except BadHeader, e:
-                    if e.code == 400 and "Please enable JavaScript to get a reCAPTCHA challenge" in e.content:
-                        self.log_warning(_("Unsupported reCAPTCHA, retrying"))
-                        self.retry()
-
-                    else:
-                        raise
-
-                else:
-                    inputs['g-recaptcha-response'] = response
-                    self.data = self.load(mirror_page, post=inputs)
-
-            else:
-                dl_url = re.search(r'href="(.*)">.*\n.*<h5>DOWNLOAD</h5>', self.data).group(1)
-                mirror_page = urlparse.urljoin("http://www.multiup.eu/", dl_url)
-                self.data = self.load(mirror_page)
-
-        self.check_errors()
+            url, inputs = self.parse_html_form()
+            if inputs is not None:
+                self.data = self.load(urlparse.urljoin("http://www.multiup.org/", url),
+                                      post=inputs)
 
         hosts_data = {}
-        for _a in re.findall(r'<a\s*class="btn btn-small disabled link host"(.+?)/a>', self.data, re.S):
-            validity = re.search(r'validity=(\w+)', _a).group(1)
+        for _a in re.findall(r'<a (.+?) class="host btn btn-md btn-default btn-block btn-3d hvr-bounce-to-right">', self.data, re.M):
+            validity = re.search(r'validity="(\w+)"', _a).group(1)
             if validity in ("valid", "unknown"):
                 host = re.search(r'nameHost="(.+?)"', _a).group(1)
                 url = re.search(r'href="(.+?)"', _a).group(1)
