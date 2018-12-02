1834937ca1c849ead4f686376aba638797ecaba2
GammaC0de <gammac0de@users.noreply.github.com>
2018-12-02 03:18:44 +0200

    [FilejokerNet] Fix #3284

--- module/plugins/hoster/FilejokerNet.py
+++ module/plugins/hoster/FilejokerNet.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import pycurl
+import re
+import urlparse
 
+from ..captcha.ReCaptcha import ReCaptcha
 from ..internal.XFSHoster import XFSHoster
 
 
 class FilejokerNet(XFSHoster):
     __name__ = "FilejokerNet"
     __type__ = "hoster"
-    __version__ = "0.04"
+    __version__ = "0.05"
     __status__ = "testing"
 
     __pattern__ = r'https?://(?:www\.)?filejoker\.net/\w{12}'
@@ -24,7 +28,7 @@ class FilejokerNet(XFSHoster):
     PLUGIN_DOMAIN = "filejoker.net"
 
     WAIT_PATTERN = r'Please [Ww]ait (?:<span id="count" class="alert-success">)?([\w ]+?)(?:</span> seconds</p>| until the next download)'
-    ERROR_PATTERN = r'Wrong Captcha'
+    ERROR_PATTERN = r'Wrong Captcha|Session expired'
 
     PREMIUM_ONLY_PATTERN = 'Free Members can download files no bigger'
 
@@ -32,3 +36,35 @@ class FilejokerNet(XFSHoster):
     SIZE_REPLACEMENTS = [('Kb', 'KB'), ('Mb', 'MB'), ('Gb', 'GB')]
 
     LINK_PATTERN = r'<div class="premium-download">\s+<a href="(.+?)"'
+
+
+    def handle_captcha(self, inputs):
+        m = re.search(r'\$\.post\( "/ddl",\s*\{(.+?) \} \);', self.data)
+        if m is not None:
+            recaptcha = ReCaptcha(self.pyfile)
+            captcha_key = recaptcha.detect_key()
+            if captcha_key:
+                self.captcha = recaptcha
+                response, _ = recaptcha.challenge(captcha_key)
+
+                captcha_inputs = {}
+                for _i in m.group(1).split(','):
+                    _k, _v = _i.split(':', 1)
+                    _k = _k.strip('" ')
+                    if _k == "g-recaptcha-response":
+                        _v = response
+
+                    captcha_inputs[_k] = _v.strip('" ')
+
+                self.req.http.c.setopt(pycurl.HTTPHEADER, ["X-Requested-With: XMLHttpRequest"])
+
+                html = self.load(urlparse.urljoin(self.pyfile.url, "/ddl"),
+                                 post=captcha_inputs)
+
+                self.req.http.c.setopt(pycurl.HTTPHEADER, ["X-Requested-With:"])
+
+                if html == "OK":
+                    self.captcha.correct()
+
+                else:
+                    self.retry_captcha()
