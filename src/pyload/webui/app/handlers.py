5f7d57b6d942b0e1c97c0ae571d3df5d8f970e3d
Walter Purcaro <vuolter@gmail.com>
2018-12-15 01:50:48 +0100

    Error template

--- src/pyload/webui/app/handlers.py
+++ src/pyload/webui/app/handlers.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 
+import traceback
+import sys
 import flask
-from .helpers import render_base
+from .helpers import render_template
 
 
 def handle_error(exc):
     try:
         code = exc.code
         desc = exc.desc
+        tb = None
     except AttributeError:
         code = 500
         desc = exc
+        tb = traceback.format_exception(*sys.exc_info())
         
     flask.current_app.logger.debug(exc, exc_info=True)
     
-    messages = ["Sorry, something went wrong... :(", f"Error {code}: {desc}"]
-    return render_base(messages), code
+    messages = [f"Error {code}: {desc}", tb]
+    return render_template("error.html", messages=messages), code
             
             
 ERROR_HANDLERS = [(Exception, handle_error)]
