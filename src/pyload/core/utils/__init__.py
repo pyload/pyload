66c8b3354556c2941d1c8723e16f042c16243d10
Walter Purcaro <vuolter@gmail.com>
2018-12-17 00:39:01 +0100

    Update utils.py

--- src/pyload/core/utils/__init__.py
+++ src/pyload/core/utils/__init__.py
@@ -15,6 +15,19 @@ import string
 import sys
 import time
 from html.entities import name2codepoint
+import html.parser
+import unicodedata
+import datetime
+from datetime import timedelta
+import shutil
+import urllib.parse
+from functools import partial, wraps
+from .. import exc_logger
+
+try:
+    import send2trash
+except ImportError:
+    send2trash = None
 
 
 def invertmap(obj):
@@ -35,97 +48,167 @@ def chmod(*args):
     except Exception:
         pass
 
+        
+# Hotfix UnicodeDecodeError: 'ascii' codec can't decode..
+def normalize(value):
+    return unicodedata.normalize("NFKD", value).encode("ascii", "ignore")
 
+
+# NOTE: Revert to `decode` in Python 3
 def decode(value):
     """
-    decode string with utf if possible.
+    Encoded string (default to own system encoding) -> unicode string.
     """
     try:
-        return value.decode("utf-8", "replace")
-    except Exception:
-        return value
+        return str(res)
+    except UnicodeEncodeError:
+        return normalize(res)
 
 
 def remove_chars(value, repl):
     """
-    os.removes all chars in repl from string.
+    Removes all chars in repl from string.
     """
-    if isinstance(value, str):
-        return str.translate(str.maketrans("", ""), repl)
-    elif isinstance(value, str):
-        return str.translate({ord(s): None for s in repl})
+    for char in repl:
+        value = value.replace(char, "")
+    return value
 
 
-def save_path(name):
-    # os.remove some chars
+# def save_path(name):
+    # # os.remove some chars
+    # if os.name == "nt":
+        # return remove_chars(name, '/\\?%*:|"<>')
+    # else:
+        # return remove_chars(name, '/\\"')
+
+
+# def save_join(*args):
+    # """
+    # joins a path, encoding aware.
+    # """
+    # return fs_encode(
+        # os.path.join(*[x if isinstance(x, str) else decode(x) for x in args])
+    # )
+
+
+def truncate(name, length):
+    max_trunc = len(name) // 2
+    if length > max_trunc:
+        raise OSError("File name too long")
+
+    trunc = (len(name) - length) // 3
+    return "{}~{}".format(name[: trunc * 2], name[-trunc:])
+
+
+# TODO: Recheck in 0.6.x
+def safepath(value):
+    """
+    Remove invalid characters and truncate the path if needed.
+    """
     if os.name == "nt":
-        return remove_chars(name, '/\\?%*:|"<>')
+        unt, value = os.path.splitunc(value)
     else:
-        return remove_chars(name, '/\\"')
-
-
-def save_join(*args):
-    """
-    joins a path, encoding aware.
-    """
-    return fs_encode(
-        os.path.join(*[x if isinstance(x, str) else decode(x) for x in args])
+        unt = ""
+    drive, filename = os.path.splitdrive(value)
+    filename = os.path.join(
+        os.sep if os.path.isabs(filename) else "",
+        *list(map(safename, filename.split(os.sep))),
     )
+    path = unt + drive + filename
+
+    try:
+        if os.name != "nt":
+            return
+
+        length = len(path) - 259
+        if length < 1:
+            return
+
+        dirname, basename = os.path.split(filename)
+        name, ext = os.path.splitext(basename)
+        path = unt + drive + dirname + truncate(name, length) + ext
+
+    finally:
+        return path
 
 
-# File System Encoding functions:
-# Use fs_encode before accesing files on disk, it will encode the string properly
+def safejoin(*args):
+    """
+    os.path.join + safepath.
+    """
+    return safepath(os.path.join(*args))
 
-if sys.getfilesystemencoding().startswith("ANSI"):
 
-    def fs_encode(value):
-        try:
-            value = str.encode("utf-8")
-        finally:
-            return value
-
-    fs_decode = decode  #: decode utf-8
-
-else:
-    fs_encode = fs_decode = lambda x: x  #: do nothing
+def safename(value):
+    """
+    Remove invalid characters.
+    """
+    repl = '<>:"/\\|?*' if os.name == "nt" else '\0/\\"'
+    name = remove_chars(value, repl)
+    return name
 
 
 def compare_time(start, end):
-    start = list(map(int, start))
-    end = list(map(int, end))
+    start = map(int, start)
+    end = map(int, end)
 
     if start == end:
         return True
 
     now = list(time.localtime()[3:5])
-    if start < now < end:
+
+    if start < end:
+        if now < end:
+            return True
+
+    elif now > start or now < end:
         return True
-    elif start > end and (now > start or now < end):
-        return True
-    elif start < now > end < start:
-        return True
-    else:
-        return False
+
+    return False
 
 
-def formatSize(size):
+def format_time(value):
+    dt = datetime.datetime(1, 1, 1) + timedelta(seconds=abs(int(value)))
+    days = ("{} days".format(dt.day - 1)) if dt.day > 1 else ""
+    tm = ", ".join(
+        "{} {}s".format(getattr(dt, attr), attr)
+        for attr in ("hour", "minute", "second")
+        if getattr(dt, attr)
+    )
+    return days + (" and " if days and tm else "") + tm
+
+
+def format_size(value):
     """
-    formats size of bytes.
+    formats size of bytes
     """
-    size = int(size)
-    steps = 0
-    sizes = ["B", "KiB", "MiB", "GiB", "TiB"]
-    while size > 1000:
-        size >>= 10
-        steps += 1
-    return "{:.2f} {}".format(size, sizes[steps])
+    for unit in ("B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"):
+        if abs(value) < 1 << 10:
+            return "{:3.2f} {}".format(value, unit)
+        else:
+            value >>= 10
+    return "{:.2f} {}".format(value, "YiB")
 
 
 def formatSpeed(speed):
-    return formatSize(speed) + "/s"
+    return format_size(speed) + "/s"
 
 
-def freeSpace(folder):
+# def freeSpace(folder):
+    # if os.name == "nt":
+        # import ctypes
+
+        # free_bytes = ctypes.c_ulonglong(0)
+        # ctypes.windll.kernel32.GetDiskFreeSpaceExW(
+            # ctypes.c_wchar_p(folder), None, None, ctypes.pointer(free_bytes)
+        # )
+        # return free_bytes.value
+    # else:
+        # s = os.statvfs(folder)
+        # return s.f_bsize * s.f_bavail
+
+
+def free_space(folder):
     if os.name == "nt":
         import ctypes
 
@@ -134,90 +217,177 @@ def freeSpace(folder):
             ctypes.c_wchar_p(folder), None, None, ctypes.pointer(free_bytes)
         )
         return free_bytes.value
+
     else:
         s = os.statvfs(folder)
-        return s.f_bsize * s.f_bavail
+        return s.f_frsize * s.f_bavail
 
 
-def uniqify(seq, idfun=lambda x: x):
-    # order preserving
-    seen = {}
-    result = []
-    for item in seq:
-        marker = idfun(item)
-        # in old Python versions:
-        # if seen.has_key(marker)
-        # but in new ones:
-        if marker in seen:
-            continue
-        seen[marker] = 1
-        result.append(item)
-    return result
+def uniqify(seq):
+    """
+    Remove duplicates from list preserving order Originally by Dave Kirby.
+    """
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if x not in seen and not seen_add(x)]
 
 
-def parseFileSize(value, unit=None):  #: returns bytes
-    if not unit:
-        m = re.match(r"(\d*[\.,]?\d+)(.*)", value.strip().lower())
-        if m:
-            traffic = float(m.group(1).replace(",", "."))
-            unit = m.group(2)
-        else:
-            return 0
-    else:
-        if isinstance(value, str):
-            traffic = float(value.replace(",", "."))
-        else:
-            traffic = value
+# def parseFileSize(value, unit=None):  #: returns bytes
+    # if not unit:
+        # m = re.match(r"(\d*[\.,]?\d+)(.*)", value.strip().lower())
+        # if m:
+            # traffic = float(m.group(1).replace(",", "."))
+            # unit = m.group(2)
+        # else:
+            # return 0
+    # else:
+        # if isinstance(value, str):
+            # traffic = float(value.replace(",", "."))
+        # else:
+            # traffic = value
 
-    # ignore case
-    unit = unit.lower().strip()
+    # # ignore case
+    # unit = unit.lower().strip()
 
-    if unit in ("gb", "gig", "gbyte", "gigabyte", "gib", "g"):
-        traffic *= 1 << 30
-    elif unit in ("mb", "mbyte", "megabyte", "mib", "m"):
-        traffic *= 1 << 20
-    elif unit in ("kb", "kib", "kilobyte", "kbyte", "k"):
-        traffic *= 1 << 10
+    # if unit in ("gb", "gig", "gbyte", "gigabyte", "gib", "g"):
+        # traffic *= 1 << 30
+    # elif unit in ("mb", "mbyte", "megabyte", "mib", "m"):
+        # traffic *= 1 << 20
+    # elif unit in ("kb", "kib", "kilobyte", "kbyte", "k"):
+        # traffic *= 1 << 10
 
-    return traffic
+    # return traffic
 
 
-def lock(func):
-    def new(*args):
-        # print(f"Handler: func} args: {args[1:]}")
-        args[0].lock.acquire()
+# TODO: Change 'trash' to False because send2trash is optional now
+def remove(path, trash=True):
+    path = encode(path)
+
+    if not exists(path):
+        return
+
+    if trash:
         try:
-            return func(*args)
+            send2trash.send2trash(path)
+        except AttributeError as exc:
+            exc_logger.exception(exc)
+
+    elif os.path.isdir(path):
+        shutil.rmtree(path, ignore_errors=True)
+
+    else:
+        os.remove(path)
+
+
+def fixurl(url, unquote=None):
+    old = url
+    url = urllib.parse.unquote(url)
+
+    if unquote is None:
+        unquote = url is old
+
+    url = decode(url)
+    try:
+        url = url.decode("unicode-escape")
+    except UnicodeDecodeError:
+        pass
+
+    url = html_unescape(url)
+    url = re.sub(r"(?<!:)/{2,}", "/", url).strip().lstrip(".")
+
+    if not unquote:
+        url = urllib.parse.quote(url)
+
+    return url
+    
+    
+def parse_name(value, safechar=True):
+    path = fixurl(decode(value), unquote=False)
+    url_p = urllib.parse.urlparse(path.rstrip("/"))
+    name = (
+        url_p.path.split("/")[-1]
+        or url_p.query.split("=", 1)[::-1][0].split("&", 1)[0]
+        or url_p.netloc.split(".", 1)[0]
+    )
+
+    name = urllib.parse.unquote(name)
+    return safename(name) if safechar else name
+
+
+def parse_size(value, unit=""):  #: returns bytes
+    m = re.match(r"((?:[\d.,]*)\d)\s*([\w^_]*)", str(value).lower())
+
+    if m is None:
+        return 0
+
+    if re.match(r"\d{1,3}(?:,\d{3})+(?:\.\d+)?$", m.group(1)):
+        size = float(m.group(1).replace(",", ""))
+
+    elif re.match(r"\d+,\d{2}$", m.group(1)):
+        size = float(m.group(1).replace(",", "."))
+
+    elif re.match(r"\d+(?:\.\d+)?$", m.group(1)):
+        size = float(m.group(1))
+
+    else:
+        return 0  #: Unknown format
+
+    unit = (unit.strip().lower() or m.group(2) or "byte")[0]
+
+    if unit == "b":
+        return int(size)
+
+    sizeunits = ("b", "k", "m", "g", "t", "p", "e", "z", "y")
+    sizemap = {u: i * 10 for i, u in enumerate(sizeunits)}
+    magnitude = sizemap[unit]
+
+    i, d = divmod(size, 1)
+    integer = int(i) << magnitude
+    decimal = int(d * (1 << 10 ** (magnitude // 10)))
+
+    return integer + decimal
+
+
+def lock(func=None, *decor_args, **decor_kwargs):
+    if func is None:
+        return partial(lock, *decor_args, **decor_kwargs)
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        self.lock.acquire(*decor_args, **decor_kwargs)
+        try:
+            return func(self, *args, **kwargs)
         finally:
-            args[0].lock.release()
+            self.lock.release()
 
-    return new
+    return wrapper
 
 
-def fixup(m):
-    text = m.group(0)
-    if text[:2] == "&#":
-        # character reference
-        try:
-            if text[:3] == "&#x":
-                return chr(int(text[3:-1], 16))
-            else:
-                return chr(int(text[2:-1]))
-        except ValueError:
-            pass
-    else:
-        # named entity
-        try:
-            name = text[1:-1]
-            text = chr(name2codepoint[name])
-        except KeyError:
-            pass
+# def fixup(m):
+    # text = m.group(0)
+    # if text[:2] == "&#":
+        # # character reference
+        # try:
+            # if text[:3] == "&#x":
+                # return chr(int(text[3:-1], 16))
+            # else:
+                # return chr(int(text[2:-1]))
+        # except ValueError:
+            # pass
+    # else:
+        # # named entity
+        # try:
+            # name = text[1:-1]
+            # text = chr(name2codepoint[name])
+        # except KeyError:
+            # pass
 
-    return text  #: leave as is
+    # return text  #: leave as is
 
 
 def html_unescape(text):
     """
     Removes HTML or XML character references and entities from a text string.
     """
-    return re.sub(r"&#?\w+;", fixup, text)
+    h = html.parser.HTMLParser()
+    return h.unescape(text)
