50a8dde69bfa042dca7c08693ed41c91b8766b9a
Yann <yann.jouanique@gmail.com>
2019-03-31 08:56:33 +0700

    Libgen Comics Plugin

---
+++ module/plugins/hoster/LibGenComics.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from .Http import Http
+
+
+class LibGenComics(Http):
+    __name__ = "LibGenComics"
+    __type__ = "hoster"
+    __version__ = "0.10"
+    __status__ = "testing"
+
+    __pattern__ = r'https?://libgen.io/comics0/.+\.(cbr|cbz|zip|rar|pdf)'
+    __config__ = [("activated", "bool", "Activated", True)]
+
+    __description__ = """Download comics from libgen.io respecting throttling limits"""
+    __license__ = "GPLv3"
+    __authors__ = [("Yann Jouanique", "yann.jouanique@gmail.com")]
+
+    def setup(self):
+        self.chunk_limit = -1
+        self.resume_download = True
+        self.multiDL = False
+        self.multi_dl = False
+        self.limitDL = 1
+        self.limit_dl = 1
+
