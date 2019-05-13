06a562db141f6e556a2b63ef77fcc432173940a1
GammaC0de <gammac0de@users.noreply.github.com>
2019-05-13 23:54:28 +0300

    [New hoster] AnonfileCom (Fix #3383)

---
+++ module/plugins/hoster/AnonfileCom.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from ..internal.SimpleHoster import SimpleHoster
+
+
+class AnonfileCom(SimpleHoster):
+    __name__ = "AnonfileCom"
+    __type__ = "hoster"
+    __version__ = "0.01"
+    __status__ = "testing"
+
+    __pattern__ = r'https?://(?:www\.)?anonfile\.com/\w+'
+    __config__ = [("activated", "bool", "Activated", True),
+                  ("use_premium", "bool", "Use premium account if available", True),
+                  ("fallback", "bool", "Fallback to free download if premium fails", True),
+                  ("chk_filesize", "bool", "Check file size", True),
+                  ("max_wait", "int", "Reconnect if waiting time is greater than minutes", 10)]
+
+    __description__ = """Anonfile.com hoster plugin"""
+    __license__ = "GPLv3"
+    __authors__ = [("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
+
+    NAME_PATTERN = r'class="text-center text-wordwrap">(?P<N>.+?)<'
+    SIZE_PATTERN = r'Download\s*\((?P<S>[\d.,]+) (?P<U>[\w^_]+)\)'
+
+    LINK_PATTERN = r'href="(https://cdn-\d+.anonfile.com/.+?)"'
+
+    def setup(self):
+        self.multiDL = True
+        self.resume_download = True
+        self.chunk_limit = -1
+
