a03a5d6f9befa106467d1c4613b6780f858a0ae4
snilt <snilt@users.noreply.github.com>
2019-09-14 14:36:42 +0200

    Internal version bump

--- pyLoadGui.py
+++ pyLoadGui.py
@@ -18,7 +18,7 @@
     @author: mkaay
 """
 CURRENT_VERSION = '0.4.9'
-CURRENT_INTERNAL_VERSION = 'Development version'         # YYYY-MM-DD, append a lowercase letter for a new version on the same day
+CURRENT_INTERNAL_VERSION = '2019-09-14'         # YYYY-MM-DD, append a lowercase letter for a new version on the same day
 
 import os
 import sys
