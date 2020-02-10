993468ea443d7ce18d535b6c6b4bc95d2e7e98de
GammaC0de <gammac0de@users.noreply.github.com>
2020-02-10 22:24:18 +0200

    [HighWayMe] Fix #3511

--- module/plugins/accounts/HighWayMe.py
+++ module/plugins/accounts/HighWayMe.py
@@ -5,9 +5,9 @@ from ..internal.MultiAccount import MultiAccount
 
 
 class HighWayMe(MultiAccount):
-    __name__ = "HighWayMe.py"
+    __name__ = "HighWayMe"
     __type__ = "account"
-    __version__ = "0.10"
+    __version__ = "0.11"
     __status__ = "testing"
 
     __config__ = [("mh_mode", "all;listed;unlisted", "Filter hosters to use", "all"),
@@ -16,11 +16,21 @@ class HighWayMe(MultiAccount):
 
     __description__ = """High-Way.me account plugin"""
     __license__ = "GPLv3"
-    __authors__ = [("EvolutionClip", "evolutionclip@live.de")]
+    __authors__ = [("EvolutionClip", "evolutionclip@live.de"),
+                   ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
+
+    API_URL = "https://high-way.me/api.php"
+
+    def api_response(self, method, **kwargs):
+        post=dict([(k.lstrip('_'), v) for k,v in kwargs.items()])
+        json_data = self.load(self.API_URL,
+                              get={method: ''},
+                              post=post)
+        return json.loads(json_data)
 
     def grab_hosters(self, user, password, data):
-        html = self.load("https://high-way.me/api.php", get={'hoster': 1})
-        json_data = json.loads(html)
+        json_data = self.api_response("hoster")
+
         return [element['name'] for element in json_data['hoster']]
 
     def grab_info(self, user, password, data):
@@ -28,21 +38,15 @@ class HighWayMe(MultiAccount):
         validuntil = -1
         trafficleft = None
 
-        json_data = self.load('https://high-way.me/api.php?user')
-
-        self.log_debug("JSON data: %s" % json_data)
-
-        json_data = json.loads(json_data)
+        json_data = self.api_response("user")
 
         if 'premium' in json_data['user'] and json_data['user']['premium']:
             premium = True
 
-        if 'premium_bis' in json_data[
-                'user'] and json_data['user']['premium_bis']:
+        if 'premium_bis' in json_data['user'] and json_data['user']['premium_bis']:
             validuntil = float(json_data['user']['premium_bis'])
 
-        if 'premium_traffic' in json_data[
-                'user'] and json_data['user']['premium_traffic']:
+        if 'premium_traffic' in json_data['user'] and json_data['user']['premium_traffic']:
             # @TODO: Remove `/ 1024` in 0.4.10
             trafficleft = float(json_data['user']['premium_traffic']) / 1024
 
@@ -51,10 +55,7 @@ class HighWayMe(MultiAccount):
                 'trafficleft': trafficleft}
 
     def signin(self, user, password, data):
-        html = self.load("https://high-way.me/api.php?login",
-                         post={'login': '1',
-                               'user': user,
-                               'pass': password})
+        json_data = self.api_response("login", user=user, _pass=password)
 
-        if 'UserOrPassInvalid' in html:
+        if not json_data.get('loggedin', False):
             self.fail_login()
