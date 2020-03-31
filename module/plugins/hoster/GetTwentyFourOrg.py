edfd6232cc2f635dd60ee1d3553c2dcc139333b5
Piotr Staroszczyk <oczkers@gmail.com>
2020-03-31 16:35:24 +0200

    [GetTwentyFourOrg] api changes (#3563)

--- module/plugins/hoster/GetTwentyFourOrg.py
+++ module/plugins/hoster/GetTwentyFourOrg.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pycurl
+import json
 
 from ..internal.MultiHoster import MultiHoster
 
@@ -8,7 +9,7 @@ from ..internal.MultiHoster import MultiHoster
 class GetTwentyFourOrg(MultiHoster):
     __name__ = 'GetTwentyFourOrg'
     __type__ = 'hoster'
-    __version__ = '0.03'
+    __version__ = '0.04'
     __status__ = 'testing'
 
     __pattern__ = r'^unmatchable$'
@@ -31,5 +32,17 @@ class GetTwentyFourOrg(MultiHoster):
         )
         post = {'email': self.account.user,
                 'passwd_sha256': self.account.info['data']['passwd_sha256'],
-                'url': pyfile.url}
-        self.download('%s/download' % self.API_URL, post=post)
+                'link': pyfile.url}
+        rc = json.loads(self.load('%s/debrid/geturl' % self.API_URL, post=post))
+        if rc.get('ok') is True:
+            pyfile.name = rc['filename']
+            pyfile.size = rc['filesize']  # bytes is not good here?
+            self.link = rc['url']
+            # self.download(rc['url'])
+        elif rc.get('reason') in ('wrong url', 'file removed'):
+            self.offline()
+        elif rc.get('reason') in ('host daily limit exceeded', 'host disabled', 'temporary error', 'unknown error'):
+            self.log_warning(rc['reason'])
+            self.temp_offline()
+        else:
+            self.fail(rc.get('reason', 'unknown error'))
