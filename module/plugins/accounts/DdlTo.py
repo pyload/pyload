e2e1affb8c0ca972ffc7f9d95eb3c574e35e4936
GammaC0de <gammac0de@users.noreply.github.com>
2020-04-18 02:02:30 +0300

    [DdlTo] Fix #3590

--- module/plugins/accounts/DdlTo.py
+++ module/plugins/accounts/DdlTo.py
@@ -8,14 +8,14 @@ from ..internal.XFSAccount import XFSAccount
 class DdlTo(XFSAccount):
     __name__ = "DdlTo"
     __type__ = "account"
-    __version__ = "0.01"
+    __version__ = "0.02"
     __status__ = "testing"
 
     __description__ = """Ddl.to account plugin"""
     __license__ = "GPLv3"
     __authors__ = [("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
-    PLUGIN_DOMAIN = "ddl.to"
+    PLUGIN_DOMAIN = "ddownload.com"
 
     PREMIUM_PATTERN = r'remium Account \(expires'
     TRAFFIC_LEFT_PATTERN = r'<span>Traffic available</span>\s*<div class="price">(?:<sup>(?P<U>[^<>]+)</sup>)?(?P<S>\d+|[Uu]nlimited)</div>'
--- module/plugins/hoster/DdlTo.py
+++ module/plugins/hoster/DdlTo.py
@@ -11,10 +11,10 @@ from ..internal.XFSHoster import XFSHoster
 class DdlTo(XFSHoster):
     __name__ = "DdlTo"
     __type__ = "hoster"
-    __version__ = "0.05"
+    __version__ = "0.06"
     __status__ = "testing"
 
-    __pattern__ = r'https?://(?:www\.)?ddl.to/(?P<ID>\w{12})'
+    __pattern__ = r'https?://(?:www\.)?(?:ddl\.to|ddownload\.com)/(?P<ID>\w{12})'
     __config__ = [("activated", "bool", "Activated", True),
                   ("use_premium", "bool", "Use premium account if available", True),
                   ("fallback", "bool", "Fallback to free download if premium fails", True),
@@ -25,14 +25,15 @@ class DdlTo(XFSHoster):
     __license__ = "GPLv3"
     __authors__ = [("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
-    PLUGIN_DOMAIN = "ddl.to"
+    PLUGIN_DOMAIN = "ddownload.com"
 
-    URL_REPLACEMENTS = [(__pattern__ + '.*', "https://ddl.to/\g<ID>")]
+    URL_REPLACEMENTS = [(__pattern__ + '.*', r"https://ddownload.com/\g<ID>")]
 
     NAME_PATTERN = r'<div class="name">\s*<h4>(?P<N>.+?)</h4>'
     SIZE_PATTERN = r'<div class="name">.+?<span>(?P<S>[\d.,]+) (?P<U>[\w^_]+)</span>', re.S
 
     OFFLINE_PATTERN = r'<h4>File Not Found</h4>'
+    DL_LIMIT_PATTERN = r'You have to wait (.+?) till next download'
 
     API_KEY = "37699zuaj90n9hxado2m7"
     API_URL = "https://api-v2.ddl.to/api/"
