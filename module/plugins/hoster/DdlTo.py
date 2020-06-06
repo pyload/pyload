2d187401c13fd709aa488e01987516af6a74cc66
GammaC0de <gammac0de@users.noreply.github.com>
2020-06-06 03:19:07 +0300

    [DdlTo] Fix #3667

--- module/plugins/hoster/DdlTo.py
+++ module/plugins/hoster/DdlTo.py
@@ -11,7 +11,7 @@ from ..internal.XFSHoster import XFSHoster
 class DdlTo(XFSHoster):
     __name__ = "DdlTo"
     __type__ = "hoster"
-    __version__ = "0.06"
+    __version__ = "0.07"
     __status__ = "testing"
 
     __pattern__ = r'https?://(?:www\.)?(?:ddl\.to|ddownload\.com)/(?P<ID>\w{12})'
@@ -29,8 +29,8 @@ class DdlTo(XFSHoster):
 
     URL_REPLACEMENTS = [(__pattern__ + '.*', r"https://ddownload.com/\g<ID>")]
 
-    NAME_PATTERN = r'<div class="name">\s*<h4>(?P<N>.+?)</h4>'
-    SIZE_PATTERN = r'<div class="name">.+?<span>(?P<S>[\d.,]+) (?P<U>[\w^_]+)</span>', re.S
+    NAME_PATTERN = r'<div class="name position-relative">\s*<h4>(?P<N>.+?)</h4>'
+    SIZE_PATTERN = r'<span class="file-size">(?P<S>[\d.,]+) (?P<U>[\w^_]+)</span>'
 
     OFFLINE_PATTERN = r'<h4>File Not Found</h4>'
     DL_LIMIT_PATTERN = r'You have to wait (.+?) till next download'
@@ -44,18 +44,18 @@ class DdlTo(XFSHoster):
         json_data = get_url(cls.API_URL + method, get=kwargs)
         return json.loads(json_data)
 
-    @classmethod
-    def api_info(cls, url):
-        info = {}
-        api_data = cls.api_response("file/info", file_code=re.match(cls.__pattern__, url).group('ID'))
-
-        if api_data['status'] == 200:
-            if api_data['result'][0]['status'] == 200:
-                info['status'] = 2
-                info['name'] = api_data['result'][0]['name']
-                info['size'] = api_data['result'][0]['size']
-
-            else:
-                info['status'] = 8
-
-        return info
+    # @classmethod
+    # def api_info(cls, url):
+    #     info = {}
+    #     api_data = cls.api_response("file/info", file_code=re.match(cls.__pattern__, url).group('ID'))
+    #
+    #     if api_data['status'] == 200:
+    #         if api_data['result'][0]['status'] == 200:
+    #             info['status'] = 2
+    #             info['name'] = api_data['result'][0]['name']
+    #             info['size'] = api_data['result'][0]['size']
+    #
+    #         else:
+    #             info['status'] = 8
+    #
+    #     return info
