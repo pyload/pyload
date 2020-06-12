9b5641003964ec5dfbeab91bee63da5ed6459c0c
GammaC0de <gammac0de@users.noreply.github.com>
2020-06-13 01:33:59 +0300

    [AlfafileNet] Update

--- module/plugins/hoster/AlfafileNet.py
+++ module/plugins/hoster/AlfafileNet.py
@@ -10,7 +10,7 @@ from ..internal.SimpleHoster import SimpleHoster
 class AlfafileNet(SimpleHoster):
     __name__ = "AlfafileNet"
     __type__ = "hoster"
-    __version__ = "0.03"
+    __version__ = "0.04"
     __status__ = "testing"
 
     __pattern__ = r'https?://(?:www\.)?(alfafile\.net)/file/(?P<ID>\w+)'
@@ -25,6 +25,7 @@ class AlfafileNet(SimpleHoster):
     __authors__ = [("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
     URL_REPLACEMENTS = [(__pattern__ + ".*", r'https://alfafile.net/file/\g<ID>')]
+    COOKIES = [("alfafile.net", "lang", "en")]
 
     NAME_PATTERN = r'<strong id="st_file_name" title="(?P<N>.+?)"'
     SIZE_PATTERN = r'<span class="size">(?P<S>[\d.,]+) (?P<U>[\w^_]+)<'
