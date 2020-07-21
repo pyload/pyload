70fc9120968ce97463810eb4ede7c6a4ef8a1de7
GammaC0de <gammac0de@users.noreply.github.com>
2020-07-22 00:39:34 +0300

    [LinksnappyCom] Fix #3692

--- module/plugins/hoster/LinksnappyCom.py
+++ module/plugins/hoster/LinksnappyCom.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import re
-import urlparse
+import time
 
 from ..internal.misc import json
 from ..internal.MultiHoster import MultiHoster
@@ -10,7 +9,7 @@ from ..internal.MultiHoster import MultiHoster
 class LinksnappyCom(MultiHoster):
     __name__ = "LinksnappyCom"
     __type__ = "hoster"
-    __version__ = "0.18"
+    __version__ = "0.19"
     __status__ = "testing"
 
     __pattern__ = r'https?://(?:[^/]+\.)?linksnappy\.com'
@@ -24,29 +23,48 @@ class LinksnappyCom(MultiHoster):
     __description__ = """Linksnappy.com multi-hoster plugin"""
     __license__ = "GPLv3"
     __authors__ = [("stickell", "l.stickell@yahoo.it"),
-                   ("Bilal Ghouri", None)]
+                   ("Bilal Ghouri", None),
+                   ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
+
+    API_URL = "https://linksnappy.com/api/"
+
+    def api_response(self, method, **kwargs):
+        return json.loads(self.load(self.API_URL + method,
+                                    get=kwargs))
 
     def handle_premium(self, pyfile):
-        host = self._get_host(pyfile.url)
-        json_params = json.dumps({'link': pyfile.url,
-                                  'type': host,
-                                  'username': self.account.user,
-                                  'password': self.account.get_login('password')})
+        json_params = json.dumps({'link': pyfile.url})
 
-        r = self.load("https://linksnappy.com/api/linkgen",
-                      post={'genLinks': json_params})
+        api_data = self.api_response("linkgen", genLinks=json_params)['links'][0]
 
-        self.log_debug("JSON data: " + r)
+        if api_data['status'] != "OK":
+            self.fail(api_data['error'])
 
-        j = json.loads(r)['links'][0]
+        if api_data.get('cacheDL', False):
+            self._cache_dl(api_data['hash'])
 
-        if j['error']:
-            self.error(_("Error converting the link"))
+        pyfile.name = api_data['filename']
+        self.link = api_data['generated']
 
-        pyfile.name = j['filename']
-        self.link = j['generated']
+    def _cache_dl(self,file_hash):
+        self.pyfile.setCustomStatus("server dl")
+        self.pyfile.setProgress(0)
 
-    @staticmethod
-    def _get_host(url):
-        host = urlparse.urlsplit(url).netloc
-        return re.search(r'[\w\-]+\.\w+$', host).group(0)
+        while True:
+            api_data = self.api_response("CACHEDLSTATUS", id=file_hash)
+
+            if api_data['status'] != "OK":
+                self.fail(api_data['error'])
+
+            progress = api_data['return']['percent']
+            self.pyfile.setProgress(progress)
+            if progress == 100:
+                break
+
+            self._sleep(2)
+
+    def _sleep(self, sec):
+        for _i in range(sec):
+            if self.pyfile.abort:
+                break
+            time.sleep(1)
