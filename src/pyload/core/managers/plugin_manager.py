093c130029468df7bab348cf072c12b17a5a9bd8
Walter Purcaro <vuolter@gmail.com>
2020-08-16 22:57:51 +0200

    Don't check for pyLoad version in plugin manager

--- src/pyload/core/managers/plugin_manager.py
+++ src/pyload/core/managers/plugin_manager.py
@@ -7,7 +7,7 @@ import sys
 from ast import literal_eval
 from itertools import chain
 
-import semver
+# import semver
 
 from pyload import APPID, PKGDIR
 
@@ -27,7 +27,7 @@ class PluginManager:
 
     _PATTERN = re.compile(r'\s*__pattern__\s*=\s*r?(?:"|\')([^"\']+)')
     _VERSION = re.compile(r'\s*__version__\s*=\s*(?:"|\')([\d.]+)')
-    _PYLOAD_VERSION = re.compile(r'\s*__pyload_version__\s*=\s*(?:"|\')([\d.]+)')
+    # _PYLOAD_VERSION = re.compile(r'\s*__pyload_version__\s*=\s*(?:"|\')([\d.]+)')
     _CONFIG = re.compile(r"\s*__config__\s*=\s*(\[[^\]]+\])", re.MULTILINE)
     _DESC = re.compile(r'\s*__description__\s*=\s*(?:"|"""|\')([^"\']+)', re.MULTILINE)
 
@@ -149,31 +149,31 @@ class PluginManager:
                 if name[-1] == ".":
                     name = name[:-4]
 
-                m_pyver = self._PYLOAD_VERSION.search(content)
-                if m_pyver is None:
-                    self.pyload.log.debug(
-                        f"__pyload_version__ not found in plugin {name}"
-                    )
-                else:
-                    pyload_version = m_pyver.group(1)
+                # m_pyver = self._PYLOAD_VERSION.search(content)
+                # if m_pyver is None:
+                #     self.pyload.log.debug(
+                #         f"__pyload_version__ not found in plugin {name}"
+                #     )
+                # else:
+                #     pyload_version = m_pyver.group(1)
 
-                    requires_version = f"{pyload_version}.0"
-                    requires_version_info = semver.parse_version_info(requires_version)
+                #     requires_version = f"{pyload_version}.0"
+                #     requires_version_info = semver.parse_version_info(requires_version)
 
-                    if self.pyload.version_info.major:
-                        core_version = self.pyload.version_info.major
-                        plugin_version = requires_version_info.major
-                    else:
-                        core_version = self.pyload.version_info.minor
-                        plugin_version = requires_version_info.minor
+                #     if self.pyload.version_info.major:
+                #         core_version = self.pyload.version_info.major
+                #         plugin_version = requires_version_info.major
+                #     else:
+                #         core_version = self.pyload.version_info.minor
+                #         plugin_version = requires_version_info.minor
 
-                    if core_version > plugin_version:
-                        self.pyload.log.warning(
-                            self._(
-                                "Plugin {} not compatible with current pyLoad version"
-                            ).format(name)
-                        )
-                        continue
+                #     if core_version > plugin_version:
+                #         self.pyload.log.warning(
+                #             self._(
+                #                 "Plugin {} not compatible with current pyLoad version"
+                #             ).format(name)
+                #         )
+                #         continue
 
                 m_ver = self._VERSION.search(content)
                 if m_ver is None:
