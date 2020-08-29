21abb0391c48bf4db51862f272441341b2c018fe
GammaC0de <gammac0de@users.noreply.github.com>
2020-08-30 01:47:21 +0300

    [GoogledriveCom] fix #3705

--- module/plugins/hoster/GoogledriveCom.py
+++ module/plugins/hoster/GoogledriveCom.py
@@ -9,13 +9,13 @@ import urlparse
 from module.network.HTTPRequest import BadHeader
 
 from ..internal.Hoster import Hoster
-from ..internal.misc import json
+from ..internal.misc import json, parse_size, search_pattern
 
 
 class GoogledriveCom(Hoster):
     __name__ = "GoogledriveCom"
     __type__ = "hoster"
-    __version__ = "0.31"
+    __version__ = "0.32"
     __status__ = "testing"
 
     __pattern__ = r'https?://(?:www\.)?(?:drive|docs)\.google\.com/(?:file/d/|uc\?.*id=)(?P<ID>[-\w]+)'
@@ -29,6 +29,8 @@ class GoogledriveCom(Hoster):
     __license__ = "GPLv3"
     __authors__ = [("zapp-brannigan", "fuerst.reinje@web.de"),
                    ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
+
+    INFO_PATTERN = r'<span class="uc-name-size"><a href="[^"]+">(?P<N>.+?)</a> \((?P<S>[\d.,]+)(?P<U>[\w^_]+)\)</span>'
 
     API_URL = "https://www.googleapis.com/drive/v3/"
     API_KEY = "AIzaSyAcA9c4evtwSY1ifuvzo6HKBkeot5Bk_U4"
@@ -79,6 +81,8 @@ class GoogledriveCom(Hoster):
                 raise
 
     def process(self, pyfile):
+        disposition = False
+        self.data = self.load(pyfile.url)
         json_data = self.api_response("files/" + self.info['pattern']['ID'], fields="md5Checksum,name,size")
 
         if json_data is None:
@@ -86,19 +90,28 @@ class GoogledriveCom(Hoster):
 
         if 'error' in json_data:
             if json_data['error']['code'] == 404:
-                self.offline()
+                if "Virus scan warning" not in self.data:
+                    self.offline()
+
+                else:
+                    m = search_pattern(self.INFO_PATTERN, self.data)
+                    if m is not None:
+                        pyfile.name = m.group("N")
+                        pyfile.size = parse_size(m.group("S"), m.group("U"))
+                    else:
+                        disposition = True
 
             else:
                 self.fail(json_data['error']['message'])
 
-        pyfile.size = int(json_data['size'])
-        pyfile.name = json_data['name']
-        self.info['md5'] = json_data['md5Checksum']
+        else:
+            pyfile.size = int(json_data['size'])
+            pyfile.name = json_data['name']
+            self.info['md5'] = json_data['md5Checksum']
 
         # Somehow, API downloads are sacrificially slow compared to "normal" download :(
         # self.api_download()
 
-        self.data = self.load(pyfile.url)
         for _i in range(2):
             m = re.search(r'"([^"]+uc\?.*?)"', self.data)
             if m is None:
@@ -111,14 +124,14 @@ class GoogledriveCom(Hoster):
 
             #: "Only files smaller than 100 MB can be scanned for viruses"
             #: https://support.google.com/a/answer/172541?hl=en
-            if pyfile.size > 104857600:
+            if pyfile.size > 104857600 or "Virus scan warning" in self.data:
                 if re.search(r'/uc\?.*&confirm=', link):
-                    self.download(link, disposition=False)
-                    return
+                    self.download(link, disposition=disposition)
+                    break
 
                 else:
                     self.data = self.load(link)
 
             else:
-                self.download(link, disposition=False)
-                return
+                self.download(link, disposition=disposition)
+                break
