a5546ae88b233d8cbf046fc7f8f6e6dba1394352
Ozzieisaacs <ozzie.fernandez.isaacs@googlemail.com>
2020-10-17 21:27:00 +0200

    New Hoster CosmoboxOrg

---
+++ module/plugins/hoster/CosmoboxOrg.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from ..internal.XFSHoster import XFSHoster
+from ..internal.misc import search_pattern, parse_time
+
+class CosmoboxOrg(XFSHoster):
+    __name__ = "CosmoboxOrg"
+    __type__ = "hoster"
+    __version__ = "0.01"
+    __status__ = "testing"
+
+    __pattern__ = r'https://cosmobox.org/\w{12}'
+    __config__ = [("activated", "bool", "Activated", True),
+                  ("chk_filesize", "bool", "Check file size", True),
+                  ("max_wait", "int",
+                   "Reconnect if waiting time is greater than minutes", 10)]
+
+    __description__ = """Cosmobox.org hoster plugin"""
+    __license__ = "GPLv3"
+    __authors__ = [("OzzieIsaacs", "Ozzie.Fernandez.Isaacs@googlemail.com")]
+
+    PLUGIN_DOMAIN = "cosmobox.org"
+
+    NAME_PATTERN = r"You're downloading: (?P<N>.+?)<"
+    SIZE_PATTERN = r'<span class="label label-default">(?P<S>[\d.,]+) (?P<U>[\w^_]+)</span>'
+    WAIT_PATTERN = r'<span class="circle"><span class="seconds">(\d+)</span></span>'
+
+    def handle_free(self, pyfile):
+        self.check_errors()
+        self.data = self.load(pyfile.url)
+        action, inputs = self.parse_html_form(input_names={'op': re.compile(r'^download')})
+        inputs['method_free'] = "Free+Download"
+
+        self.data = self.load("https://cosmobox.org/download",
+                              post=inputs,
+                              ref=self.pyfile.url,
+                              redirect=False)
+
+        m = search_pattern(r'role="alert">You have reached your download limit', self.data)
+        wait_time = None
+        if m is not None:
+            wait_time = 3*60*60 # wait 3 hours
+        else:
+            m = search_pattern(self.WAIT_PATTERN, self.data)
+            if m is not None:
+                waitmsg = m.group(1).strip()
+                wait_time = parse_time(waitmsg)
+        self.wait(wait_time)
+
+        action, inputs = self.parse_html_form(input_names={'op': re.compile(r'^download')})
+        self.handle_captcha(inputs)
+        self.data = self.download("https://cosmobox.org/download", post=inputs)
