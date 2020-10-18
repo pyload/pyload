8b1e8025ac84e068569ad12ec58e64a6357dbc2e
Ozzieisaacs <ozzie.fernandez.isaacs@googlemail.com>
2020-10-18 08:13:49 +0200

    Update Hoster

--- module/plugins/hoster/UploadshipCom.py
+++ module/plugins/hoster/UploadshipCom.py
@@ -2,12 +2,12 @@
 import re
 
 from ..internal.XFSHoster import XFSHoster
-from ..internal.misc import search_pattern, parse_time
+from ..internal.misc import search_pattern, parse_time, json
 
 class UploadshipCom(XFSHoster):
     __name__ = "UploadshipCom"
     __type__ = "hoster"
-    __version__ = "0.01"
+    __version__ = "0.02"
     __status__ = "testing"
 
     __pattern__ = r'https://www.uploadship\.com/\w{16}'
@@ -25,43 +25,30 @@ class UploadshipCom(XFSHoster):
 
     NAME_PATTERN = r'<span style="display: block; line-height: 17px; word-break: break-all;"><small>(?P<N>.+?)</small></span>'
     SIZE_PATTERN = r'<i class="fa fa-hdd-o" aria-hidden="true"></td><td>((?P<S>[\d.,]+) (?P<U>[\w^_]+)) :'
-    WAIT_PATTERN = '<div class="download-timer-seconds">(\d+)</div>'
+    WAIT_PATTERN = r"\$\('\.download-timer-seconds'\)\.html\((\d+)\);"
 
     FORM_INPUTS_MAP = {'d': re.compile(r'^1')}
     LINK_PATTERN = r'href="(.+?)" target="_blank" class="btn btn-go">Click Here To Download'
+    ERROR_PATTERN = None
 
     def handle_free(self, pyfile):
-        self.check_errors()
-        self.data = self.load(pyfile.url)
-
         action, inputs = self.parse_html_form(self.FORM_PATTERN or "", self.FORM_INPUTS_MAP or {})
-                                        # download button id                # download button id
-        # https://www.uploadship.com/e5543844e02e54erc3d755cw2522bee5.php?e5543844e02e54erc3d755cw2522bee5=eece12568a839ba4f2fb965e369db4f7
 
-        m = search_pattern(self.WAIT_PATTERN, self.data)
-        if m is not None:
-            try:
-                waitmsg = m.group(1).strip()
+        secret = re.search('"data": {\r\n"(\w{32})": "(\w{32})"', self.data, re.S)
+        if secret is not None:
+            get_data = {}
+            get_data[secret.group(1)] = secret.group(2)
+            secret_data = self.load('https://www.uploadship.com/' + secret.group(1) + '.php',
+                                  get=get_data)
+            inputs[secret.group(1)] = secret_data
 
-            except (AttributeError, IndexError):
-                waitmsg = m.group(0).strip()
-
-            wait_time = parse_time(waitmsg)
-            self.set_wait(wait_time)
-            if wait_time < self.config.get('max_wait', 10) * 60 or \
-                    self.pyload.config.get('reconnect', 'activated') is False or \
-                    self.pyload.api.isTimeReconnect() is False:
-                self.handle_captcha(inputs)
-            self.wait()
-        else:
-            self.handle_captcha(inputs)
+        self.handle_captcha(inputs)
 
         self.data = self.load(pyfile.url,
-                              post=inputs, # self._post_parameters(),
+                              post=inputs,
                               ref=self.pyfile.url,
                               redirect=False)
 
         m = search_pattern(self.LINK_PATTERN, self.data, flags=re.M)
         if m is not None:
             self.link = m.group(1)
-            return
