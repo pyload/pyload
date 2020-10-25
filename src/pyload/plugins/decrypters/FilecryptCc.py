d7988546f3c74cc315215997ae07ae07c90e96d5
GammaC0de <gammac0de@users.noreply.github.com>
2020-10-25 23:14:03 +0200

    [FilecryptCc] update

--- src/pyload/plugins/decrypters/FilecryptCc.py
+++ src/pyload/plugins/decrypters/FilecryptCc.py
@@ -8,12 +8,14 @@ import base64
 import re
 import urllib.parse
 
-from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 from pyload.core.network.cookie_jar import CookieJar
 from pyload.core.network.exceptions import Abort
 from pyload.core.network.http.exceptions import BadHeader
 from pyload.core.network.http.http_request import HTTPRequest
+from pyload.core.utils.convert import to_str
 
 from ..anticaptchas.CoinHive import CoinHive
 from ..anticaptchas.ReCaptcha import ReCaptcha
@@ -50,7 +52,7 @@ class BIGHTTPRequest(HTTPRequest):
 class FilecryptCc(BaseDecrypter):
     __name__ = "FilecryptCc"
     __type__ = "decrypter"
-    __version__ = "0.37"
+    __version__ = "0.41"
     __status__ = "testing"
 
     __pattern__ = r"https?://(?:www\.)?filecrypt\.cc/Container/\w+"
@@ -66,16 +68,13 @@ class FilecryptCc(BaseDecrypter):
         ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com"),
     ]
 
-    # URL_REPLACEMENTS  = [(r'.html$', ""), (r'$', ".html")]  # TODO: Extend
-    # SimpleDecrypter
-
     COOKIES = [("filecrypt.cc", "lang", "en")]
 
     DLC_LINK_PATTERN = r'onclick="DownloadDLC\(\'(.+)\'\);">'
     WEBLINK_PATTERN = r"openLink.?'([\w\-]*)',"
     MIRROR_PAGE_PATTERN = r'"[\w]*" href="(https?://(?:www\.)?filecrypt.cc/Container/\w+\.html\?mirror=\d+)">'
 
-    CAPTCHA_PATTERN = r"<h2>Security prompt</h2>"
+    CAPTCHA_PATTERN = r'<h2>Security prompt</h2>'
     INTERNAL_CAPTCHA_PATTERN = r'<img id="nc" .* src="(.+?)"'
     CIRCLE_CAPTCHA_PATTERN = r'<input type="image" src="(.+?)"'
     KEY_CAPTCHA_PATTERN = r"<script language=JavaScript src='(http://backs\.keycaptcha\.com/swfs/cap\.js)'"
@@ -98,7 +97,8 @@ class FilecryptCc(BaseDecrypter):
     def decrypt(self, pyfile):
         self.data = self._filecrypt_load_url(pyfile.url)
 
-        if "content notfound" in self.data:  # NOTE: "content notfound" is NOT a typo
+        # @NOTE: "content notfound" is NOT a typo
+        if "content notfound" in self.data or ">File <strong>not</strong> found<" in self.data:
             self.offline()
 
         self.handle_password_protection()
@@ -139,7 +139,7 @@ class FilecryptCc(BaseDecrypter):
     def handle_password_protection(self):
         if (
             re.search(
-                r'div class="input">\s*<input type="password" name="password" id="p4assw0rt"',
+                r'div class="input">\s*<input type="text" name="password" id="p4assw0rt"',
                 self.data,
             )
             is None
@@ -341,12 +341,17 @@ class FilecryptCc(BaseDecrypter):
             self.log_debug(f"Error decrypting CNL: {exc}")
 
     def _get_links(self, crypted, jk):
-        #: Get key
-        key = bytes.fromhex(jk)
+        #: Get key and iv
+        key = iv = bytes.fromhex(jk)
 
         #: Decrypt
-        obj = Fernet(key)
-        text = obj.decrypt(base64.b64decode(crypted))
+        cipher = Cipher(
+            algorithms.AES(key), modes.CBC(iv), backend=default_backend()
+        )
+        decryptor = cipher.decryptor()
+        text = to_str(
+            decryptor.update(base64.b64decode(crypted)) + decryptor.finalize()
+        )
 
         #: Extract links
         text = text.replace("\x00", "").replace("\r", "")
