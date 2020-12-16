ff1f0af827f384924edb54a68d60475cf0d09767
GammaC0de <gammac0de@users.noreply.github.com>
2020-12-16 23:54:13 +0200

    [LinksnappyCom] fix #3852

--- module/plugins/hoster/LinksnappyCom.py
+++ module/plugins/hoster/LinksnappyCom.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import time
+import urlparse
 
-from ..internal.misc import json
+from ..internal.misc import format_size, json
 from ..internal.MultiHoster import MultiHoster
 
 
 class LinksnappyCom(MultiHoster):
     __name__ = "LinksnappyCom"
     __type__ = "hoster"
-    __version__ = "0.19"
+    __version__ = "0.20"
     __status__ = "testing"
 
     __pattern__ = r'https?://(?:[^/]+\.)?linksnappy\.com'
@@ -25,6 +26,8 @@ class LinksnappyCom(MultiHoster):
     __authors__ = [("stickell", "l.stickell@yahoo.it"),
                    ("Bilal Ghouri", None),
                    ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
+
+    CHECK_TRAFFIC = True
 
     API_URL = "https://linksnappy.com/api/"
 
@@ -45,6 +48,30 @@ class LinksnappyCom(MultiHoster):
 
         pyfile.name = api_data['filename']
         self.link = api_data['generated']
+
+    def out_of_traffic(self):
+        url_p = urlparse.urlparse(self.pyfile.url)
+        json_data = self.api_response("FILEHOSTS")
+
+        for k, v in json_data['return'].items():
+            url = urlparse.urlunparse(url_p._replace(netloc=k))
+            if self.pyload.pluginManager.plugins['hoster'][self.pyfile.pluginname]['re'].match(url) is not None:
+                quota = v['Quota']
+                if quota == "unlimited":
+                    return False
+
+                else:
+                    size = self.pyfile.size
+                    usage = v.get('Usage', 0)
+                    traffic_left = quota - usage
+                    self.log_info(_("Filesize: %s") % format_size(size),
+                                  _("Traffic left for user `%s`: %s") % (self.account.user, format_size(traffic_left)))
+
+                    return size > traffic_left
+
+        else:
+            self.log_warning(_("Could not determine traffic usage for host %s") % url_p.netloc)
+            return False
 
     def _cache_dl(self,file_hash):
         self.pyfile.setCustomStatus("server dl")
