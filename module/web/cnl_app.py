b55b845d63dfdab49b2e97e272326478522578cf
GammaC0de <gammac0de@users.noreply.github.com>
2021-01-10 01:31:09 +0200

    fix local_check() for IPv6 (#3883)

--- module/web/cnl_app.py
+++ module/web/cnl_app.py
@@ -17,8 +17,8 @@ except:
 
 def local_check(function):
     def _view(*args, **kwargs):
-        if request.environ.get('REMOTE_ADDR', "0") in ('127.0.0.1', 'localhost') \
-        or request.environ.get('HTTP_HOST','0') == '127.0.0.1:9666':
+        if request.environ.get('REMOTE_ADDR', "0") in ('127.0.0.1', '::ffff:127.0.0.1', '::1', 'localhost') \
+        or request.environ.get('HTTP_HOST','0') in ('127.0.0.1:9666', '[::1]:9666'):
             return function(*args, **kwargs)
         else:
             return HTTPError(403, "Forbidden")
