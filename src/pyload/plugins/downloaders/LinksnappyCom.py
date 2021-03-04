8507dde87da44ece75da26489c4f1a3ae114f08c
GammaC0de <gammac0de@users.noreply.github.com>
2021-03-04 20:45:48 +0200

    [LinksnappyCom] sync with stable

--- src/pyload/plugins/downloaders/LinksnappyCom.py
+++ src/pyload/plugins/downloaders/LinksnappyCom.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+
 import json
-import re
+import time
 import urllib.parse
+
+from pyload.core.utils.format import size as format_size
 
 from ..base.multi_downloader import MultiDownloader
 
@@ -9,12 +12,12 @@ from ..base.multi_downloader import MultiDownloader
 class LinksnappyCom(MultiDownloader):
     __name__ = "LinksnappyCom"
     __type__ = "downloader"
-    __version__ = "0.18"
+    __version__ = "0.20"
     __status__ = "testing"
 
     __pattern__ = r"https?://(?:[^/]+\.)?linksnappy\.com"
     __config__ = [
-        ("enabled", "bool", "Activated", True),
+        ("activated", "bool", "Activated", True),
         ("use_premium", "bool", "Use premium account if available", True),
         ("fallback", "bool", "Fallback to free download if premium fails", False),
         ("chk_filesize", "bool", "Check file size", True),
@@ -24,34 +27,89 @@ class LinksnappyCom(MultiDownloader):
 
     __description__ = """Linksnappy.com multi-downloader plugin"""
     __license__ = "GPLv3"
-    __authors__ = [("stickell", "l.stickell@yahoo.it"), ("Bilal Ghouri", None)]
+    __authors__ = [
+        ("stickell", "l.stickell@yahoo.it"),
+        ("Bilal Ghouri", None),
+        ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com"),
+    ]
+
+    CHECK_TRAFFIC = True
+
+    API_URL = "https://linksnappy.com/api/"
+
+    def api_response(self, method, **kwargs):
+        return json.loads(self.load(self.API_URL + method, get=kwargs))
 
     def handle_premium(self, pyfile):
-        host = self._get_host(pyfile.url)
-        json_params = json.dumps(
-            {
-                "link": pyfile.url,
-                "type": host,
-                "username": self.account.user,
-                "password": self.account.get_login("password"),
-            }
-        )
+        json_params = json.dumps({"link": pyfile.url})
 
-        r = self.load(
-            "https://linksnappy.com/api/linkgen", post={"genLinks": json_params}
-        )
+        api_data = self.api_response("linkgen", genLinks=json_params)["links"][0]
 
-        self.log_debug("JSON data: " + r)
+        if api_data["status"] != "OK":
+            self.fail(api_data["error"])
 
-        j = json.loads(r)["links"][0]
+        if api_data.get("cacheDL", False):
+            self._cache_dl(api_data["hash"])
 
-        if j["error"]:
-            self.error(self._("Error converting the link"))
+        pyfile.name = api_data["filename"]
+        self.link = api_data["generated"]
 
-        pyfile.name = j["filename"]
-        self.link = j["generated"]
+    def out_of_traffic(self):
+        url_p = urllib.parse.urlparse(self.pyfile.url)
+        json_data = self.api_response("FILEHOSTS")
 
-    @staticmethod
-    def _get_host(url):
-        host = urllib.parse.urlsplit(url).netloc
-        return re.search(r"[\w\-]+\.\w+$", host).group(0)
+        for k, v in json_data["return"].items():
+            url = urllib.parse.urlunparse(url_p._replace(netloc=k))
+            if (
+                self.pyload.plugin_manager.plugins["downloader"][self.pyfile.pluginname][
+                    "re"
+                ].match(url)
+                is not None
+            ):
+                quota = v["Quota"]
+                if quota == "unlimited":
+                    return False
+
+                else:
+                    size = self.pyfile.size
+                    usage = v.get("Usage", 0)
+                    traffic_left = quota - usage
+                    self.log_info(
+                        self._("Filesize: {}").format(format_size(size)),
+                        self._("Traffic left for user `{}`: {}").format(
+                            self.account.user, format_size(traffic_left)
+                        ),
+                    )
+
+                    return size > traffic_left
+
+        else:
+            self.log_warning(
+                self._("Could not determine traffic usage for host {}").format(
+                    url_p.netloc
+                )
+            )
+            return False
+
+    def _cache_dl(self, file_hash):
+        self.pyfile.set_custom_status("server dl")
+        self.pyfile.set_progress(0)
+
+        while True:
+            api_data = self.api_response("CACHEDLSTATUS", id=file_hash)
+
+            if api_data["status"] != "OK":
+                self.fail(api_data["error"])
+
+            progress = api_data["return"]["percent"]
+            self.pyfile.set_progress(progress)
+            if progress == 100:
+                break
+
+            self._sleep(2)
+
+    def _sleep(self, sec):
+        for _i in range(sec):
+            if self.pyfile.abort:
+                break
+            time.sleep(1)
