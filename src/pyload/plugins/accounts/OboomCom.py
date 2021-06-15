8c57fb4e12f47c53d128f25d9398c40071588914
GammaC0de <gammac0de@users.noreply.github.com>
2021-06-15 03:30:24 +0300

    [OboomCom] sync with stable

--- src/pyload/plugins/accounts/OboomCom.py
+++ src/pyload/plugins/accounts/OboomCom.py
@@ -1,64 +1,75 @@
 # -*- coding: utf-8 -*-
 
-import hashlib
 import json
+from binascii import b2a_hex
+
+from beaker.crypto.pbkdf2 import pbkdf2
+from pyload.core.network.request_factory import get_url
 
 from ..base.account import BaseAccount
+
+
+class PBKDF2:
+    def __init__(self, passphrase, salt, iterations=1000):
+        self.passphrase = passphrase
+        self.salt = salt
+        self.iterations = iterations
+
+    def hexread(self, octets):
+        return b2a_hex(pbkdf2(self.passphrase, self.salt, self.iterations, octets))
 
 
 class OboomCom(BaseAccount):
     __name__ = "OboomCom"
     __type__ = "account"
-    __version__ = "0.32"
+    __version__ = "0.34"
     __status__ = "testing"
 
     __description__ = """Oboom.com account plugin"""
     __license__ = "GPLv3"
-    __authors__ = [("stanley", "stanley.foerster@gmail.com")]
+    __authors__ = [
+        ("stanley", "stanley.foerster@gmail.com"),
+        ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com"),
+    ]
 
-    def load_account_data(self, user, password):
-        salt = password[::-1]
-        pw = hashlib.pbkdf2_hmac(password.encode(), salt.encode(), 1000).hex()[16]
+    #: See https://www.oboom.com/api
+    API_URL = "https://%s.oboom.com/1/"
 
-        html = self.load(
-            "http://www.oboom.com/1/login",  # TODO: Revert to `https` in 0.6.x
-            get={"auth": user, "pass": pw},
-        )
-        result = json.loads(html)
-
-        if result[0] != 200:
-            self.log_warning(self._("Failed to log in: {}").format(result[1]))
-            self.fail_login()
-
-        return result[1]
+    @classmethod
+    def api_request(cls, subdomain, method, args={}):
+        return json.loads(get_url(cls.API_URL % subdomain + method, post=args))
 
     def grab_info(self, user, password, data):
-        account_data = self.load_account_data(user, password)
+        salt = password[::-1]
+        pbkdf2 = PBKDF2(password, salt, 1000).hexread(16)
 
-        userData = account_data["user"]
+        res = self.api_request("www", "login", {"auth": user, "pass": pbkdf2})
 
-        premium = userData["premium"] != "null"
+        user_data = res[1]["user"]
 
-        if userData["premium_unix"] == "null":
-            validUntil = -1
+        premium = user_data["premium"] != "null"
+
+        if user_data["premium_unix"] == "null":
+            validuntil = -1
+
         else:
-            validUntil = float(userData["premium_unix"])
+            validuntil = float(user_data["premium_unix"])
 
-        traffic = userData["traffic"]
+        trafficleft = user_data["traffic"]["current"]
 
-        # TODO: Remove `>> 10` in 0.6.x
-        trafficLeft = traffic["current"] >> 10
-        maxTraffic = traffic["max"] >> 10  # TODO: Remove `>> 10` in 0.6.x
-
-        session = account_data["session"]
+        data["session"] = res[1]["session"]
 
         return {
             "premium": premium,
-            "validuntil": validUntil,
-            "trafficleft": trafficLeft,
-            "maxtraffic": maxTraffic,
-            "session": session,
+            "validuntil": validuntil,
+            "trafficleft": trafficleft,
         }
 
     def signin(self, user, password, data):
-        self.load_account_data(user, password)
+        salt = password[::-1]
+        pbkdf2 = PBKDF2(password, salt, 1000).hexread(16)
+
+        res = self.api_request("www", "login", {"auth": user, "pass": pbkdf2})
+
+        if res[0] != 200:
+            self.fail_login(res[1])
