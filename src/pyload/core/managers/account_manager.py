pyload/pyload

commit f3e71498652362a6413f931dda764287ecdca789

remove deprecated `user` variable

src/pyload/plugins/base/hoster.py
@@ -154,7 +154,6 @@ class BaseHoster(BasePlugin):
             self.load_account()  # TODO: Move to PluginThread in 0.6.x
         else:
             self.account = False
-            self.user = None  # TODO: Remove in 0.6.x
 
         try:
             self.req.close()
@@ -184,11 +183,9 @@ class BaseHoster(BasePlugin):
 
         if not self.account:
             self.account = False
-            self.user = None  # TODO: Remove in 0.6.x
 
         else:
             self.account.choose()
-            self.user = self.account.user  # TODO: Remove in 0.6.x
             if self.account.user is None:
                 self.account = False
 

commit 7e4270714455761e8056fa13f8e29abb80a7106f

fix issue caused by https://github.com/pyload/pyload/commit/f3e71498652362a6413f931dda764287ecdca789

src/pyload/core/managers/account_manager.py
@@ -198,7 +198,7 @@ class AccountManager:
         for acc in self.accounts.keys():
             if self.accounts[acc]:
                 plugin = self.get_account_plugin(acc)
-                if plugin:
+                if plugin is not None:
                     data[plugin.__name__] = plugin.get_all_accounts(force)
                 else:
                     self.pyload.log.error(self._("Bad or missing plugin: ACCOUNT {}").format(acc))

src/pyload/core/managers/thread_manager.py
@@ -354,7 +354,7 @@ class ThreadManager:
 
     def get_limit(self, thread):
         limit = thread.active.plugin.account.get_account_data(
-            thread.active.plugin.user
+            thread.active.plugin.account.user
         )["options"].get("limit_dl", ["0"])[0]
         return int(limit)
 

src/pyload/plugins/base/account.py
@@ -44,6 +44,9 @@ class BaseAccount(BasePlugin):
 
         self.init()
 
+    def __bool__(self):
+        return self.user is not None
+
     @property
     def logged(self):
         """

src/pyload/plugins/base/hoster.py
@@ -77,7 +77,6 @@ class BaseHoster(BasePlugin):
 
         #: Account handler instance, see :py:class:`Account`
         self.account = None
-        self.user = None  # TODO: Remove in 0.6.x
         self.premium = None
 
         #: Associated pyfile instance, see `PyFile`
@@ -176,18 +175,13 @@ class BaseHoster(BasePlugin):
         self.check_status()
 
     def load_account(self):
-        if not self.account:
+        if self.account is None:
             self.account = self.pyload.account_manager.get_account_plugin(
                 self.classname
             )
 
-        if not self.account:
-            self.account = False
-
-        else:
+        if self.account:
             self.account.choose()
-            if self.account.user is None:
-                self.account = False
 
     def _update_name(self):
         name = self.info.get("name")