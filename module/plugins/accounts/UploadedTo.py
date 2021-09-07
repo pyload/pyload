58b91e4165d5ce27aec30bc6a6075e41a5b16a61
GammaC0de <gammac0de@users.noreply.github.com>
2021-09-08 00:14:06 +0300

    [UploadedTo] add LOGIN_SKIP_PATTERN

--- module/plugins/accounts/UploadedTo.py
+++ module/plugins/accounts/UploadedTo.py
@@ -9,18 +9,20 @@ from ..internal.Account import Account
 class UploadedTo(Account):
     __name__ = "UploadedTo"
     __type__ = "account"
-    __version__ = "0.46"
+    __version__ = "0.47"
     __status__ = "testing"
 
     __description__ = """Uploaded.to account plugin"""
     __license__ = "GPLv3"
-    __authors__ = [("Walter Purcaro", "vuolter@gmail.com")]
+    __authors__ = [("Walter Purcaro", "vuolter@gmail.com"),
+                   ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
     COOKIES = False
 
     PREMIUM_PATTERN = r'<em>Premium</em>'
     VALID_UNTIL_PATTERN = r'<td>Duration:</td>\s*<th>\s*(.+?)<'
     TRAFFIC_LEFT_PATTERN = r'<b class="cB">(?P<S>[\d.,]+) (?P<U>[\w^_]+)'
+    LOGIN_SKIP_PATTERN = r'<a href="logout"'
 
     def grab_info(self, user, password, data):
         html = self.load("http://uploaded.net/me")
@@ -43,7 +45,7 @@ class UploadedTo(Account):
                         validuntil = time.time()
                         for n, u in m:
                             validuntil += float(n) * {
-                                'week': 3600 * 168,
+                                'week': 3600 * 24 * 7,
                                 'day': 3600 * 24,
                                 'hour': 3600,
                                 'minute': 60,
@@ -65,17 +67,18 @@ class UploadedTo(Account):
                 'premium': premium}
 
     def signin(self, user, password, data):
-        try:
-            self.load("http://uploaded.net/me")
+        html = self.load("http://uploaded.net/me")
+        if self.LOGIN_SKIP_PATTERN in html:
+            self.skip_login()
 
-            html = self.load("http://uploaded.net/io/login",
-                             post={'id': user,
-                                   'pw': password})
+        html = self.load("http://uploaded.net/io/login",
+                         post={'id': user,
+                               'pw': password})
 
-            m = re.search(r'"err":"(.+?)"', html)
-            if m is not None:
-                self.fail_login(m.group(1))
+        m = re.search(r'"err":"(.+?)"', html)
+        if m is not None:
+            self.fail_login(m.group(1))
 
-        except Exception, e:
-            self.log_error(e.message, trace=True)
-            self.fail_login(e.message)
+        html = self.load("http://uploaded.net/me")
+        if self.LOGIN_SKIP_PATTERN not in html:
+            self.fail_login()
