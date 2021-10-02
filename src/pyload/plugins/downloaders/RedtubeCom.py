1b56e37b818f3ed821d022408bc62f84904a9fc0
GammaC0de <gammac0de@users.noreply.github.com>
2021-10-03 00:09:17 +0300

    [RedtubeCom] fix #3815

--- src/pyload/plugins/downloaders/RedtubeCom.py
+++ src/pyload/plugins/downloaders/RedtubeCom.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import json
-import os
 import re
-import urllib.parse
 
 from ..base.downloader import BaseDownloader
 
@@ -11,35 +9,41 @@ from ..base.downloader import BaseDownloader
 class RedtubeCom(BaseDownloader):
     __name__ = "RedtubeCom"
     __type__ = "downloader"
-    __version__ = "0.27"
+    __version__ = "0.28"
     __status__ = "testing"
 
-    __pattern__ = r"http://(?:www\.)?redtube\.com/\d+"
+    __pattern__ = r"https?://(?:www\.)?redtube\.com/\d+"
     __config__ = [("enabled", "bool", "Activated", True)]
 
     __description__ = """Redtube.com downloader plugin"""
     __license__ = "GPLv3"
-    __authors__ = [("jeix", "jeix@hasnomail.de")]
-
-    NAME_PATTERN = r'videoTitle: "(?P<N>.+?)",'
+    __authors__ = [
+        ("jeix", "jeix@hasnomail.de"),
+        ("GammaC0de", "nitzo2001[AT}yahoo[DOT]com"),
+    ]
 
     def process(self, pyfile):
         html = self.load(pyfile.url)
 
-        m = re.search(r"sources: ({.+?}),", html)
+        m = re.search(r"playervars: ({.+}),", html)
         if m is None:
-            self.error(self._("sources pattern not found"))
+            self.error(self._("playervars pattern not found"))
 
-        sources = json.loads(m.group(1))
-        quality = str(max(int(q) for q in sources.keys()))
+        playervars = json.loads(m.group(1))
 
-        link = sources[quality]
+        media_info = [
+            x["videoUrl"]
+            for x in playervars["mediaDefinitions"]
+            if x.get("format") == "mp4" and x.get("remote") is True
+        ]
+        if len(media_info) == 0:
+            self.fail(self._("no media definitions found"))
 
-        m = re.search(self.NAME_PATTERN, html)
-        if m is None:
-            self.error(self._("name pattern not found"))
+        video_info = json.loads(self.load(media_info[0]))
+        video_info = sorted(video_info, key=lambda k: int(k["quality"]), reverse=True)
 
-        ext = os.path.splitext(urllib.parse.urlparse(link).path)[1]
-        pyfile.name = m.group(1) + ext
+        link = video_info[0]["videoUrl"]
+
+        pyfile.name = playervars["video_title"] + ".mp4"
 
         self.download(link)
