7d4545327de4f4b1fbf5e96669d3368b6ed9a580
GammaC0de <gammac0de@users.noreply.github.com>
2021-11-05 15:44:49 +0200

    [MegaCoNz] fix #4030

--- src/pyload/plugins/decrypters/MegaCoNzFolder.py
+++ src/pyload/plugins/decrypters/MegaCoNzFolder.py
@@ -12,10 +12,10 @@ from ..downloaders.MegaCoNz import MegaClient, MegaCrypto
 class MegaCoNzFolder(BaseDecrypter):
     __name__ = "MegaCoNzFolder"
     __type__ = "decrypter"
-    __version__ = "0.24"
+    __version__ = "0.25"
     __status__ = "testing"
 
-    __pattern__ = r"(?:https?://(?:www\.)?mega(?:\.co)?\.nz/|mega:|chrome:.+?)(?:folder/|#F!)(?P<ID>[\w^_]+)[!#](?P<KEY>[\w,\-=]+)"
+    __pattern__ = r"https?://(?:www\.)?mega(?:\.co)?\.nz/folder/(?P<ID>[\w^_]+)#(?P<KEY>[\w,\-=]+)/?$"
     __config__ = [
         ("enabled", "bool", "Activated", True),
         ("use_premium", "bool", "Use premium account if available", True),
@@ -54,32 +54,19 @@ class MegaCoNzFolder(BaseDecrypter):
             "ID: {}".format(id), "Key: {}".format(master_key), "Type: public folder"
         )
 
-        master_key = MegaCrypto.base64_to_a32(master_key)
-
         mega = MegaClient(self, id)
 
         #: F is for requesting folder listing (kind like a `ls` command)
         res = mega.api_request(a="f", c=1, r=1, ca=1, ssl=1)
-
         if isinstance(res, int):
             mega.check_error(res)
         elif "e" in res:
             mega.check_error(res["e"])
 
-        def get_node_key(k):
-            return to_str(
-                MegaCrypto.base64_encode(
-                    MegaCrypto.a32_to_bytes(MegaCrypto.decrypt_key(k, master_key))
-                ),
-                "ascii",
-            )
-
         urls = [
-            "https://mega.co.nz/#N!{}!{}###n={}".format(
-                f["h"], get_node_key(f["k"][f["k"].index(":") + 1 :]), id
-            )
-            for f in res["f"]
-            if f["t"] == 0 and ":" in f["k"]
+            "https://mega.co.nz/folder/{}#{}/file/{}".format(id, master_key, node["h"])
+            for node in res["f"]
+            if node["t"] == 0 and ":" in node["k"]
         ]
 
         if urls:
