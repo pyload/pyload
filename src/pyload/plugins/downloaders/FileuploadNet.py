fda0a9e6e8eee4776391876fb5bb06df8a54acb5
GammaC0de <gammac0de@users.noreply.github.com>
2021-11-28 00:46:21 +0200

    [FileUploadNet] fix #4038

--- src/pyload/plugins/downloaders/FileuploadNet.py
+++ src/pyload/plugins/downloaders/FileuploadNet.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from ..anticaptchas.ReCaptcha import ReCaptcha
 from ..base.simple_downloader import SimpleDownloader
 
 
 class FileuploadNet(SimpleDownloader):
     __name__ = "FileuploadNet"
     __type__ = "downloader"
-    __version__ = "0.07"
+    __version__ = "0.08"
     __status__ = "testing"
 
-    __pattern__ = r"https?://(?:www\.)?(en\.)?file-upload\.net/download-\d+/.+"
+    __pattern__ = r"https?://(?:www\.)?file-upload\.net/(?:en/)?download-\d+/.+"
     __config__ = [
         ("enabled", "bool", "Activated", True),
         ("use_premium", "bool", "Use premium account if available", True),
@@ -20,7 +21,10 @@ class FileuploadNet(SimpleDownloader):
 
     __description__ = """File-upload.net downloader plugin"""
     __license__ = "GPLv3"
-    __authors__ = [("zapp-brannigan", "fuerst.reinje@web.de")]
+    __authors__ = [
+        ("zapp-brannigan", "fuerst.reinje@web.de"),
+        ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com"),
+    ]
 
     NAME_PATTERN = r"<title>File-Upload.net - (?P<N>.+?)<"
     SIZE_PATTERN = r"</label><span>(?P<S>[\d.,]+) (?P<U>[\w^_]+)"
@@ -31,3 +35,19 @@ class FileuploadNet(SimpleDownloader):
     def setup(self):
         self.multi_dl = True
         self.chunk_limit = 1
+
+    def handle_free(self, pyfile):
+        action, inputs = self.parse_html_form('id="downloadstart"')
+        if not inputs:
+            self.fail(self._("download form not found"))
+
+        self.captcha = ReCaptcha(pyfile)
+        captcha_key = self.captcha.detect_key()
+
+        if captcha_key:
+            result = self.captcha.challenge(captcha_key, version="2invisible")
+            inputs["g-recaptcha-response"] = result
+            self.download(action, post=inputs)
+
+        else:
+            self.fail(self._("ReCaptcha key not found"))
