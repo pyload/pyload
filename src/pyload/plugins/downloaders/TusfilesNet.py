19e52942a44add9a710f9626ddf103283a764427
GammaC0de <gammac0de@users.noreply.github.com>
2022-03-05 23:02:26 +0200

    [TusfilesNet] (fixes #4102)

--- src/pyload/plugins/downloaders/TusfilesNet.py
+++ src/pyload/plugins/downloaders/TusfilesNet.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from pyload.core.network.exceptions import Retry
-from pyload.core.network.http.exceptions import BadHeader
-
 from ..base.xfs_downloader import XFSDownloader
 
 
 class TusfilesNet(XFSDownloader):
     __name__ = "TusfilesNet"
     __type__ = "downloader"
-    __version__ = "0.18"
+    __version__ = "0.19"
     __status__ = "testing"
 
-    __pattern__ = r"https?://(?:www\.)?tusfiles\.net/\w{12}"
+    __pattern__ = r"https?://(?:www\.)?tusfiles\.(?:net|com)/\w{12}"
     __config__ = [
         ("enabled", "bool", "Activated", True),
         ("use_premium", "bool", "Use premium account if available", True),
@@ -26,23 +23,19 @@ class TusfilesNet(XFSDownloader):
     __authors__ = [
         ("Walter Purcaro", "vuolter@gmail.com"),
         ("guidobelix", "guidobelix@hotmail.it"),
+        ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com"),
     ]
 
-    PLUGIN_DOMAIN = "tusfiles.net"
+    PLUGIN_DOMAIN = "tusfiles.com"
+    URL_REPLACEMENTS = [(r"tusfiles\.net", "tusfiles.com")]
 
-    INFO_PATTERN = r"\](?P<N>.+) - (?P<S>[\d.,]+) (?P<U>[\w^_]+)\["
+    NAME_PATTERN = r'fa-file-o"></i>\s*(?P<N>.+) <'
+    SIZE_PATTERN = r"<b>\((?P<S>[\d.,]+) (?P<U>[\w^_]+)\)</b></small>"
+
+    OFFLINE_PATTERN = r"The file you are trying to download is no longer available!"
 
     def setup(self):
-        self.chunk_limit = -1
+        self.resume_download = True
+        self.chunk_limit = 1
         self.multi_dl = True
         self.limit_dl = 2
-        self.resume_download = True
-
-    def download(self, url, *args, **kwargs):
-        try:
-            return super().download(url, *args, **kwargs)
-
-        except BadHeader as exc:
-            if exc.code == 503:
-                self.multi_dl = False
-                raise Retry("503")
