239cce9bad82ad65659e4645855aa8f915268c2f
GammaC0de <gammac0de@users.noreply.github.com>
2022-03-13 00:06:57 +0200

    [NitroflareCom] fix #4108

--- module/plugins/accounts/NitroflareCom.py
+++ module/plugins/accounts/NitroflareCom.py
@@ -9,7 +9,7 @@ from ..internal.misc import json
 class NitroflareCom(Account):
     __name__ = "NitroflareCom"
     __type__ = "account"
-    __version__ = "0.20"
+    __version__ = "0.21"
     __status__ = "testing"
 
     __description__ = """Nitroflare.com account plugin"""
@@ -17,40 +17,40 @@ class NitroflareCom(Account):
     __authors__ = [("Walter Purcaro", "vuolter@gmail.com"),
                    ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
+    # See https://nitroflare.com/member?s=general-api
+    API_URL = "https://nitroflare.com/api/v2/"
+
+    def api_request(self, method, **kwargs):
+        json_data = self.load(self.API_URL + method, get=kwargs)
+        return json.loads(json_data)
+
     def grab_info(self, user, password, data):
         validuntil = -1
         trafficleft = None
         premium = False
 
-        data = json.loads(self.load("https://nitroflare.com/api/v2/getKeyInfo",
-                                    get={'user': user,
-                                         'premiumKey': password}))
+        api_data = self.api_request("getKeyInfo", user=user, premiumKey=password)
 
-        if data['type'] == 'success':
-            trafficleft = self.parse_traffic(
-                data['result']['trafficLeft'], "byte")
-            premium = data['result']['status'] == "active"
+        if api_data['type'] == 'success':
+            trafficleft = self.parse_traffic(api_data['result']['trafficLeft'], "byte")
+            premium = api_data['result']['status'] == "active"
 
             if premium:
-                validuntil = time.mktime(
-                    time.strptime(
-                        data['result']['expiryDate'],
-                        '%Y-%m-%d %H:%M:%S'))
+                validuntil = time.mktime(time.strptime(api_data['result']['expiryDate'], '%Y-%m-%d %H:%M:%S'))
 
         return {'validuntil': validuntil,
                 'trafficleft': trafficleft,
                 'premium': premium}
 
-    def signin(self, user, password, data):
-        data = json.loads(self.load("https://nitroflare.com/api/v2/getKeyInfo",
-                                    get={'user': user,
-                                         'premiumKey': password}))
+    def signin(self, user, password, data1):
+        api_data = self.api_request("getKeyInfo", user=user, premiumKey=password)
 
-        if data['type'] != 'success':
-            self.fail_login()
+        if api_data["type"] != "success":
+            error_code = api_data["code"]
 
-        elif data['result'].get("status") == "banned":
-            self.fail_login(_("Banned"))
+            if error_code != 12:
+                self.log_error(api_data["message"])
+                self.fail_login()
 
-        elif 'recaptchaPublic' in data['result']:
-            self.fail_login(_("Account Login Requires Recaptcha"))
+            else:
+                self.fail_login(_("Account Login Requires Recaptcha"))
