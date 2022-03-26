2c3151c00e9a8f3c24e38e5bf8c01c320991b164
GammaC0de <gammac0de@users.noreply.github.com>
2022-03-26 14:10:04 +0300

    [FastshareCz] fix #4117

--- module/plugins/accounts/FastshareCz.py
+++ module/plugins/accounts/FastshareCz.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import re
+import time
 
 from ..internal.Account import Account
 from ..internal.misc import set_cookie
@@ -9,43 +10,60 @@ from ..internal.misc import set_cookie
 class FastshareCz(Account):
     __name__ = "FastshareCz"
     __type__ = "account"
-    __version__ = "0.17"
+    __version__ = "0.18"
     __status__ = "testing"
 
     __description__ = """Fastshare.cz account plugin"""
     __license__ = "GPLv3"
     __authors__ = [("zoidberg", "zoidberg@mujmail.cz"),
                    ("stickell", "l.stickell@yahoo.it"),
-                   ("ondrej", "git@ondrej.it")]
+                   ("ondrej", "git@ondrej.it"),
+                   ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
-    CREDIT_PATTERN = r'<a href="/user">.+<span>\(([\d\.]+) ([MGT]+B)\)</span></a>'
+    TRAFFICLEFT_PATTERN = r'<td>([\d\.]+) ([KMGT]B)\s*</td>'
+    VALID_UNTILL_PATTERN = r">Active until ([\d.]+)<"
 
     def grab_info(self, user, password, data):
-        validuntil = -1
-        trafficleft = None
-        premium = False
-
         html = self.load("https://www.fastshare.cz/user")
 
-        m = re.search(self.CREDIT_PATTERN, html)
+        m = re.search(self.VALID_UNTILL_PATTERN, html)
         if m is not None:
-            trafficleft = self.parse_traffic(m.group(1), m.group(2))
+            validuntil = time.mktime(
+                time.strptime(m.group(1) + " 23:59:59", "%d.%m.%Y %H:%M:%S")
+            )
+            premium = True
+            trafficleft = -1
 
-        premium = bool(trafficleft)
+        else:
+            validuntil = -1
+            m = re.search(self.TRAFFICLEFT_PATTERN, html)
+            if m is not None:
+                trafficleft = self.parse_traffic(m.group(1), m.group(2))
+                premium = bool(trafficleft)
+                if not premium:
+                    trafficleft = None
 
-        return {'validuntil': validuntil,
-                'trafficleft': trafficleft,
-                'premium': premium}
+            else:
+                premium = False
+                trafficleft = None
+
+        return {
+            "validuntil": validuntil,
+            "trafficleft": trafficleft,
+            "premium": premium,
+        }
 
     def signin(self, user, password, data):
         set_cookie(self.req.cj, "fastshare.cz", "lang", "en")
 
-        # @NOTE: Do not remove or it will not login
-        self.load('https://www.fastshare.cz/login')
+        html = self.load("https://www.fastshare.cz/user")
+        if 'href="/logout.php"' in html:
+            self.skip_login()
 
-        html = self.load("https://www.fastshare.cz/sql.php",
-                         post={'login': user,
-                               'heslo': password})
+        html = self.load(
+            "https://www.fastshare.cz/sql.php",
+            post={"login": user, "heslo": password},
+        )
 
-        if ">Wrong username or password" in html:
+        if 'href="/logout.php"' not in html:
             self.fail_login()
