550a2f18a6bbba02e2a3c3ca7fbcd8307b24622e
GammaC0de <gammac0de@users.noreply.github.com>
2022-05-17 02:44:02 +0300

    add support for BayfilesCom (#4141)

---
+++ module/plugins/hoster/BayfilesCom.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from ..internal.misc import json
+from ..internal.SimpleHoster import SimpleHoster
+
+
+class BayfilesCom(SimpleHoster):
+    __name__ = "BayfilesCom"
+    __type__ = "hoster"
+    __version__ = "0.01"
+    __status__ = "testing"
+
+    __pattern__ = r"https://bayfiles.com/(?P<ID>\w+)"
+    __config__ = [("enabled", "bool", "Activated", True),
+                  ("use_premium", "bool", "Use premium account if available", True),
+                  ("fallback", "bool", "Fallback to free download if premium fails", True),
+                  ("chk_filesize", "bool", "Check file size", True),
+                  ("max_wait", "int", "Reconnect if waiting time is greater than minutes", 10)]
+
+    __description__ = """Bayfiles.com hoster plugin"""
+    __license__ = "GPLv3"
+    __authors__ = [("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
+
+    URL_REPLACEMENTS = [(r"^http://", "https://")]
+
+    LINK_PATTERN = r'href="(https://cdn-\d+\.bayfiles\.com/.+?)"'
+
+    def api_info(self, url):
+        info = {}
+        file_id = re.match(self.__pattern__, url).group("ID")
+        json_data = self.load("https://api.bayfiles.com/v2/file/%s/info" % file_id)
+        api_data = json.loads(json_data)
+
+        if api_data["status"] is True:
+            info["status"] = 2
+            info["name"] = api_data["data"]["file"]["metadata"]["name"]
+            info["size"] = api_data["data"]["file"]["metadata"]["size"]["bytes"]
+
+        else:
+            if api_data["error"]["type"] in ("FILE_NOT_FOUND", "ERROR_FILE_BANNED"):
+                info["status"] = 1
+
+            else:
+                info["error"] = api_data["error"]["message"]
+                info["status"] = 8
+
+        return info
