88ea6ab8ea5cffea1a1d872a22feeb6389aeda0a
Web-Eye <Web-Eye@gmx.net>
2022-05-21 14:48:53 +0200

    [TurbobitNet] - add another base url

--- src/pyload/plugins/downloaders/TurbobitNet.py
+++ src/pyload/plugins/downloaders/TurbobitNet.py
@@ -13,10 +13,10 @@ from ..base.simple_downloader import SimpleDownloader
 class TurbobitNet(SimpleDownloader):
     __name__ = "TurbobitNet"
     __type__ = "downloader"
-    __version__ = "0.42"
+    __version__ = "0.43"
     __status__ = "testing"
 
-    __pattern__ = r'https?://(?:(?:www|m)\.)?(?:turbobit\.net|turbo?\.(?:to|cc))/(?:download/free/)?(?P<ID>\w+)'
+    __pattern__ = r'https?://(?:(?:www|m)\.)?(?:turbobit\.net|turbo?\.(?:to|cc)|turb\.pw)/(?:download/free/)?(?P<ID>\w+)'
     __config__ = [
         ("enabled", "bool", "Activated", True),
         ("use_premium", "bool", "Use premium account if available", True),
