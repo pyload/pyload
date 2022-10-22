3e0e56c32a5645c7cb5efd2b6760d4e95ee5daf3
GammaC0de <gammac0de@users.noreply.github.com>
2022-10-22 13:11:08 +0300

    [VeohCom] update

--- module/plugins/hoster/VeohCom.py
+++ module/plugins/hoster/VeohCom.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import re
-
+from ..internal.misc import json
 from ..internal.SimpleHoster import SimpleHoster
 
 
 class VeohCom(SimpleHoster):
     __name__ = "VeohCom"
     __type__ = "hoster"
-    __version__ = "0.28"
+    __version__ = "0.29"
     __status__ = "testing"
 
-    __pattern__ = r'http://(?:www\.)?veoh\.com/(tv/)?(watch|videos)/(?P<ID>v\w+)'
+    __pattern__ = r'https?://(?:www\.)?veoh\.com/(?:tv/)?(?:watch|videos)/(?P<ID>v\w+)'
     __config__ = [("activated", "bool", "Activated", True),
                   ("use_premium", "bool", "Use premium account if available", True),
                   ("fallback", "bool",
@@ -21,7 +20,8 @@ class VeohCom(SimpleHoster):
 
     __description__ = """Veoh.com hoster plugin"""
     __license__ = "GPLv3"
-    __authors__ = [("Walter Purcaro", "vuolter@gmail.com")]
+    __authors__ = [("Walter Purcaro", "vuolter@gmail.com"),
+                   ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
     NAME_PATTERN = r'<meta name="title" content="(?P<N>.*?)"'
     OFFLINE_PATTERN = r'>Sorry, we couldn\'t find the video you were looking for'
@@ -38,18 +38,7 @@ class VeohCom(SimpleHoster):
         self.chunk_limit = -1
 
     def handle_free(self, pyfile):
-        quality = self.config.get('quality')
-        if quality == "Auto" or quality is None:
-            quality = ("High", "Low")
-
-        for q in quality:
-            pattern = r'"fullPreviewHash%sPath":"(.+?)"' % q
-            m = re.search(pattern, self.data)
-            if m is not None:
-                pyfile.name += ".mp4"
-                self.link = m.group(1).replace("\\", "")
-                return
-            else:
-                self.log_info(_("No %s quality video found") % q.upper())
-        else:
-            self.fail(_("No video found!"))
+        video_id = self.info['pattern']['ID']
+        video_data = json.loads(self.load(r"https://www.veoh.com/watch/getVideo/%s" % video_id))
+        pyfile.name = video_data["video"]['title'] + ".mp4"
+        self.link = video_data["video"]['src']['HQ']
