a472e16077a19d81142301532729a665e29a2d5f
GammaC0de <gammac0de@users.noreply.github.com>
2023-02-12 03:44:36 +0200

    bump version

--- src/pyload/plugins/downloaders/DropDownload.py
+++ src/pyload/plugins/downloaders/DropDownload.py
@@ -6,7 +6,7 @@ from ..base.xfs_downloader import XFSDownloader
 class DropDownload(XFSDownloader):
     __name__ = "DropDownload"
     __type__ = "downloader"
-    __version__ = "0.01"
+    __version__ = "0.02"
     __status__ = "testing"
 
     __pattern__ = r"https?://(?:www\.)?drop\.download/\w{12}"
