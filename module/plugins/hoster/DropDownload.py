89f4cc0553af8ad392444677121317c1c96a2eeb
GammaC0de <gammac0de@users.noreply.github.com>
2023-02-18 22:14:17 +0200

    bugfix

--- module/plugins/hoster/DropDownload.py
+++ module/plugins/hoster/DropDownload.py
@@ -5,20 +5,20 @@ from ..internal.XFSHoster import XFSHoster
 
 class DropDownload(XFSHoster):
     __name__ = "DropDownload"
-    __type__ = "downloader"
-    __version__ = "0.02"
+    __type__ = "hoster"
+    __version__ = "0.03"
     __status__ = "testing"
 
     __pattern__ = r"https?://(?:www\.)?drop\.download/\w{12}"
     __config__ = [
-        ("enabled", "bool", "Activated", True),
+        ("activated", "bool", "Activated", True),
         ("use_premium", "bool", "Use premium account if available", True),
         ("fallback", "bool", "Fallback to free download if premium fails", True),
         ("chk_filesize", "bool", "Check file size", True),
         ("max_wait", "int", "Reconnect if waiting time is greater than minutes", 10),
     ]
 
-    __description__ = """Drop.download downloader plugin"""
+    __description__ = """Drop.download hoster plugin"""
     __license__ = "GPLv3"
     __authors__ = [("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
