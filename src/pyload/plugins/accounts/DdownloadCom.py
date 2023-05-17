9e8ada9dadd661a4634fd14630c64315c3c26208
GammaC0de <gammac0de@users.noreply.github.com>
2023-05-18 01:19:48 +0300

    temp fix for #4292

--- src/pyload/plugins/accounts/DdownloadCom.py
+++ src/pyload/plugins/accounts/DdownloadCom.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import pycurl
+
 from ..base.xfs_account import XFSAccount
-from ..helpers import search_pattern
 
 
 class DdownloadCom(XFSAccount):
     __name__ = "DdownloadCom"
     __type__ = "account"
-    __version__ = "0.06"
+    __version__ = "0.07"
     __status__ = "testing"
 
     __description__ = """Ddownload.com account plugin"""
@@ -15,7 +16,12 @@ class DdownloadCom(XFSAccount):
     __authors__ = [("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
     PLUGIN_DOMAIN = "ddownload.com"
+    PLUGIN_URL = "http://ddownload.com"
 
     PREMIUM_PATTERN = r"Premium Account \(expires"
     TRAFFIC_LEFT_PATTERN = r'<span>Traffic available</span>\s*<div class="price">(?:<sup>(?P<U>[^<>]+)</sup>)?(?P<S>-?\d+|[Uu]nlimited)</div>'
     VALID_UNTIL_PATTERN = r"Premium Account \(expires ([^)]+)\)"
+
+    def setup(self):
+        super(DdownloadCom, self).setup()
+        self.req.http.c.setopt(pycurl.USERAGENT, "pyLoad/{}".format(self.pyload.version))
