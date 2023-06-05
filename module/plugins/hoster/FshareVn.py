59d4932257008cfacd8eee3f7e2ed2b9ff82278f
GammaC0de <gammac0de@users.noreply.github.com>
2023-06-05 18:16:48 +0300

    [FshareVn] fix #4306

--- module/plugins/hoster/FshareVn.py
+++ module/plugins/hoster/FshareVn.py
@@ -14,7 +14,7 @@ from ..internal.SimpleHoster import SimpleHoster
 class FshareVn(SimpleHoster):
     __name__ = "FshareVn"
     __type__ = "hoster"
-    __version__ = "0.37"
+    __version__ = "0.38"
     __status__ = "testing"
 
     __pattern__ = r'https?://(?:www\.)?fshare\.vn/file/(?P<ID>\w+)'
@@ -29,7 +29,10 @@ class FshareVn(SimpleHoster):
     __authors__ = [("zoidberg", "zoidberg@mujmail.cz"),
                    ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
+    DIRECT_LINK = False
+
     OFFLINE_PATTERN = ur'Tập tin của bạn yêu cầu không tồn tại'
+    TEMP_OFFLINE_PATTERN = r"^unmatchable$"
 
     URL_REPLACEMENTS = [("http://", "https://")]
 
