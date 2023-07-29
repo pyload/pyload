9093b6939e78dcc84e17dcb8f49a01498a54dce5
WebEye <web-eye@gmx.net>
2023-07-29 16:55:41 +0200

    merging from pyload

--- src/pyload/plugins/accounts/TurbobitNet.py
+++ src/pyload/plugins/accounts/TurbobitNet.py
@@ -21,7 +21,6 @@ class TurbobitNet(BaseAccount):
 
     LOGIN_FAIL_PATTERN = r">(?:E-Mail address appears to be invalid\. Please try again|Incorrect login or password)</div>"
 
-
     def grab_info(self, user, password, data):
         html = self.load("https://turbobit.net/")
 
