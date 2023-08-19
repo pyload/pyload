0a8e0ea58049120e7da4e6bafa85a024895423ac
GammaC0de <gammac0de@users.noreply.github.com>
2023-08-20 01:17:46 +0300

    add support filtering a list of mega.nz files down to a single sub folder (#4349)

--- src/pyload/plugins/decrypters/MegaCoNzFolder.py
+++ src/pyload/plugins/decrypters/MegaCoNzFolder.py
@@ -12,10 +12,10 @@ from ..downloaders.MegaCoNz import MegaClient, MegaCrypto
 class MegaCoNzFolder(BaseDecrypter):
     __name__ = "MegaCoNzFolder"
     __type__ = "decrypter"
-    __version__ = "0.26"
+    __version__ = "0.27"
     __status__ = "testing"
 
-    __pattern__ = r"https?://(?:www\.)?mega(?:\.co)?\.nz/folder/(?P<ID>[\w^_]+)#(?P<KEY>[\w,\-=]+)(/(folder/(?P<SUBDIR>[\w]+)))?$"
+    __pattern__ = r"https?://(?:www\.)?mega(?:\.co)?\.nz/folder/(?P<ID>[\w^_]+)#(?P<KEY>[\w,\-=]+)(?:/folder/(?P<SUBDIR>[\w]+))?/?$"
     __config__ = [
         ("enabled", "bool", "Activated", True),
         ("use_premium", "bool", "Use premium account if available", True),
@@ -49,7 +49,7 @@ class MegaCoNzFolder(BaseDecrypter):
     def decrypt(self, pyfile):
         id = self.info["pattern"]["ID"]
         master_key = self.info["pattern"]["KEY"]
-        subdir = self.info["pattern"].get("SUBDIR")
+        subdir = self.info["pattern"]["SUBDIR"]
 
         self.log_debug(
             "ID: {}".format(id), "Key: {}".format(master_key), "Type: public folder"
@@ -68,7 +68,7 @@ class MegaCoNzFolder(BaseDecrypter):
             "https://mega.co.nz/folder/{}#{}/file/{}".format(id, master_key, node["h"])
             for node in res["f"]
             if node["t"] == 0 and ":" in node["k"]
-            if not subdir or node["p"] == subdir
+            if subdir is None or node["p"] == subdir
         ]
 
         if urls:
