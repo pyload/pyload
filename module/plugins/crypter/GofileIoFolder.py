5eaeaae63be53dde63052774a4e71c91a611a1c9
GammaC0de <gammac0de@users.noreply.github.com>
2024-04-01 00:09:14 +0300

    [GofileIoFolder] fix #4445

--- module/plugins/crypter/GofileIoFolder.py
+++ module/plugins/crypter/GofileIoFolder.py
@@ -3,6 +3,7 @@
 import base64
 import urllib
 
+import pycurl
 from module.network.HTTPRequest import BadHeader
 
 from ..internal.Crypter import Crypter
@@ -12,7 +13,7 @@ from ..internal.misc import json
 class GofileIoFolder(Crypter):
     __name__ = "GofileIoFolder"
     __type__ = "crypter"
-    __version__ = "0.02"
+    __version__ = "0.03"
     __status__ = "testing"
 
     __pattern__ = r"https?://(?:www\.)?gofile\.io/d/(?P<ID>\w+)"
@@ -29,24 +30,27 @@ class GofileIoFolder(Crypter):
 
     API_URL = "https://api.gofile.io/"
 
-    def api_request(self, method, **kwargs):
+    def api_request(self, method, token=None, get={}, post={}):
+        if token is not None:
+            self.req.http.c.setopt(
+                pycurl.HTTPHEADER, ["Authorization: Bearer " + token]
+            )
         try:
-            json_data = self.load(self.API_URL + method, get=kwargs)
+            json_data = self.load(self.API_URL + method, get=get, post=post)
         except BadHeader, e:
             json_data = e.content
 
         return json.loads(json_data)
 
     def decrypt(self, pyfile):
-        api_data = self.api_request("createAccount")
+        api_data = self.api_request("accounts", post=True)
         if api_data["status"] != "ok":
-            self.fail(_("createAccount API failed | %s") % api_data["status"])
+            self.fail(_("accounts API failed | %s") % api_data["status"])
 
         token = api_data["data"]["token"]
-        api_data = self.api_request("getContent",
-                                    contentId=self.info["pattern"]["ID"],
+        api_data = self.api_request("contents/%s" % self.info["pattern"]["ID"],
                                     token=token,
-                                    websiteToken=12345)
+                                    get={"wt": "4fd6sg89d7s6"})
         status = api_data["status"]
         if status == "ok":
             pack_links = ["https://gofile.io/dl?q=%s" %
@@ -55,7 +59,7 @@ class GofileIoFolder(Crypter):
                                                        "n": urllib.quote_plus(file_data["name"].encode("utf8")),
                                                        "s": file_data["size"],
                                                        "m": file_data["md5"]}))
-                          for file_data in api_data["data"]["contents"].values()
+                          for file_data in api_data["data"]["children"].values()
                           if file_data["type"] == "file"]
 
             if pack_links:
