8e4000df01008dea78225c9cdb8472a001795402
GammaC0de <gammac0de@users.noreply.github.com>
2024-05-11 19:58:42 +0300

    [WorldbytezCom] update patterns (fixes #4458)

--- src/pyload/plugins/accounts/WorldbytezCom.py
+++ src/pyload/plugins/accounts/WorldbytezCom.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import re
+
 from ..base.xfs_account import XFSAccount
 
 
 class WorldbytezCom(XFSAccount):
     __name__ = "WorldbytezCom"
     __type__ = "account"
-    __version__ = "0.07"
+    __version__ = "0.08"
     __status__ = "testing"
 
     __description__ = """Worldbytez.com account plugin"""
     __license__ = "GPLv3"
-    __authors__ = [("Walter Purcaro", "vuolter@gmail.com")]
+    __authors__ = [
+        ("Walter Purcaro", "vuolter@gmail.com"),
+        ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")
+    ]
 
     PLUGIN_DOMAIN = "worldbytez.com"
     PLUGIN_URL = "https://worldbytez.com/"
 
-    PREMIUM_PATTERN = r">Premium Is Enabled<"
-    VALID_UNTIL_PATTERN = r">Expire (.+?)<"
-    TRAFFIC_LEFT_PATTERN = r">Daily Traffic .*<p [\w'\"=-]+> (?P<U>[\w^_]+) <sup></sup> (?P<S>[\d.,]+)<"
+    PREMIUM_PATTERN = r">Premium<"
+    VALID_UNTIL_PATTERN = r"- Expire (.+)"
+    TRAFFIC_LEFT_PATTERN = r"Daily Traffic.*<p [\w'\"=-]+><sup></sup>\s*(?P<S>.+?)<", re.S
