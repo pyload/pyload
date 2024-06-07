b787f55cc76fe42a128abbfb8761e17cd79eab2d
EnergoStalin <ens.stalin@gmail.com>
2024-06-08 02:05:48 +0300

    style: black format

--- src/pyload/plugins/downloaders/SKnigavuhe.py
+++ src/pyload/plugins/downloaders/SKnigavuhe.py
@@ -2,12 +2,15 @@
 
 from ..base.simple_downloader import SimpleDownloader
 
+
 class SKnigavuhe(SimpleDownloader):
     __name__ = "SKnigavuhe"
     __type__ = "downloader"
     __version__ = "0.1"
     __status__ = "testing"
-    __pattern__ = r"https?:\/\/s\d+\.knigavuhe\.org\/\d+\/audio\/\d+\/[0-9a-z\-]+\.mp3.+"
+    __pattern__ = (
+        r"https?:\/\/s\d+\.knigavuhe\.org\/\d+\/audio\/\d+\/[0-9a-z\-]+\.mp3.+"
+    )
 
     __authors__ = [("EnergoStalin", "ens.stalin@gmail.com")]
     __license__ = "GPLv3"
@@ -15,4 +18,4 @@ class SKnigavuhe(SimpleDownloader):
     OFFLINE_PATTERN = r">404 Not Found<"
 
     def handle_direct(self, pyfile):
-        [self.link, pyfile.name] = pyfile.url.split('#')
+        [self.link, pyfile.name] = pyfile.url.split("#")
