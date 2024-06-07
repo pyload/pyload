6fc7c73389ab8145fe863d59e3b6f6fbd58268ab
EnergoStalin <ens.stalin@gmail.com>
2024-06-08 01:20:47 +0300

    feat: knigavuhe books decrypter and downloader
    downloader needed to conform filename i couldn't figure out a better way
    at a time

---
+++ src/pyload/plugins/decrypters/Knigavuhe.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+from ..base.decrypter import BaseDecrypter
+import re, json, os
+
+class Knigavuhe(BaseDecrypter):
+    __name__ = "Knigavuhe"
+    __type__ = "decrypter"
+    __version__ = "0.1"
+    __status__ = "testing"
+    __pattern__ = r"https?:\/\/knigavuhe\.org\/book\/.+"
+
+    __authors__ = [("EnergoStalin", "ens.stalin@gmail.com")]
+    __license__ = "GPLv3"
+
+    BOOK_PLAYER_DATA_PATTERN = r"new BookPlayer\(\d+,\s?(\[.+?\]),"
+    BOOK_TITLE_PATTERN = r"book = ({.+});"
+    VOLUME_PATTERN = r'((?:\d+.?)\d+)\.<\/span>\s+<strong>{}'
+
+    def _get_book_title(self, html):
+        text = re.search(self.BOOK_TITLE_PATTERN, html)
+        if not text:
+            self.fail("Could not obtain title.")
+            return
+
+        title = json.loads(text[1])["name"]
+        volume = re.search(self.VOLUME_PATTERN.format(title), html)
+        if volume: # If single volume omit number in series
+            title = f"{volume.group(1)}. {title}"
+
+        return title
+
+    def _get_book_data(self, html):
+        text = re.search(self.BOOK_PLAYER_DATA_PATTERN, html)
+        if not text:
+            self.fail("Could not find BookPlayer data.")
+            return
+
+        return json.loads(text.group(1))
+
+    def decrypt(self, pyfile):
+        html = self.load(pyfile.url)
+        data = self._get_book_data(html)
+        title = self._get_book_title(html)
+
+        if not data or not title:
+            return
+
+        self.packages.append(
+            (
+                title or pyfile.package().name,
+                [f"{x['url']}#{x['title']}{os.path.splitext(x['url'])[1]}" for x in data],
+                title or pyfile.package().folder,
+            )
+        )
---
+++ src/pyload/plugins/downloaders/SKnigavuhe.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from ..base.simple_downloader import SimpleDownloader
+
+class SKnigavuhe(SimpleDownloader):
+    __name__ = "SKnigavuhe"
+    __type__ = "downloader"
+    __version__ = "0.1"
+    __status__ = "testing"
+    __pattern__ = r"https?:\/\/s\d+\.knigavuhe\.org\/\d+\/audio\/\d+\/[0-9a-z\-]+\.mp3.+"
+
+    __authors__ = [("EnergoStalin", "ens.stalin@gmail.com")]
+    __license__ = "GPLv3"
+
+    OFFLINE_PATTERN = r">404 Not Found<"
+
+    def handle_direct(self, pyfile):
+        [self.link, pyfile.name] = pyfile.url.split('#')
