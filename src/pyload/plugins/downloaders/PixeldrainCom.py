295077c85e3d12ddb96f73c8143da9dc2335527c
GammaC0de <gammac0de@users.noreply.github.com>
2024-06-08 01:54:41 +0300

    [PixeldrainCom] fix #4473

--- src/pyload/plugins/downloaders/PixeldrainCom.py
+++ src/pyload/plugins/downloaders/PixeldrainCom.py
@@ -9,7 +9,7 @@ from ..base.simple_downloader import SimpleDownloader
 class PixeldrainCom(SimpleDownloader):
     __name__ = "PixeldrainCom"
     __type__ = "downloader"
-    __version__ = "0.02"
+    __version__ = "0.03"
     __status__ = "testing"
 
     __pattern__ = r"https?://(?:www\.)?pixeldrain\.com/u/(?P<ID>\w+)"
@@ -27,8 +27,8 @@ class PixeldrainCom(SimpleDownloader):
 
     DIRECT_LINK = False
 
-    API_URL = "https://pixeldrain.com/api/"
     #: See https://pixeldrain.com/api/
+    API_URL = "https://pixeldrain.com/api/"
 
     def api_info(self, url):
         file_id = re.match(self.__pattern__, url).group("ID")
@@ -41,6 +41,13 @@ class PixeldrainCom(SimpleDownloader):
         else:
             return {"name": file_info["name"], "size": file_info["size"], "status": 2}
 
+    def setup(self):
+        if self.premium:
+            self.req.add_auth(":{}".format(self.account.info["login"]["password"]))
+
     def handle_free(self, pyfile):
         file_id = self.info["pattern"]["ID"]
         self.download(f"{self.API_URL}/file/{file_id}")
+
+    def handle_premium(self, pyfile):
+        self.handle_free(pyfile)
