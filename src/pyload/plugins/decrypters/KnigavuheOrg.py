07f0fdc799263d3150e342d6ae6d100fed4ad38f
EnergoStalin <ens.stalin@gmail.com>
2024-07-07 12:27:53 +0300

    feat: book cover

--- src/pyload/plugins/decrypters/KnigavuheOrg.py
+++ src/pyload/plugins/decrypters/KnigavuheOrg.py
@@ -20,6 +20,7 @@ class KnigavuheOrg(BaseDecrypter):
     BOOK_PLAYER_DATA_PATTERN = r"new BookPlayer\(\d+,\s?(\[.+?\]),"
     BOOK_TITLE_PATTERN = r"book = ({.+});"
     VOLUME_PATTERN = r"((?:\d+.?)\d+)\.<\/span>\s+<strong>{}"
+    COVER_PATTERN = r"https:\/\/s2\.knigavuhe\.org\/2\/covers\/\d+\/1-2\.jpg\?1"
 
     def _get_book_title(self, html):
         text = re.search(self.BOOK_TITLE_PATTERN, html)
@@ -42,21 +43,32 @@ class KnigavuheOrg(BaseDecrypter):
 
         return json.loads(text.group(1))
 
+    def _get_book_cover(self, html):
+        text = re.search(self.COVER_PATTERN, html)
+        if not text:
+            self.fail("Could not find cover url.")
+            return
+
+        s, e = text.span()
+        return html[s:e]
+
     def decrypt(self, pyfile):
         html = self.load(pyfile.url)
         data = self._get_book_data(html)
         title = self._get_book_title(html)
+        cover = self._get_book_cover(html)
 
         if not data or not title:
             return
 
+        urls = [f"{x['url']}#{x['title']}{os.path.splitext(x['url'])[1]}" for x in data]
+        urls.append(f"{cover}#cover.jpg")
+
+        package = pyfile.package()
         self.packages.append(
             (
-                title or pyfile.package().name,
-                [
-                    f"{x['url']}#{x['title']}{os.path.splitext(x['url'])[1]}"
-                    for x in data
-                ],
-                title or pyfile.package().folder,
+                title or package.name,
+                urls,
+                title or package.folder,
             )
         )
