d8ffaabd8bc3286ac08d3d113ec398ee5100bc14
GammaC0de <gammac0de@users.noreply.github.com>
2024-07-31 21:41:00 +0300

    [ClickNLoad] fix #4496(2)

--- src/pyload/plugins/addons/ClickNLoad.py
+++ src/pyload/plugins/addons/ClickNLoad.py
@@ -187,7 +187,7 @@ class ClickNLoad(BaseAddon):
                         if self.pyload.webserver.use_ssl:
                             try:
                                 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-                                backend_socket = context.wrap_socket(backend_socket)
+                                backend_socket = context.wrap_socket(backend_socket, server_hostname=self.web_addr[0])
 
                             except Exception as exc:
                                 self.log_error(self._("SSL error: {}").format(exc))
