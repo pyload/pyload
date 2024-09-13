ce3201845a8921e9c4672c4d33e427b9b4ac4a2b
GammaC0de <gammac0de@users.noreply.github.com>
2024-09-13 19:31:20 +0300

    new hoster [SendCm] (fixes #4513)

---
+++ module/plugins/hoster/SendCm.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from ..internal.XFSHoster import XFSHoster
+
+
+class SendCm(XFSHoster):
+    __name__ = "SendCm"
+    __type__ = "hoster"
+    __version__ = "0.01"
+    __status__ = "testing"
+
+    __pattern__ = r'https?://(?:www\.)?send\.cm/\w{12}'
+    __config__ = [("activated", "bool", "Activated", True),
+                  ("use_premium", "bool", "Use premium account if available", True),
+                  ("fallback", "bool", "Fallback to free download if premium fails", True),
+                  ("chk_filesize", "bool", "Check file size", True),
+                  ("max_wait", "int", "Reconnect if waiting time is greater than minutes", 10)]
+
+    __description__ = """Send.cm downloader plugin"""
+    __license__ = "GPLv3"
+    __authors__ = [("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
+
+    PLUGIN_DOMAIN = "send.cm"
+
+    LINK_PATTERN = r"(https://d\d+.download-send.com/d/.*?)[\"'<]"
+    NAME_PATTERN = r"\"\[URL=https://send\.cm/\w{12}\](?P<N>.+?) -  \d+\[/URL\]\""
+
+    DISPOSITION = False
