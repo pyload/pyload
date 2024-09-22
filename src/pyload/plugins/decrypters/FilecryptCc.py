709c057890d4b1b8aa6d64b206e45646bdc06286
Milan Hauth <milahu@gmail.com>
2024-09-22 12:55:14 +0200

    fix FilecryptCc DRAFT

--- src/pyload/plugins/decrypters/FilecryptCc.py
+++ src/pyload/plugins/decrypters/FilecryptCc.py
@@ -48,11 +48,19 @@ class FilecryptCc(BaseDecrypter):
     WEBLINK_PATTERN = r"<button onclick=\"[\w\-]+?/\*\d+?\*/\('([\w/-]+?)',"
     MIRROR_PAGE_PATTERN = r'"[\w]*" href="(https?://(?:www\.)?filecrypt.cc/Container/\w+\.html\?mirror=\d+)">'
 
-    CAPTCHA_PATTERN = r"<h2>Security prompt</h2>"
+    #CAPTCHA_PATTERN = r"<h2>Security prompt</h2>"
+
     INTERNAL_CAPTCHA_PATTERN = r'<img id="nc" .* src="(.+?)"'
     CIRCLE_CAPTCHA_PATTERN = r'<input type="image" src="(.+?)"'
     KEY_CAPTCHA_PATTERN = r"<script language=JavaScript src='(http://backs\.keycaptcha\.com/swfs/cap\.js)'"
     SOLVEMEDIA_CAPTCHA_PATTERN = r'<script type="text/javascript" src="(https?://api(?:-secure)?\.solvemedia\.com/papi/challenge.+?)"'
+
+    CAPTCHA_PATTERNS = [
+        INTERNAL_CAPTCHA_PATTERN,
+        CIRCLE_CAPTCHA_PATTERN,
+        KEY_CAPTCHA_PATTERN,
+        SOLVEMEDIA_CAPTCHA_PATTERN,
+    ]
 
     def setup(self):
         self.urls = []
@@ -73,28 +81,34 @@ class FilecryptCc(BaseDecrypter):
         self.log_info("74 pyfile.url", pyfile.url)
 
         self.data = self._filecrypt_load_url(pyfile.url)
+        self.log_info("plugins/decrypters/FilecryptCc.py: self.data[:1000]", self.data[:1000])
 
         # @NOTE: "content notfound" is NOT a typo
         if (
             "content notfound" in self.data
             or ">File <strong>not</strong> found<" in self.data
         ):
+            self.log_info("plugins/decrypters/FilecryptCc.py: offline")
             self.offline()
 
-        self.log_info("84 handle_password_protection")
+        self.log_info("plugins/decrypters/FilecryptCc.py: handle_password_protection")
         self.handle_password_protection()
 
+        self.log_info("plugins/decrypters/FilecryptCc.py: handle_captcha")
         self.site_with_links = self.handle_captcha(pyfile.url)
+        self.log_info("plugins/decrypters/FilecryptCc.py: self.site_with_links", repr(self.site_with_links)[:1000])
+
         if self.site_with_links is None:
-            self.log_info("89 retry_captcha")
+            # FIXME why? captcha should be solved
+            self.log_info("plugins/decrypters/FilecryptCc.py: retry_captcha")
             self.retry_captcha()
 
         elif self.site_with_links == "":
-            self.log_info("93 self.retry")
+            self.log_info("plugins/decrypters/FilecryptCc.py: retry")
             self.retry()
 
         if self.config.get("handle_mirror_pages"):
-            self.log_info("97 handle_mirror_pages")
+            self.log_info("plugins/decrypters/FilecryptCc.py: handle_mirror_pages")
             self.handle_mirror_pages()
 
         for handle in (
@@ -102,6 +116,7 @@ class FilecryptCc(BaseDecrypter):
             self.handle_weblinks,
             self.handle_dlc_container,
         ):
+            self.log_info("plugins/decrypters/FilecryptCc.py: handle", handle)
             handle()
             if self.urls:
                 self.packages = [
@@ -121,9 +136,11 @@ class FilecryptCc(BaseDecrypter):
             self.site_with_links = self.site_with_links + self._filecrypt_load_url(i)
 
     def handle_password_protection(self):
+        # <input type="password" name="password" id="p4assw0rt"  autofocus autocomplete="off" placeholder="Enter password">
         if (
             re.search(
-                r'<input type="text" name="pssw" id="p4assw0rt"',
+                #r'div class="input">\s*<input type="text" name="password" id="p4assw0rt"',
+                r'placeholder="Enter password"',
                 self.data,
             )
             is None
@@ -144,8 +161,16 @@ class FilecryptCc(BaseDecrypter):
             self.pyfile.url, post={"pssw": password}
         )
 
+    def search_captcha(self, html):
+        for pattern in self.CAPTCHA_PATTERNS:
+            if match := re.search(pattern, html):
+                self.log_info("plugins/decrypters/FilecryptCc.py: search_captcha: found captcha", repr(match.group(0)))
+                return True
+        return False
+
     def handle_captcha(self, submit_url):
-        if re.search(self.CAPTCHA_PATTERN, self.data):
+        self.log_info("plugins/decrypters/FilecryptCc.py: handle_captcha: submit_url", submit_url)
+        if self.search_captcha(self.data):
             for handle in (
                 self._handle_internal_captcha,
                 self._handle_circle_captcha,
@@ -155,14 +180,22 @@ class FilecryptCc(BaseDecrypter):
                 self._handle_recaptcha_captcha,
             ):
 
+                self.log_info("plugins/decrypters/FilecryptCc.py: handle_captcha: handle", handle)
+
                 res = handle(submit_url)
+
+                #self.log_info("plugins/decrypters/FilecryptCc.py: handle_captcha: handle -> res[:1000]", repr(res)[:1000])
+                self.log_info("plugins/decrypters/FilecryptCc.py: handle_captcha: handle -> res", repr(res))
+                # res: html code
+
                 if res is None:
                     continue
 
                 elif res == "":
                     return res
-
-                if re.search(self.CAPTCHA_PATTERN, res):
+                
+                if self.search_captcha(res):
+                    self.log_info("plugins/decrypters/FilecryptCc.py: handle_captcha: found another captcha in res")
                     return None
 
                 else:
@@ -205,12 +238,24 @@ class FilecryptCc(BaseDecrypter):
 
             self.log_debug(f"Circle Captcha URL: {captcha_url}")
 
+            """
+            FIXME OCR fails to solve captcha
+
+            [2024-09-18 21:03:33]  DEBUG               pyload  DECRYPTER FilecryptCc[3306]: Circle Captcha URL: https://filecrypt.cc/captcha/circle.php
+            [2024-09-18 21:03:33]  DEBUG               pyload  DECRYPTER FilecryptCc[3306]: Circle Captcha URL: https://filecrypt.cc/captcha/circle.php
+            [2024-09-18 21:03:33]  DEBUG               pyload  ANTICAPTCHA FilecryptCc[3306]: BaseCaptcha | LOAD URL https://filecrypt.cc/captcha/circle.php | get={} | post={} | ref=False | cookies=True | just_header=False | decode=False | multipart=False | redirect=True | req=<src.pyload.core.network.browser.Browser object at 0x7f77fc1c3490>
+            [2024-09-18 21:03:34]  INFO                pyload  ANTICAPTCHA FilecryptCc[3306]: BaseCaptcha | Using OCR to decrypt captcha...
+            [2024-09-18 21:03:34]  WARNING             pyload  ANTICAPTCHA FilecryptCc[3306]: BaseCaptcha | No OCR result
+            """
+
             captcha_code = self.captcha.decrypt(
                 captcha_url, input_type="png", output_type="positional"
             )
 
             return self._filecrypt_load_url(
-                url, post={"button.x": captcha_code[0], "button.y": captcha_code[1]}
+                # TODO parse dynamic input name: "button" or "buttonx" or ...
+                #url, post={"button.x": captcha_code[0], "button.y": captcha_code[1]}
+                url, post={"buttonx.x": captcha_code[0], "buttonx.y": captcha_code[1]}
             )
 
         else:
