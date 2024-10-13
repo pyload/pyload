d0dfae33a552ff6808aab4c170b5da29c5c885ab
h0nIg <h0nIg@users.noreply.github.com>
2024-10-13 12:50:38 +0200

    fix cnl with ssh forwarding

--- module/web/cnl_app.py
+++ module/web/cnl_app.py
@@ -18,7 +18,7 @@ except ImportError:
 def local_check(function):
     def _view(*args, **kwargs):
         if request.environ.get('REMOTE_ADDR', "0") in ('127.0.0.1', '::ffff:127.0.0.1', '::1', 'localhost') \
-        or request.environ.get('HTTP_HOST','0') in ('127.0.0.1:9666', '[::1]:9666'):
+        or request.environ.get('HTTP_HOST','0') in ('127.0.0.1:9666', '[::1]:9666', 'localhost:9666')):
             return function(*args, **kwargs)
         else:
             return HTTPError(403, "Forbidden")
