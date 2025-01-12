52b220773a9eb94c71bee7dcd91f361911ee4279
GammaC0de <gammac0de@users.noreply.github.com>
2025-01-13 00:39:00 +0200

    cgi module is deprecated in Python 3.13, stop using it for HTTP header parsing (fixes #4535)

--- src/pyload/core/utils/web/parse.py
+++ src/pyload/core/utils/web/parse.py
@@ -118,10 +118,35 @@ def name(url, safe_name=True):
     return safe_nm(name) if safe_name else name
 
 
-# TODO: Recheck in 0.5.x
-# def grab_name(url, *args, **kwargs):
-#     kwargs.setdefault('allow_redirects', True)
-#     kwargs.setdefault('verify', False)
-#     r = requests.head(url, *args, **kwargs)
-#     cd = r.headers.get('content-disposition')
-#     return url_to_name(cd or url)
+def http_header(line):
+    """
+    Parse a Content-type like http header.
+
+    :param line:
+    :Return: the main content-type and a dictionary of options.
+    """
+    def _parseparam(s):
+        while s[:1] == ';':
+            s = s[1:]
+            end = s.find(';')
+            while end > 0 and (s.count('"', 0, end) - s.count('\\"', 0, end)) % 2:
+                end = s.find(';', end + 1)
+            if end < 0:
+                end = len(s)
+            f = s[:end]
+            yield f.strip()
+            s = s[end:]
+
+    parts = _parseparam(';' + line)
+    key = parts.__next__()
+    pdict = {}
+    for p in parts:
+        i = p.find('=')
+        if i >= 0:
+            name = p[:i].strip().lower()
+            value = p[i+1:].strip()
+            if len(value) >= 2 and value[0] == value[-1] == '"':
+                value = value[1:-1]
+                value = value.replace('\\\\', '\\').replace('\\"', '"')
+            pdict[name] = value
+    return key, pdict
