57dab0efb9c72775e560be39417ae67de3d6d79d
GammaC0de <gammac0de@users.noreply.github.com>
2025-06-09 00:26:38 +0300

    restore install_pycurl_win.sh just in case

--- .github/scripts/install_pycurl_win.sh
+++ .github/scripts/install_pycurl_win.sh
@@ -2,6 +2,15 @@
 
 BASE_URL="https://raw.githubusercontent.com/GammaC0de/pycurl-win-mirror/master/"
 case "$1" in
+  "3.6")
+    WHEEL_FILE="pycurl-7.43.0.4-cp36-cp36m-win_amd64.whl"
+    ;;
+  "3.7")
+    WHEEL_FILE="pycurl-7.45.1-cp37-cp37m-win_amd64.whl"
+    ;;
+  "3.8")
+    WHEEL_FILE="pycurl-7.45.1-cp38-cp38-win_amd64.whl"
+    ;;
   "3.9")
     WHEEL_FILE="pycurl-7.45.1-cp39-cp39-win_amd64.whl"
     ;;
