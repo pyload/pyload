d82f8b323230d9b6e66e8260e4dac1b7b93bd2b1
GammaC0de <gammac0de@users.noreply.github.com>
2025-10-11 17:55:24 +0300

    [KatfileCom] update account (fix #4629)

--- module/plugins/accounts/KatfileCom.py
+++ module/plugins/accounts/KatfileCom.py
@@ -6,16 +6,16 @@ from ..internal.XFSAccount import XFSAccount
 class KatfileCom(XFSAccount):
     __name__ = "KatfileCom"
     __type__ = "account"
-    __version__ = "0.01"
+    __version__ = "0.02"
     __status__ = "testing"
 
     __description__ = """Katfile.com account plugin"""
     __license__ = "GPLv3"
     __authors__ = [("GammaC0de", "nitzo2001[AT]yahoo[DOT]com")]
 
-    PLUGIN_DOMAIN = "katfile.com"
-    PLUGIN_URL = "https://katfile.com"
+    PLUGIN_DOMAIN = "katfile.cloud"
+    PLUGIN_URL = "https://katfile.cloud"
 
     PREMIUM_PATTERN = r"Extend Premium account"
-    VALID_UNTIL_PATTERN = r"<TD>Premium account expire</TD><TD><b>(.+?)<"
+    VALID_UNTIL_PATTERN = r"<TD>Premium Pro account expire</TD><TD><b>(.+?)<"
     TRAFFIC_LEFT_PATTERN = r"Traffic available today.*?<b>\s*(?P<S>[\d.,]+|[Uu]nlimited)\s*(?:(?P<U>[\w^_]+)\s*)?"
