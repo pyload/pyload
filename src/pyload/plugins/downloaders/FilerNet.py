786e3484c4db8e3d1fc1bc7b70ec32b3799c06dc
Benedikt Bauer <benedikt.bauer@ambient.digital>
2026-03-29 20:19:51 +0200

    Fix Filer.net new API as of Dec 25

--- src/pyload/plugins/downloaders/FilerNet.py
+++ src/pyload/plugins/downloaders/FilerNet.py
@@ -3,21 +3,20 @@ import re
 
 from pyload.core.network.http.exceptions import BadHeader
 
-from ..anticaptchas.HCaptcha import HCaptcha
 from ..base.simple_downloader import SimpleDownloader
 
 
 class FilerNet(SimpleDownloader):
     __name__ = "FilerNet"
     __type__ = "downloader"
-    __version__ = "0.35"
+    __version__ = "0.36"
     __status__ = "testing"
 
     __pattern__ = r"https?://(?:www\.)?filer\.net/get/(?P<ID>\w+)"
     __config__ = [
         ("enabled", "bool", "Activated", True),
         ("use_premium", "bool", "Use premium account if available", True),
-        ("fallback", "bool", "Fallback to free download if premium fails", True),
+        ("fallback", "bool", "Fallback to free download if premium fails", False),
         ("chk_filesize", "bool", "Check file size", True),
         ("max_wait", "int", "Reconnect if waiting time is greater than minutes", 10),
     ]
@@ -30,17 +29,12 @@ class FilerNet(SimpleDownloader):
         ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com"),
     ]
 
-    HCAPTCHA_KEY = "45623a98-7b08-43ae-b758-c21c13024e2a"
+    # See https://filer.net/api/json
+    API_URL = "https://filer.net/api"
 
-    # See https://filer.net/api
-    API_URL = "https://filer.net/api/"
-
-    def api_request(self, method, is_post=True, **kwargs):
+    def api_request(self, endpoint, **kwargs):
         try:
-            if is_post:
-                json_data = self.load(self.API_URL + method, post=kwargs)
-            else:
-                json_data = self.load(self.API_URL + method, get=kwargs)
+            json_data = self.load(f"{self.API_URL}/{endpoint}", get=kwargs or None, redirect=False)
         except BadHeader as exc:
             json_data = exc.content
 
@@ -50,47 +44,45 @@ class FilerNet(SimpleDownloader):
         info = {}
         file_id = re.match(self.__pattern__, url).group("ID")
 
-        api_data = self.api_request(f"file/{file_id}")
-        if api_data.get("message") == "File not found":
-            info["status"] = 1
+        api_data = self.api_request(f"status/{file_id}.json")
+        if api_data.get("code") == 500:
+            info["status"] = 1  #: offline
         else:
+            data = api_data["data"]
             info.update({
-                "name": api_data["name"],
-                "size": api_data["size"],
-                "premium_only": api_data["premiumOnly"],
-                "status": 2
+                "name": data["file_name"],
+                "size": data["file_size"],
+                "premium_only": data["premium_only"],
+                "status": 2,  #: online
             })
 
         return info
 
     def handle_free(self, pyfile):
-        if self.info["premium_only"] is True and not self.premium:
-            self.fail(self._("File can be downloaded by premium users only"))
-
-        file_id = self.info["pattern"]["ID"]
-
-        self.captcha = HCaptcha(pyfile)
-        captcha_response = self.captcha.challenge(self.HCAPTCHA_KEY)
-
-        api_data = self.api_request(f"file/request/{file_id}", is_post=False, hCaptchaToken=captcha_response)
-        error = api_data.get("error")
-        if error:
-            self.log_error(error)
-            if error == "HOURLY_DOWNLOAD_LIMIT":
-                self.retry(wait=3600)
-            elif error == "CONCURRENT_DOWNLOAD_LIMIT":
-                self.temp_offline()
-            else:
-                self.fail(error)
-
-        wait_time = api_data["wt"]
-        self.wait(wait_time)
-        api_data = self.api_request("file/download", ticket=api_data["t"])
-        if "error" in api_data:
-            self.fail(api_data["error"])
-        else:
-            self.link = api_data["downloadUrl"]
+        self.fail(self._("Only premium users can download from Filer.net"))
 
     def handle_premium(self, pyfile):
-        self.handle_free(pyfile)
+        file_id = self.info["pattern"]["ID"]
+
+        user = self.account.user
+        password = self.account.info["login"]["password"]
+        self.req.add_auth(f"{user}:{password}")
+        try:
+            api_data = self.api_request(f"dl/{file_id}.json")
+        finally:
+            self.req.remove_auth()
+
+        code = api_data.get("code")
+        if code == 401:
+            self.fail(self._("Authentication required"))
+        elif code == 403:
+            self.fail(self._("Premium account required"))
+        elif code == 429:
+            self.temp_offline(self._("Concurrent download limit reached"))
+        elif code == 500:
+            self.offline()
+        elif code == 503:
+            self.temp_offline(self._("No download server available"))
+
+        self.link = api_data["data"]["download_url"]
 
