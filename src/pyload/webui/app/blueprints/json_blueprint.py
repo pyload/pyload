pyload/pyload

commit 572b5dd739cffa24df0c788e5f200990e16e0684

Change JSON API endpoints from GET to POST for state-modifying operations

src/pyload/webui/app/blueprints/json_blueprint.py
@@ -103,7 +103,7 @@ def package():
         return jsonify(False), 500
 
 
-@bp.route("/json/package_order", endpoint="package_order")
+@bp.route("/json/package_order", methods=["POST"], endpoint="package_order")
 # @apiver_check
 @login_required("ADD")
 def package_order():
@@ -118,7 +118,7 @@ def package_order():
         return jsonify(False), 500
 
 
-@bp.route("/json/abort_link", endpoint="abort_link")
+@bp.route("/json/abort_link", methods=["POST"], endpoint="abort_link")
 # @apiver_check
 @login_required("DELETE")
 def abort_link():
@@ -132,7 +132,7 @@ def abort_link():
         return jsonify(False), 500
 
 
-@bp.route("/json/link_order", endpoint="link_order")
+@bp.route("/json/link_order", methods=["POST"], endpoint="link_order")
 # @apiver_check
 @login_required("ADD")
 def link_order():
@@ -184,7 +184,7 @@ def add_package():
     return jsonify(True)
 
 
-@bp.route("/json/move_package", endpoint="move_package")
+@bp.route("/json/move_package", methods=["POST"], endpoint="move_package")
 # @apiver_check
 @login_required("MODIFY")
 def move_package():

src/pyload/webui/app/themes/modern/templates/js/dashboard.js
@@ -86,7 +86,7 @@ class LinkEntry {
     this.fadeBar = this.elements.pgbTr;
 
     $(this.elements.remove).click(() => {
-      $.get({
+      $.post({
         url: "{{url_for('json.abort_link')}}",
         data: { id: this.id },
         traditional: true,

src/pyload/webui/app/themes/modern/templates/js/packages.js
@@ -44,7 +44,7 @@ class PackageUI {
           return false;
         }
         uiHandler.indicateLoad();
-        $.get({
+        $.post({
           url: "{{url_for('json.package_order')}}",
           data: { pid: ui.item.data('pid'), pos: newIndex },
           traditional: true,
@@ -246,7 +246,7 @@ class Package {
           return false;
         }
         uiHandler.indicateLoad();
-        $.get({
+        $.post({
           url: "{{url_for('json.link_order')}}",
           data: { fid: ui.item.data('lid'), pos: newIndex },
           traditional: true,
@@ -374,7 +374,7 @@ class Package {
     event.stopPropagation();
     event.preventDefault();
     uiHandler.indicateLoad();
-    $.get({
+    $.post({
       url: "{{url_for('json.move_package')}}",
       data: { id: this.id, dest: ((this.ui.type + 1) % 2) },
       traditional: true
@@ -400,7 +400,7 @@ class Package {
       .done((data) => {
         const length = data.links.length;
         for (let i = 1; i <= length / 2; i++) {
-          $.get({
+          $.post({
             url: "{{url_for('json.link_order')}}",
             data: { fid: data.links[length - i].fid, pos: i - 1 },
             traditional: true

src/pyload/webui/app/themes/pyplex/templates/js/dashboard.js
@@ -86,7 +86,7 @@ class LinkEntry {
     this.fadeBar = this.elements.pgbTr;
 
     $(this.elements.remove).click(() => {
-      $.get({
+      $.post({
         url: "{{url_for('json.abort_link')}}",
         data: { id: this.id },
         traditional: true,

src/pyload/webui/app/themes/pyplex/templates/js/packages.js
@@ -44,7 +44,7 @@ class PackageUI {
           return false;
         }
         uiHandler.indicateLoad();
-        $.get({
+        $.post({
           url: "{{url_for('json.package_order')}}",
           data: { pid: ui.item.data('pid'), pos: newIndex },
           traditional: true,
@@ -246,7 +246,7 @@ class Package {
           return false;
         }
         uiHandler.indicateLoad();
-        $.get({
+        $.post({
           url: "{{url_for('json.link_order')}}",
           data: { fid: ui.item.data('lid'), pos: newIndex },
           traditional: true,
@@ -374,7 +374,7 @@ class Package {
     event.stopPropagation();
     event.preventDefault();
     uiHandler.indicateLoad();
-    $.get({
+    $.post({
       url: "{{url_for('json.move_package')}}",
       data: { id: this.id, dest: ((this.ui.type + 1) % 2) },
       traditional: true
@@ -400,7 +400,7 @@ class Package {
       .done((data) => {
         const length = data.links.length;
         for (let i = 1; i <= length / 2; i++) {
-          $.get({
+          $.post({
             url: "{{url_for('json.link_order')}}",
             data: { fid: data.links[length - i].fid, pos: i - 1 },
             traditional: true

commit 9a5542cef7486ba71d9ce797a05f9bac5be5b910

fix bug caused by https://github.com/pyload/pyload/commit/572b5dd739cffa24df0c788e5f200990e16e0684

src/pyload/webui/app/blueprints/json_blueprint.py
@@ -109,8 +109,8 @@ def package():
 def package_order():
     api = flask.current_app.config["PYLOAD_API"]
     try:
-        pid = int(flask.request.args.get('pid'))
-        pos = int(flask.request.args.get('pos'))
+        pid = int(flask.request.form.get('pid'))
+        pos = int(flask.request.form.get('pos'))
         api.order_package(pid, pos)
         return jsonify(response="success")
 
@@ -124,7 +124,7 @@ def package_order():
 def abort_link():
     api = flask.current_app.config["PYLOAD_API"]
     try:
-        id = int(flask.request.args.get('id'))
+        id = int(flask.request.form.get('id'))
         api.stop_downloads([id])
         return jsonify(response="success")
 
@@ -138,8 +138,8 @@ def abort_link():
 def link_order():
     api = flask.current_app.config["PYLOAD_API"]
     try:
-        fid = int(flask.request.args.get('fid'))
-        pos = int(flask.request.args.get('pos'))
+        fid = int(flask.request.form.get('fid'))
+        pos = int(flask.request.form.get('pos'))
         api.order_file(fid, pos)
         return jsonify(response="success")
 
@@ -190,8 +190,8 @@ def add_package():
 def move_package():
     api = flask.current_app.config["PYLOAD_API"]
     try:
-        id = int(flask.request.args.get('id'))
-        dest = int(flask.request.args.get('dest'))
+        id = int(flask.request.form.get('id'))
+        dest = int(flask.request.form.get('dest'))
         api.move_package(dest, id)
         return jsonify(response="success")
 