0fc250c94270a43605e4197dcea5b44998190977
GammaC0de <gammac0de@users.noreply.github.com>
2026-04-04 19:51:36 +0300

    fix GHSA-mvwx-582f-56r7 security advisory

--- src/pyload/plugins/extractors/UnTar.py
+++ src/pyload/plugins/extractors/UnTar.py
@@ -2,21 +2,15 @@ import os
 import sys
 import tarfile
 
-from pyload.core.utils.fs import safejoin
+from pyload.core.utils.fs import is_within_directory, safejoin
 from pyload.plugins.base.extractor import ArchiveError, BaseExtractor, CRCError
 
 
 # Fix for tarfile CVE-2007-4559
 def _safe_extractall(tar, path=".", members=None, *, numeric_owner=False):
-    def _is_within_directory(directory, target):
-        abs_directory = os.path.abspath(directory)
-        abs_target = os.path.abspath(target)
-        prefix = os.path.commonprefix([abs_directory, abs_target])
-        return prefix == abs_directory
-
     for member in tar.getmembers():
         member_path = os.path.join(path, member.name)
-        if not _is_within_directory(path, member_path):
+        if not is_within_directory(path, member_path):
             raise ArchiveError("Attempted Path Traversal in Tar File (CVE-2007-4559)")
 
     tar.extractall(path, members, numeric_owner=numeric_owner)
