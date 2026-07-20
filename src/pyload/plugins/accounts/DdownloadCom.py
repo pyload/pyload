2a7d04a93c7b72653d26061490af75bc82b7035b
GammaC0de <gammac0de@users.noreply.github.com>
2026-07-20 22:47:00 +0300

    [DdownloadCom] fix #4767

--- src/pyload/plugins/accounts/DdownloadCom.py
+++ src/pyload/plugins/accounts/DdownloadCom.py
@@ -1,12 +1,18 @@
+import urllib.parse
+
 import pycurl
 
+from pyload.core.datatypes.pyfile import PyFile
+
+from ..anticaptchas.Turnstile import Turnstile
 from ..base.xfs_account import XFSAccount
+from ..helpers import parse_html_form, search_pattern
 
 
 class DdownloadCom(XFSAccount):
     __name__ = "DdownloadCom"
     __type__ = "account"
-    __version__ = "0.10"
+    __version__ = "0.11"
     __status__ = "testing"
 
     __description__ = """Ddownload.com account plugin"""
@@ -29,3 +35,46 @@ class DdownloadCom(XFSAccount):
         self.log_debug(f"Size: {size}", f"Unit: {unit or 'N/D'}")
         # to match with ddownload's dashboard value, we need to convert the traffic value in a different way
         return int(int(size) / 1000 * 1024**3)
+
+    def signin(self, user, password, data):
+        self.data = self.load(self.LOGIN_URL, cookies=self.COOKIES)
+
+        if search_pattern(self.LOGIN_SKIP_PATTERN, self.data):
+            self.skip_login()
+
+        action, inputs = parse_html_form('name="FL"', self.data)
+        if not inputs:
+            inputs = {"op": "login", "redirect": self.PLUGIN_URL}
+
+        inputs.update({"login": user, "password": password})
+
+        if action:
+            url = urllib.parse.urljoin(self.LOGIN_URL, action)
+        else:
+            url = self.LOGIN_URL
+
+        # dummy pyfile
+        pyfile = PyFile(self.pyload.files, -1, self.PLUGIN_URL, self.PLUGIN_URL, 0, 0, "", self.classname, -1, -1)
+        pyfile.plugin = self
+
+        self.captcha = Turnstile(pyfile)
+        captcha_key = self.captcha.detect_key()
+        if captcha_key:
+            inputs["cf-turnstile-response"] = self.captcha.challenge(captcha_key)
+
+        self.data = self.load(url, post=inputs, cookies=self.COOKIES)
+
+        self.check_errors()
+
+
+    """
+     @NOTE: below are methods
+      necessary for captcha to work with account plugins
+    """
+
+    def check_status(self):
+        pass
+
+    def retry_captcha(self, attempts=10, wait=1, msg="Max captcha retries reached"):
+        self.captcha.invalid()
+        self.fail_login(msg=self._("Invalid captcha"))
