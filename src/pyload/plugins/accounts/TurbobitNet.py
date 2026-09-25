68e2e7ded5ab955902844f32098c63a654e361ab
GammaC0de <gammac0de@users.noreply.github.com>
2026-09-26 00:48:13 +0300

    [TurbobitNet] fix #4785

--- src/pyload/plugins/accounts/TurbobitNet.py
+++ src/pyload/plugins/accounts/TurbobitNet.py
@@ -1,14 +1,13 @@
-import re
+import json
 import time
 
 from ..base.account import BaseAccount
-from ..helpers import parse_html_form
 
 
 class TurbobitNet(BaseAccount):
     __name__ = "TurbobitNet"
     __type__ = "account"
-    __version__ = "0.15"
+    __version__ = "0.16"
     __status__ = "testing"
 
     __description__ = """TurbobitNet account plugin"""
@@ -18,15 +17,24 @@ class TurbobitNet(BaseAccount):
         ("GammaC0de", "nitzo2001[AT]yahoo[DOT]com"),
     ]
 
-    LOGIN_FAIL_PATTERN = r">(?:E-Mail address appears to be invalid\. Please try again|Incorrect login or password)</div>"
+    API_URL = "https://app.turbobit.net/api/"
+
+    def api_request(self, method, **kwargs):
+        if kwargs:
+            self.req.http.set_header("Content-Type", "application/json")
+            post_data = json.dumps(kwargs)
+        else:
+            post_data = None
+
+        data = self.load(self.API_URL + method, post=post_data)
+        return json.loads(data)
 
     def grab_info(self, user, password, data):
-        html = self.load("https://turbobit.net/")
+        api_data = self.api_request("user/info")
 
-        m = re.search(r">Turbo access till ([\d.]+)<", html)
-        if m is not None:
+        if "premium" in api_data and api_data["premium"] .get("status") == "active":
             premium = True
-            validuntil = time.mktime(time.strptime(m.group(1), "%d.%m.%Y"))
+            validuntil = time.mktime(time.strptime(api_data["premium"]["expiredAt"], "%Y-%m-%d %H:%M:%S"))
         else:
             premium = False
             validuntil = -1
@@ -34,45 +42,6 @@ class TurbobitNet(BaseAccount):
         return {"premium": premium, "trafficleft": -1, "validuntil": validuntil}
 
     def signin(self, user, password, data):
-        self.req.cj.set_cookie("turbobit.net", "user_lang", "en")
-
-        self.data = self.load("https://turbobit.net/login")
-
-        if "<a href='/user/logout'" in self.data:
-            self.skip_login()
-
-        action, inputs = parse_html_form(
-            'class="form-horizontal login mail"', self.data
-        )
-        if not inputs:
-            self.fail_login(self._("Login form not found"))
-
-        inputs["user[login]"] = user
-        inputs["user[pass]"] = password
-        inputs["user[submit]"] = "Sign in"
-        inputs["user[memory]"] = "on"
-
-        if inputs.get("user[captcha_type]"):
-            self.fail_login(
-                self._(
-                    "Logging in with captcha is not supported, please disable catcha in turbobit's account settings"
-                )
-            )
-
-        self.data = self.load("https://turbobit.net/user/login", post=inputs)
-
-        if "<a href='/user/logout'" in self.data:
-            self.log_debug("Login successful")
-
-        elif re.search(self.LOGIN_FAIL_PATTERN, self.data):
-            self.fail_login()
-
-        elif ">Please enter the captcha code.</div>" in self.data:
-            self.fail_login(
-                self._(
-                    "Logging in with captcha is not supported, please disable catcha in turbobit's account settings"
-                )
-            )
-
-        else:
-            self.fail_login(self._("Unknown response"))
+        api_data = self.api_request("auth/login", email=user, password=password)
+        if api_data:
+            self.fail_login(api_data.get("error_name", "Login handshake has failed"))
--- src/pyload/plugins/downloaders/TurbobitNet.py
+++ src/pyload/plugins/downloaders/TurbobitNet.py
@@ -1,6 +1,5 @@
+import json
 import re
-
-from pyload.core.utils.misc import eval_js
 
 from ..anticaptchas.HCaptcha import HCaptcha
 from ..anticaptchas.ReCaptcha import ReCaptcha
@@ -11,7 +10,7 @@ from ..base.simple_downloader import SimpleDownloader
 class TurbobitNet(SimpleDownloader):
     __name__ = "TurbobitNet"
     __type__ = "downloader"
-    __version__ = "0.57"
+    __version__ = "0.58"
     __status__ = "testing"
 
     __pattern__ = r"https?://(?:(?:www|m)\.)?(?:(?:trbbt|turbo(?:beet|bit[ea]?)|torbobit)\.net|(?:tourbobit|turbobi(?:tn?|f))\.com|turbo?\.(?:to|cc)|turb\.pw|trbt\.cc)/(?:download/free/)?(?P<ID>\w+)"
@@ -36,93 +35,82 @@ class TurbobitNet(SimpleDownloader):
 
     COOKIES = [("turbobit.net", "user_lang", "en")]
 
-    INFO_PATTERN = (
-        r"<title>\s*Download file (?P<N>.+?) \((?P<S>[\d.,]+) (?P<U>[\w^_]+)\)"
-    )
-    OFFLINE_PATTERN = r"<h2>File Not Found</h2>|html\(\'File (?:was )?not found|>Searching for the file\.\.\.Please wait"
-    TEMP_OFFLINE_PATTERN = r'^unmatchable$'
-
-    LINK_FREE_PATTERN = r'(/download/redirect/[^"\']+)'
     LINK_PREMIUM_PATTERN = r'href=[\'"](.+?/download/redirect/[^"\']+)'
 
-    LIMIT_WAIT_PATTERN = r"<div id=\'timeout\'>(\d+)<"
+    API_URL = "https://app.turbobit.net/api/"
+
+    def api_request(self, method, **kwargs):
+        self.req.http.set_header("Accept", "application/json, text/plain, */*")
+        if kwargs:
+            self.req.http.set_header("Content-Type", "application/json")
+            post_data = json.dumps(kwargs)
+        else:
+            post_data = None
+
+        data = self.load(self.API_URL + method, post=post_data)
+        return json.loads(data)
+
+    def api_info(self, url):
+        info = {}
+        file_id = re.match(self.__pattern__, url).group("ID")
+        api_data = self.api_request(
+            "download/info",
+            fileId=file_id,
+            referrer=None,
+            site=None,
+            shortDomain=""
+        )
+
+        err = api_data.get("error_name")
+        if err == "file_is_not_available_for_download":
+            info["status"] = 1
+        else:
+            info["status"] = 2
+            info["name"] = api_data["file"]["name"]
+            info["size"] = api_data["file"]["size"]
+            if api_data.get("premium"):
+                info["premium_download_url"] = api_data["downloadUrls"][0]
+            else:
+                info["premium_only"] = api_data["premiumOnlyDownload"]
+
+        return info
 
     def handle_free(self, pyfile):
-        self.free_url = "https://turbobit.net/download/free/{}".format(
-            self.info["pattern"]["ID"]
-        )
-        self.data = self.load(self.free_url)
+        file_id=self.info["pattern"]["ID"]
+        if self.info["premium_only"] is True:
+            self.fail(self._("File can be downloaded by premium users only"))
 
-        m = re.search(self.LIMIT_WAIT_PATTERN, self.data)
-        if m is not None:
-            self.retry(wait=m.group(1))
-
-        self.solve_captcha()
-
-        m = re.search(r"minLimit : (.+?),", self.data)
-        if m is None:
-            self.retry_captcha()
-
-        self.captcha.correct()
-
-        wait_time = eval_js(m.group(1))
-        self.wait(wait_time)
-
-        self.req.http.set_header("X-Requested-With", "XMLHttpRequest")
-        self.data = self.load(
-            "https://turbobit.net/download/getLinkTimeout/{}".format(
-                self.info["pattern"]["ID"]
-            ),
-            referrer=self.free_url,
-        )
-        self.req.http.remove_header("X-Requested-With")
-
-        if "/download/started/" in self.data:
-            self.data = self.load(
-                "https://turbobit.net/download/started/{}".format(
-                    self.info["pattern"]["ID"]
-                )
-            )
-
-            m = re.search(self.LINK_FREE_PATTERN, self.data)
-            if m is not None:
-                self.link = "https://turbobit.net{}".format(m.group(1))
-
-    def solve_captcha(self):
-        action, inputs = self.parse_html_form("id='form-captcha'")
-        if inputs is None:
-            self.fail(self._("Captcha form not found"))
-
+        api_data = self.api_request("captcha")
+        captcha_key = api_data.get("publicKey")
+        if not captcha_key:
+            self.fail(self._("Captcha key not found in API response"))
+        captcha_type = api_data.get("driver")
+        if captcha_type == "recaptcha":
+            self.captcha = ReCaptcha(self.pyfile)
+        elif captcha_type == "hcaptcha":
+            self.captcha = HCaptcha(self.pyfile)
+        elif captcha_type == "turnstile":
+            self.captcha = Turnstile(self.pyfile)
         else:
-            recaptcha = ReCaptcha(self.pyfile)
-            captcha_key = recaptcha.detect_key()
-            if captcha_key:
-                self.captcha = recaptcha
-                response = recaptcha.challenge(captcha_key)
-                inputs["g-recaptcha-response"] = response
+            self.fail(self._("Unsupported captcha type: {}").format(captcha_type))
 
-            else:
-                hcaptcha = HCaptcha(self.pyfile)
-                captcha_key = hcaptcha.detect_key()
-                if captcha_key:
-                    self.captcha = hcaptcha
-                    response = hcaptcha.challenge(captcha_key)
-                    inputs["g-recaptcha-response"] = inputs["h-captcha-response"] = response
+        api_data = self.api_request("download/free/init", fileId=file_id)
+        delay = (api_data.get("ipBan") or {}).get("delay")
+        if delay:
+            self.retry(wait=api_data["ipBan"]["delay"], msg=self._("IP is banned for free downloads"))
 
-                else:
-                    turnstile = Turnstile(self.pyfile)
-                    captcha_key= turnstile.detect_key()
-                    if captcha_key:
-                        response = turnstile.challenge(captcha_key)
-                        inputs["cf-turnstile-response"] = response
-
-            if captcha_key:
-                self.data = self.load(self.free_url, post=inputs, referrer=self.free_url)
-
-            else:
-                self.fail(self._("Could not detect captcha type"))
+        response = self.captcha.challenge(captcha_key)
+        api_data = self.api_request("download/free/captcha", fileId=file_id, captchaResponse=response)
+        if "delay" not in api_data:
+            self.fail(self._("API response does not contain delay information"))
+        self.wait(api_data["delay"])
+        api_data = self.api_request("download/free/prepare", fileId=file_id)
+        if api_data.get("success") is not True:
+            self.fail(self._("API response indicates failure to prepare download"))
+        api_data = self.api_request("download/free/start", fileId=file_id)
+        if "downloadUrl" not in api_data:
+            self.fail(self._("API response does not contain download URL"))
+        self.link = api_data["downloadUrl"]
 
     def handle_premium(self, pyfile):
-        m = re.search(self.LINK_PREMIUM_PATTERN, self.data)
-        if m is not None:
-            self.link = m.group(1)
+        self.link = self.info["premium_download_url"]
